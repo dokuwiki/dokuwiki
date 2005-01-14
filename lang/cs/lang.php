08ab002ac8c4b70665f7d7ea93712a80e66d52af
andi <andi@splitbrain.org>
2005-01-14 17:53:00 +0100

    cs translation
    darcs-hash:20050114165300-9977f-89198706a74b3e2fbf80e21e8364f8a41e722e2c.gz

---
+++ lang/cs/lang.php
@@ -0,0 +1,104 @@
+<?
+/**
+ * Czech language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Bohumir Zamecnik <bohous@luhy.net>
+ */
+$lang['encoding']   = 'iso-8859-2';
+$lang['locales']    = array(
+                        'cze',
+                        'cs_CZ',
+                        'cs',
+                        'czech',
+                      );
+
+$lang['btn_edit']   = 'Upravit stránku';
+$lang['btn_source'] = 'Zobrazit zdroj stránky';
+$lang['btn_show']   = 'Zobrazit stránku';
+$lang['btn_create'] = 'Vytvoøit stránku';
+$lang['btn_search'] = 'Hledat';
+$lang['btn_save']   = 'Ulo¾it';
+$lang['btn_preview']= 'Náhled';
+$lang['btn_top']    = 'Nahoru';
+$lang['btn_revs']   = 'Star¹í verze';
+$lang['btn_recent'] = 'Poslední úpravy';
+$lang['btn_upload'] = 'Pøilo¾it';
+$lang['btn_cancel'] = 'Storno';
+$lang['btn_index']  = 'Index';
+$lang['btn_secedit']= 'Upravit';
+$lang['btn_login']  = 'Pøihlásit se';
+$lang['btn_logout'] = 'Odhlásit se';
+
+$lang['loggedinas'] = 'Pøihlá¹en(a) jako';
+$lang['user']       = 'U¾ivatelské jméno';
+$lang['pass']       = 'Heslo';
+$lang['fullname']   = 'Celé jméno';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Registrovat';
+$lang['badlogin']   = 'Zadané u¾ivatelské jméno a heslo není správnì.';
+
+$lang['regmissing'] = 'Musíte vyplnit v¹echny údaje.';
+$lang['reguexists'] = 'U¾ivatel se stejným jménem u¾ je zaregistrován.';
+$lang['regsuccess'] = 'U¾ivatelský úèet byl vytvoøen a heslo zasláno mailem.';
+$lang['regmailfail']= 'Zdá se, ¾e nastala chyba pøi posílání mailu s heslem. Zkuste kontaktovat správce.';
+$lang['regbadmail'] = 'Zadaná mailová adresa není platná. Pokud si myslíte, ¾e to je ¹patnì, zkuste kontaktovat správce.';
+$lang['regpwmail']  = 'Va¹e heslo do systému DokuWiki';
+$lang['reghere']    = 'Nemáte u¾ivatelský úèet? Zøiïte si ho';
+
+$lang['txt_upload']   = 'Vyberte soubor jako pøílohu';
+$lang['txt_filename'] = 'Wiki jméno (volitelné)';
+$lang['lockedby']     = 'Právì zamknuto:';
+$lang['lockexpire']   = 'Zámek vypr¹í:';
+$lang['willexpire']   = 'Vá¹ zámek pro editaci za chvíli vypr¹í.\nAbyste pøede¹li konfliktùm, stisknìte tlaèítko Náhled a zámek se prodlou¾í.';
+
+$lang['notsavedyet'] = 'Neulo¾ené zmìny budou ztraceny.\nChcete opravdu pokraèovat?';
+$lang['rssfailed']   = 'Nastala chyba pøi vytváøení tohoto RSS: ';
+$lang['nothingfound']= 'Nic nenalezeno.';
+
+$lang['mediaselect'] = 'Výbìr dokumentu';
+$lang['fileupload']  = 'Nahrávání dokumentu';
+$lang['uploadsucc']  = 'Pøenos probìhl v poøádku';
+$lang['uploadfail']  = 'Chyba pøi nahrávání. Mo¾ná kvùli ¹patnì nastaveným právùm?';
+$lang['uploadwrong'] = 'Pøilo¾ení souboru s takovouto pøíponou není dovoleno.';
+$lang['namespaces']  = 'Jmenné prostory';
+$lang['mediafiles']  = 'Dostupné soubory';
+
+$lang['hits']       = '- poèet výskytù';
+$lang['quickhits']  = 'Odpovídající stránky';
+$lang['toc']        = 'Obsah';
+$lang['current']    = 'aktuální';
+$lang['yours']      = 'Va¹e verze';
+$lang['diff']       = 'zobrazit rozdíly vùèi aktuální verzi';
+$lang['line']       = 'Øádek';
+$lang['breadcrumb'] = 'Historie';
+$lang['lastmod']    = 'Poslední úprava';
+$lang['deleted']    = 'odstranìno';
+$lang['created']    = 'vytvoøeno';
+$lang['restored']   = 'stará verze byla obnovena';
+$lang['summary']    = 'Komentáø k úpravám';
+
+$lang['mail_newpage'] = '[DokuWiki] nová stránka:';
+$lang['mail_changed'] = '[DokuWiki] zmìna stránky:';
+
+$lang['nosmblinks'] = 'Odkazování na sdílené prostøedky Windows funguje jen v Internet Exploreru.\nPøesto tento odkaz mù¾ete zkopírovat a vlo¾it jinde.';
+
+$lang['qb_alert']   = 'Vlo¾te prosím text, který chcete formátovat.\nTen bude pøidán na konec dokumentu.';
+$lang['qb_bold']    = 'Tuènì';
+$lang['qb_italic']  = 'Kurzíva';
+$lang['qb_underl']  = 'Podtr¾ení';
+$lang['qb_code']    = 'Neformátovat (zdrojový kód)';
+$lang['qb_h1']      = 'Nadpis 1. úrovnì';
+$lang['qb_h2']      = 'Nadpis 2. úrovnì';
+$lang['qb_h3']      = 'Nadpis 3. úrovnì';
+$lang['qb_h4']      = 'Nadpis 4. úrovnì';
+$lang['qb_h5']      = 'Nadpis 5. úrovnì';
+$lang['qb_link']    = 'Interní odkaz';
+$lang['qb_extlink'] = 'Externí odkaz';
+$lang['qb_hr']      = 'Horizontální linka';
+$lang['qb_ol']      = 'Èíslovaný seznam';
+$lang['qb_ul']      = 'Neèíslovaný seznam';
+$lang['qb_media']   = 'Vlo¾it obrázky nebo jiné soubory';
+$lang['qb_sig']     = 'Vlo¾it podpis';
+
+?>
