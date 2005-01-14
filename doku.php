78e788accbcea7d7723fb2940ca949d4c6de30c4
andi <andi@splitbrain.org>
2005-01-14 23:59:58 +0100

    version string fix
    darcs-hash:20050114225958-9977f-c5234044adab6f51eee36362d106926464ee1680.gz

--- doku.php
+++ doku.php
@@ -6,7 +6,7 @@
  * @author     Andreas Gohr <andi@splitbrain.org>
  */
 
-  define('DOKUWIKIVERSION','2004-01-14');
+  define('DOKUWIKIVERSION','2005-01-14');
 
   ini_set('short_open_tag',"1");
   require_once("conf/dokuwiki.php");
