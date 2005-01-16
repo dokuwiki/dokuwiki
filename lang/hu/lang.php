95ced01f953ccd5397adc3c99b8b7f82a44c9203
andi <andi@splitbrain.org>
2005-01-17 00:07:11 +0100

    hungarian translation
    darcs-hash:20050116230711-9977f-6ba63fd5cb72e037c28303d2c52ea7509dfecb5c.gz

---
+++ lang/hu/lang.php
@@ -0,0 +1,102 @@
+<?
+/**
+ * Hungarian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Ziegler Gábor <gziegler@freemail.hu>
+ */
+$lang['encoding']   = 'iso-8859-2';
+$lang['locales']    = array(
+                        'hu',
+                        'hu_HU'
+                      );
+
+$lang['btn_edit']   = 'Oldal szerkesztése';
+$lang['btn_source'] = 'Oldalforrás megtekintése';
+$lang['btn_show']   = 'Oldal megtekintése';
+$lang['btn_create'] = 'Oldal létrehozása';
+$lang['btn_search'] = 'Megtekintés';
+$lang['btn_save']   = 'Mentés';
+$lang['btn_preview']= 'Elõnézet';
+$lang['btn_top']    = 'Vissza a tetejére';
+$lang['btn_revs']   = 'Elõzõ változatok';
+$lang['btn_recent'] = 'Legfrissebb változások';
+$lang['btn_upload'] = 'Feltöltés';
+$lang['btn_cancel'] = 'Mégsem';
+$lang['btn_index']  = 'Áttekintés';
+$lang['btn_secedit']= 'Szerkesztés';
+$lang['btn_login']  = 'Bejelentkezés';
+$lang['btn_logout'] = 'Kijelentkezés';
+
+$lang['loggedinas'] = 'Belépett felhasználó: ';
+$lang['user']       = 'Azonosító';
+$lang['pass']       = 'Jelszó';
+$lang['fullname']   = 'Teljes név';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Regisztráció';
+$lang['badlogin']   = 'Sajnáljuk, az azonsító, vagy a jelszó nem jó.';
+
+$lang['regmissing'] = 'Sajnáljuk, az összes mezõt ki kell töltened.';
+$lang['reguexists'] = 'Sajnáljuk, ilyen azonosítójú felhasználónk már van.';
+$lang['regsuccess'] = 'A felhasználóiazonosítót létrehoztuk. A jelszót postáztuk..';
+$lang['regmailfail']= 'Úgy tûnik hiba történt a jelszó postázásáa során. Kérjük lépj kapcsolatba a rendszergazdával!!';
+$lang['regbadmail'] = 'A megadott e-mail cím érvénytelennek tûnik. Ha az gondolod ez hiba, lépjkapcsolatba rendszergazdával';
+$lang['regpwmail']  = 'A DokuWiki jelszavad';
+$lang['reghere']    = 'Még nincs azonosítód? Hát kérj egyet';
+
+$lang['txt_upload']   = 'Válaszd ki a feltöltendõ fájlt';
+$lang['txt_filename'] = 'Add meg a wikineved (elhagyható)';
+$lang['lockedby']     = 'Jelenleg zárolta';
+$lang['lockexpire']   = 'A zárolás lejár';
+$lang['willexpire']   = 'Az oldal zárolásod szerkesztéshez körülbelül egy percen belül lejár.\nAz ütközések elkerülése végett használd az elõnézet gombot a zárolási idõzítés frissítéséhez.';
+
+$lang['notsavedyet'] = 'Elmentetlen változások vannak, amelyek el fognak veszni.\nTényleg ezt akarod?';
+$lang['rssfailed']   = 'Hiba történt ennek a betöltésekor: ';
+$lang['nothingfound']= 'Semmit nem találtam.';
+
+$lang['mediaselect'] = 'Médiafájl kiválasztása';
+$lang['fileupload']  = 'Médiafájl felöltése';
+$lang['uploadsucc']  = 'A feltöltés sikerült';
+$lang['uploadfail']  = 'A feltöltés nem sikerült. Talán rosszak a jogosultságok?';
+$lang['uploadwrong'] = 'A feltölés megtagadva. Ez a fájl kiterjesztés tiltott.';
+$lang['namespaces']  = 'Névtér';
+$lang['mediafiles']  = 'Elérhetõ fájlok itt:';
+
+$lang['hits']       = 'Találatok';
+$lang['quickhits']  = 'Illeszkedõ oldalnevek';
+$lang['toc']        = 'Tartalomjegyzék';
+$lang['current']    = 'aktuális';
+$lang['yours']      = 'A te változatod';
+$lang['diff']       = 'a különbségeket mutatja az aktuális változathoz képest';
+$lang['line']       = 'sorszám';
+$lang['breadcrumb'] = 'Nyomvonal';
+$lang['lastmod']    = 'Utolsó módosítás';
+$lang['deleted']    = 'eltávolítva';
+$lang['created']    = 'létrehozva';
+$lang['restored']   = 'az elõzõ változat helyreállítva';
+$lang['summary']    = 'A változások összefoglalása';
+
+$lang['mail_newpage'] = '[DokuWiki] oldalak hozzáadva:';
+$lang['mail_changed'] = '[DokuWiki] oldalak megváltoztatva:';
+
+$lang['nosmblinks'] = 'A Windows megosztott könyvtárak kereszthivatkozása csak  Microsoft Internet Explorerben mûködik közvetlenül.\nA hivatkozást másolni és beszúrni ettõl fügetlenül mndig tudod.';
+
+$lang['qb_alert']   = 'Írd be a formázandó szöveget.\nEz a dokumentum szövegének végéhez fog hozzáadódni.';
+$lang['qb_bold']    = 'Félkövér szöveg';
+$lang['qb_italic']  = 'Dõlt szöveg';
+$lang['qb_underl']  = 'Aláhúzott szöveg';
+$lang['qb_code']    = 'Forráskód szöveg';
+$lang['qb_h1']      = '1. színtû címsor';
+$lang['qb_h2']      = '2. színtû címsor';
+$lang['qb_h3']      = '3. színtû címsor';
+$lang['qb_h4']      = '4. színtû címsor';
+$lang['qb_h5']      = '5. színtû címsor';
+$lang['qb_link']    = 'Belsõ hivatkozás';
+$lang['qb_extlink'] = 'Külsõ hivatkozás';
+$lang['qb_hr']      = 'Vízszintes elválasztó vonal';
+$lang['qb_ol']      = 'Sorszámozott lista elem';
+$lang['qb_ul']      = 'Bajuszos lista elem';
+$lang['qb_media']   = 'Képek és más fájlok hozzáadása';
+$lang['qb_sig']     = 'Aláírás beszúrása';
+
+?>
