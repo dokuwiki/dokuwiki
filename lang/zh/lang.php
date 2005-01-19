4b282abadc616ab67e208e405a245822a189c961
andi <andi@splitbrain.org>
2005-01-19 21:42:33 +0100

    zh translation
    darcs-hash:20050119204233-9977f-d6d5e75a35a01e63be0332e9de8592311c169e17.gz

---
+++ lang/zh/lang.php
@@ -0,0 +1,116 @@
+<?
+/**
+ * Chinese(Simplified) language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     skyroam <skyroam@126.com> 
+ * @author     based on tw tranlastion by chinsan <chinsan@mail2000.com.tw>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['locales']    = array(
+                        'GBK',
+                        'zh_CN.GBK',
+                        'zh',
+                        'chinese(simplified)',
+                      );
+
+$lang['btn_edit']   = '编修本页';
+$lang['btn_source'] = '显示页面来源';
+$lang['btn_show']   = '显示页面';
+$lang['btn_create'] = '建立此页';
+$lang['btn_search'] = '搜寻';
+$lang['btn_save']   = '储存';
+$lang['btn_preview']= '预览';
+$lang['btn_top']    = '回到页顶';
+$lang['btn_revs']   = '旧版';
+$lang['btn_recent'] = '最近更新';
+$lang['btn_home'] = 'Home';
+$lang['btn_upload'] = '上传';
+$lang['btn_cancel'] = '取消';
+$lang['btn_index']  = '索引页';
+$lang['btn_secedit']= '改这段';
+$lang['btn_login']  = '登入';
+$lang['btn_logout'] = '登出';
+
+$lang['loggedinas'] = '登入为';
+$lang['user']       = '帐号';
+$lang['pass']       = '密码';
+$lang['fullname']   = '昵称';
+$lang['email']      = 'E-Mail';
+$lang['register']   = '注册';
+$lang['badlogin']   = '很抱歉，您的使用者名称或密码可能错误';
+
+$lang['regmissing'] = '很抱歉，所有的栏位都要填哦';
+$lang['reguexists'] = '很抱歉，已有人注册该帐号了喔';
+$lang['regsuccess'] = '使用者已建立，密码也以 email 寄出到您信箱了。';
+$lang['regmailfail']= '寄出密码信似乎发生错误，请跟管理者联络！';
+$lang['regbadmail'] = '您给的 email 似乎不对，如果您认为是正确的，请与管理者联络。';
+$lang['regpwmail']  = 'Your DokuWiki Password';
+$lang['reghere']    = '您还没有帐号对吧？来注册一个吧。';
+
+$lang['txt_upload']   = '选择要上传的档案';
+$lang['txt_filename'] = '输入 wikiname (optional)';
+$lang['lockedby']     = '目前已被下列人员锁定';
+$lang['lockexpire']   = '预计解除锁定於';
+$lang['willexpire']   = '您目前编辑这页的锁定将会在一分钟内解除。\若要避免发生意外，请按「预览」键来重新设定锁定状态';
+
+$lang['notsavedyet'] = '有尚未储存的变更将会遗失。\n真的要继续吗？';
+$lang['rssfailed']   = '当抓取它处喂送过来的资料时发生错误: ';
+$lang['nothingfound']= '没找到任何结果。';
+
+$lang['mediaselect'] = '选择影音档';
+$lang['fileupload']  = '上传影音档';
+$lang['uploadsucc']  = '上传成功';
+$lang['uploadfail']  = '上传失败。或许权限设定错误了吗？';
+$lang['uploadwrong'] = '拒绝上传。该档案类型不被支援。';
+$lang['namespaces']  = 'Namespaces';
+$lang['mediafiles']  = '可用的档案有';
+
+$lang['hits']       = '个符合';
+$lang['quickhits']  = '符合的页面名称';
+$lang['toc']        = '本页目录';
+$lang['current']    = '目前版本';
+$lang['yours']      = '您的版本';
+$lang['diff']       = '显示跟目前版本的差异';
+$lang['line']       = '行';
+$lang['breadcrumb'] = '目前的足迹';
+$lang['lastmod']    = '上一次变更';
+$lang['deleted']    = '移除';
+$lang['created']    = '建立';
+$lang['restored']   = '已恢复为旧版';
+$lang['summary']    = '编辑摘要';
+
+$lang['mail_newpage'] = '[DokuWiki] 增加的页面:';
+$lang['mail_changed'] = '[DokuWiki] 变更的页面:';
+
+$lang['nosmblinks'] = '只有在 Microsoft IE 下才能执行「连结到 Windows shares」。\n不过您仍可拷贝、复制这连结';
+
+$lang['qb_alert']   = '请输入想要格式化的文字，\n这会附加到文件的结尾。';
+$lang['qb_bold']    = '粗体';
+$lang['qb_italic']  = '斜体';
+$lang['qb_underl']  = '底线';
+$lang['qb_code']    = '程式码';
+$lang['qb_h1']      = 'H1 标题';
+$lang['qb_h2']      = 'H2 标题';
+$lang['qb_h3']      = 'H3 标题';
+$lang['qb_h4']      = 'H4 标题';
+$lang['qb_h5']      = 'H5 标题';
+$lang['qb_link']    = 'WIKI内部连结';
+$lang['qb_extlink'] = '连结外部URL';
+$lang['qb_hr']      = '水平线';
+$lang['qb_ol']      = '项目表(数字)';
+$lang['qb_ul']      = '项目表(符号)';
+$lang['qb_media']   = '加入图片或档案';
+$lang['qb_sig']     = '插入签名';
+
+$lang['txt_insert']    = '放入日历';
+$lang['qb_calendar']   = '放入一个日历';
+$lang['btn_insert']    = '产生日历';
+$lang['txt_toinsert']  = '这将会被放入';
+$lang['txt_clickhere'] = '按这就会帮您自动放好了';
+$lang['txt_year']      = '年份';
+$lang['txt_month']     = '月份';
+$lang['arr_daysofweek']= array('日','一','二','三','四','五','六');
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
+?>
