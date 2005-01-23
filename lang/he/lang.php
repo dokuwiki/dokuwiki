6085bc36939375739499f2184050e961b315ef36
andi <andi@splitbrain.org>
2005-01-23 21:40:35 +0100

    hebrew translation
    darcs-hash:20050123204035-9977f-95827c61cfa51ae29b2cd0520d5fb28ebd2b94ba.gz

---
+++ lang/he/lang.php
@@ -0,0 +1,63 @@
+<?
+/**
+ * Hebrew language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     גיא שפר <guysoft@ort.org.il>
+ * @link       http://sourceforge.net/projects/hebdokuwiki/
+ */
+$lang['encoding']   = 'utf-8';
+
+$lang['btn_edit']   = 'עריכה';
+$lang['btn_search'] = 'חפש';
+$lang['btn_save']   = 'שמור';
+$lang['btn_preview']= 'תצוגה מקדימה';
+$lang['btn_top']    = 'חזור למעלה';
+$lang['btn_revs']   = 'גרסאות קודמות';
+$lang['btn_recent'] = 'שינויים אחרונים';
+$lang['btn_upload'] = 'העלה';
+$lang['btn_cancel'] = 'בטל';
+$lang['btn_index']  = 'אינדקס';
+
+$lang['txt_upload'] = 'בחר קובץ להעלות';
+$lang['txt_filename'] = 'הכנס\י שם לויקי (בחירה)';
+
+$lang['mediaselect'] = 'בחירת קובץ מדיה';
+$lang['fileupload']  = 'העלאת קובץ מדיה';
+$lang['uploadsucc']  = 'העלאת הקובץ בוצעה בהצלחה';
+$lang['uploadfail']  = 'קרתה שגיאה בעת העלאת הקובץ. תיתכן ובעייה זו נוצרה עקב הרשאות שגיות.';
+
+$lang['hits']       = 'פגיעות';
+$lang['quickhits']  = 'דפים שנמצאו';
+$lang['toc']        = 'תוכן עניינים';
+$lang['current']    = 'גירסה נוכחית';
+$lang['yours']      = 'הגרסה שלך';
+$lang['diff']       = 'הצג שינוים מגרסה זו ועד הנוכחית';
+$lang['line']       = 'Line';
+$lang['breadcrumb'] = 'ביקורים אחרונים';
+$lang['lastmod']    = 'שונה לאחרונה ב';
+$lang['secedit']    = 'עריכה';
+
+$lang['mail_newpage'] = 'דף [HebDokuWiki[ נוסף:';
+$lang['mail_changed'] = 'דף [HebDokuWiki[ שונה:';
+
+$lang['nosmblinks'] = 'קישור למערכת קבצים של חלונות פועל רק בדפדפן אינטרנט אקספלורר.\n זה בסדר, אין צורך לעבור. אפשר להעתיק ולהדביק את הקישור';
+
+$lang['qb_bold']    = 'טקסט מודגש';
+$lang['qb_italic']  = 'טקסט נטוי';
+$lang['qb_underl']  = 'טקסט עם קו תחתון';
+$lang['qb_code']    = 'קוד';
+$lang['qb_h1']      = 'כותרת רמה 1';
+$lang['qb_h2']      = 'כותרת רמה 2';
+$lang['qb_h3']      = 'כותרת רמה 3';
+$lang['qb_h4']      = 'כותרת רמה 4';
+$lang['qb_h5']      = 'כותרת רמה 5';
+$lang['qb_link']    = 'קישור פנימי';
+$lang['qb_extlink'] = 'קישור חיצוני';
+$lang['qb_hr']      = 'קו אופקי';
+$lang['qb_ol']      = 'איבר ברשימה ממוספרת';
+$lang['qb_ul']      = 'אבר ברשימה לא ממוספרת';
+$lang['qb_media']   = 'תמונות או קובץ אחר';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
+?>
