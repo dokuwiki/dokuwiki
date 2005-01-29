8dfde767976c6219cf960e924f1f4cd5c624c4f0
andi <andi@splitbrain.org>
2005-01-29 14:03:54 +0100

    italian language file
    darcs-hash:20050129130354-9977f-3d22df27c9e70ae82672a2d4a955d69d83d14d3b.gz

---
+++ lang/it/lang.php
@@ -0,0 +1,99 @@
+<?
+/**
+ * italian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Giorgio Vecchiocattivi <giorgio@vecchio.it>
+ */
+$lang['encoding']   = 'utf-8';
+
+$lang['btn_edit']   = 'Modifica questa pagina';
+$lang['btn_source'] = 'Mostra sorgente';
+$lang['btn_show']   = 'Mostra pagina';
+$lang['btn_create'] = 'Crea questa pagina';
+$lang['btn_search'] = 'Cerca';
+$lang['btn_save']   = 'Salva';
+$lang['btn_preview']= 'Anteprima';
+$lang['btn_top']    = 'Torna su';
+$lang['btn_revs']   = 'Revisioni precedenti';
+$lang['btn_recent'] = 'Ultime modifiche';
+$lang['btn_upload'] = 'Invia file';
+$lang['btn_cancel'] = 'Annulla';
+$lang['btn_index']  = 'Indice';
+$lang['btn_secedit']= 'Modifica';
+$lang['btn_login']  = 'Entra';
+$lang['btn_logout'] = 'Esci';
+
+$lang['loggedinas'] = 'Collegato come';
+$lang['user']       = 'Nome utente';
+$lang['pass']       = 'Password';
+$lang['remember']   = 'Ricorda automaticamente';
+$lang['fullname']   = 'Nome completo';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Registra';
+$lang['badlogin']   = 'Sono spiacente. Il nome utente o la password non è valida.';
+
+$lang['regmissing'] = 'Sono spiacente, devi riempire tutti i campi.';
+$lang['reguexists'] = 'Sono spiacente, questo nome utente esiste già.';
+$lang['regsuccess'] = 'L\'utente è stato creato. La tua password ti è stata inviata con un messaggio di posta elettronica.';
+$lang['regmailfail']= 'Sembra che ci sia stato un errore nell\'invio del messaggio e-mail. Per favore contatta il tuo amministratore!';
+$lang['regbadmail'] = 'L\'inidirizzo email fornito sembra essere non valido - se pensi che ci sia un errore per favore contatta il tuo amministratore';
+$lang['regpwmail']  = 'La tua password DokuWiki';
+$lang['reghere']    = 'Non hai ancora un accesso? Registrati qui.';
+
+$lang['txt_upload']   = 'Seleziona un file da caricare';
+$lang['txt_filename'] = 'Inserisci un "wikiname" (opzionale)';
+$lang['lockedby']     = 'Attualmente bloccato da';
+$lang['lockexpire']   = 'Il blocco scade alle';
+$lang['willexpire']   = 'Il tuo blocco su questa pagina scadrà tra circa un minuto.\nPer evitare incongruenze usa il pulsante di anteprima per prolungare il periodo di blocco.';
+
+$lang['notsavedyet'] = 'Alcune modifiche non salvate andranno perse.\nSei sicuro?';
+$lang['rssfailed']   = 'Si è verifico un errore cercando questo feed : ';
+$lang['nothingfound']= 'Nessun risultato trovato.';
+
+$lang['mediaselect'] = 'Selezione dei file';
+$lang['fileupload']  = 'File caricato';
+$lang['uploadsucc']  = 'Invio riuscito';
+$lang['uploadfail']  = 'Invio fallito. Contatta l\'amministratore.';
+$lang['uploadwrong'] = 'Invio rifiutato. Questa estensione di file non è ammessa';
+$lang['namespaces']  = 'Categorie';
+$lang['mediafiles']  = 'File disponibili in';
+
+$lang['hits']       = 'Occorrenze trovate';
+$lang['quickhits']  = 'Pagine trovate';
+$lang['toc']        = 'Indice';
+$lang['current']    = 'versione corrente';
+$lang['yours']      = 'la tua versione';
+$lang['diff']       = 'differenze con la versione attuale';
+$lang['line']       = 'Linea';
+$lang['breadcrumb'] = 'Traccia';
+$lang['lastmod']    = 'Ultima modifica';
+$lang['deleted']    = 'cancellata';
+$lang['created']    = 'creata';
+$lang['restored']   = 'versione precedente ripristinata';
+$lang['summary']    = 'Oggetto della modifica';
+
+$lang['mail_newpage'] = '[DokuWiki] pagina aggiunta:';
+$lang['mail_changed'] = '[DokuWiki] pagina modificata:';
+
+$lang['nosmblinks'] = 'I collegamenti con le risorse condivise di Windows funzionano solo con Microsoft Internet Explorer.\nPuoi fare un copia/incolla di questo collegamento.';
+
+$lang['qb_alert']   = 'Puoi inserire il testo che vuoi formattare.\nSarà aggiunto alla fine del documento.';
+$lang['qb_bold']    = 'Grassetto';
+$lang['qb_italic']  = 'Italico';
+$lang['qb_underl']  = 'Sottolineato';
+$lang['qb_code']    = 'Codice';
+$lang['qb_h1']      = 'Intestazione (livello 1)';
+$lang['qb_h2']      = 'Sottointestazione (livello 2)';
+$lang['qb_h3']      = 'Sottointestazione (livello 3)';
+$lang['qb_h4']      = 'Sottointestazione (livello 4)';
+$lang['qb_h5']      = 'Sottointestazione (livello 5)';
+$lang['qb_link']    = 'Collegamento interno';
+$lang['qb_extlink'] = 'Collegamento esterno';
+$lang['qb_hr']      = 'Riga orizzontale';
+$lang['qb_ol']      = 'Elenco numerato';
+$lang['qb_ul']      = 'Elenco puntato';
+$lang['qb_media']   = 'Inserisci immagino o altri file';
+$lang['qb_sig']     = 'Inserisci la firma';
+
+?>
