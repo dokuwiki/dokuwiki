7c5a70ebd1f751fba892e3cb87aea7eb92562e0a
andi <andi@splitbrain.org>
2005-02-05 10:23:15 +0100

    romanian language
    darcs-hash:20050205092315-9977f-be805fc4a2f6451aace4eaa6cb8ebf3ed4532119.gz

---
+++ lang/ro/lang.php
@@ -0,0 +1,108 @@
+<?
+/**
+ * romanian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Tiberiu Micu <tibimicu@gmx.net>
+ */
+$lang['encoding']   = 'utf-8';
+
+$lang['btn_edit']   = 'Editează această pagină';
+$lang['btn_source'] = 'Arată sursa paginii';
+$lang['btn_show']   = 'Arată pagina';
+$lang['btn_create'] = 'Crează această pagină';
+$lang['btn_search'] = 'Caută';
+$lang['btn_save']   = 'Salvează';
+$lang['btn_preview']= 'Previzualizează';
+$lang['btn_top']    = 'Back to top';
+$lang['btn_revs']   = 'Versiuni vechi';
+$lang['btn_recent'] = 'Schimbări recente';
+$lang['btn_upload'] = 'Upload';
+$lang['btn_cancel'] = 'Renunţare';
+$lang['btn_index']  = 'Index';
+$lang['btn_secedit']= 'Editează';
+$lang['btn_login']  = 'Login';
+$lang['btn_logout'] = 'Logout';
+
+$lang['loggedinas'] = 'Logat ca şi';
+$lang['user']       = 'Utilizator';
+$lang['pass']       = 'Parola';
+$lang['remember']   = 'Aminteşte-mi';
+$lang['fullname']   = 'Nume complet';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Înregistrează';
+$lang['badlogin']   = 'Imi pare rău, utilizatorul şi/sau parola au fost greşite.';
+
+$lang['regmissing'] = 'Îmi pare rău, trebuie să completezi toate cîmpurile.';
+$lang['reguexists'] = 'Îmi pare rău, un utilizator cu acest nume există deja logat.';
+$lang['regsuccess'] = 'Utilizatorul a fost creat. Parola a fost trimiş prin email.';
+$lang['regmailfail']= 'Se pare că a fost o eroare la trimiterea parolei prin email. Contactaţi administratorul!';
+$lang['regbadmail'] = 'Adresa de email este invalidă - dacă credeţi că este o eroare contactaţi administratorul.';
+$lang['regpwmail']  = 'Parola ta DokuWiki';
+$lang['reghere']    = 'Înca nu ai un cont? Fă-ţi unul';
+
+$lang['txt_upload']   = 'Selecteaă fisierul de trimis';
+$lang['txt_filename'] = 'Introdu numele wiki (opţional)';
+$lang['lockedby']     = 'Momentan blocat de';
+$lang['lockexpire']   = 'Blocarea expiră la';
+$lang['willexpire']   = 'Blocarea pentru editarea paginii expiră intr-un minut.\nPentru a preveni conflictele foloseşte butonul de previzualizare pentru resetarea blocării.'; 
+
+$lang['notsavedyet'] = 'Există modificări nesalvate, care se vor pierde.\nDoreşti să continui?';
+$lang['rssfailed']   = 'A apărut o eroare in timpul descărcării acestui cîmp: ';
+$lang['nothingfound']= 'Nu am găsit nimic.';
+
+$lang['mediaselect'] = 'Selectare fişier media';
+$lang['fileupload']  = 'Încarcă fişier media';
+$lang['uploadsucc']  = 'Încărcare reuşită';
+$lang['uploadfail']  = 'Încărcare eşuată. Poate din cauza permisiunilor?';
+$lang['uploadwrong'] = 'Încărcare nepermisă. Extensia fişierului e nepermisă';
+$lang['namespaces']  = 'Spaţii de nume';
+$lang['mediafiles']  = 'Fişiere disponibile în';
+
+$lang['hits']       = 'Hituri';
+$lang['quickhits']  = 'Nume de pagini potrivite';
+$lang['toc']        = 'Cuprins';
+$lang['current']    = 'curent';
+$lang['yours']      = 'Versiunea ta';
+$lang['diff']       = 'arată diferenţele faţă de versiunea curentă';
+$lang['line']       = 'Linia';
+$lang['breadcrumb'] = 'Traseu';
+$lang['lastmod']    = 'Ultima modificare';
+$lang['deleted']    = 'şters';
+$lang['created']    = 'creat';
+$lang['restored']   = 'versiune veche restaurată';
+$lang['summary']    = 'Editează sumarul';
+
+$lang['mail_newpage'] = '[DokuWiki] pagina adăugată:';
+$lang['mail_changed'] = '[DokuWiki] page schimbată:';
+
+$lang['nosmblinks'] = 'Legăturile către sharing-uri Windows funcţioneaza numai in Microsoft Internet Explorer.\nPuteţi însă copia şi insera legătura.';
+
+$lang['qb_alert']   = 'Introduceţi vă rog textul care doriţi să-l formataţi.\nVa fi adăugat la sfîtşitul documentului.';
+$lang['qb_bold']    = 'Text Îngroşat';
+$lang['qb_italic']  = 'Text Italic';
+$lang['qb_underl']  = 'Text Subliniat';
+$lang['qb_code']    = 'Text Cod';
+$lang['qb_h1']      = 'Linie de început de Nivel 1';
+$lang['qb_h2']      = 'Linie de început de Nivel 2';
+$lang['qb_h3']      = 'Linie de început de Nivel 3';
+$lang['qb_h4']      = 'Linie de început de Nivel 4';
+$lang['qb_h5']      = 'Linie de început de Nivel 5';
+$lang['qb_link']    = 'Legătură internă';
+$lang['qb_extlink'] = 'Legătura externă';
+$lang['qb_hr']      = 'Linie Orizontal';
+$lang['qb_ol']      = 'Listă Ordonată';
+$lang['qb_ul']      = 'Listă Neordoată';
+$lang['qb_media']   = 'Adaugă imagini şi alte fişiere';
+$lang['qb_sig']     = 'Inserează semnătură';
+
+$lang['txt_insert']    = 'Inserează Calendar';
+$lang['qb_calendar']   = 'Inserează un Calendar';
+$lang['btn_insert']    = 'Generează Calendar';
+$lang['txt_toinsert']  = 'Acesta va fi inserat';
+$lang['txt_clickhere'] = 'Clic Aici';
+$lang['txt_year']      = 'An';
+$lang['txt_month']     = 'Luna';
+$lang['arr_daysofweek']= array('Du','Lu','Ma','Mi','Jo','Vi','Sî');
+
+?>
