42e8a6c854f0b736a99f4d301471083f4f1ff9b5
andi <andi@splitbrain.org>
2005-04-05 20:41:15 +0200

    vietnamese language added
    darcs-hash:20050405184115-9977f-ae2ce43f911e9a28f0d9c78c91e69b9271fb649a.gz

---
+++ lang/vi/lang.php
@@ -0,0 +1,101 @@
+<?
+/**
+ * vietnamese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     James Do <jdo@myrealbox.com>
+ */
+$lang['encoding']   = 'utf-8';
+
+$lang['btn_edit']   = 'Biên soạn trang này'; //'Edit this page';
+$lang['btn_source'] = 'Xem mã nguồn'; //'Show pagesource';
+$lang['btn_show']   = 'Xem trang'; //'Show page';
+$lang['btn_create'] = 'Tạo trang này'; //'Create this page';
+$lang['btn_search'] = 'Tìm'; //'Search';
+$lang['btn_save']   = 'Bảo lưu'; //'Save';
+$lang['btn_preview']= 'Duyệt trước'; //'Preview';
+$lang['btn_top']    = 'Trở lên trên'; //'Back to top';
+$lang['btn_revs']   = 'Xem phiên bản cũ'; //'Old revisions';
+$lang['btn_recent'] = 'Thay đổi gần đây'; //'Recent changes';
+$lang['btn_upload'] = 'Tải lên'; //'Upload';
+$lang['btn_cancel'] = 'Huỷ bỏ'; //'Cancel';
+$lang['btn_index']  = 'Mục lục'; //'Index';
+$lang['btn_secedit']= 'Biên soạn'; //'Edit';
+$lang['btn_login']  = 'Đăng nhập'; //'Login';
+$lang['btn_logout'] = 'Thoát'; //'Logout';
+
+$lang['loggedinas'] = 'Username đang dùng'; //'Logged in as';
+$lang['user']       = 'Username';
+$lang['pass']       = 'Password';
+$lang['remember']   = 'Lưu username/password lại'; //'Remember me';
+$lang['fullname']   = 'Họ và tên'; //'Full name';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Đăng ký'; //'Register';
+$lang['badlogin']   = 'Username hoặc password không đúng.'; //'Sorry, username or password was wrong.';
+
+$lang['regmissing'] = 'Bạn cần điền vào tất cả các trường'; //'Sorry, you must fill in all fields.';
+$lang['reguexists'] = 'Bạn khác đã dùng username này rồi.'; //'Sorry, a user with this login already exists.';
+$lang['regsuccess'] = 'Đã tạo username, và đã gởi password.'; //'The user was created. The password was sent by mail.';
+$lang['regmailfail']= 'Không gởi password được. Xin bạn liên hệ với người quản lý.'; //'Looks like there was an error on sending the password mail. Please contact the admin!';
+$lang['regbadmail'] = 'Email hình như không đúng. Xin bạn liên hệ với người quản lý.'; //'The given email address looks invalid - if you think this is an error, contact the admin';
+$lang['regpwmail']  = 'Password DokuWiki của bạn là'; //'Your DokuWiki password';
+$lang['reghere']    = 'Xin bạn đăng ký username nếu chưa có.'; //'You don\'t have an account yet? Just get one';
+
+$lang['txt_upload']   = 'Chọn tệp để tải lên'; //'Select file to upload';
+$lang['txt_filename'] = 'Điền wikiname (tuỳ ý)'; //'Enter wikiname (optional)';
+$lang['lockedby']     = 'Đang khoá bởi'; //'Currently locked by';
+$lang['lockexpire']   = 'Khoá sẽ hết hạn vào lúc'; //'Lock expires at';
+$lang['willexpire']   = 'Khoá của bạn để biên soạn trang này sẽ hết hạn trong vòng 1 phút.\nĐể tránh xung đột, bạn nên bấm nút xem trước để lập lại thời gian khoá'; //'Your lock for editing this page is about to expire in a minute.\nTo avoid conflicts use the preview button to reset the locktimer.';
+
+$lang['notsavedyet'] = 'Hiện có những thay đổi chưa được bảo lưu, và sẽ mất.\nBạn thật sự muốn tiếp tục?'; //'There are unsaved changes, that will be lost.\nReally continue?';
+$lang['rssfailed']   = 'Rút nguồn này gặp phải lỗi'; //'An error occurred while fetching this feed: ';
+$lang['nothingfound']= 'Không tìm được gì'; //'Nothing was found.';
+
+$lang['mediaselect'] = 'Chọn tệp media'; //'Mediafile Selection';
+$lang['fileupload']  = 'Tải lên tệp media'; //'Mediafile Upload';
+$lang['uploadsucc']  = 'Tải lên thành công'; //'Upload successful';
+$lang['uploadfail']  = 'Tải lên thất bại. Có thể vì không đủ phép?'; //'Upload failed. Maybe wrong permissions?';
+$lang['uploadwrong'] = 'Tải lên bị từ chối. Cấm tải loại tệp này'; //'Upload denied. This file extension is forbidden';
+$lang['namespaces']  = 'Đề tài'; //'Namespaces';
+$lang['mediafiles']  = 'Tệp có sẵn ở'; //'Available files in';
+
+$lang['hits']       = 'Trúng'; //'Hits';
+$lang['quickhits']  = 'Tên trang trùng hợp'; //'Matching pagenames';
+$lang['toc']        = 'Bản mục lục'; //'Table of Contents';
+$lang['current']    = 'hiện thời'; //'current';
+$lang['yours']      = 'Phiên bản hiện tại'; //'Your Version';
+$lang['diff']       = 'cho xem khác biệt với phiên bản hiện tại'; //'show differences to current version';
+$lang['line']       = 'Dòng'; //'Line';
+$lang['breadcrumb'] = 'Trang đã xem'; //'Trace';
+$lang['lastmod']    = 'Kỳ thời thay đổi cuối'; //'Last modified';
+$lang['by']         = 'do'; //'by';
+$lang['deleted']    = 'bị xoá'; //'removed';
+$lang['created']    = 'được tạo ra'; //'created';
+$lang['restored']   = 'phiên bản cũ đã được khôi phục'; //'old revision restored';
+$lang['summary']    = 'Tóm tắt biên soạn'; //'Edit summary';
+
+$lang['mail_newpage'] = 'Trang DokuWiki được thêm:'; //'[DokuWiki] page added:';
+$lang['mail_changed'] = 'Trang DokuWiki thay đổi:'; //'[DokuWiki] page changed:';
+
+$lang['nosmblinks'] = 'Nối với các Windows shares chỉ có hiệu lực với Microsoft Internet Explorer.\nBạn vẫn có thể sao và chép các mốc nối.'; //'Linking to Windows shares only works in Microsoft Internet Explorer.\nYou still can copy and paste the link.';
+
+$lang['qb_alert']   = 'Xin bạn điền đoạn văn để định dạng.\nĐoạn này sẽ được nối vào cuối văn kiện.'; //'Please enter the text you want to format.\nIt will be appended to the end of the document.';
+$lang['qb_bold']    = 'Chữ đậm'; //'Bold Text';
+$lang['qb_italic']  = 'Chữ nghiêng'; //'Italic Text';
+$lang['qb_underl']  = 'Chữ gạch dưới'; //'Underlined Text';
+$lang['qb_code']    = 'Chữ mã nguồn'; //'Code Text';
+$lang['qb_h1']      = 'Đầu đề cấp 1'; //'Level 1 Headline';
+$lang['qb_h2']      = 'Đầu đề cấp 2'; //'Level 2 Headline';
+$lang['qb_h3']      = 'Đầu đề cấp 3'; //'Level 3 Headline';
+$lang['qb_h4']      = 'Đầu đề cấp 4'; //'Level 4 Headline';
+$lang['qb_h5']      = 'Đầu đề cấp 5'; //'Level 5 Headline';
+$lang['qb_link']    = 'Mốc nối nội tại'; //'Internal Link';
+$lang['qb_extlink'] = 'Mốc nối ra ngoài'; //'External Link';
+$lang['qb_hr']      = 'Gạch ngang'; //'Horizontal Rule';
+$lang['qb_ol']      = 'Điểm trong danh sách có thứ tự'; //'Ordered List Item';
+$lang['qb_ul']      = 'Điểm trong danh sách không đánh số'; //'Unordered List Item';
+$lang['qb_media']   = 'Thêm ảnh và tệp khác'; //'Add Images and other files';
+$lang['qb_sig']     = 'Đặt chữ ký'; //'Insert Signature';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
+?>
