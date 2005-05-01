e1975fd85f83a9d53bb0d292d853ab3053dd3cf5
andi <andi@splitbrain.org>
2005-05-01 09:10:48 +0200

    esperanto translation added
    darcs-hash:20050501071048-9977f-a4549029b481b625f00619bb1384a3130fd45505.gz

---
+++ lang/eo/lang.php
@@ -0,0 +1,98 @@
+<?
+/**
+ * Esperantiga dosiero
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Antono Vasiljev <esperanto.minsk ĈE tut.by>
+ */
+
+$lang['btn_edit']   = 'Redakti paĝon';
+$lang['btn_source'] = 'Montri fontatekston';
+$lang['btn_show']   = 'Montri paĝon';
+$lang['btn_create'] = 'Krei paĝon';
+$lang['btn_search'] = 'Serĉo';
+$lang['btn_save']   = 'Konservi';
+$lang['btn_preview']= 'Superflugo';
+$lang['btn_top']    = 'Supren';
+$lang['btn_revs']   = 'Antaŭaj revizioj';
+$lang['btn_recent'] = 'Antaŭaj amendoj';
+$lang['btn_upload'] = 'Alŝuti';
+$lang['btn_cancel'] = 'Aboli';
+$lang['btn_index']  = 'Enhavo';
+$lang['btn_secedit']= 'Redakti';
+$lang['btn_login']  = 'Ensaluti';
+$lang['btn_relogin']= 'Resaluti';
+$lang['btn_logout'] = 'Elsaluti';
+
+$lang['loggedinas'] = 'En sistemo kiel uzanto';
+$lang['user']       = 'Uzantnomo';
+$lang['pass']       = 'Pasvorto';
+$lang['remember']   = 'Memorfiksu min';
+$lang['fullname']   = 'Plena nomo';
+$lang['email']      = 'Retpoŝto';
+$lang['register']   = 'Registru';
+$lang['badlogin']   = 'Pardonu, uzantnomo aŭ pasvorto estas malveraj.';
+
+$lang['regmissing'] = 'Pardonu, vi devas plenigi ĉiujn kampojn.';
+$lang['reguexists'] = 'Pardonu, ĉi tia uzantnomo jam estas okupita.';
+$lang['regsuccess'] = 'Konto estas kreita. Pasvorto estas elsendita al via retpoŝto.';
+$lang['regmailfail']= 'Okazis eraro dum elsendado de la pasvorto. Bonvolu informi administranton pri tio!';
+$lang['regbadmail'] = 'Entajpita retpoŝta adreso ne estas regula.';
+$lang['regpwmail']  = 'Via pasvorto';
+$lang['reghere']    = 'Se vi ne havas konton, do vi povos akiri ĝin';
+
+$lang['txt_upload']   = 'Elektu dosieron por alŝuto';
+$lang['txt_filename'] = 'Entajpu wiki-nomo (ne estas nepra)';
+$lang['lockedby']     = 'Ĉimomente estas blokita';
+$lang['lockexpire']   = 'Blokado ĉesos en';
+$lang['willexpire']   = 'Vi povos redakti ĉi tiun paĝon post unu minuto.\nSe vi volas nuligi horloĝon de la blokado, do premu butonon "superflugo".';
+
+$lang['notsavedyet'] = 'Ne konservitaj amendoj perdiĝos.\nĈu vi certe volas daŭrigi procezon?';
+$lang['rssfailed']   = 'Okazis eraro dum ricevado de la novaĵ-strio: ';
+$lang['nothingfound']= 'Ni trovis nenion.';
+
+$lang['mediaselect'] = 'Elekto de medio-dosiero';
+$lang['fileupload']  = 'Alŝuto de medio-dosiero';
+$lang['uploadsucc']  = 'Alŝuto estis sukcesa';
+$lang['uploadfail']  = 'Alŝuto estis malsukcesa. Eble vi havas problemojn pro via rajto de atingo?';
+$lang['namespaces']  = 'Spaco de nomoj';
+$lang['mediafiles']  = 'Akireblaj dosieroj';
+
+$lang['hits']       = 'koincido';
+$lang['quickhits']  = 'Koincidoj trovitaj en nomoj de paĝoj';
+$lang['toc']        = 'Listo de enhavo';
+$lang['current']    = 'kuranta';
+$lang['yours']      = 'Via versio';
+$lang['diff']       = 'montru distingojn de la kuranta versio';
+$lang['line']       = 'Ĉeno';
+$lang['breadcrumb'] = 'Historio';
+$lang['lastmod']    = 'Lastaj amendoj';
+$lang['deleted']    = 'eliminita';
+$lang['created']    = 'kreita';
+$lang['restored']   = 'malnova revizio restarigita';
+$lang['summary']    = 'Bulteno de amendoj';
+
+
+$lang['mail_newpage'] = 'Paĝo [DokuWiki] estas aldonita:';
+$lang['mail_changed'] = 'Paĝo [DokuWiki] estas modifita:';
+
+$lang['nosmblinks'] = '';
+
+$lang['qb_alert']   = 'Bonvolu enskribi redaktendan tekston.\n Ĝi estos aldonita al malsupro de la dokumento.';
+$lang['qb_bold']    = 'Dika tiparo';
+$lang['qb_italic']  = 'Dekliva tiparo';
+$lang['qb_underl']  = 'Substrekita tiparo';
+$lang['qb_code']    = 'Koda teksto';
+$lang['qb_h1']      = 'Titolo de 1-a nivelo';
+$lang['qb_h2']      = 'Titolo de 2-a nivelo';
+$lang['qb_h3']      = 'Titolo de 3-a nivelo';
+$lang['qb_h4']      = 'Titolo de 4-a nivelo';
+$lang['qb_h5']      = 'Titolo de 5-a nivelo';
+$lang['qb_link']    = 'Interna referenco';
+$lang['qb_extlink'] = 'Ekstera referenco';
+$lang['qb_hr']      = 'Hotizontala streko';
+$lang['qb_ol']      = 'Elemento de numerita listo';
+$lang['qb_ul']      = 'Elemento de ne numerita listo';
+$lang['qb_media']   = 'Aldonu boildojn kaj aliajn dosierojn';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
