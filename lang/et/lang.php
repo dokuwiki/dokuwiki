018981ea62d3df5833b6c50f9a1958ab50b3d946
andi <andi@splitbrain.org>
2005-05-07 19:51:31 +0200

    et translation
    darcs-hash:20050507175131-9977f-5978bf2e21251482645a0e726b8ed7f4386b70d6.gz

---
+++ lang/et/lang.php
@@ -0,0 +1,101 @@
+<?
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Oliver S6ro <seem.iges@mail.ee>
+ */
+$lang['encoding']   = 'utf-8';
+
+$lang['btn_edit']   = 'Redigeeri lehekülge';
+$lang['btn_source'] = 'Näita lehe algteksti';
+$lang['btn_show']   = 'Näita lehekülge';
+$lang['btn_create'] = 'Loo see lehekülg';
+$lang['btn_search'] = 'Otsi';
+$lang['btn_save']   = 'Salvesta';
+$lang['btn_preview']= 'Eelvaade';
+$lang['btn_top']    = 'Tagasi üles';
+$lang['btn_revs']   = 'Vanad revisionid';
+$lang['btn_recent'] = 'Viimased muudatused';
+$lang['btn_upload'] = 'Laadi üles';
+$lang['btn_cancel'] = 'Tühista';
+$lang['btn_index']  = 'Indeks';
+$lang['btn_secedit']= 'Redigeeri';
+$lang['btn_login']  = 'Logi sisse';
+$lang['btn_logout'] = 'Logi välja';
+
+$lang['loggedinas'] = 'Logitud sisse kasutajana';
+$lang['user']       = 'Kasutajanimi';
+$lang['pass']       = 'Parool';
+$lang['remember']   = 'Jäta mind meelde';
+$lang['fullname']   = 'Täisnimi';
+$lang['email']      = 'E-post';
+$lang['register']   = 'Registreeri';
+$lang['badlogin']   = 'Aiaiai, vale kasutajanimi ja/või parool.';
+
+$lang['regmissing'] = 'Väljad peavad olema täidetud.';
+$lang['reguexists'] = 'Selline kasutaja juba eksisteerib.';
+$lang['regsuccess'] = 'Kasutaja loodud. Parool saadeti e-postiga.';
+$lang['regmailfail']= 'Paistab, et parooli saatmine e-posti teel ebõnnestus. Palun kontakteeruge administraatoriga.';
+$lang['regbadmail'] = 'Antud e-posti aadress tundub ebakorrektne - kui arvatate, et see on viga, kontakteeruge administraatoriga';
+$lang['regpwmail']  = 'Sinu DokuWiki parool';
+$lang['reghere']    = 'Sul ei ole veel kontot? Lihtsalt registreeri end.';
+
+$lang['txt_upload']   = 'Vali fail üleslaadimiseks';
+$lang['txt_filename'] = 'Sisesta vikinimi (valikuline)';
+$lang['lockedby']     = 'Hetkel lukustatud kasutaja poolt';
+$lang['lockexpire']   = 'Lukustus aegub';
+$lang['willexpire']   = 'Sinu selle lehekülje redigeerimise lukustus aegub 1 minuti pärast.\nLukustuse aegumise vältimise vajuta korra Eelvaade nuppu, mis taaskäivitab lukustuse taimeri.';
+
+$lang['notsavedyet'] = 'Salvestamata muudatused lähevad kaduma.\nSoovid tõesti jätkata?';
+$lang['rssfailed']   = 'Feedi fetchimisel ilmnes viga: ';
+$lang['nothingfound']= 'Ei leitud midagi.';
+
+$lang['mediaselect'] = 'Meediafaili valik';
+$lang['fileupload']  = 'Meediafaili üleslaadimine';
+$lang['uploadsucc']  = 'Üleslaadimine õnnestus';
+$lang['uploadfail']  = 'Üleslaadimine ebaõnnestus. Võibolla valed õiguste seadistused?';
+$lang['uploadwrong'] = 'Sellise laiendiga failide üleslaadimine on keelatud';
+$lang['namespaces']  = 'Namespaces';
+$lang['mediafiles']  = 'Olemasolevaid failid';
+
+$lang['hits']       = 'Tabamusi';
+$lang['quickhits']  = 'Sobivad leheküljenimed';
+$lang['toc']        = 'Sisukord';
+$lang['current']    = 'jooksev';
+$lang['yours']      = 'Sinu versioon';
+$lang['diff']       = 'näita erinevusi jooksvast versioonist';
+$lang['line']       = 'Rida';
+$lang['breadcrumb'] = 'Jälg';
+$lang['lastmod']    = 'Viimati muudetud';
+$lang['by']         = 'by';
+$lang['deleted']    = 'kustutatud';
+$lang['created']    = 'loodud';
+$lang['restored']   = 'vana revision taastatud';
+$lang['summary']    = 'Muudatuste kokkuvõte';
+
+$lang['mail_newpage'] = '[DokuWiki] lehekülg lisatud:';
+$lang['mail_changed'] = '[DokuWiki] lehekülg muudetud:';
+
+$lang['nosmblinks'] = 'Lingid Windowsi võrgudraividele töötavaid ainult Internet Exploreris.\nKuid saad teha siis lõika&kleebi';
+
+$lang['qb_alert']   = 'Palun sisesta tekst mida soovid vormindada.\nVormindatud tekst lisatakse dokumendi lõppu.';
+$lang['qb_bold']    = 'Rasvane kiri';
+$lang['qb_italic']  = 'Kaldkiri';
+$lang['qb_underl']  = 'Alljoonitud kiri';
+$lang['qb_code']    = 'Kood';
+$lang['qb_h1']      = '1 taseme pealkiri';
+$lang['qb_h2']      = '2 taseme pealkiri';
+$lang['qb_h3']      = '3 taseme pealkiri';
+$lang['qb_h4']      = '4 taseme pealkiri';
+$lang['qb_h5']      = '5 taseme pealkiri';
+$lang['qb_link']    = 'Sisemine link';
+$lang['qb_extlink'] = 'Välimine link';
+$lang['qb_hr']      = 'Vertikaalne reegel';
+$lang['qb_ol']      = 'Järjestatud nimekirja kanne';
+$lang['qb_ul']      = 'Järjestamata nimekirja kanne';
+$lang['qb_media']   = 'Lisa pildid ja muud failid';
+$lang['qb_sig']     = 'Lisa allkiri';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
+?>
