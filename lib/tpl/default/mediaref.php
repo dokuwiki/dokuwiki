a00f13e13221d2be214ebff5b883dfc0a361638e
matthiasgrimm <matthiasgrimm@users.sourceforge.net>
2005-06-17 15:10:41 +0200

    XML conformity for media reference dialog
    XML has problems with the usual PHP shortcuts '<?' or '<?
    
    darcs-hash:20050617131041-7ef76-9b8704cffebff4b3fe8ecf8c59b07cb7af53f6c7.gz

--- lib/tpl/default/mediaref.php
+++ lib/tpl/default/mediaref.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?
+<?php
 /**
  * DokuWiki Default Template
  *
@@ -14,42 +14,42 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$conf['lang']?>" lang="<?=$conf['lang']?>" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
 <head>
-  <title><?=hsc($lang['mediaselect'])?> [<?=hsc($conf['title'])?>]</title>
+  <title><?php echo hsc($lang['mediaselect'])?> [<?php echo hsc($conf['title'])?>]</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-  <?tpl_metaheaders()?>
+  <?php tpl_metaheaders()?>
 
-  <link rel="shortcut icon" href="<?=DOKU_BASE?>images/favicon.ico" />
-  <link rel="stylesheet" media="screen" type="text/css" href="<?=DOKU_TPL?>layout.css" />
-  <link rel="stylesheet" media="screen" type="text/css" href="<?=DOKU_TPL?>design.css" />
+  <link rel="shortcut icon" href="<?php echo DOKU_BASE?>images/favicon.ico" />
+  <link rel="stylesheet" media="screen" type="text/css" href="<?php echo DOKU_TPL?>layout.css" />
+  <link rel="stylesheet" media="screen" type="text/css" href="<?php echo DOKU_TPL?>design.css" />
 
   <!--[if gte IE 5]>
   <style type="text/css">
     /* that IE 5+ conditional comment makes this only visible in IE 5+ */
     /* IE bugfix for transparent PNGs */
-    //img { behavior: url("<?=DOKU_BASE?>/pngbehavior.htc"); }
+    //img { behavior: url("<?php echo DOKU_BASE?>/pngbehavior.htc"); }
   </style>
   <![endif]-->
 </head>
 
 <body>
 <div class="dokuwiki">
-  <?html_msgarea()?>
+  <?php html_msgarea()?>
 
-  <h1><?=hsc($lang['reference'])?> <code><?=hsc(noNS($DEL))?></code></h1>
+  <h1><?php echo hsc($lang['reference'])?> <code><?php echo hsc(noNS($DEL))?></code></h1>
 
   <div class="mediaref">
     <div class="mediaref_head">
-      <p><?=hsc($lang['ref_inuse'])?></p>
+      <p><?php echo hsc($lang['ref_inuse'])?></p>
     </div>
   
-    <?tpl_showreferences($mediareferences)?>
+    <?php tpl_showreferences($mediareferences)?>
 	
 	<div class="mediaref_footer">
 	  <hr>
-      <?tpl_button('backtomedia')?>
+      <?php tpl_button('backtomedia')?>
     </div>
   </div>
 
