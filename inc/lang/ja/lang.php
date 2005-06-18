5451511ac2842399c326c8b0f7fc547f18522abb
Davilin <webmaster@davilin.com>
2005-06-18 09:17:54 +0200

    japanese language files
    darcs-hash:20050618071754-b0902-37b14df3abee7d82b771737a650a683963eee96b.gz

---
+++ inc/lang/ja/lang.php
@@ -0,0 +1,126 @@
+<?
+/**
+ * japanese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Davilin <webmaster@davilin.com>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = '文書の編集';
+$lang['btn_source'] = 'ソースの表示';
+$lang['btn_show']   = '文書の表示';
+$lang['btn_create'] = '文書の作成';
+$lang['btn_search'] = '検索';
+$lang['btn_save']   = '保存';
+$lang['btn_preview']= '確認';
+$lang['btn_top']    = '文書の先頭へ';
+$lang['btn_prevpage']= '<< 前の文書';
+$lang['btn_nextpage']= '次の文書 >>';
+$lang['btn_revs']   = '以前のリビジョン';
+$lang['btn_recent'] = '最近の変更';
+$lang['btn_upload'] = 'アップロード';
+$lang['btn_cancel'] = 'キャンセル';
+$lang['btn_index']  = '索引';
+$lang['btn_secedit']= '編集';
+$lang['btn_login']  = 'ログイン';
+$lang['btn_logout'] = 'ログアウト';
+$lang['btn_admin']  = '管理';
+$lang['btn_update'] = '更新';
+$lang['btn_delete'] = '削除';
+
+$lang['loggedinas'] = 'ようこそ';
+$lang['user']       = 'ユーザー名';
+$lang['pass']       = 'パスワード';
+$pass['passchk']    = '確認';
+$lang['remember']   = 'ユーザー名とパスワードを記憶する';
+$lang['fullname']   = 'フルネーム';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'ユーザー登録';
+$lang['badlogin']   = 'ユーザー名かパスワードが違います。';
+
+$lang['regmissing'] = '全ての項目を入力してください。';
+$lang['reguexists'] = 'このユーザー名は既に存在しています。';
+$lang['regsuccess'] = '新しいユーザーが作成されました。パスワードは登録したメールアドレス宛てに送付されます。';
+$lang['regsuccess2']= '新しいユーザーが作成されました。';
+$lang['regmailfail']= 'パスワードのメール送信に失敗しました。お手数ですが管理者まで連絡をお願いします。';
+$lang['regbadmail'] = 'メールアドレスが有効ではありません。';
+$lang['regbadpass'] = '確認用のパスワードが正しくありません。';
+$lang['regpwmail']  = 'あなたの DokuWiki パスワード';
+$lang['reghere']    = 'ご自分用のアカウントを取ってみては如何ですか？';
+
+$lang['txt_upload']   = 'アップロードするファイルを選んでください。';
+$lang['txt_filename'] = 'Wiki名を入力（オプション）';
+$lang['lockedby']     = 'この文書は次のユーザによってロックされています';
+$lang['lockexpire']   = '排他期限';
+$lang['willexpire']   = '編集中の文書は排他期限を過ぎようとしています。このままロックする場合は、一度文書の確認を行ってロックタイマーをリセットしてください。';
+
+$lang['notsavedyet'] = '変更は保存されません。このまま処理を続けてよろしいですか？';
+$lang['rssfailed']   = 'RSSの取り出しに失敗しました：';
+$lang['nothingfound']= '該当文書はありませんでした。';
+
+$lang['mediaselect'] = 'メディアファイルを選択';
+$lang['fileupload']  = 'メディアファイルをアップロード';
+$lang['uploadsucc']  = 'アップロード完了';
+$lang['uploadfail']  = 'アップロードに失敗しました。権限がありません。';
+$lang['uploadwrong'] = 'アップロードは拒否されました。この拡張子は許可されていません。';
+$lang['namespaces']  = '名前空間';
+$lang['mediafiles']  = '有効なファイル：';
+
+$lang['hits']       = 'ヒット';
+$lang['quickhits']  = 'マッチした文書名';
+$lang['toc']        = '目次';
+$lang['current']    = '現在';
+$lang['yours']      = 'あなたのバージョン';
+$lang['diff']       = '現在のバージョンとの差分を表示';
+$lang['line']       = 'ライン';
+$lang['breadcrumb'] = 'トレース';
+$lang['lastmod']    = '最終更新';
+$lang['by']         = 'by';
+$lang['deleted']    = '削除';
+$lang['created']    = '作成';
+$lang['restored']   = '以前のバージョンを復元';
+$lang['summary']    = 'サマリーを編集';
+
+$lang['mail_newpage'] = '[DokuWiki] 文書の追加：';
+$lang['mail_changed'] = '[DokuWiki] 文書の変更：';
+
+$lang['nosmblinks'] = 'Linking to Windows shares only works in Microsoft Internet Explorer.\nYou still can copy and paste the link.';
+
+$lang['qb_alert']   = '文書の最後に挿入したい定型文書を入力して下さい。';
+$lang['qb_bold']    = '太字';
+$lang['qb_italic']  = '斜体';
+$lang['qb_underl']  = '下線';
+$lang['qb_code']    = 'コード';
+$lang['qb_h1']      = '第一見出し';
+$lang['qb_h2']      = '第二見出し';
+$lang['qb_h3']      = '第三見出し';
+$lang['qb_h4']      = '第四見出し';
+$lang['qb_h5']      = '第五見出し';
+$lang['qb_link']    = '内部リンク';
+$lang['qb_extlink'] = '外部リンク';
+$lang['qb_hr']      = '横罫線';
+$lang['qb_ol']      = '記号付きリスト';
+$lang['qb_ul']      = '記号なしリスト';
+$lang['qb_media']   = 'イメージやファイルの追加';
+$lang['qb_sig']     = '署名の挿入';
+
+$lang['del_confirm']= 'エントリを削除しますか？';
+
+$lang['admin_acl']  = 'アクセスコントロール管理...';
+$lang['admin_register']= '新規ユーザー登録...';
+
+$lang['acl_group']  = 'グループ';
+$lang['acl_user']   = 'ユーザー';
+$lang['acl_perms']  = '権限を追加';
+$lang['page']       = '文書';
+$lang['namespace']  = '名前空間';
+
+$lang['acl_perm1']  = '読取';
+$lang['acl_perm2']  = '編集';
+$lang['acl_perm4']  = '作成';
+$lang['acl_perm8']  = 'アップロード';
+$lang['acl_new']    = '新規エントリ';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
