d477fdcf1c8e21ef479942fe1d14acb7991d8a4a
Linas Valiukas <shirshegsm@gmail.com>
2005-06-25 20:14:26 +0200

    lithuanian language added
    darcs-hash:20050625181426-9a1cb-5d1a4d7208ceea1e3b327898b2bbdfcb838e2ae2.gz

---
+++ inc/lang/lt/lang.php
@@ -0,0 +1,118 @@
+<?
+/**
+ * lithuanian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Linas Valiukas <shirshegsm@gmail.com>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = 'Redaguoti šį puslapį';
+$lang['btn_source'] = 'Parodyti puslapio kodą';
+$lang['btn_show']   = 'Parodyti puslapį';
+$lang['btn_create'] = 'Sukurti šį puslapį';
+$lang['btn_search'] = 'Paieška';
+$lang['btn_save']   = 'Išsaugoti';
+$lang['btn_preview']= 'Peržiūra';
+$lang['btn_top']    = 'Į viršų';
+$lang['btn_revs']   = 'Senos versijos';
+$lang['btn_recent'] = 'Naujausi keitimai';
+$lang['btn_upload'] = 'Atsiųsti bylą';
+$lang['btn_cancel'] = 'Atšaukti';
+$lang['btn_index']  = 'Indeksas';
+$lang['btn_secedit']= 'Redaguoti';
+$lang['btn_login']  = 'Prisijungti';
+$lang['btn_logout'] = 'Atsijungti';
+$lang['btn_admin']  = 'Administracija';
+$lang['btn_update'] = 'Atnaujinti';
+$lang['btn_delete'] = 'Ištrinti';
+
+$lang['loggedinas'] = 'Prisijungęs kaip';
+$lang['user']       = 'Vartotojo vardas';
+$lang['pass']       = 'Slaptažodis';
+$lang['remember']   = 'Prisiminti mane';
+$lang['fullname']   = 'Visas vardas';
+$lang['email']      = 'El. pašto adresas';
+$lang['register']   = 'Registruotis';
+$lang['badlogin']   = 'Nurodėte blogą vartotojo vardą arba slaptažodį.';
+
+$lang['regmissing'] = 'Turite užpildyti visus laukus.';
+$lang['reguexists'] = 'Vartotojas su pasirinktu prisijungimo vardu jau egzistuoja.';
+$lang['regsuccess'] = 'Vartotojas sukurtas, slaptažodis išsiųstas el. paštu.';
+$lang['regmailfail']= 'Siunčiant slaptažodį el. paštu įvyko klaida - susisiekite su administracija!';
+$lang['regbadmail'] = 'Nurodytas el. pašto adresas yra neteisingas - jei manote, kad tai klaida, susisiekite su administracija';
+$lang['regpwmail']  = 'Jūsų DokuWiki slaptažodis';
+$lang['reghere']    = 'Dar neužsiregistravote? Padarykite tai dabar';
+
+$lang['txt_upload']   = 'Išsirinkite atsiunčiamą bylą';
+$lang['txt_filename'] = 'Įveskite wikivardą (nebūtina)';
+$lang['lockedby']     = 'Užrakintas vartotojo';
+$lang['lockexpire']   = 'Užraktas bus nuimtas';
+$lang['willexpire']   = 'Šio puslapio redagavimo užrakto galiojimo laikas baigsis po minutės.\nNorėdami išvengti nesklandumų naudokite peržiūros mygtuką ir užraktas atsinaujins.';
+
+$lang['notsavedyet'] = 'Pakeitimai nebus išsaugoti.\nTikrai tęsti?';
+$lang['rssfailed']   = 'Siunčiant šį feed\'ą įvyko klaida: ';
+$lang['nothingfound']= 'Paieškos rezultatų nėra.';
+
+$lang['mediaselect'] = 'Mediabylos išsirinkimas';
+$lang['fileupload']  = 'Mediabylos atsiuntimas';
+$lang['uploadsucc']  = 'Atsiuntimas pavyko';
+$lang['uploadfail']  = 'Atsiuntimas nepavyko. Blogi priėjimo leidimai??';
+$lang['uploadwrong'] = 'Atsiuntimas atmestas. Bylos tipas neleistinas';
+$lang['namespaces']  = 'Pavadinimai';
+$lang['mediafiles']  = 'Prieinamos bylos';
+
+$lang['hits']       = 'Atidarymai';
+$lang['quickhits']  = 'Sutampantys pavadinimai';
+$lang['toc']        = 'Turinys';
+$lang['current']    = 'esamas';
+$lang['yours']      = 'Jūsų versija';
+$lang['diff']       = 'rodyti skirtumus tarp šios ir esamos versijos';
+$lang['line']       = 'Linija';
+$lang['breadcrumb'] = 'Kelias';
+$lang['lastmod']    = 'Keista';
+$lang['by']         = 'vartotojo';
+$lang['deleted']    = 'ištrintas';
+$lang['created']    = 'sukurtas';
+$lang['restored']   = 'atstatyta sena versija';
+$lang['summary']    = 'Redaguoti santrauką';
+
+$lang['mail_newpage'] = '[DokuWiki] puslapis pridėtas:';
+$lang['mail_changed'] = '[DokuWiki] puslapis pakeistas:';
+
+$lang['nosmblinks'] = 'Nurodos į "Windows shares" veikia tik su Microsoft Internet Explorer naršykle.\nVis dėlto, jūs galite nukopijuoti šią nuorodą.';
+
+$lang['qb_alert']   = 'Įveskite formatuojamą tekstą.\nJis bus pridėtas dokumento apačioje.';
+$lang['qb_bold']    = 'Pusjuodis';
+$lang['qb_italic']  = 'Kursyvas';
+$lang['qb_underl']  = 'Pabrauktas';
+$lang['qb_code']    = 'Kodas';
+$lang['qb_h1']      = 'Pirmo lygio antraštė';
+$lang['qb_h2']      = 'Antro lygio antraštė';
+$lang['qb_h3']      = 'Trečio lygio antraštė';
+$lang['qb_h4']      = 'Ketvirto lygio antraštė';
+$lang['qb_h5']      = 'Penkto lygio antraštė';
+$lang['qb_link']    = 'Vidinė nuoroda';
+$lang['qb_extlink'] = 'Išorinė nuoroda';
+$lang['qb_hr']      = 'Horizontali linija';
+$lang['qb_ol']      = 'Numeruotas sąrašas';
+$lang['qb_ul']      = 'Nenumetuotas sąrašas';
+$lang['qb_media']   = 'Paveikslėliai ir kitos bylos';
+$lang['qb_sig']     = 'Įterpti parašą';
+
+$lang['del_confirm']= 'Ištrinti šį įrašą?';
+
+$lang['admin_acl']  = 'Priėjimo Kontrolės Sąrašų valdymas...';
+
+$lang['acl_group']  = 'Grupė';
+$lang['acl_user']   = 'Vartotojas';
+$lang['acl_perms']  = 'Leidimai';
+$lang['page']       = 'Puslapis';
+$lang['namespace']  = 'Pavadinimas';
+
+$lang['acl_perm1']  = 'Skaityti';
+$lang['acl_perm2']  = 'Redaguoti';
+$lang['acl_perm4']  = 'Sukurti';
+$lang['acl_perm8']  = 'Atsiųsti';
+$lang['acl_new']    = 'Pridėti naują įrašą';
