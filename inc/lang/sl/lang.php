d4c0c76eab7c3a3d9e9a24e1fae3c3d422eecbee
Jaka Kranjc <lynxlupodian@hotmail.com>
2005-06-26 21:45:05 +0200

    slovenian language added
    darcs-hash:20050626194505-410e9-e5629b89c54132b7a51dafcd26fe8340f2e70eb2.gz

---
+++ inc/lang/sl/lang.php
@@ -0,0 +1,100 @@
+<?
+/**
+ * slovenian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Jaka Kranjc <lynxlupodian@hotmail.com>
+ */
+$lang['encoding']   = 'utf-8';
+
+$lang['btn_edit']   = 'Uredi to stran';
+$lang['btn_source'] = 'Prikaži izvorno kodo strani';
+$lang['btn_show']   = 'Prikaži stran';
+$lang['btn_create'] = 'Ustvari to stran';
+$lang['btn_search'] = 'Išči';
+$lang['btn_save']   = 'Shrani';
+$lang['btn_preview']= 'Predogled';
+$lang['btn_top']    = 'Nazaj na vrh';
+$lang['btn_revs']   = 'Stare različice';
+$lang['btn_recent'] = 'Novosti';
+$lang['btn_upload'] = 'Pošlji';
+$lang['btn_cancel'] = 'Prekliči';
+$lang['btn_index']  = 'Kazalo';
+$lang['btn_secedit']= 'Uredi';
+$lang['btn_login']  = 'Prijava';
+$lang['btn_logout'] = 'Odjava';
+
+$lang['loggedinas'] = 'Prijavljen kot';
+$lang['user']       = 'Uporabniško ime';
+$lang['pass']       = 'Geslo';
+$lang['remember']   = 'Zapomni si me';
+$lang['fullname']   = 'Pravo ime';
+$lang['email']      = 'Elektronska pošta';
+$lang['register']   = 'Odpri nov račun';
+$lang['badlogin']   = 'Oprostite, uporabniško ime ali geslo ni pravo.';
+
+$lang['regmissing'] = 'Oprostite, zapolniti morate vsa polja.';
+$lang['reguexists'] = 'Oprostite, uporabnik s tem imenom že obstaja.';
+$lang['regsuccess'] = 'Uporabnik je bil ustvarjen. Geslo je bilo poslano na vaš elektronski naslov.';
+$lang['regmailfail']= 'Zgleda, da je prišlo do napake pri pošiljanju gesla. Prosimo da stopite v stik z administratorjem!';
+$lang['regbadmail'] = 'Podan elektronski naslov izgleda neveljaven - če mislite da je to napaka, stopite v stik z administratorjem.';
+$lang['regpwmail']  = 'Vaše geslo za DokuWiki';
+$lang['reghere']    = 'Nimate še računa? Priskrbite si ga';
+
+$lang['txt_upload']   = 'Izberite datoteko za pošiljanje';
+$lang['txt_filename'] = 'Vnesite wikiname (neobvezno)';
+$lang['lockedby']     = 'Trenutno zaklenjeno od';
+$lang['lockexpire']   = 'Zaklep preteče';
+$lang['willexpire']   = 'Vaš zaklep za urejevanje bo pretekel čez eno minuto.\nDa se izognete konfliktom, uporabite predogled, da se merilnik časa za zaklep ponastavi.';
+
+$lang['notsavedyet'] = 'Obstajajo neshranjene spremembe, ki bodo izgubljene.\nRes želite nadaljevati?';
+$lang['rssfailed']   = 'Prišlo je do napake pri prenašanju tega dovoda: ';
+$lang['nothingfound']= 'Nič ni bilo najdeno.';
+
+$lang['mediaselect'] = 'Mediafile Izbira';
+$lang['fileupload']  = 'Mediafile Pošiljanje';
+$lang['uploadsucc']  = 'Pošiljanje uspelo';
+$lang['uploadfail']  = 'Pošiljanje je spodletelo. Mogoče nimate dovoljenj?';
+$lang['uploadwrong'] = 'Pošiljanje zavrnjeno. Ta datotečna končnica je prepovedana';
+$lang['namespaces']  = 'Imenski prostori';
+$lang['mediafiles']  = 'Datoteke ki so na voljo v';
+
+$lang['hits']       = 'Zadetkov';
+$lang['quickhits']  = 'Ujemanja v imenih strani';
+$lang['toc']        = 'Kazalo';
+$lang['current']    = 'trenutna';
+$lang['yours']      = 'Vaša različica';
+$lang['diff']       = 'prikaži razlike s trenutno različico';
+$lang['line']       = 'Vrstica';
+$lang['breadcrumb'] = 'Sled';
+$lang['lastmod']    = 'Zadnjič spremenil/a';
+$lang['by']         = '';
+$lang['deleted']    = 'odstranjena';
+$lang['created']    = 'ustvarjena';
+$lang['restored']   = 'stara različica povrnjena';
+$lang['summary']    = 'Povzetek urejanja';
+
+$lang['mail_newpage'] = '[DokuWiki] stran dodana:';
+$lang['mail_changed'] = '[DokuWiki] stran spremenjena:';
+
+$lang['nosmblinks'] = 'Povezovanje do Windows deljenih datotek deluje samo v Microsoft Internet Explorer-ju.\nŠe vedno pa lahko ročno kopirate povezavo.';
+
+$lang['qb_alert']   = 'Prosimo vnesite besedilo, ki ga želite oblikovati.\nDodano bo na konec dokumenta.';
+$lang['qb_bold']    = 'Krepki tisk';
+$lang['qb_italic']  = 'Ležeči tisk';
+$lang['qb_underl']  = 'Podčrtano besedilo';
+$lang['qb_code']    = 'Koda';
+$lang['qb_h1']      = 'Naslov prve stopnje';
+$lang['qb_h2']      = 'Naslov drugee stopnje';
+$lang['qb_h3']      = 'Naslov tretje stopnje';
+$lang['qb_h4']      = 'Naslov četrte stopnje';
+$lang['qb_h5']      = 'Naslov pete stopnje';
+$lang['qb_link']    = 'Notranja povezava';
+$lang['qb_extlink'] = 'Zunanja povezava';
+$lang['qb_hr']      = 'Vodoravna črta';
+$lang['qb_ol']      = 'Element urejenega seznama';
+$lang['qb_ul']      = 'Element neurejenega seznama';
+$lang['qb_media']   = 'Dodaj slike in druge datoteke';
+$lang['qb_sig']     = 'Vstavi podpis';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
