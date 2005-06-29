549111efab008942c840b0bd13eab46daddcf644
Aivars Miška <allefm@gmail.com>
2005-06-29 18:11:46 +0200

    Latvian translation added
    darcs-hash:20050629161146-f5ed9-539dc83a1660e9445bf5bea647e376b09626ec2e.gz

---
+++ inc/lang/lv/lang.php
@@ -0,0 +1,120 @@
+<?
+/**
+ * latvian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aivars Miška <allefm@gmail.com>
+*/
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = 'Labot lapu';
+$lang['btn_source'] = 'Parādīt lapas kodu';
+$lang['btn_show']   = 'Parādīt lapu';
+$lang['btn_create'] = 'Izveidot lapu';
+$lang['btn_search'] = 'Meklēt';
+$lang['btn_save']   = 'Saglabāt';
+$lang['btn_preview']= 'Priekšskats';
+$lang['btn_top']    = 'Atpakaļ uz sākumu';
+$lang['btn_revs']   = 'Vecās versijas';
+$lang['btn_recent'] = 'Jaunākie grozījumi';
+$lang['btn_upload'] = 'Augšupielādēt';
+$lang['btn_cancel'] = 'Atlikt';
+$lang['btn_index']  = 'Rādītājs';
+$lang['btn_secedit']= 'Labot';
+$lang['btn_login']  = 'Ieiet';
+$lang['btn_logout'] = 'Iziet';
+$lang['btn_admin']  = 'Administrēt';
+$lang['btn_update'] = 'Atjaunot';
+$lang['btn_delete'] = 'Dzēst';
+
+$lang['loggedinas'] = 'Pieteicies kā';
+$lang['user']       = 'Lietotājvārds';
+$lang['pass']       = 'Parole';
+$lang['remember']   = 'Atceries mani';
+$lang['fullname']   = 'Pilns vārds';
+$lang['email']      = 'E-pasts';
+$lang['register']   = 'Reģistrēties';
+$lang['badlogin']   = 'Atvaino, lietotājvārds vai parole aplama.';
+
+$lang['regmissing'] = 'Atvaino, jāaizpilda visas ailes.';
+$lang['reguexists'] = 'Atvaino, tāds lietājs jau ir.';
+$lang['regsuccess'] = 'Lietotājs izveidots. Parole nosūtīta pa pastu.';
+$lang['regmailfail']= 'Šķiet, ka ir problēmas nosūtīt pastu. Lūdzu sazinies ar administratoru!';
+$lang['regbadmail'] = 'Uzdotā epasta adrese izskatās aplama. Ja tas nav tiesa, sazinies ar administratoru.';
+$lang['regpwmail']  = 'Tava DokuWiki parole';
+$lang['reghere']    = 'Tev vēl nav sava konta? Izveido!';
+
+$lang['txt_upload']   = 'Norādi augšupielādējamo failu';
+$lang['txt_filename'] = 'Ievadi wikivārdu (nav obligāts)';
+$lang['lockedby']     = 'Patlaban bloķējis ';
+$lang['lockexpire']   = 'Bloķējums beigsies ';
+$lang['willexpire']   = 'Tavs bloķējums uz šo lapu pēc minūtes beigsies.\nLai izvairītos no konflikta, nospied Iepriekšapskata pogu\n un bloķējuma laiku sāks skaitīt no jauna.';
+
+$lang['notsavedyet'] = 'Veiktas bet nav saglabātas izmaiņas.\nVai tiešām tās nevajag?';
+$lang['rssfailed']   = 'Kļūda saņemot saturu no ';
+$lang['nothingfound']= 'Nekas nav atrasts.';
+
+$lang['mediaselect'] = 'Mēdiju faila izvēle';
+$lang['fileupload']  = 'Mēdiju faila augšupielāde';
+$lang['uploadsucc']  = 'Veiksmīgi ielādēts';
+$lang['uploadfail']  = 'Ielādes kļūme. Varbūt aplamas tiesības?';
+$lang['uploadwrong'] = 'Ielāde aizliegta. Neatļauts faila palšinājums';
+$lang['namespaces']  = 'Nodaļas';
+$lang['mediafiles']  = 'Pieejamie faili';
+
+$lang['hits']       = 'Apmeklējumi';
+$lang['quickhits']  = 'Atbilstošās lapas';
+$lang['toc']        = 'Satura rādītājs';
+$lang['current']    = 'patlaban';
+$lang['yours']      = 'Tava versija';
+$lang['diff']       = 'atšķirības no patreizējas versijas';
+$lang['line']       = 'Rinda';
+$lang['breadcrumb'] = 'Ceļš';
+$lang['lastmod']    = 'Labota';
+$lang['by']         = ', veicis';
+$lang['deleted']    = 'dzēsts';
+$lang['created']    = 'izveidots';
+$lang['restored']   = 'vecā versija atjaunota';
+$lang['summary']    = 'Anotācija';
+
+$lang['mail_newpage'] = '[DokuWiki] lapa pievienota:';
+$lang['mail_changed'] = '[DokuWiki] lapa mainīta:';
+
+$lang['nosmblinks'] = 'Saites uz Windows resursiem darbojas tikai Microsoft Internet Explorer.\nProtams, ka vari saiti kopēt un iespraust citā programmā.';
+
+$lang['qb_alert']   = 'Lūdzu ieraksti formatējamo teku.\nTo pievienos dokumenta galā.';
+$lang['qb_bold']    = 'Trekninājums';
+$lang['qb_italic']  = 'Kursīvs';
+$lang['qb_underl']  = 'Pasvītrojums';
+$lang['qb_code']    = 'Vienplatuma burti';
+$lang['qb_h1']      = '1. līmeņa virsraksts';
+$lang['qb_h2']      = '2. līmeņa virsraksts';
+$lang['qb_h3']      = '3. līmeņa virsraksts';
+$lang['qb_h4']      = '4. līmeņa virsraksts';
+$lang['qb_h5']      = '5. līmeņa virsraksts';
+$lang['qb_link']    = 'Iekšēja saite';
+$lang['qb_extlink'] = 'Ārēja saite';
+$lang['qb_hr']      = 'Horizontāla līnija';
+$lang['qb_ol']      = 'Numurēts saraksts';
+$lang['qb_ul']      = 'Nenumurēts saraksts';
+$lang['qb_media']   = 'Pielikt attēlus un citus failus.';
+$lang['qb_sig']     = 'Ievietot parakstu';
+
+$lang['del_confirm']= 'Dzēst šo šķirkli?';
+
+$lang['admin_acl']  = 'Piekļuves tiesību vadība...';
+
+$lang['acl_group']  = 'Grupa';
+$lang['acl_user']   = 'Lietotājs';
+$lang['acl_perms']  = 'Tiesības';
+$lang['page']       = 'Lapa';
+$lang['namespace']  = 'Nodaļa';
+
+$lang['acl_perm1']  = 'Lasīt';
+$lang['acl_perm2']  = 'Labot';
+$lang['acl_perm4']  = 'Izveidot';
+$lang['acl_perm8']  = 'augšupielādēt';
+$lang['acl_new']    = 'Pievienot jaunu';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
