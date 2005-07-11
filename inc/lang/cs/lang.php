210a54be3f48a25697c3bdf259ffe94af4b65cf8
Bohumir Zamecnik <bohumir@zamecnik.org>
2005-07-11 18:39:37 +0200

    cs lang update
    darcs-hash:20050711163937-9be09-9194238d6aeb6b526ebb30a501f0499f54e23424.gz

--- inc/lang/cs/lang.php
+++ inc/lang/cs/lang.php
@@ -3,19 +3,21 @@
  * Czech language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Bohumir Zamecnik <bohous@luhy.net>
+ * @author     Bohumir Zamecnik <bohumir@zamecnik.org>
  */
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
 
 $lang['btn_edit']   = 'Upravit stránku';
-$lang['btn_source'] = 'Zobrazit zdroj stránky';
+$lang['btn_source'] = 'Zdrojový kód stránky';
 $lang['btn_show']   = 'Zobrazit stránku';
 $lang['btn_create'] = 'Vytvořit stránku';
 $lang['btn_search'] = 'Hledat';
 $lang['btn_save']   = 'Uložit';
 $lang['btn_preview']= 'Náhled';
 $lang['btn_top']    = 'Nahoru';
+$lang['btn_newer']  = '<< novější';
+$lang['btn_older']  = 'starší >>';
 $lang['btn_revs']   = 'Starší verze';
 $lang['btn_recent'] = 'Poslední úpravy';
 $lang['btn_upload'] = 'Přiložit';
@@ -24,10 +26,17 @@ $lang['btn_index']  = 'Index';
 $lang['btn_secedit']= 'Upravit';
 $lang['btn_login']  = 'Přihlásit se';
 $lang['btn_logout'] = 'Odhlásit se';
+$lang['btn_admin']  = 'Správa';
+$lang['btn_update'] = 'Aktualizovat';
+$lang['btn_delete'] = 'Vymazat';
+$lang['btn_back']   = 'Zpět';
+$lang['btn_backtomedia'] = 'Zpět do Výběru dokumentu';
 
 $lang['loggedinas'] = 'Přihlášen(a) jako';
 $lang['user']       = 'Uživatelské jméno';
 $lang['pass']       = 'Heslo';
+$lang['passchk']    = 'ještě jednou';
+$lang['remember']   = 'Přihlásit se nastálo';
 $lang['fullname']   = 'Celé jméno';
 $lang['email']      = 'E-Mail';
 $lang['register']   = 'Registrovat';
@@ -36,13 +45,16 @@ $lang['badlogin']   = 'Zadané uživatelské jméno a heslo není správně.';
 $lang['regmissing'] = 'Musíte vyplnit všechny údaje.';
 $lang['reguexists'] = 'Uživatel se stejným jménem už je zaregistrován.';
 $lang['regsuccess'] = 'Uživatelský účet byl vytvořen a heslo zasláno mailem.';
+$lang['regsuccess2']= 'Uživatelský účet byl vytvořen.';
 $lang['regmailfail']= 'Zdá se, že nastala chyba při posílání mailu s heslem. Zkuste kontaktovat správce.';
 $lang['regbadmail'] = 'Zadaná mailová adresa není platná. Pokud si myslíte, že to je špatně, zkuste kontaktovat správce.';
+$lang['regbadpass'] = 'Heslo nebylo zadáno dvakrát stejně, zkuste to prosím znovu.';
 $lang['regpwmail']  = 'Vaše heslo do systému DokuWiki';
 $lang['reghere']    = 'Nemáte uživatelský účet? Zřiďte si ho';
 
 $lang['txt_upload']   = 'Vyberte soubor jako přílohu';
 $lang['txt_filename'] = 'Wiki jméno (volitelné)';
+$lang['txt_overwrt']  = 'Přepsat existující soubor';
 $lang['lockedby']     = 'Právě zamknuto:';
 $lang['lockexpire']   = 'Zámek vyprší:';
 $lang['willexpire']   = 'Váš zámek pro editaci za chvíli vyprší.\nAbyste předešli konfliktům, stiskněte tlačítko Náhled a zámek se prodlouží.';
@@ -56,8 +68,16 @@ $lang['fileupload']  = 'Nahrávání dokumentu';
 $lang['uploadsucc']  = 'Přenos proběhl v pořádku';
 $lang['uploadfail']  = 'Chyba při nahrávání. Možná kvůli špatně nastaveným právům?';
 $lang['uploadwrong'] = 'Přiložení souboru s takovouto příponou není dovoleno.';
+$lang['uploadexist'] = 'Soubor už existuje, necháme ho být.';
+$lang['deletesucc']  = 'Soubor "%s" byl vymazán.';
+$lang['deletefail']  = 'Soubor "%s" nelze vymazat - zkontrolujte oprávnění.';
+$lang['mediainuse']  = 'Soubor "%s" nebyl vymazán - používá se.';
 $lang['namespaces']  = 'Jmenné prostory';
 $lang['mediafiles']  = 'Dostupné soubory';
+
+$lang['reference']   = 'Odkazy na';
+$lang['ref_inuse']   = 'Soubor nelze vymazat, jelikož ho využívají následující stránky:';
+$lang['ref_hidden']  = 'Některé odkazy jsou na stránkách, kam nemáte právo přístupu';
 
 $lang['hits']       = '- počet výskytů';
 $lang['quickhits']  = 'Odpovídající stránky';
@@ -68,6 +88,7 @@ $lang['diff']       = 'zobrazit rozdíly vůči aktuální verzi';
 $lang['line']       = 'Řádek';
 $lang['breadcrumb'] = 'Historie';
 $lang['lastmod']    = 'Poslední úprava';
+$lang['by']         = 'autor:';
 $lang['deleted']    = 'odstraněno';
 $lang['created']    = 'vytvořeno';
 $lang['restored']   = 'stará verze byla obnovena';
@@ -83,6 +104,7 @@ $lang['qb_bold']    = 'Tučně';
 $lang['qb_italic']  = 'Kurzíva';
 $lang['qb_underl']  = 'Podtržení';
 $lang['qb_code']    = 'Neformátovat (zdrojový kód)';
+$lang['qb_strike']  = 'Přeškrtnutý text';
 $lang['qb_h1']      = 'Nadpis 1. úrovně';
 $lang['qb_h2']      = 'Nadpis 2. úrovně';
 $lang['qb_h3']      = 'Nadpis 3. úrovně';
@@ -95,5 +117,30 @@ $lang['qb_ol']      = 'Číslovaný seznam';
 $lang['qb_ul']      = 'Nečíslovaný seznam';
 $lang['qb_media']   = 'Vložit obrázky nebo jiné soubory';
 $lang['qb_sig']     = 'Vložit podpis';
+
+$lang['del_confirm']= 'Vymazat tuto položku?';
+
+$lang['admin_acl']  = 'Správa přístupových práv...';
+$lang['admin_register']= 'Přidat nového uživatele...';
+
+$lang['acl_group']  = 'Skupina';
+$lang['acl_user']   = 'Uživatel';
+$lang['acl_perms']  = 'Práva pro';
+$lang['page']       = 'Stránka';
+$lang['namespace']  = 'Jmenný prostor';
+
+$lang['acl_perm1']  = 'Čtení';
+$lang['acl_perm2']  = 'Úpravy';
+$lang['acl_perm4']  = 'Vytvoření';
+$lang['acl_perm8']  = 'Upload';
+$lang['acl_perm16'] = 'Mazání';
+$lang['acl_new']    = 'Přidat novou položku';
+
+$lang['spell_start'] = 'Zkontrolovat pravopis';
+$lang['spell_stop']  = 'Pokračovat v úpravách';
+$lang['spell_wait']  = 'Prosím počkejte...';
+$lang['spell_noerr'] = 'Bez chyb';
+$lang['spell_nosug'] = 'Žádné návrhy';
+$lang['spell_change']= 'Změnit';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
