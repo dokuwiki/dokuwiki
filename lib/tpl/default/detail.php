3f467924ce92dc839fe38dd034e8c731cffec819
Andreas Gohr <andi@splitbrain.org>
2005-07-25 20:34:10 +0200

    added missing detail.php
    darcs-hash:20050725183410-7ad00-14559cd592ab41f9cc14ea4b502ce6da019eb262.gz

---
+++ lib/tpl/default/detail.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+/**
+ * DokuWiki Image Detail Template
+ *
+ * This is the template for displaying image details
+ *
+ * You should leave the doctype at the very top - It should
+ * always be the very first line of a document.
+ *
+ * @link   http://wiki.splitbrain.org/wiki:tpl:templates
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
+<head>
+  <title>
+     <?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG))?>
+    [<?php echo hsc($conf['title'])?>]
+  </title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+  <?php tpl_metaheaders()?>
+
+  <link rel="shortcut icon" href="<?php echo DOKU_BASE?>lib/images/favicon.ico" />
+  <link rel="stylesheet" media="screen" type="text/css" href="<?php echo DOKU_TPL?>layout.css" />
+  <link rel="stylesheet" media="screen" type="text/css" href="<?php echo DOKU_TPL?>design.css" />
+
+  <!--[if gte IE 5]>
+  <style type="text/css">
+    /* that IE 5+ conditional comment makes this only visible in IE 5+ */
+    /* IE bugfix for transparent PNGs */
+    //DISABLED img { behavior: url("<?php echo DOKU_BASE?>lib/scripts/pngbehavior.htc"); }
+  </style>
+  <![endif]-->
+</head>
+
+<body>
+<div class="dokuwiki">
+  <?php html_msgarea()?>
+
+  <div class="page">
+	  <?php if($ERROR){ print $ERROR; }else{ ?>
+
+	  <h1><?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG))?></h1>
+
+	  <div class="img_big">
+	  	<?php tpl_img() ?>
+	  </div>
+
+		<div class="img_detail">
+		  <p class="img_caption">
+			  <?php print nl2br(hsc(tpl_img_getTag(array('IPTC.Caption',
+                                               'EXIF.UserComment',
+                                               'EXIF.TIFFImageDescription',
+                                               'EXIF.TIFFUserComment')))); ?>
+	  	</p>
+
+      <p>&larr; <?php echo $lang['img_backto']?> <?php tpl_pagelink($ID)?></p>
+
+      <dl class="img_tags">
+        <?php
+					$t = tpl_img_getTag('Date.EarliestTime');
+          if($t) print '<dt>'.$lang['img_date'].':</dt><dd>'.date($conf['dformat'],$t).'</dd>';
+
+          $t = tpl_img_getTag('File.Name');
+          if($t) print '<dt>'.$lang['img_fname'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag(array('Iptc.Byline','Exif.TIFFArtist','Exif.Artist','Iptc.Credit'));
+          if($t) print '<dt>'.$lang['img_artist'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag(array('Iptc.CopyrightNotice','Exif.TIFFCopyright','Exif.Copyright'));
+          if($t) print '<dt>'.$lang['img_copyr'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag('File.Format');
+          if($t) print '<dt>'.$lang['img_format'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag('File.NiceSize');
+          if($t) print '<dt>'.$lang['img_fsize'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag('Simple.Camera');
+          if($t) print '<dt>'.$lang['img_camera'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag(array('IPTC.Keywords','IPTC.Category'));
+          if($t) print '<dt>'.$lang['img_keywords'].':</dt><dd>'.hsc($t).'</dd>';
+
+        ?>
+      </dl>
+      <?php //Comment in for Debug// dbg(tpl_img_getTag('Simple.Raw'));?>
+		</div>
+
+  <?php } ?>
+  </div>
+</div>
+</body>
+</html>
+
