6cf3d6379da5dfc059a22d417554adee2610df7f
Andreas Gohr <andi@splitbrain.org>
2005-07-26 21:14:12 +0200

    es language update
    darcs-hash:20050726191412-7ad00-ec9939a2f4667a36e3eb943d9cd3196be6708a3a.gz

--- inc/lang/es/lang.php
+++ inc/lang/es/lang.php
@@ -5,6 +5,8 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Zigor Astarbe <zigor@astarbe.com>
  * @author     Adrián Ariza <adrian_ariza.ciudad.com.ar>
+ * @author     Gabiel Molina <gabriel191@gmail.com>
+ * @author     Paco Avila <monkiki@gmail.com>
  */
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
@@ -28,18 +30,33 @@ $lang['btn_logout'] = 'Salir';
 $lang['btn_admin']  = 'Administrar';
 $lang['btn_update'] = 'Actualizar';
 $lang['btn_delete'] = 'Borrar';
+$lang['btn_newer']  = '<< más reciente';
+$lang['btn_older']  = 'menos reciente >>';
+$lang['btn_backtomedia'] = 'Vuelta a la selección de medios';
 
 $lang['loggedinas'] = 'Conectado como ';
 $lang['user']       = 'Usuario';
 $lang['pass']       = 'Contrasña';
+$lang['passchk']    = 'otra vez';
 $lang['remember']   = 'Recordarme';
 $lang['fullname']   = 'Nombre completo';
 $lang['email']      = 'E-Mail';
 $lang['register']   = 'Registrarse';
 $lang['badlogin']   = 'El usuario y la contraseña ingresados son incorrectos.';
 
+$lang['regmissing'] = 'Lo siento, tienes que completar todos los campos.';
+$lang['reguexists'] = 'Lo siento, el nombre de usuario ya existe.';
+$lang['regsuccess'] = 'El usuario ha sido creado. La contraseña se ha enviado por correo.';
+$lang['regsuccess2']= 'El usuario ha sido creado.';
+$lang['regmailfail']= 'Parece que ha habido un error al enviar el correo con el password. ¡Por favor, ponte en contacto con el administrador del Wiki!';
+$lang['regbadmail'] = 'La dirección de correo no parece válida. Si piensas que esto es un error, ponte en contacto con el administrador del Wiki';
+$lang['regbadpass'] = 'Las dos contraseñas no son iguales, por favor prueba de nuevo.';
+$lang['regpwmail']  = 'Tu contraseña del DokuWiki';
+$lang['reghere']    = '¿No tienes una cuenta todavía?';
+
 $lang['txt_upload'] = 'Selecciona el fichero a cargar';
 $lang['txt_filename'] = 'Escribe el wikiname (opcional)';
+$lang['txt_overwrt']  = 'Sobreescribe el fichero existente';
 
 $lang['mediaselect'] = 'Selección Mediafile';
 $lang['fileupload']  = 'Subida de Mediafile';
@@ -57,6 +74,9 @@ $lang['lastmod']    = 'Última modificación';
 $lang['by']         = 'por';
 $lang['deleted']    = 'borrado';
 $lang['created']    = 'creado';
+$lang['lockedby']     = 'Actualmente bloqueado por';
+$lang['lockexpire']   = 'El bloqueo expira en';
+$lang['willexpire']   = 'Tu bloqueo para editar esta página expira en un minuto.\nPara evitar confictos usa el botón previsualizar para reinicial el contador de tiempo.';
 
 $lang['mail_newpage'] = 'página añadida:';
 $lang['mail_changed'] = 'página cambiada:';
@@ -76,5 +96,28 @@ $lang['qb_hr']      = 'Línea horizontal';
 $lang['qb_ol']      = 'Item de Lista Ordenada';
 $lang['qb_ul']      = 'Item de Lista Desordenada';
 $lang['qb_media']   = 'Añadir Imágenes otros ficheros';
+
+$lang['admin_acl']  = 'Mantenimiento del contol de accesos...';
+$lang['admin_register']= 'Añadir nuevo usuario...';
+
+$lang['acl_group']  = 'Grupo';
+$lang['acl_user']   = 'Usuario';
+$lang['acl_perms']  = 'Permisos para';
+$lang['page']       = 'Pagína';
+$lang['namespace']  = 'Namespace';
+ 
+$lang['acl_perm1']  = 'Lectura';
+$lang['acl_perm2']  = 'Edición';
+$lang['acl_perm4']  = 'Crear';
+$lang['acl_perm8']  = 'Subir';
+$lang['acl_perm16'] = 'Eliminar';
+$lang['acl_new']    = 'Añadir nueva entrada';
+ 
+$lang['spell_start'] = 'Comprobar Ortografía';
+$lang['spell_stop']  = 'Continuar Edición';
+$lang['spell_wait']  = 'Por favor espera...';
+$lang['spell_noerr'] = 'No se encontraton errores';
+$lang['spell_nosug'] = 'No hay sugerencias';
+$lang['spell_change']= 'Cambio';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
