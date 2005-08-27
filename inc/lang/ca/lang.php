f137f476d5aaf309d4cf14c3ca125140d1ec5127
Carles Bellver <carles.bellver@gmail.com>
2005-08-27 13:39:20 +0200

    catalan language files
    darcs-hash:20050827113920-cc4ee-a186f2ffe14c835e45506f626efbf745fccc080e.gz

---
+++ inc/lang/ca/lang.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * catalan language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Carles Bellver <carles.bellver@cent.uji.es>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = 'Edita aquesta pàgina';
+$lang['btn_source'] = 'Mostra codi font';
+$lang['btn_show']   = 'Mostra pàgina';
+$lang['btn_create'] = 'Crea aquesta pàgina';
+$lang['btn_search'] = 'Cerca';
+$lang['btn_save']   = 'Desa';
+$lang['btn_preview']= 'Previsualitza';
+$lang['btn_top']    = 'Torna dalt';
+$lang['btn_newer']  = '<< més recent';
+$lang['btn_older']  = 'menys recent >>';
+$lang['btn_revs']   = 'Revisions anteriors';
+$lang['btn_recent'] = 'Canvis recents';
+$lang['btn_upload'] = 'Penja';
+$lang['btn_cancel'] = 'Cancel·la';
+$lang['btn_index']  = 'Índex';
+$lang['btn_secedit']= 'Edita';
+$lang['btn_login']  = 'Entra';
+$lang['btn_logout'] = 'Surt';
+$lang['btn_admin']  = 'Admin';
+$lang['btn_update'] = 'Actualitza';
+$lang['btn_delete'] = 'Suprimeix';
+$lang['btn_back']   = 'Enrere';
+$lang['btn_backtomedia'] = 'Torna a la selecció de fitxers';
+
+$lang['loggedinas'] = 'Heu entrat com';
+$lang['user']       = 'Nom d\'usuari';
+$lang['pass']       = 'Contrassenya';
+$lang['passchk']    = 'una altra vegada';
+$lang['remember']   = 'Recorda\'m';
+$lang['fullname']   = 'Nom complet';
+$lang['email']      = 'Correu electrònic';
+$lang['register']   = 'Registra\'m';
+$lang['badlogin']   = 'Nom d\'usuari o contrasenya incorrectes.';
+
+$lang['regmissing'] = 'Heu d\'omplir tots els camps.';
+$lang['reguexists'] = 'Ja existeix un altre usuari amb aquest nom.';
+$lang['regsuccess'] = 'S\'ha creat l\'usuari. La contrasenya s\'ha enviat per correu.';
+$lang['regsuccess2']= 'S\'ha creat l\'usuari.';
+$lang['regmailfail']= 'Sembla que un error ha impedit enviar la contrasenya per correu. Contacteu amb l\'administrador.';
+$lang['regbadmail'] = 'L\'adreça de correu que heu donat no sembla vàlida. Si creieu que això és un error, contacu amb l\'administrador.';
+$lang['regbadpass'] = 'Les dues contrasenyes no són iguals. Torneu a intentar-ho.';
+$lang['regpwmail']  = 'La vostra contrasenya per al Wiki';
+$lang['reghere']    = 'Si no teniu un compte, aquí en podeu obtenir un';
+
+$lang['txt_upload']   = 'Trieu el fitxer que voleu penjar';
+$lang['txt_filename'] = 'Introduïu el nom wiki (opcional)';
+$lang['txt_overwrt']  = 'Sobreescriu el fitxer actual';
+$lang['lockedby']     = 'Actualment blocat per:';
+$lang['lockexpire']   = 'Venciment del blocatge:';
+$lang['willexpire']   = 'El blocatge per a editar aquesta pàgina venç d\'aquí a un minut.\nUtilitzeu la visualització prèvia per reiniciar el rellotge i evitar conflictes.';
+
+$lang['notsavedyet'] = 'Heu fet canvis que es perdran si no els deseu.\nVoleu continuar?';
+$lang['rssfailed']   = 'S\'ha produït un error en recollir aquesta alimentació: ';
+$lang['nothingfound']= 'No s\'ha trobat res.';
+
+$lang['mediaselect'] = 'Selecció de fitxers';
+$lang['fileupload']  = 'Càrrega de fitxers';
+$lang['uploadsucc']  = 'S\'ha penjat el fitxer';
+$lang['uploadfail']  = 'No es pot penjar el fitxer. Potser no teniu prou permisos?';
+$lang['uploadwrong'] = 'No es pot penjar el fitxer. Aquesta extensió està prohibida.';
+$lang['uploadexist'] = 'El fitxer ja existeix. No s\'ha penjat.';
+$lang['deletesucc']  = 'S\'ha suprimit el fitxer "%s".';
+$lang['deletefail']  = 'No s\'ha pogut suprimir el fitxer "%s". Comproveu els permisos.';
+$lang['mediainuse']  = 'No s\'ha pogut suprimir el fitxer "%s". Encara s\'està utilitzant.';
+$lang['namespaces']  = 'Espais';
+$lang['mediafiles']  = 'Fitxers disponibles en';
+
+
+$lang['reference']   = 'Referències per a';
+$lang['ref_inuse']   = 'El fitxer no es pot suprimir perquè l\'estan utilitzant les pàgines següents:';
+$lang['ref_hidden']  = 'Algunes referències apareixen en pàgines per a les quals no teniu permís de lectura';
+
+$lang['hits']       = 'Resultats';
+$lang['quickhits']  = 'Noms de pàgina coincidents';
+$lang['toc']        = 'Taula de continguts';
+$lang['current']    = 'actual';
+$lang['yours']      = 'La vostra versió';
+$lang['diff']       = 'mostra diferències amb la versió actual';
+$lang['line']       = 'Línia';
+$lang['breadcrumb'] = 'Camí';
+$lang['lastmod']    = 'Darrera modificació';
+$lang['by']         = 'per';
+$lang['deleted']    = 'suprimit';
+$lang['created']    = 'creat';
+$lang['restored']   = 's\'ha restaurat una versió anterior';
+$lang['summary']    = 'Resum d\'edició';
+
+$lang['mail_newpage'] = 'pàgina afegida:';
+$lang['mail_changed'] = 'pàgina modificada:';
+
+$lang['nosmblinks'] = 'Els enllaços amb recursos compartits de Windows només funcionen amb el Microsoft Internet Explorer.\nSi voleu podeu copiar i enganxar l\'enllaç.';
+
+$lang['qb_alert']   = 'Introduïu el text que voleu formatar.\nS\'afegirà al final del document.';
+$lang['qb_bold']    = 'Negreta';
+$lang['qb_italic']  = 'Cursiva';
+$lang['qb_underl']  = 'Subratllat';
+$lang['qb_code']    = 'Codi';
+$lang['qb_strike']  = 'Text barrat';
+$lang['qb_h1']      = 'Encapçalament nivell 1';
+$lang['qb_h2']      = 'Encapçalament nivell 2';
+$lang['qb_h3']      = 'Encapçalament nivell 3';
+$lang['qb_h4']      = 'Encapçalament nivell 4';
+$lang['qb_h5']      = 'Encapçalament nivell 5';
+$lang['qb_link']    = 'Enllaç intern';
+$lang['qb_extlink'] = 'Enllaç extern';
+$lang['qb_hr']      = 'Ratlla horitzontal';
+$lang['qb_ol']      = 'Element de llista numerada';
+$lang['qb_ul']      = 'Element de llista de pics';
+$lang['qb_media']   = 'Afegeix imatges o altres fitxers';
+$lang['qb_sig']     = 'Insereix signatura';
+
+$lang['del_confirm']= 'Suprimiu aquesta entrada?';
+
+$lang['admin_acl']  = 'Gestió de la Llista de Control d\'Accés...';
+$lang['admin_register']= 'Afegeix nou usuari...';
+
+$lang['acl_group']  = 'Grup';
+$lang['acl_user']   = 'Usuari';
+$lang['acl_perms']  = 'Permisos per a';
+$lang['page']       = 'Pàgina';
+$lang['namespace']  = 'Espai';
+
+$lang['acl_perm1']  = 'Llegir';
+$lang['acl_perm2']  = 'Editar';
+$lang['acl_perm4']  = 'Crear';
+$lang['acl_perm8']  = 'Penjar fitxers';
+$lang['acl_perm16'] = 'Suprimir';
+$lang['acl_new']    = 'Afegeix nova entrada';
+
+$lang['spell_start'] = 'Revisa l\'ortografia';
+$lang['spell_stop']  = 'Torna a edició';
+$lang['spell_wait']  = 'Espereu un moment...';
+$lang['spell_noerr'] = 'No s\'han trobat errors';
+$lang['spell_nosug'] = 'No hi ha suggeriments';
+$lang['spell_change']= 'Canvia';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
