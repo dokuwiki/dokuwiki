5c24888337d1f317180a43b0d3ffc86539458230
jan <jan@jandecaluwe.com>
2005-09-20 17:15:30 +0200

    basename instead of basedir
    darcs-hash:20050920151530-45605-53d07bfd3467f40f809296d983676b6e7c1a7756.gz

--- inc/pageutils.php
+++ inc/pageutils.php
@@ -30,7 +30,7 @@ function getID($param='id'){
   if(empty($id) && $conf['userewrite'] == 2){
     //get the script URL
     if($conf['basedir']){
-      $script = $conf['basedir'].basedir($_SERVER['SCRIPT_FILENAME']);
+      $script = $conf['basedir'].basename($_SERVER['SCRIPT_FILENAME']);
     }elseif($_SERVER['DOCUMENT_ROOT'] && $_SERVER['SCRIPT_FILENAME']){
       $script = preg_replace ('/^'.preg_quote($_SERVER['DOCUMENT_ROOT'],'/').'/','',
                               $_SERVER['SCRIPT_FILENAME']);
