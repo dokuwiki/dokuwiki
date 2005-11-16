bb406e63a28e16160da6cc7a5bae60732425ef2a
Selim Farsakogflu <farsakogluselim@yahoo.de>
2005-11-16 23:10:55 +0100

    turkish translation
    darcs-hash:20051116221055-cb711-78445993d520a9157fa8e5ddf0a20ed99adf5e49.gz

---
+++ inc/lang/tr/lang.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * turkish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Selim Farsakoğlu <farsakogluselim@yahoo.de>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = 'Sayfayı değiştir';
+$lang['btn_source'] = 'Kaynağı göster';
+$lang['btn_show']   = 'Sayfayı göster';
+$lang['btn_create'] = 'Bu sayfayı oluştur';
+$lang['btn_search'] = 'Ara';
+$lang['btn_save']   = 'Kaydet';
+$lang['btn_preview']= 'Önizleme';
+$lang['btn_top']    = 'Başa dön';
+$lang['btn_newer']  = '<< daha yeniler';
+$lang['btn_older']  = 'daha eskiler >>';
+$lang['btn_revs']   = 'Eski sürümler';
+$lang['btn_recent'] = 'En son değişiklikler';
+$lang['btn_upload'] = 'Yükle';
+$lang['btn_cancel'] = 'İptal';
+$lang['btn_index']  = 'İndeks';
+$lang['btn_secedit']= 'Değiştir';
+$lang['btn_login']  = 'Giriş yap';
+$lang['btn_logout'] = 'Çıkış yap';
+$lang['btn_admin']  = 'Yönetici';
+$lang['btn_update'] = 'Güncelle';
+$lang['btn_delete'] = 'Sil';
+$lang['btn_back']   = 'Geri';
+$lang['btn_backlink']    = "Geri linkler";
+$lang['btn_backtomedia'] = 'Çokluortam dosyası seçimine dön';
+$lang['btn_subscribe']   = 'Değişikliklere kaydol';
+$lang['btn_unsubscribe'] = 'Değişikliklerden çık';
+
+$lang['loggedinas'] = 'Giriş ismi';
+$lang['user']       = 'Kullanıcı ismi';
+$lang['pass']       = 'Şifre';
+$lang['passchk']    = 'tekrarla';
+$lang['remember']   = 'Beni hatırla';
+$lang['fullname']   = 'Tam isim';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Kayıt ol';
+$lang['badlogin']   = 'Maalesef kullanıcı adı veya şifre yanlış oldu.';
+
+$lang['regmissing'] = 'Lütfen tüm alanları doldur.';
+$lang['reguexists'] = 'Maalesef bu isimle bir kullanıcı zaten mevcut.';
+$lang['regsuccess'] = 'Kullanıcı oluşturuldu ve şifre adresine maillendi.';
+$lang['regsuccess2']= 'Kullanıcı oluşturuldu.';
+$lang['regmailfail']= 'Şifre mailini yollarken bir hata oluştu galiba. Lütfen yönetici ile kontağa geç!';
+$lang['regbadmail'] = 'Verilen e-mail adresi geçersiz sanırım - bunun sence bir hataysa adminle kontağa geç.';
+$lang['regbadpass'] = 'Şifreler birbirini tumuyor. Lütfen tekrar dene.';
+$lang['regpwmail']  = 'DokuWiki şifren';
+$lang['reghere']    = 'Daha hesabın yok mu? Hemen bir tane açtır!';
+
+$lang['txt_upload']   = 'Yüklenecek dosyayı seç';
+$lang['txt_filename'] = 'Wiki ismini gir (mecburi değil)';
+$lang['txt_overwrt']  = 'Mevcut dosyanın üstüne yaz';
+$lang['lockedby']     = 'Şu an şunun tarafından kilitli:';
+$lang['lockexpire']   = 'Kilitin açılma tarihi';
+$lang['willexpire']   = 'Bu sayfayı değiştirme kilidinin bir dakikaya kadar süresi geçiyor.\nÇatışmaları önlemek için önizleme tuşunu kullanarak kilit sayacını sıfırla.';
+
+$lang['notsavedyet'] = 'Kaybedilmemiş değişiklikler kaybolacak.\nDevam edeyim mi?';
+
+$lang['rssfailed']   = 'Bu beslemeyi çekerken hata oluştu: ';
+$lang['nothingfound']= 'Hiçbir şey yok.';
+
+$lang['mediaselect'] = 'Çokluortam dosyası seçimi';
+$lang['fileupload']  = 'Çokluortam dosyası yükleme';
+$lang['uploadsucc']  = 'Yükleme tamam';
+$lang['uploadfail']  = 'Yükleme başarısız. Yetki hatssı olabilir mi?';
+$lang['uploadwrong'] = 'Yükleme engellendi. Bu dosya uzantısı yasak!';
+$lang['uploadexist'] = 'Dosya zaten var. Hiçbir şey yapılmadı.';
+$lang['deletesucc']  = '"%s" dosyası silindi.';
+$lang['deletefail']  = '"%s" silinemedi - yetkileri kontrol et.';
+$lang['mediainuse']  = '"%s" dosyası silinmedi, hala kullanımda.';
+$lang['namespaces']  = 'İsimalanları';
+$lang['mediafiles']  = 'Şurda kullanıma hazır dosyalar:';
+
+$lang['reference']   = 'Şunun için referanslar:';
+$lang['ref_inuse']   = 'Dosya silinemiyor, çünkü şu sayfalar tarafından hala kullanımda:';
+$lang['ref_hidden']  = 'Bazı referanslar okuma yetkin olmayan sayfalarda';
+
+$lang['hits']       = 'Bulunanlar';
+$lang['quickhits']  = 'Uyan sayfalar';
+$lang['toc']        = 'Fihrist';
+$lang['current']    = 'mevcut';
+$lang['yours']      = 'Senin versiyonun';
+$lang['diff']       = 'bu versiyonla farkları göster';
+$lang['line']       = 'Satır';
+$lang['breadcrumb'] = 'İz';
+$lang['lastmod']    = 'Son değiştirilme';
+$lang['by']         = 'Değiştiren:';
+$lang['deleted']    = 'silinme';
+$lang['created']    = 'oluşturma';
+$lang['restored']   = 'eski sürüme çevrilme';
+$lang['summary']    = 'Değiştirme özeti';
+
+$lang['mail_newpage'] = 'sayfa eklenme:';
+$lang['mail_changed'] = 'sayfa değiştirilme:';
+
+$lang['nosmblinks'] = 'Windows paylaşımlarına link verme sadece IE\'de çalışır.\nAma linki her zaman kopyala-yapıştur yapabilirsin.';
+
+$lang['qb_alert']   = 'Lütfen biçimlendirmek istediğin yazıyı gir.\nSayfanın sonuna eklenecektir.';
+$lang['qb_bold']    = 'Kalın Yazı';
+$lang['qb_italic']  = 'Eğik Yazı';
+$lang['qb_underl']  = 'Altı çizili Yazı';
+$lang['qb_code']    = 'Kod haline getir';
+$lang['qb_strike']  = 'Ortası çizili Yazı';
+$lang['qb_h1']      = '1. Seviye Başlık';
+$lang['qb_h2']      = 'Seviye Başlık';
+$lang['qb_h3']      = '2. Seviye Başlık';
+$lang['qb_h4']      = '3. Seviye Başlık';
+$lang['qb_h5']      = '4. Seviye Başlık';
+$lang['qb_link']    = 'İç Bağlantı';
+$lang['qb_extlink'] = 'Dış Bağlantı';
+$lang['qb_hr']      = 'Yatay Çizgi';
+$lang['qb_ol']      = 'Sıralı liste';
+$lang['qb_ul']      = 'Sırasız liste';
+$lang['qb_media']   = 'Resim ve başka dosyalar ekle';
+$lang['qb_sig']     = 'İmza Ekle';
+
+$lang['del_confirm']= 'Bu girişi sil?';
+
+$lang['admin_acl']  = 'Erişim kontrol listesi yönetimi';
+$lang['admin_register']= 'Yeni kullanıcı ekle...';
+
+$lang['acl_group']  = 'Grup';
+$lang['acl_user']   = 'Kullanıcı';
+$lang['acl_perms']  = 'Şunun için yetkiler:';
+$lang['page']       = 'Sayfa';
+$lang['namespace']  = 'İsim aralığı';
+
+$lang['acl_perm1']  = 'Okuma';
+$lang['acl_perm2']  = 'Değiştirme';
+$lang['acl_perm4']  = 'Oluşturma';
+$lang['acl_perm8']  = 'Yükleme';
+$lang['acl_perm16'] = 'Silme';
+$lang['acl_new']    = 'Yeni giriş ekle';
+
+$lang['spell_start'] = 'Hecelemeyi kontrol et';
+$lang['spell_stop']  = 'Değiştirmeye Devam et';
+$lang['spell_wait']  = 'Lütfen bekleyiniz...';
+$lang['spell_noerr'] = 'Hata bulunamadı';
+$lang['spell_nosug'] = 'Öneri yok';
+$lang['spell_change']= 'Değiştir';
+
+$lang['metaedit']    = 'Metaverileri Değiştir';
+$lang['metasaveerr'] = 'Metaveri yazma başarısız ';
+$lang['metasaveok']  = 'Metaveri kaydedildi';
+$lang['img_backto']  = 'Şuna dön:';
+$lang['img_title']   = 'başlık';
+$lang['img_caption'] = 'Serlevha';
+$lang['img_date']    = 'Tarih';
+$lang['img_fname']   = 'Dosya Adı';
+$lang['img_fsize']   = 'Boyut';
+$lang['img_artist']  = 'Fotoğrafçı';
+$lang['img_copyr']   = 'Telif Hakkı';
+$lang['img_format']  = 'Biçim';
+$lang['img_camera']  = 'Fotoğraf Makinası';
+$lang['img_keywords']= 'Anahtar Sözcükler';
+
+$lang['subscribe_success']  = '%s, %s\'in abonelik listesine eklendi';
+$lang['subscribe_error']    = '%s; %s\'in abonelik listesine eklenirken hata oluştu';
+$lang['subscribe_noaddress']= 'Giriş bilgilerinizle ilişkili bir adres yok, abonelik listesine eklenemezsiniz';
+$lang['unsubscribe_success']= '%s, %s\'in abonelik listesinden çıkartıldı';
+$lang['unsubscribe_error']  = '%s, %s\'in listesinden çıkartılırken hata oluştu';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
