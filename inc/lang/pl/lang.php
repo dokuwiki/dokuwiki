5fb14d63b6a7de75860bd8a94caa8d2f2fe8ee5e
Grzegorz Zur <grzegorz.zur@gmail.com>
2005-12-04 15:25:22 +0100

    polish language update
    darcs-hash:20051204142522-e561a-6b557c039c980da9645443834bb23623a8176933.gz

--- inc/lang/pl/lang.php
+++ inc/lang/pl/lang.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * polish language file
- * based on english one
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Grzegorz Żur <grzesiekzur@tlen.pl>
+ * @author     Grzegorz Żur <grzegorz.zur@gmail.com>
  */
+
+
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
 
@@ -35,16 +36,23 @@ $lang['btn_backlink']    = 'Odnośniki';
 $lang['btn_backtomedia'] = 'Powrót do wyboru pliku';
 $lang['btn_subscribe']   = 'Subskrybuj zmiany';
 $lang['btn_unsubscribe'] = 'Zaprzestań subskrypcji zmian';
+$lang['btn_profile']    = 'Aktualizuj profil';
+$lang['btn_reset']     = 'Resetuj';
+$lang['btn_resendpwd'] = 'Prześlij nowe hasło';
 
-$lang['loggedinas'] = 'Zalogowany/a jako';
+$lang['loggedinas'] = 'Zalogowany jako';
 $lang['user']       = 'Użytkownik';
 $lang['pass']       = 'Hasło';
+$lang['newpass']    = 'Nowe hasło';
+$lang['oldpass']    = 'Potwierdź aktualne hasło';
 $lang['passchk']    = 'Powtórz hasło';
 $lang['remember']   = 'Zapamiętaj';
 $lang['fullname']   = 'Imię i nazwisko';
 $lang['email']      = 'E-mail';
 $lang['register']   = 'Zarejestruj się!';
+$lang['profile']    = 'Profil użytkownika';
 $lang['badlogin']   = 'Nazwa użytkownika lub hasło są nieprawidłowe.';
+$lang['minoredit']  = 'Mniejsze zmiany';
 
 $lang['regmissing'] = 'Wypełnij wszystkie pola.';
 $lang['reguexists'] = 'Użytkownik o tej nazwie już istnieje.';
@@ -55,6 +63,18 @@ $lang['regbadmail'] = 'Adres e-mail jest nieprawidłowy!';
 $lang['regbadpass'] = 'Hasła nie są identyczne, spróbuj ponownie.';
 $lang['regpwmail']  = 'Twoje hasło do DokuWiki';
 $lang['reghere']    = 'Nie masz jeszcze konta? Zdobądź je';
+
+$lang['profna']       = 'To wiki nie pozwala na zmianę profilu.';
+$lang['profnochange'] = 'Żadnych zmian, nic do zrobienia.';
+$lang['profnoempty']  = 'Pusta nazwa lub adres e-mail nie dozwolone.';
+$lang['profchanged']  = 'Zaktualizowano profil użytkownika.';
+
+$lang['pwdforget'] = 'Nie pamiętasz hasła? Zdobądź nowe!';
+$lang['resendna']  = 'To wiki nie pozwala na powtórne przesyłanie hasła.';
+$lang['resendpwd'] = 'Prześlij nowe hasło dla';
+$lang['resendpwdmissing'] = 'Wypełnij wszystkie pola.';
+$lang['resendpwdnouser']  = 'Nie można znaleźć tego użytkownika w bazie danych.';
+$lang['resendpwdsuccess'] = 'Hasło zostało przesłane pocztą.';
 
 $lang['txt_upload']   = 'Wybierz plik do wysłania';
 $lang['txt_filename'] = 'Nazwa pliku (opcjonalnie)';
@@ -121,6 +141,8 @@ $lang['qb_ol']      = 'Numeracja';
 $lang['qb_ul']      = 'Wypunktowanie';
 $lang['qb_media']   = 'Dodaj obrazek lub inny plik';
 $lang['qb_sig']     = 'Wstaw podpis';
+$lang['qb_smileys'] = 'Emotikony';
+$lang['qb_chars']   = 'Znaki specjalne';
 
 $lang['del_confirm']= 'Czy napewno usunąć?';
 
@@ -167,5 +189,15 @@ $lang['subscribe_error']    = 'Błąd przy dodawaniu %s do listy subskrypcji str
 $lang['subscribe_noaddress']= 'Nie podałeś adresu e-mail przy rejestracji, nie możesz zostać dodany do listy subskrypcji.';
 $lang['unsubscribe_success']= 'Usunięto %s z listy subskrypcji strony %s';
 $lang['unsubscribe_error']  = 'Błąd przu usuwaniu %s z listy subskrypcji strony %s';
+
+/* auth.class lanuage support */
+$lang['authmodfailed']   = 'Błąd uwierzytelnienia. Powiadom administratora tego wiki.';
+
+/* mysql.class language support */
+$lang['noMySQL']         = "Brak obsługi MySQL w PHP. Powiadom administratora tego wiki.";
+$lang['noDatabase']      = "MySQL: Brak dostępu do bazy '%d'. Powiadom administratora tego wiki.";
+$lang['noConnect']       = "MySQL: Brak połączenia z '%u@%s'. Powiadom administratora tego wiki.";
+$lang['joinGroupFailed'] = "Nie utworzono konta '%u' ponieważ nie można dodać go do grupy '%g'.";
+$lang['modUserFailed']   = "Nie można zmienić danych użytkownika. Powiadom administratora tego wiki.";
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
