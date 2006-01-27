1bb49acb063c0b6842615a142bc66c85ee03a933
gb <gb@isis.u-strasbg.fr>
2006-01-27 18:18:34 +0100

    Synced french lang.php
    darcs-hash:20060127171834-2060e-75d34744fd23d33208cf977f86fdf928fa8d6db5.gz

--- inc/lang/fr/lang.php
+++ inc/lang/fr/lang.php
@@ -19,7 +19,7 @@ $lang['btn_source'] = 'Montrer le source de la page';
 $lang['btn_show']   = 'Montrer la page';
 $lang['btn_create'] = 'Créer cette page';
 $lang['btn_search'] = 'Rechercher';
-$lang['btn_save']   = 'Sauver';
+$lang['btn_save']   = 'Enregistrer';
 $lang['btn_preview']= 'Aperçu';
 $lang['btn_top']    = 'Haut de page';
 $lang['btn_newer']  = '<< plus récent';
@@ -36,7 +36,7 @@ $lang['btn_admin']  = 'Admin';
 $lang['btn_update'] = 'Mise à jour';
 $lang['btn_delete'] = 'Effacer';
 $lang['btn_back']   = 'Retour';
-$lang['btn_backlink']    = "Liens vers cette page";
+$lang['btn_backlink']    = 'Liens vers cette page';
 $lang['btn_backtomedia'] = 'Retour à la sélection du ficher média';
 $lang['btn_subscribe']   = 'Notifier les modifications';
 $lang['btn_unsubscribe'] = 'Ne pas notifier les modifications';
@@ -47,12 +47,16 @@ $lang['btn_resendpwd'] = 'Envoyer le mot de passe';
 $lang['loggedinas'] = 'Connecté en tant que';
 $lang['user']       = 'Utilisateur';
 $lang['pass']       = 'Mot de passe';
-$lang['passchk']    = 'encore une fois';
+$lang['newpass']    = 'Nouveau mot de passe';
+$lang['oldpass']    = 'Mot de passe actuel';
+$lang['passchk']    = 'Répéter nouveau mot de passe';
 $lang['remember']   = 'Mémoriser';
 $lang['fullname']   = 'Nom complet';
 $lang['email']      = 'Adresse de courriel';
 $lang['register']   = 'S\'enregistrer';
+$lang['profile']    = 'Profil utilisateur';
 $lang['badlogin']   = 'L\'utilisateur ou le mot de passe est incorrect.';
+$lang['minoredit']  = 'Modifications mineures';
 
 $lang['regmissing'] = 'Désolé, vous devez remplir tous les champs.';
 $lang['reguexists'] = 'Désolé, ce nom d\'utilisateur appartient déjà à un autre utilisateur.';
@@ -63,6 +67,18 @@ $lang['regbadmail']  = 'L\'adresse mail semble incorrecte - Si vous pensez que c
 $lang['regbadpass'] = 'Les deux mots de passe donnés sont différents, recommencez SVP';
 $lang['regpwmail']   = 'Votre mot de passe DokuWiki';
 $lang['reghere']     = 'Vous n\'avez pas encore de compte ? Enregistrez-vous ici';
+
+$lang['profna']       = 'Ce wiki ne permet pas de modifier les profils';
+$lang['profnochange'] = 'Pas de changement, rien à faire.';
+$lang['profnoempty']  = 'Un nom ou une adresse de courriel vide n\'est pas permis.';
+$lang['profchanged']  = 'Mise à jour du profil réussie.';
+
+$lang['pwdforget'] = 'Mot de passe oublié ? Faites-vous envoyer votre mot de passe';
+$lang['resendna']  = 'Ce wiki ne permet pas le renvoi de mot de passe.';
+$lang['resendpwd'] = 'Renvoyer le mot de passe de';
+$lang['resendpwdmissing'] = 'Désolé, vous devez remplir tous les champs.';
+$lang['resendpwdnouser']  = 'Désolé, cet utilisateur est introuvable dans notre base.';
+$lang['resendpwdsuccess'] = 'Votre nouveau mot de passe vous a été expédié par courriel.';
 
 $lang['txt_upload'] = 'Sélectionnez un fichier à envoyer';
 $lang['txt_filename'] = 'Donnez un "wikiname" (optionnel)';
@@ -109,7 +125,7 @@ $lang['summary']    = 'Résumé des modifications';
 $lang['mail_newpage'] = 'page ajoutée :';
 $lang['mail_changed'] = 'page changée :';
 
-$lang['nosmblinks'] = 'Les liens vers les disques partagés de Windows ne fonctionnent qu\'avec Microsoft Internet Explorer.\nVous pouvez toujours faire un copier/coller du lien.';
+$lang['nosmblinks'] = 'Les liens vers les partages Windows ne fonctionnent qu\'avec Microsoft Internet Explorer.\nVous pouvez toujours faire un copier/coller du lien.';
 
 $lang['qb_alert']   = 'SVP, entrez le texte que vous souhaitez formater.\nIl sera ajouté à la fin du document.';
 $lang['qb_bold']    = 'Gras';
@@ -129,13 +145,15 @@ $lang['qb_ol']      = 'Liste numérotée';
 $lang['qb_ul']      = 'Liste libre';
 $lang['qb_media']   = 'Ajouter des images et autres fichiers';
 $lang['qb_sig']     = 'Insérer une signature';
+$lang['qb_smileys'] = 'Émoticons';
+$lang['qb_chars']   = 'Caractères spéciaux';
 
-$lang['del_confirm']= 'Effacer cette entrée?';
+$lang['del_confirm']= 'Effacer cette entrée ?';
 $lang['admin_register']= 'Ajouter un nouvel utilisateur';
 
 $lang['spell_start'] = 'Vérifier l\'orthographe';
 $lang['spell_stop']  = 'Reprendre l\'édition';
-$lang['spell_wait']  = 'SVP patienter';
+$lang['spell_wait']  = 'Patientez SVP...';
 $lang['spell_noerr'] = 'Aucune erreur trouvée';
 $lang['spell_nosug'] = 'Pas de suggestion';
 $lang['spell_change']= 'Changer';
@@ -154,5 +172,15 @@ $lang['img_copyr']   = 'Copyright';
 $lang['img_format']  = 'Format';
 $lang['img_camera']  = 'Appareil photo';
 $lang['img_keywords']= 'Mots clef';
+
+$lang['subscribe_success']  = 'Ajout de %s à la liste d\'abonnés de %s';
+$lang['subscribe_error']    = 'Erreur à l\'ajout de %s à la liste d\'abonnés de %s';
+$lang['subscribe_noaddress']= 'Aucune adresse associée à votre nom d\'utilisateur, impossible de vous ajouter à la liste d\'abonnés';
+$lang['unsubscribe_success']= 'Suppression de %s de la liste d\'abonnés de %s';
+$lang['unsubscribe_error']  = 'Erreur à la suppression de %s de la liste d\'abonnés de %s';
+
+/* auth.class language support */
+$lang['authmodfailed']   = 'Mauvais paramétrage de l\'authentification. Merci d\'informer l\'administrateur du Wiki.';
+$lang['authtempfail']    = 'L\'authentification est temporairement indisponible. Si cela perdure, merci d\'informer l\'administrateur du Wiki.';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
