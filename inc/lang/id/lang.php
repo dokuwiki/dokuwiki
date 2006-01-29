71592caec748177f115e3d73c2d375c8f90915b5
mubaidillah <mubaidillah@gmail.com>
2006-01-29 11:21:09 +0100

    indonesian translation
    darcs-hash:20060129102109-e8571-5fbd8694737fbfcf2c10056d7aed05193bb15e39.gz

---
+++ inc/lang/id/lang.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Indonesian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     mubaidillah <mubaidillah@gmail.com>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = 'Ubah halaman ini';
+$lang['btn_source'] = 'Lihat sumber halaman';
+$lang['btn_show']   = 'Tampilkan halaman';
+$lang['btn_create'] = 'Buat halaman baru';
+$lang['btn_search'] = 'Cari';
+$lang['btn_save']   = 'Simpan';
+$lang['btn_preview']= 'Preview';
+$lang['btn_top']    = 'kembali ke atas';
+$lang['btn_newer']  = '<< lebih lanjut';
+$lang['btn_older']  = 'sebelumnya >>';
+$lang['btn_revs']   = 'Revisi-revisi lama';
+$lang['btn_recent'] = 'Perubahan terbaru';
+$lang['btn_upload'] = 'Upload';
+$lang['btn_cancel'] = 'Cancel';
+$lang['btn_index']  = 'Index';
+$lang['btn_secedit']= 'Edit';
+$lang['btn_login']  = 'Login';
+$lang['btn_logout'] = 'Logout';
+$lang['btn_admin']  = 'Admin';
+$lang['btn_update'] = 'Update';
+$lang['btn_delete'] = 'Delete';
+$lang['btn_back']   = 'Kembali';
+$lang['btn_backlink']    = "Backlinks";
+$lang['btn_backtomedia'] = 'Back to Mediafile Selection';
+$lang['btn_subscribe']   = 'Subscribe Changes';
+$lang['btn_unsubscribe'] = 'Unsubscribe Changes';
+
+$lang['loggedinas'] = 'Login sebagai ';
+$lang['user']       = 'Username';
+$lang['pass']       = 'Password';
+$lang['passchk']    = 'sekali lagi';
+$lang['remember']   = 'ingat saya';
+$lang['fullname']   = 'Nama lengkap';
+$lang['email']      = 'E-Mail';
+$lang['register']   = 'Daftar';
+$lang['badlogin']   = 'Maaf, username atau password salah';
+
+$lang['regmissing'] = 'Maaf, Anda harus mengisi semua field.';
+$lang['reguexists'] = 'Maaf, user dengan user login ini telah ada.';
+$lang['regsuccess'] = 'User telah didaftarkan dan password telah dikirim ke email Anda.';
+$lang['regsuccess2']= 'User telah dibuatkan.';
+$lang['regmailfail']= 'Kami menemukan kesalahan saat mengirimkan password ke alamat email Anda. Please contact the admin!';
+$lang['regbadmail'] = 'Alamat email yang Anda masukkan tidak valid - jika menurut Anda hal ini adalah kesalahan sistem, hubungi admin';
+$lang['regbadpass'] = 'Passwod yang dimasukkan tidak sama. Silahkan ulangi lagi.';
+$lang['regpwmail']  = 'Password DokuWiki Anda';
+$lang['reghere']    = 'Anda belum mempunyai account? silahkan ';
+
+$lang['txt_upload']   = 'File yang akan diupload';
+$lang['txt_filename'] = 'Masukkan nama wiki(optional)';
+$lang['txt_overwrt']  = 'file yang telah ada akan ditindih';
+$lang['lockedby']     = 'Sedang dikunci oleh';
+$lang['lockexpire']   = 'Penguncian artikel sampai dengan';
+$lang['willexpire']   = 'Halaman yang sedang Anda kunci akan berakhir dalam waktu kurang lebih satu menit.\nUntuk menghindari konflik, gunakan tombol Preview untuk me-reset timer pengunci.';
+
+$lang['notsavedyet'] = 'Perubahan yang belum disimpan akan hilang.\nYakin akan dilanjutkan?';
+
+$lang['rssfailed']   = 'Error terjadi saat mengambil feed: ';
+$lang['nothingfound']= 'Tidak menemukan samasekali.';
+
+$lang['mediaselect'] = 'Mediafile Selection';
+$lang['fileupload']  = 'Mediafile Upload';
+$lang['uploadsucc']  = 'Upload sukses';
+$lang['uploadfail']  = 'Upload gagal. Apakah hak ijinnya salah?';
+$lang['uploadwrong'] = 'Upload ditolak. Ekstensi file ini tidak diperbolehkan!';
+$lang['uploadexist'] = 'File telah ada. Tidak mengerjakan apa-apa.';
+$lang['deletesucc']  = 'File "%s" telah dihapus.';
+$lang['deletefail']  = '"%s" tidak dapat dihapus - cek hak aksesnya.';
+$lang['mediainuse']  = 'File "%s" belum dihapus - file ini sedang digunakan.';
+$lang['namespaces']  = 'Namespaces';
+$lang['mediafiles']  = 'File tersedia didalam';
+
+$lang['reference']   = 'Referensi untuk';
+$lang['ref_inuse']   = 'File tidak dapat dihapus karena sedang digunakan oleh halaman:';
+$lang['ref_hidden']  = 'Beberapa referensi ada didalam halaman yang tidak diijinkan untuk Anda baca.';
+
+$lang['hits']       = 'Hits';
+$lang['quickhits']  = 'Matching pagenames';
+$lang['toc']        = 'Daftar isi';
+$lang['current']    = 'sekarang';
+$lang['yours']      = 'Versi Anda';
+$lang['diff']       = 'tampilkan perbedaan dengan versi sekarang';
+$lang['line']       = 'Baris';
+$lang['breadcrumb'] = 'Trace';
+$lang['lastmod']    = 'terakhir diubah';
+$lang['by']         = 'oleh';
+$lang['deleted']    = 'terhapus';
+$lang['created']    = 'dibuat';
+$lang['restored']   = 'revisi lama ditampilkan kembali';
+$lang['summary']    = 'Edit summary';
+
+$lang['mail_newpage'] = 'halaman ditambahkan:';
+$lang['mail_changed'] = 'halaman diubah:';
+
+$lang['nosmblinks'] = 'Link ke share Windows hanya bekerja di Microsoft Internet Explorer.\nAnda masih dapat mengcopy and paste linknya.';
+
+$lang['qb_alert']   = 'Masukkaan text yang akan diformat.\n Text ini akan ditambahkan di akhir dokumen.';
+$lang['qb_bold']    = 'Tebal';
+$lang['qb_italic']  = 'Miring';
+$lang['qb_underl']  = 'Garis Bawah';
+$lang['qb_code']    = 'Kode';
+$lang['qb_strike']  = 'Text Tercoret';
+$lang['qb_h1']      = 'Level 1 Headline';
+$lang['qb_h2']      = 'Level 2 Headline';
+$lang['qb_h3']      = 'Level 3 Headline';
+$lang['qb_h4']      = 'Level 4 Headline';
+$lang['qb_h5']      = 'Level 5 Headline';
+$lang['qb_link']    = 'Link Internal';
+$lang['qb_extlink'] = 'Link External';
+$lang['qb_hr']      = 'Garis Horisontal';
+$lang['qb_ol']      = 'Item Berurutan';
+$lang['qb_ul']      = 'Item tidak berurutan';
+$lang['qb_media']   = 'Tambahkan gambar atau file lain';
+$lang['qb_sig']     = 'Sisipkan tanda tangan';
+
+$lang['del_confirm']= 'Hapus tulisan ini?';
+$lang['admin_register']= 'Tambah user baru';
+
+$lang['spell_start'] = 'Cek Spelling';
+$lang['spell_stop']  = 'Lanjutkan editing';
+$lang['spell_wait']  = 'Harap tunggu...';
+$lang['spell_noerr'] = 'Tidak ada kesalahan ditemukan';
+$lang['spell_nosug'] = 'Tidak ada saran';
+$lang['spell_change']= 'Ubah';
+
+$lang['metaedit']    = 'Edit Metadata';
+$lang['metasaveerr'] = 'Gagal menulis metadata';
+$lang['metasaveok']  = 'Metadata tersimpan';
+$lang['img_backto']  = 'Kembali ke';
+$lang['img_title']   = 'Judul';
+$lang['img_caption'] = 'Label';
+$lang['img_date']    = 'Tanggal';
+$lang['img_fname']   = 'Nama file';
+$lang['img_fsize']   = 'Ukuran';
+$lang['img_artist']  = 'Tukang foto';
+$lang['img_copyr']   = 'Hakcipta';
+$lang['img_format']  = 'Format';
+$lang['img_camera']  = 'Kamera';
+$lang['img_keywords']= 'Katakunci';
+
+$lang['subscribe_success']  = 'Added %s ke data subscription untuk %s';
+$lang['subscribe_error']    = 'Gagal menambahkan %s ke data subscription untuk %s';
+$lang['subscribe_noaddress']= 'Tidak ditemukan alamat yang berhubungan dengan login Anda, Anda tidak dapat menambahkan daftar subscription';
+$lang['unsubscribe_success']= 'Menghapus %s dari daftar subscription untuk %s';
+$lang['unsubscribe_error']  = 'Gagal menghapus %s dari daftar subscription untuk %s';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lib/plugins/acl/lang/id/lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Indonesian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     mubaidillah <mubaidillah@gmail.com>
+ */
+
+$lang['admin_acl']  = 'Manajemen Daftar Pengendali Akses';
+
+$lang['acl_group']  = 'Grup';
+$lang['acl_user']   = 'User';
+$lang['acl_perms']  = 'Ijin untuk';
+$lang['page']       = 'Halaman';
+$lang['namespace']  = 'Namespace';
+
+$lang['acl_perm1']  = 'Baca';
+$lang['acl_perm2']  = 'Ubah';
+$lang['acl_perm4']  = 'Buat';
+$lang['acl_perm8']  = 'Upload';
+$lang['acl_perm16'] = 'Hapus';
+$lang['acl_new']    = 'Tambah Entry baru';
