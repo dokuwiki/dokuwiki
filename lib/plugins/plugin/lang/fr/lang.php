cf157a806193c0cabfbe957c3b9158c5273b7b3b
gb <gb@isis.u-strasbg.fr>
2006-01-29 01:11:20 +0100

    Several more french updates (UI and plugins)
    darcs-hash:20060129001120-2060e-aca189a18e75d422619f8c4e66d0abceaaf76593.gz

---
+++ lib/plugins/plugin/lang/fr/lang.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * french language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Guy Brand <gb@isis.u-strasbg.fr>
+ */
+
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'Gérer les plugins...'; 
+
+// custom language strings for the plugin
+$lang['refresh'] = "Actualiser la liste des plugins installés";
+$lang['refresh_x'] = "Utiliser cette option si vous avez altéré manuellement un plugin"; 
+$lang['download'] = "Télécharger et installer un nouveau plugin";
+$lang['manage'] = "Plugins installés";
+
+$lang['btn_info'] = 'info';
+$lang['btn_update'] = 'update';
+$lang['btn_delete'] = 'effacer';
+$lang['btn_settings'] = 'paramètres';
+$lang['btn_refresh'] = 'Actualiser';
+$lang['btn_download'] = 'Ramener';
+
+$lang['url'] = 'URL';
+//$lang[''] = '';
+
+$lang['installed'] = 'Installé:';
+$lang['lastupdate'] = 'Dernière mise à jour:';
+$lang['source'] = 'Source:';
+$lang['unknown'] = 'inconnu';
+
+// ..ing = header message
+// ..ed = success message
+
+$lang['refreshing'] = 'Actualisation ...';
+$lang['refreshed'] = 'Actualisation du plugin terminée.';
+
+$lang['updating'] = 'Mise à jour ...';
+$lang['updated'] = 'Plugin %s mis à jour avec succès';
+$lang['updates'] = 'Les plugins suivant ont été mis à jour avec succès';
+$lang['update_none'] = 'Aucune mise à jour n\'a été trouvée.';
+
+$lang['deleting'] = 'Suppression ...';
+$lang['deleted'] = 'Plugin %s supprimé.';
+
+$lang['downloading'] = 'Téléchargement ...';
+$lang['downloaded'] = 'Plugin %s installé avec succès';
+$lang['downloads'] = 'Les plugins suivant ont été installés avec succès:';
+$lang['download_none'] = 'Aucun plugin trouvé, ou un problème inconnu est survenu durant le téléchargement et l\'installation.';
+
+// info titles
+$lang['plugin'] = 'Plugin:';
+$lang['components'] = 'Composants';
+$lang['noinfo'] = 'Ce plugin n\'a transmis aucune information, il pourrait être invalide.';
+$lang['name'] = 'Nom:';
+$lang['date'] = 'Date:';
+$lang['type'] = 'Type:';
+$lang['desc'] = 'Description:';
+$lang['author'] = 'Auteur:';
+$lang['www'] = 'Web:';
+    
+// error messages
+$lang['error'] = 'Une erreur inconnue est survenue.';
+$lang['error_download'] = 'Impossible de télécharger le fichier du plugin: %s';
+$lang['error_badurl'] = 'URL suspecte - impossible de déterminer le nom du fichier à partir de l\'URL';
+$lang['error_dircreate'] = 'Impossible de créer le répertoire temporaire pour réceptionner le téléchargement';
+$lang['error_decompress'] = 'Le gestionnaire de plugin était incapable de décompresser le fichier téléchargé. '.
+            'Ceci peut être le résultat d\'un mauvais téléchargement, auquel cas vous devriez réessayer ; '.
+            'ou bien le format de compression est inconnu, auquel cas vous devez télécharger et installer le plugin manuellement.';
+$lang['error_copy'] = 'Une erreur de copie est survenue lors de l\'installation des fichiers du plugin <em>%s</em>: '.
+            'votre disque est peut être plein ou les droits d\'accès au fichier incorrects. '.
+            'Il a pu en résulter une installation partielle du plugin rendant votre installation du wiki instable.';
+$lang['error_delete'] = 'Une erreur est survenue à la suppression du plugin <em>%s</em>.  '.
+            'La raison la plus probable est l\'insuffisance des droits sur les fichiers ou le répertoire';
+//$lang['error_'] = '';    
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
--- lib/plugins/usermanager/lang/fr/lang.php
+++ lib/plugins/usermanager/lang/fr/lang.php
@@ -12,14 +12,42 @@ $lang['direction']  = 'ltr';
 $lang['menu'] = 'Gestion des utilisateurs'; 
  
 // custom language strings for the plugin
+$lang['noauth']      = '(authentification utilisateur non disponible)';
+$lang['nosupport']   = '(gestion utilisateur non supportée)';
+
+$lang['badauth']     = 'méchanisme d\'authentification invalide';     // should never be displayed!
+
 $lang["user_id"] = "Identifiant";
 $lang["user_pass"] = "Mot de passe";
 $lang["user_name"] = "Nom";
-$lang["user_mail"] = "E-mail";
+$lang["user_mail"] = "Courriel";
 $lang["user_groups"] = "Groupes";
 
 $lang["field"] = "Champ";
 $lang["value"] = "Valeur";
 $lang["add"] = "Ajouter";
 $lang["delete"] = "Supprimer";
-$lang["edit"] = "Modifier";
+$lang['delete_selected'] = 'Supprimer la sélection';
+$lang["edit"] = "Éditer";
+$lang['edit_prompt'] = 'Éditer cet utilisateur';
+$lang['modify']      = 'Enregistrer les modifications';
+$lang['search']      = 'Rechercher';
+$lang['search_prompt'] = 'Effectuer la recherche';
+$lang['clear']       = 'Réinitialiser la recherche';
+$lang['filter']      = 'Filtre';
+
+$lang['summary']     = 'Affichage des utilisateurs %1$d-%2$d parmi %3$d trouvés. %4$d utilisateurs au total.';
+$lang['nonefound']   = 'Aucun utilisateur trouvé %d utilisateurs au total.';
+$lang['delete_ok']   = '%d utilisateurs effacés';
+$lang['delete_fail'] = '%d effacement échoué.';
+$lang['update_ok']   = 'utilisateur mis à jour avec succès';
+$lang['update_fail'] = 'échec de la mise à jour utilisateur';
+$lang['update_exists'] = 'échec du changement de nom d\'utilisateur,le nom spécifié (%s) existe déjà (toutes les autres modifications seront effectuées).';
+
+$lang['start']  = 'démarrage';
+$lang['prev']   = 'précédent';
+$lang['next']   = 'suivant';
+$lang['last']   = 'dernier';
+
+
+?>
