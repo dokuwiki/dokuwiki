a329ae32fe3d35706dbb5ef4feee1bb825194051
larsch8 <larsch8@m2.stud.ku.dk>
2006-02-28 22:40:36 +0100

    Added Danish localization for usermanager plugin and fixed typos in ditto for acl plugin
    darcs-hash:20060228214036-a08f5-16c209ecc627715fad5875c5c0b317ab6d73ee16.gz

---
+++ lib/plugins/usermanager/lang/da/lang.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Danish language file
+ *
+ * @author     Lars Næsbye Christensen <larsnaesbye@stud.ku.dk>
+ */
+ 
+$lang['menu'] = 'Brugerstyring'; 
+ 
+// custom language strings for the plugin
+$lang['noauth']      = '(brugervalidering er ikke tilgængelig)';
+$lang['nosupport']   = '(brugerstyring er ikke understøttet)';
+
+$lang['badauth']     = 'ugyldig valideringsmekanisme';     // should never be displayed!
+
+$lang['user_id']     = 'Bruger';
+$lang['user_pass']   = 'Password';
+$lang['user_name']   = 'Navn';
+$lang['user_mail']   = 'E-mail';
+$lang['user_groups'] = 'Grupper';
+
+$lang['field']       = 'Felt';
+$lang['value']       = 'Værdi';
+$lang['add']         = 'Tilføj';
+$lang['delete']      = 'Slet';
+$lang['delete_selected'] = 'Slet valgte';
+$lang['edit']        = 'Rediger';
+$lang['edit_prompt'] = 'Rediger denne bruger';
+$lang['modify']      = 'Gem ændringer';
+$lang['search']      = 'Søg';
+$lang['search_prompt'] = 'Udfør søgning';
+$lang['clear']       = 'Nulstil søgefilter';
+$lang['filter']      = 'Filter';
+
+$lang['summary']     = 'Viser brugerne %1$d-%2$d ud af %3$d fundne. %4$d brugere totalt.';
+$lang['nonefound']   = 'Ingen brugere fundet. %d brugere totalt.';
+$lang['delete_ok']   = '%d brugere slettet';
+$lang['delete_fail'] = '%d kunne ikke slettes.';
+$lang['update_ok']   = 'Bruger opdateret korrekt';
+$lang['update_fail'] = 'Brugeropdatering mislykkedes';
+$lang['update_exists'] = 'Ændring af brugernavn mislykkedes, det valgte brugernavn (%s) er allerede optaget (andre ændringer vil blive udført).';
+
+$lang['start']  = 'start';
+$lang['prev']   = 'forrige';
+$lang['next']   = 'næste';
+$lang['last']   = 'sidste';
+
