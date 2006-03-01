a0c3b0eab226fdbd1cf62f24eda906fb5d0b1f17
larsch8 <larsch8@m2.stud.ku.dk>
2006-03-01 01:33:07 +0100

    Preliminary Danish localization for the rest of the plugins. Too tired to complete it right now...
    darcs-hash:20060301003307-a08f5-68dde44c8a9be592d8823a1a5e24538f813e1133.gz

---
+++ lib/plugins/config/lang/da/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Danish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lars Næsbye Christensen <larsnaesbye@stud.ku.dk>
+ */
+
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu']       = 'Konfigurationsindstillinger'; 
+
+$lang['error']      = 'Indstillingerne blev ikke opdateret på grund af en ugyldig værdi, gennemse venligst dine ændringer og gem dem igen.
+                       <br />De(n) ugyldig(e) værdie(r) vil blive rammet ind med rødt.';
+$lang['updated']    = 'Indstillingerne blev opdateret korrekt.';
+$lang['nochoice']   = '(ingen andre valgmuligheder)';
+$lang['locked']     = 'Indstillingsfilen kunne ikke opdateres, hvis dette er en fejl, <br />
+                       sørg da for at den lokale indstillingsfils navn og rettigheder er korrekte.';
+
---
+++ lib/plugins/plugin/lang/da/lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Danish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lars Næsbye Christensen <larsnaesbye@stud.ku.dk>
+ */
+
+$lang['menu'] = 'Håndter plugins'; 
+
+// custom language strings for the plugin
+$lang['download'] = "Download og installer nyt plugin";
+$lang['manage'] = "Installerede plugins";
+
+$lang['btn_info'] = 'info';
+$lang['btn_update'] = 'opdater';
+$lang['btn_delete'] = 'slet';
+$lang['btn_settings'] = 'indstillinger';
+$lang['btn_download'] = 'Download';
+
+$lang['url']              = 'URL';
+
+$lang['installed']        = 'Installeret:';
+$lang['lastupdate']       = 'Sidst opdateret:';
+$lang['source']           = 'Kilde:';
+$lang['unknown']          = 'ukendt';
+
