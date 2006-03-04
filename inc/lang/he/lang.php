574957a4007e0b4110e8efffc1f6478e1caecb21
Denis Simakov <akinoame1@gmail.com>
2006-03-04 19:22:15 +0100

    hebrew translation (partial)
    darcs-hash:20060304182215-3c565-11b8a6e0de76e6e87b489e5693f3218a8c14e8d1.gz

--- inc/lang/he/lang.php
+++ inc/lang/he/lang.php
@@ -6,6 +6,7 @@
  * @author     גיא שפר <guysoft@ort.org.il>
  * @author     DoK <kamberd@yahoo.com>
  * @link       http://sourceforge.net/projects/hebdokuwiki/
+*  @author Denis Simakov <akinoame1@gmail.com>
  */
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'rtl';
@@ -29,6 +30,16 @@ $lang['btn_logout'] = 'יציאה';
 $lang['btn_newer']  = '<< יותר חדש';
 $lang['btn_older']  = 'פחות חדש >>';
 $lang['btn_admin']  = 'מנהל';
+$lang['btn_update'] = 'עדכן';
+$lang['btn_delete'] = 'מחק';
+$lang['btn_back']   = 'חזור';
+$lang['btn_backlink']    = 'קישורים לכאן';
+$lang['btn_backtomedia'] = 'לחזור לבחירת קובץ מדיה';
+$lang['btn_subscribe']   = 'עקוב אחרי שינוים';
+$lang['btn_unsubscribe'] = 'הפסק לעקוב';
+$lang['btn_profile']    = 'עדכן פרופיל';
+$lang['btn_reset']     = 'איפוס';
+$lang['btn_resendpwd'] = 'שלח סיסמה חדשה';
 
 $lang['loggedinas']  = 'רשום כ-';
 $lang['user']        = 'שם משתמש';
@@ -39,6 +50,10 @@ $lang['passchk']    = 'שוב';
 $lang['fullname']    = 'שם מלא';
 $lang['register']    = 'הרשמה';
 $lang['badlogin']    = 'סליחה, שם המשתמש או הסיסמה שגויים';
+$lang['newpass']    = 'סיסמה חדשה';
+$lang['oldpass']    = 'אשר את הסיסמה הנוכחית';
+$lang['profile']    = 'פרופיל';
+$lang['minoredit']  = 'שינוים מינוריים';
 
 $lang['regmissing']  = 'סליחה, עליך למלא את כל השדות';
 $lang['reguexists']  = 'סליחה, משתמש בשם זה כבר נרשם';
@@ -48,9 +63,23 @@ $lang['regmailfail'] = 'שליחת הודעת הדואר כשלה, נא ליצו
 $lang['regbadmail']  = 'כתובת דואר כנראה לא תקפה, אם לא כך היא יש ליצור קשר עם מנהל האתר';
 $lang['regpwmail']   = 'סיסמת הדוקוויקי שלך';
 $lang['reghere']     = 'עדיין ללא שם-משתמש? ההרשמה כאן';
+$lang['regbadpass'] = 'שתי הסיסמות הן לא זהות, נא לנסות שוב';
+
+$lang['profna']       = 'בוויקי הזה לא ניתן לשנות פרופיל'; 
+$lang['profnochange'] = 'אין שינוים, פרופיל לא עודכן';
+$lang['profnoempty']  = 'שם וכתובת דוא"ל לא יכולים להיות ריקים';
+$lang['profchanged']  = 'פרופיל עודכן בהצלחה';
+
+$lang['pwdforget'] = 'שכחת סיסמה? קבל חדשה';
+$lang['resendna']  = 'הוויקי הזה לא תומך בחידוש סיסמה';
+$lang['resendpwd'] = 'שלח סיסמה חדשה עבור';
+$lang['resendpwdmissing'] = 'סליחה, עליך למלא את כל השדות';
+$lang['resendpwdnouser']  = 'סליחה, משתמש בשם זה לא נמצא';
+$lang['resendpwdsuccess'] = 'סיסמה חדשה נשלחה בדואר';
 
 $lang['txt_upload']   = 'בחר קובץ להעלות';
-$lang['txt_filename'] = 'הכנס שם לויקי (בחירה)';
+$lang['txt_filename'] = 'הכנס שם לוויקי (בחירה)';
+$lang['txt_overwrt']  = 'לכתוב במקום קובץ קיים'; 
 $lang['lockedby']     = 'נעול על ידי';
 $lang['lockexpire']   = 'נעילה פגה';
 $lang['willexpire']   = 'נעילה תחלוף עוד זמן קצר. \nלמניעת התנגשויות יש להשתמש בכפתור הרענון מטה כדי לאתחל את הנעילה שנית';
@@ -64,8 +93,16 @@ $lang['fileupload']  = 'העלאת קובץ מדיה';
 $lang['uploadsucc']  = 'העלאת הקובץ בוצעה בהצלחה';
 $lang['uploadfail']  = 'קרתה שגיאה בעת העלאת הקובץ. תיתכן ובעייה זו נוצרה עקב הרשאות שגיות.';
 $lang['uploadwrong'] = 'העלאה לא אושרה. קבצים בסיומת זו אסורים';
+$lang['uploadexist'] = 'הקובץ כבר קיים. פעולה בוטלה';
+$lang['deletesucc']  = 'קובץ %s נמחק';
+$lang['deletefail']  = 'לא יכולתי למחוק "%s" -- בדקו הרשאות';
+$lang['mediainuse']  = 'קובץ "%s" לא נמחק - הוא עדיין בשימוש';
 $lang['namespaces'] = 'שמות מתחם';
 $lang['mediafiles'] = 'קבצים זמינים ב-';
+
+$lang['reference']   = 'קישורים ל';
+$lang['ref_inuse']   = 'לא יכולתי למחוק קובץ, הדפים הבאים עדיין משתמשים בו:';
+$lang['ref_hidden']  = 'יש קישורים לדפים ללא הרשאת קריאה';
 
 $lang['hits']       = 'פגיעות';
 $lang['quickhits']  = 'דפים שנמצאו';
@@ -73,8 +110,9 @@ $lang['toc']        = 'תוכן עניינים';
 $lang['current']    = 'גירסה נוכחית';
 $lang['yours']      = 'הגרסה שלך';
 $lang['diff']       = 'הצג שינוים מגרסה זו ועד הנוכחית';
-$lang['line']       = 'Line';
+$lang['line']       = 'שורה';
 $lang['breadcrumb'] = 'ביקורים אחרונים';
+$lang['youarehere'] = 'אתה נמצה כאן'; 
 $lang['lastmod']    = 'שונה לאחרונה ב';
 $lang['by']         = 'על ידי';
 $lang['deleted']    = 'נמחק';
@@ -87,11 +125,12 @@ $lang['mail_changed'] = 'דף שונה:';
 
 $lang['nosmblinks'] = ':( קישור למערכת קבצים של חלונות פועל רק בדפדפן אינטרנט אקספלורר.\n זה בסדר, אין צורך לעבור. אפשר להעתיק ולהדביק את הקישור';
 
-$lang['qb_alert']   = 'נא להזין את הטקטס לעריכה. \n הטקסט יתוסף בקצה המסמך';
+$lang['qb_alert']   = 'נא להזין את הטקסט לעריכה. \n הטקסט יוסף בקצה המסמך';
 $lang['qb_bold']    = 'טקסט מודגש';
 $lang['qb_italic']  = 'טקסט נטוי';
 $lang['qb_underl']  = 'טקסט עם קו תחתון';
 $lang['qb_code']    = 'קוד';
+$lang['qb_strike']  = 'טקסט מחוק';
 $lang['qb_h1']      = 'כותרת רמה 1';
 $lang['qb_h2']      = 'כותרת רמה 2';
 $lang['qb_h3']      = 'כותרת רמה 3';
@@ -104,5 +143,32 @@ $lang['qb_ol']      = 'איבר ברשימה ממוספרת';
 $lang['qb_ul']      = 'אבר ברשימה לא ממוספרת';
 $lang['qb_media']   = 'תמונות או קובץ אחר';
 $lang['qb_sig']     = 'הזנת חתימה';
+$lang['qb_smileys'] = 'פרצופונים';
+$lang['qb_chars']   = 'סימנים מיוחדים';
+
+$lang['del_confirm']= 'באמת למחוק?';
+$lang['admin_register']= 'להוסיף משתמש חדש';
+
+$lang['spell_start'] = 'בדוק איות';
+$lang['spell_stop']  = 'חזור לעריכה';
+$lang['spell_wait']  = 'נא להמתין...';
+$lang['spell_noerr'] = 'לא נמצאו שגיאות';
+$lang['spell_nosug'] = 'אין הצעות';
+$lang['spell_change']= 'החלף';
+
+$lang['metaedit']    = 'ערוך נתונים';
+$lang['metasaveerr'] = 'כשל בשמירת נתונים';
+$lang['metasaveok']  = 'נתונים נשמרו';
+$lang['img_backto']  = 'הזור ל';
+$lang['img_title']   = 'כותרת';
+$lang['img_caption'] = 'תיאור';
+$lang['img_date']    = 'תאריך';
+$lang['img_fname']   = 'שם הקובץ';
+$lang['img_fsize']   = 'גודל';
+$lang['img_artist']  = 'צלם';
+$lang['img_copyr']   = 'זכויות';
+$lang['img_format']  = 'פורמט';
+$lang['img_camera']  = 'מצלמה';
+$lang['img_keywords']= 'מילות מפתח';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
