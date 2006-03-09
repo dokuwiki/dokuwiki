81b576bd6b90307e107c3c9a886b456e0b0c6aeb
larsch8 <larsch8@m2.stud.ku.dk>
2006-03-09 01:03:38 +0100

    Danish lang update
    darcs-hash:20060309000338-a08f5-57f9eb59c8e110bd35103c8dcc8a2cdf91d5cc68.gz

--- lib/plugins/config/lang/da/lang.php
+++ lib/plugins/config/lang/da/lang.php
@@ -20,6 +20,95 @@ $lang['locked']     = 'Indstillingsfilen kunne ikke opdateres, hvis dette er en 
 
 /* -------------------- Config Options --------------------------- */
 
+//$lang['fmode']       = 'File creation mode';
+//$lang['dmode']       = 'Directory creation mode';
+$lang['lang']        = 'Sprog';
+//$lang['basedir']     = 'Base directory';
+//$lang['baseurl']     = 'Base URL';
+//$lang['savedir']     = 'Directory for saving data';
+$lang['start']       = 'Startsidens navn';
+$lang['title']       = 'Wiki titel';
+$lang['template']    = 'Skabelon';
+//$lang['fullpath']    = 'Reveal full path of pages in the footer';
+$lang['recent']      = 'Nylige ændringer';
+$lang['breadcrumbs'] = 'Stilængde';
+$lang['youarehere']  = 'Hierarkisk sti';
+$lang['typography']  = 'Typografiske erstatninger';
+$lang['htmlok']      = 'Tillad indlejret HTML';
+$lang['phpok']       = 'Tillad indlejret PHP';
+$lang['dformat']     = 'Datoformat (se PHP\'s <a href="http://www.php.net/date">date</a> funktion)';
+$lang['signature']   = 'Signatur';
+//$lang['toptoclevel'] = 'Top level for table of contents';
+//$lang['maxtoclevel'] = 'Maximum level for table of contents';
+//$lang['maxseclevel'] = 'Maximum section edit level';
+$lang['camelcase']   = 'Brug CamelCase til links';
+//$lang['deaccent']    = 'Clean pagenames';
+//$lang['useheading']  = 'Use first heading for pagenames';
+//$lang['refcheck']    = 'Media reference check';
+//$lang['refshow']     = 'Number of media references to show';
+$lang['allowdebug']  = 'Tillad debugging <b>slå fra hvis unødvendig!</b>';
+
+$lang['usewordblock']= 'Bloker spam baseret på ordliste';
+//$lang['indexdelay']  = 'Time delay before indexing';
+$lang['relnofollow'] = 'Brug rel="nofollow"';
+//$lang['mailguard']   = 'Obfuscate email addresses';
+
+/* Authentication Options */
+//$lang['useacl']      = 'Use access control lists';
+//$lang['openregister']= 'Allow everyon to register';
+//$lang['autopasswd']  = 'Autogenerate passwords';
+//$lang['resendpasswd']= 'Allow resend password';
+//$lang['authtype']    = 'Authentication backend';
+//$lang['passcrypt']   = 'Password encryption method';
+//$lang['defaultgroup']= 'Default group';
+$lang['superuser']   = 'Superbruger';
+//$lang['profileconfirm'] = 'Confirm profile changes with password';
+
+/* Advanced Options */
+//$lang['userewrite']  = 'Use nice URLs';
+//$lang['useslash']    = 'Use slash as namespace separator in URLs';
+//$lang['sepchar']     = 'Page name word separator';
+//$lang['canonical']   = 'Use fully canonical URLs';
+//$lang['autoplural']  = 'Check for plural forms in links';
+//$lang['usegzip']     = 'Use gzip (for attic)';
+//$lang['cachetime']   = 'Maximum age for cache (sec)';
+$lang['purgeonadd']  = 'Ryd cache når nye sider tilføjes';
+//$lang['locktime']    = 'Maximum age for lock files (sec)';
+//$lang['notify']      = 'Send change notifications to this email address';
+$lang['mailfrom']    = 'Email adresse til brug for automatiske mails';
+$lang['gdlib']       = 'GD Lib version';
+//$lang['im_convert']  = 'Path to ImageMagick\'s convert tool';
+$lang['spellchecker']= 'Slå stavekontrol til';
+//$lang['subscribers'] = 'Enable page subscription support';
+$lang['compress']    = 'Komprimer CSS og Javascript filer';
+//$lang['hidepages']   = 'Hide matching pages (regular expressions)';
+$lang['send404']     = 'Send "HTTP 404/Page Not Found" for ikke-eksisterende sider';
+$lang['sitemap']     = 'Generer Google sitemap (dage)';
+
+$lang['rss_type']    = 'XML feed type';
+$lang['rss_linkto']  = 'XML feed linker til';
+
+/* Target options */
+$lang['target____wiki']      = 'Destinationsvindue til interne links';/$lang['target____interwiki'] = 'Destinationsvindue til interwiki links';
+$lang['target____extern']    = 'Destinationsvindue til externe links';
+$lang['target____media']     = 'Destinationsvindue til medie links';
+$lang['target____windows']   = 'Destinationsvindue til Windows links';
+
+/* Proxy Options */
+$lang['proxy____host'] = 'Proxy servernavn';
+$lang['proxy____port'] = 'Proxy port';
+$lang['proxy____user'] = 'Proxy brugernavn';
+$lang['proxy____pass'] = 'Proxy password';
+$lang['proxy____ssl']  = 'Brug SSL til at forbinde til proxy';
+
+/* Safemode Hack */
+//$lang['safemodehack'] = 'enable safemode hack';
+//$lang['ftp____host'] = 'FTP server for safemode hack';
+//$lang['ftp____port'] = 'FTP port for safemode hack';
+//$lang['ftp____user'] = 'FTP user name for safemode hack';
+//$lang['ftp____pass'] = 'FTP password for safemode hack';
+//$lang['ftp____root'] = 'FTP root directory for safemode hack';
+
 /* userewrite options */
 $lang['userewrite_o_0'] = 'ingen';
 $lang['userewrite_o_1'] = 'htaccess';
