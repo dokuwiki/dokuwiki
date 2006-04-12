c6480b76a58fe21a89e7ee0c41626742aa5342ea
Andreas Gohr <andi@splitbrain.org>
2006-04-12 21:57:03 +0200

    consistent filetype icons
    Now all filetype icons have the same style and work on light and dark
    backgrounds. A test file was added to lib/images/fileicons/index.php
    
    darcs-hash:20060412195703-7ad00-4d31a65788e8da0255ea6e064382595e8ae183ff.gz

---
+++ lib/images/fileicons/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"
+ lang="en" dir="ltr">
+    <title>filetype icons</title>
+
+    <style type="text/css">
+        body {
+            background-color: #ccc;
+            font-family: Arial;
+        }
+
+        .box {
+            width: 200px;
+            float:left;
+            padding: 0.5em;
+            margin: 0;
+        }
+
+        .white {
+            background-color: #fff;
+        }
+
+        .black {
+            background-color: #000;
+        }
+    </style>
+
+</head>
+<body>
+
+<div class="white box">
+<?php
+foreach (glob('*.png') as $img) {
+    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+}
+?>
+</div>
+
+<div class="black box">
+<?php
+foreach (glob('*.png') as $img) {
+    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+}
+?>
+</div>
+
+</body>
+</html>
