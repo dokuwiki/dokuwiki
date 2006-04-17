5f9846d0a9c83fe0debcf52fa9f6ad9aea4871fa
Andreas Gohr <andi@splitbrain.org>
2006-04-17 18:04:53 +0200

    add missing FeedParser.php #782
    darcs-hash:20060417160453-7ad00-e54322159dbe1c1c75cd2c8494fda14a3f4bc021.gz

---
+++ inc/FeedParser.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Class used to parse RSS and ATOM feeds
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../').'/');
+require_once(DOKU_INC.'inc/HTTPClient.php');
+require_once(DOKU_INC.'inc/SimplePie.php');
+
+
+/**
+ * We override some methods of the original SimplePie class here
+ */
+class FeedParser extends SimplePie {
+
+    /**
+     * Constructor. Set some defaults
+     */
+    function FeedParser(){
+        $this->SimplePie();
+        $this->caching = false;
+    }
+
+    /**
+     * Fetch an URL using our own HTTPClient
+     */
+    function getFile($url){
+        $http = new DokuHTTPClient();
+        return $http->get($url);
+    }
+}
