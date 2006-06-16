11e384f72d646266f90cc7ff18c59dea58fd30c7
Thanos Massias <tm@navarino.gr>
2006-06-16 14:51:01 +0200

    greek language updates
    darcs-hash:20060616125101-3b7d2-ba5934430fb3253834c5bb71eadf7de2d1b87934.gz

---
+++ lib/plugins/acl/lang/el/lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * greek language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
+ * @author     Anika Henke <henke@cosmocode.de>
+ * @author     Matthias Grimm <matthiasgrimm@users.sourceforge.net>
+ * @author     Thanos Massias <tm@navarino.gr>
+*/
+
+$lang['admin_acl']  = 'Διαχείριση Δικαιωμάτων Πρόσβασης';
+$lang['acl_group']  = 'Ομάδα';
+$lang['acl_user']   = 'Χρήστης';
+$lang['acl_perms']  = 'Δικαιώματα για';
+$lang['page']       = 'Σελίδα';
+$lang['namespace']  = 'Κατάλογος';
+
+$lang['acl_perm1']  = 'Ανάγνωση';
+$lang['acl_perm2']  = 'Τροποποίηση';
+$lang['acl_perm4']  = 'Δημιουργία';
+$lang['acl_perm8']  = 'Μεταφόρτωση';
+$lang['acl_perm16'] = 'Διαγραφή';
+$lang['acl_new']    = 'Προσθήκη νέας εγγραφής';
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lib/plugins/usermanager/lang/el/lang.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * English language file
+ *
+ * @author Chris Smith <chris@jalakai.co.uk>
+ * @author     Thanos Massias <tm@navarino.gr>
+*/
+ 
+$lang['menu'] = 'Διαχείριση Χρηστών'; 
+ 
+// custom language strings for the plugin
+$lang['noauth']      = '(η είσοδος χρηστών δεν έιναι δυνατή)';
+$lang['nosupport']   = '(δεν υποστηρίζεται η διαχείριση χρηστών)';
+
+$lang['badauth']     = 'μη επιτρεπτός μηχανισμός πιστοποίησης';     // should never be displayed!
+
+$lang['user_id']     = 'Χρήστης';
+$lang['user_pass']   = 'Κωδικός';
+$lang['user_name']   = 'Πλήρες όνομα';
+$lang['user_mail']   = 'e-mail';
+$lang['user_groups'] = 'Ομάδες';
+
+$lang['field']       = 'Πεδίο';
+$lang['value']       = 'Τιμή';
+$lang['add']         = 'Προσθήκη';
+$lang['delete']      = 'Διαγραφή';
+$lang['delete_selected'] = 'Διαγραφή επιλεγμένων χρηστών';
+$lang['edit']        = 'Τροποποίηση';
+$lang['edit_prompt'] = 'Τροποποίηση χρήστη';
+$lang['modify']      = 'Αποθήκευση αλλαγών';
+$lang['search']      = 'Αναζήτηση';
+$lang['search_prompt'] = 'Εκκίνηση αναζήτησης';
+$lang['clear']       = 'Καθαρισμός φίλτρων';
+$lang['filter']      = 'Φίλτρο';
+
+$lang['summary']     = 'Εμφάνιση χρηστών %1$d-%2$d από %3$d σχετικούς. %4$d χρήστες συνολικά.';
+$lang['nonefound']   = 'Δεν βρέθηκαν σχετικοί χρήστες. %d χρήστες συνολικά.';
+$lang['delete_ok']   = '%d χρήστες διαγράφησαν';
+$lang['delete_fail'] = '%d χρήστες δεν διαγράφησαν.';
+$lang['update_ok']   = 'Επιτυχημένη τροποποίηση προφίλ χρήστη';
+$lang['update_fail'] = 'Αποτυχημένη τροποποίηση προφίλ χρήστη';
+$lang['update_exists'] = 'Η αλλαγή ονόματος χρήστη απέτυχε -- το νέο όνομα χρήστη (%s) ήδη υπάρχει (τυχόν άλλες αλλαγές θα εφαρμοστούν).';
+
+$lang['start']  = 'αρχή';
+$lang['prev']   = 'προηγούμενα';
+$lang['next']   = 'επόμενα';
+$lang['last']   = 'τέλος';
+
