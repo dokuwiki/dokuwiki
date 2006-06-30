f8ffff9d607c13cdb08d79d20ef916fbd8ee4f52
Andreas Gohr <andi@splitbrain.org>
2006-06-30 11:28:52 +0200

    finish lang update
    darcs-hash:20060630092852-7ad00-400d13aeb2f051d0e37e042b06162c223251a296.gz

--- inc/lang/fi/lang.php
+++ inc/lang/fi/lang.php
@@ -17,10 +17,13 @@ $lang['btn_search'] = 'Etsi';
 $lang['btn_save']   = 'Tallenna';
 $lang['btn_preview']= 'Esikatselu';
 $lang['btn_top']    = 'Takaisin ylös';
+$lang['btn_newer']  = '<< uudemmat';
+$lang['btn_older']  = 'vanhemmat >>';
 $lang['btn_revs']   = 'Vanhat versiot';
 $lang['btn_recent'] = 'Viimeiset muutokset';
 $lang['btn_upload'] = 'Lähetä tiedosto';
 $lang['btn_cancel'] = 'Peru';
+$lang['btn_index']  = 'Hakemisto';
 $lang['btn_admin']  = 'Ylläpito';
 $lang['btn_update'] = 'Päivitä';
 $lang['btn_delete'] = 'Poista';
@@ -32,26 +35,50 @@ $lang['btn_login']  = 'Kirjaudu sisään';
 $lang['btn_logout'] = 'Kirjaudu ulos';
 $lang['btn_subscribe'] = 'Tarkkaile';
 $lang['btn_unsubscribe'] = 'Lopeta tarkkailu';
+$lang['btn_backtomedia'] = 'Takaisin mediatiedostojen valintaan';
+$lang['btn_profile']     = 'Päivitä profiili';
+$lang['btn_reset']       = 'Tyhjennä';
+$lang['btn_resendpwd']   = 'Lähetä uusi salasana';
+$lang['btn_draft']    = 'Muokkaa luonnosta';
+$lang['btn_recover']  = 'Palauta luonnos';
+$lang['btn_draftdel'] = 'Poista luonnos';
 
 $lang['loggedinas'] = 'Kirjautunut nimellä';
 $lang['user']       = 'Käyttäjänimi';
 $lang['pass']       = 'Salasana';
+$lang['newpass']    = 'Uusi salasana';
+$lang['oldpass']    = 'Vahvista nykyinen salasana';
 $lang['passchk']    = 'uudelleen';
 $lang['remember']   = 'Muista minut';
-$lang['fullname']   = 'Kokonimi';
+$lang['fullname']   = 'Koko nimi';
 $lang['email']      = 'Sähköposti';
 $lang['register']   = 'Rekisteröidy';
+$lang['profile']    = 'Käyttäjän profiili';
 $lang['badlogin']   = 'Käyttäjänimi tai salasana oli väärä.';
 $lang['minoredit']  = 'Pieni muutos';
+$lang['draftdate']  = 'Luonnos tallennettu automaattisesti'; // full dformat date will be added
 
-$lang['regmissing'] = 'Anteeksi, sinun täytyy täyttää kaikki kentät.';
-$lang['reguexists'] = 'Anteeksi, käyttäjä tällä käyttäjänimellä on jo olemassa';
+$lang['regmissing'] = 'Kaikki kentät tulee täyttää.';
+$lang['reguexists'] = 'Käyttäjä tällä käyttäjänimellä on jo olemassa';
 $lang['regsuccess'] = 'Käyttäjä luotiin ja salasana lähetettiin sähköpostilla';
-$lang['regmailfail']= 'Näyttää siltä, että tuli virheitä salasanan lähettämisessä 
-sähköpostiin. Ota yhteys ylläpitäjään!';
+$lang['regsuccess2']= 'Käyttäjänimi on luotu.';
+$lang['regmailfail']= 'Näyttää siltä, että salasanan lähettämisessä tapahtui virhe. Ota yhteys ylläpitäjään!';
 $lang['regbadmail'] = 'Antamasi sähköposti näyttää epäkelvolta. Jos pidät tätä virheenä ota yhteys ylläpitäjään.';
+$lang['regbadpass'] = 'Annetut kaksi salasanaa eivät täsmää. Yritä uudelleen.';
 $lang['regpwmail']  = 'Sinun DokuWiki salasanasi';
-$lang['reghere']    = 'Sinulla ei ole vielä käyttäjätiliä? Hanki sellainen';
+$lang['reghere']    = 'Puuttuuko sinulta käyttäjätili? Hanki sellainen';
+
+$lang['profna']       = 'Tässä wikissä profiilien muokkaaminen ei ole mahdollista';
+$lang['profnochange'] = 'Ei muutoksia.';
+$lang['profnoempty']  = 'Tyhjä nimi tai sähköpostiosoite ei ole sallittu.';
+$lang['profchanged']  = 'Käyttäjän profiilin päivitys onnistui.';
+
+$lang['pwdforget'] = 'Unohtuiko salasana? Hanki uusi';
+$lang['resendna']  = 'Tämä wiki ei tue salasanan uudelleenlähettämistä.';
+$lang['resendpwd'] = 'Lähetä uusi salasana käyttäjälle';
+$lang['resendpwdmissing'] = 'Kaikki kentät on täytettävä.';
+$lang['resendpwdnouser']  = 'Käyttäjää ei löydy tietokannastamme.';
+$lang['resendpwdsuccess'] = 'Uusi salasanasi on lähetetty sähköpostilla.';
 
 $lang['txt_upload']   = 'Valitse tiedosto lähetettäväksi';
 $lang['txt_filename'] = 'Syötä wikinimi (valinnainen)';
@@ -61,15 +88,31 @@ $lang['willexpire']   = 'Sinun lukitus tämän sivun muokkaukseen päättyy minu
 
 $lang['notsavedyet'] = 'Dokumentissa on tallentamattomia muutoksia, jotka häviävät.\n Haluatko varmasti jatkaa?';
 $lang['rssfailed']   = 'Virhe tapahtui noudettaessa tätä syötettä: ';
-$lang['nothingfound']= 'Mitään ei löydetty.';
+$lang['nothingfound']= 'Mitään ei löytynyt.';
 
 $lang['mediaselect'] = 'Mediatiedoston valinta';
 $lang['fileupload']  = 'Mediatiedoston lähetys';
 $lang['uploadsucc']  = 'Tiedoston lähetys onnistui';
-$lang['uploadfail']  = 'Tiedoston lähetys epäonnistui. Ehkä väärät oikeudet?';
+$lang['uploadfail']  = 'Tiedoston lähetys epäonnistui. Syynä ehkä väärät oikeudet?';
 $lang['uploadwrong'] = 'Tiedoston lähetys evätty. Tämä tiedostopääte on kielletty';
+$lang['uploadexist'] = 'Tiedosto on jo olemassa. Mitään ei tehty.';
+$lang['deletesucc']  = 'Tiedosto "%s" on poistettu.';
+$lang['deletefail']  = 'Kohdetta "%s" poistaminen ei onnistunut - tarkista oikeudet.';
+$lang['mediainuse']  = 'Tiedostoa "%s" ei ole poistettu - se on vielä käytössä.';
 $lang['namespaces']  = 'Nimiavaruudet';
 $lang['mediafiles']  = 'Tarjolla olevat tiedostot';
+
+$lang['js']['keepopen']    = 'Pidä valinnan ikkuna avoinna.';
+$lang['js']['hidedetails'] = 'Piilota yksityiskohdat';
+$lang['mediausage']  = 'Käytä seuraavaa merkintätapaa viittausta tehtäessä:';
+$lang['mediaview']   = 'Katsele alkuperäistä tiedostoa';
+$lang['mediaroot']   = 'root';
+$lang['mediaupload'] = 'Siirrä tiedosto nykyiseen nimiavaruuteen täällä. Voit luoda uusia alinimiavaruuksia käyttämällä kaksoispistettä tiedoston polussa.';
+$lang['mediaextchange'] = 'Tiedoston pääte muutettu: .%s on nyt .%s!';
+
+$lang['reference']   = 'Viitteet';
+$lang['ref_inuse']   = 'Tiedostoa ei voi poistaa, koska seuraavat sivut käyttävät sitä:';
+$lang['ref_hidden']  = 'Osa viitteistä on sivuilla, joihin sinulla ei ole lukuoikeutta';
 
 $lang['hits']       = 'Osumia';
 $lang['quickhits']  = 'Sopivat sivunimet';
@@ -95,6 +138,7 @@ $lang['qb_bold']    = 'Lihavoitu teksti';
 $lang['qb_italic']  = 'Kursivoitu teksti';
 $lang['qb_underl']  = 'Alleviivattu teksti';
 $lang['qb_code']    = 'Kooditeksti';
+$lang['qb_strike']  = 'Yliviivattu teksti';
 $lang['qb_h1']      = 'Taso 1 otsikko';
 $lang['qb_h2']      = 'Taso 2 otsikko';
 $lang['qb_h3']      = 'Taso 3 otsikko';
@@ -107,5 +151,43 @@ $lang['qb_ol']      = 'Järjestetyn listan osa ';
 $lang['qb_ul']      = 'Epäjärjestetyn listan osa';
 $lang['qb_media']   = 'Lisää kuvia ja muita tiedostoja';
 $lang['qb_sig']     = 'Lisää allekirjoitus';
+$lang['qb_smileys'] = 'Hymiöt';
+$lang['qb_chars']   = 'Erikoismerkit';
+
+$lang['del_confirm']= 'Haluatko todella poistaa valitut kohteet?';
+$lang['admin_register']= 'Lisää uusi käyttäjä';
+
+$lang['spell_start'] = 'Tarkista oikeinkirjoitus';
+$lang['spell_stop']  = 'Jatka muokkaamista';
+$lang['spell_wait']  = 'Hetkinen...';
+$lang['spell_noerr'] = 'Virheitä ei löytynyt';
+$lang['spell_nosug'] = 'Ei ehdotuksia';
+$lang['spell_change']= 'Muuta';
+
+$lang['metaedit']    = 'Muokkaa metadataa';
+$lang['metasaveerr'] = 'Metadatan kirjoittaminen epäonnistui';
+$lang['metasaveok']  = 'Metadata tallennettu';
+$lang['img_backto']  = 'Takaisin';
+$lang['img_title']   = 'Otsikko';
+$lang['img_caption'] = 'Kuvateksti';
+$lang['img_date']    = 'Päivämäärä';
+$lang['img_fname']   = 'Tiedoston nimi';
+$lang['img_fsize']   = 'Koko';
+$lang['img_artist']  = 'Kuvaaja';
+$lang['img_copyr']   = 'Tekijänoikeus';
+$lang['img_format']  = 'Formaatti';
+$lang['img_camera']  = 'Kamera';
+$lang['img_keywords']= 'Avainsanat';
+
+$lang['subscribe_success']  = '%s lisättiin käyttäjän %s seurattavien listaan';
+$lang['subscribe_error']    = 'Lisättäessä %s käyttäjän %s seurattavien listaan tapahtui virhe';
+$lang['subscribe_noaddress']= 'Käyttäjänimelle ei löydy osoitetta. Seurattavien listaan lisääminen ei onnistu.';
+$lang['unsubscribe_success']= '%s poistettiin käyttäjän %s seurattavien listasta';
+$lang['unsubscribe_error']  = 'Poistettaessa %s käyttäjän %s seurattavien listasta tapahtui virhe';
+
+/* auth.class language support */
+$lang['authmodfailed']   = 'Käyttäjien autentikoinnin asetukset ovat virheelliset. Ilmoita asiasta wikin ylläpitäjälle.';
+$lang['authtempfail']    = 'Käyttäjien autentikointi ei tällä hetkellä onnistu. Jos ongelma jatkuu, ota yhteyttä wikin ylläpitäjään.';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
+
