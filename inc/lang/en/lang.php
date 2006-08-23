11120ca983fe7c13f48c2d13728f937d61d98b57
chris <chris@jalakai.co.uk>
2006-08-23 13:50:06 +0200

    minor en lang string fix
    darcs-hash:20060823115006-9b6ab-af7b27a3fa1830e5bd7e152d7e589ec3a2fbcd72.gz

--- inc/lang/en/lang.php
+++ inc/lang/en/lang.php
@@ -64,7 +64,7 @@ $lang['regsuccess'] = 'The user has been created and the password was sent by em
 $lang['regsuccess2']= 'The user has been created.';
 $lang['regmailfail']= 'Looks like there was an error on sending the password mail. Please contact the admin!';
 $lang['regbadmail'] = 'The given email address looks invalid - if you think this is an error, contact the admin';
-$lang['regbadpass'] = 'The two given passwords are not identically, please try again.';
+$lang['regbadpass'] = 'The two given passwords are not identical, please try again.';
 $lang['regpwmail']  = 'Your DokuWiki password';
 $lang['reghere']    = 'You don\'t have an account yet? Just get one';
 
