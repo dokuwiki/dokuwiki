8e6cfd4d8687ecdcf3c20b31a34a0f85b1b6d6fb
Andreas Gohr <andi@splitbrain.org>
2006-09-21 19:56:55 +0200

    croatian language files
    darcs-hash:20060921175655-7ad00-f4d1d9639928ead2840b2d9199503f46ef3260ca.gz

---
+++ inc/lang/hr/lang.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * croatian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Tomo Krajina <aaa@puzz.info>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+$lang['btn_edit']   = 'Izmijeni dokument';
+$lang['btn_source'] = 'Prikaži kod';
+$lang['btn_show']   = 'Prikaži dokument';
+$lang['btn_create'] = 'Novi dokument';
+$lang['btn_search'] = 'Pretraživanje';
+$lang['btn_save']   = 'Snimi';
+$lang['btn_preview']= 'Prikaži';
+$lang['btn_top']    = 'Na vrh';
+$lang['btn_newer']  = '<< noviji';
+$lang['btn_older']  = 'stariji >>';
+$lang['btn_revs']   = 'Stare verzije';
+$lang['btn_recent'] = 'Nedavne izmjene';
+$lang['btn_upload'] = 'Upload';
+$lang['btn_cancel'] = 'Poništi';
+$lang['btn_index']  = 'Indeks';
+$lang['btn_secedit']= 'Izmjena';
+$lang['btn_login']  = 'Prijava';
+$lang['btn_logout'] = 'Odjava';
+$lang['btn_admin']  = 'Admin';
+$lang['btn_update'] = 'Update'; // ODO
+$lang['btn_delete'] = 'Brisanje'; 
+$lang['btn_back']   = 'Povratak';
+$lang['btn_backlink']    = "Povratni linkovi";
+$lang['btn_backtomedia'] = 'Povratak na Mediafile izbornik'; // TODO
+$lang['btn_subscribe']   = 'Pretplati se na promjene';
+$lang['btn_unsubscribe'] = 'Odjavi pretplatu o promjenama';
+$lang['btn_profile']    = 'Izmjena profila';
+$lang['btn_reset']     = 'Reset';
+$lang['btn_resendpwd'] = 'Pošalji lozinku';
+
+$lang['loggedinas'] = 'Prijavljen kao';
+$lang['user']       = 'Korisničko ime';
+$lang['pass']       = 'Lozinka';
+$lang['newpass']    = 'Nova lozinka';
+$lang['oldpass']    = 'Confirm current password';
+$lang['passchk']    = 'Ponovi';
+$lang['remember']   = 'Zapamti me';
+$lang['fullname']   = 'Ime i prezime';
+$lang['email']      = 'Email';
+$lang['register']   = 'Registracija';
+$lang['profile']    = 'Profil korisnika';
+$lang['badlogin']   = 'Krivo korisničko ime ili lozinka.';
+$lang['minoredit']  = 'Manja promjena';
+
+$lang['regmissing'] = 'Morate popuniti sva polja.';
+$lang['reguexists'] = 'Korisnik s tim korisničkim imenom već postoji.';
+$lang['regsuccess'] = 'Novi korisnik je uspješno unesen i poslana mu je lozinka mailom.';
+$lang['regsuccess2']= 'Korisnik je uspješno.';
+$lang['regmailfail']= 'Došlo je do greške pri slanju lozinke. Molimo Vas da kontaktirate administratora!';
+$lang['regbadmail'] = 'Email adreasa nije ispravna';
+$lang['regbadpass'] = 'Unesene lozinke nisu jednake!';
+$lang['regpwmail']  = 'Vaša DokuWiki lozinka';
+$lang['reghere']    = 'Nemate još korisničko ime? Registrirajte se.';
+
+$lang['profna']       = 'Nije dopušteno mijenjati korisnikov profil';
+$lang['profnochange'] = 'Nije bilo izmjena.';
+$lang['profnoempty']  = 'Nije ispunjeno polje za ime i/ili email.';
+$lang['profchanged']  = 'Korisnikov profil uredno izmijenjen.';
+
+$lang['pwdforget'] = 'Izgubili ste lozinku?';
+$lang['resendna']  = 'Nije dopušteno slanje lozinke emailom.';
+$lang['resendpwd'] = 'Slanje nove lozinke za';
+$lang['resendpwdmissing'] = 'Molimo Vas da ispunite sva polja.';
+$lang['resendpwdnouser']  = 'Korisnik nije nađen.';
+$lang['resendpwdsuccess'] = 'Nova lozinka je poslana emailom.';
+
+$lang['txt_upload']   = 'Odaberite datoteku za upload';
+$lang['txt_filename'] = 'Upload kao (nije obavezno)';
+$lang['txt_overwrt']  = 'Preko već postojeće datoteke';
+$lang['lockedby']     = 'Zaključano od strane korisnika';
+$lang['lockexpire']   = 'Zaključano do';
+$lang['willexpire']   = 'Dokument kojeg mijenjate će biti zaključan još 1 minutu. Ukoliko želite i dalje raditi izmjene na dokumentu - molimo Vas da kliknete na "Pregled".'; // TODO
+
+$lang['notsavedyet'] = 'Vaše izmjene že ostati izgubljene.\nZaista želite nastaviti?';
+$lang['rssfailed']   = 'Došlo je do greške prilikom preuzimanja ovog feed-a: ';
+$lang['nothingfound']= 'Ništa nije nađeno.';
+
+$lang['mediaselect'] = 'Mediafile izbor';
+$lang['fileupload']  = 'Mediafile upload';
+$lang['uploadsucc']  = 'Upload uspješan';
+$lang['uploadfail']  = 'Upload neuspješan. Možda nešto ne valja s dozvolama na serveru?';
+$lang['uploadwrong'] = 'Upload nije dopušten - nedozvoljena ekstenzija!';
+$lang['uploadexist'] = 'Datoteka već postoji.';
+$lang['deletesucc']  = 'Datoteka "%s" je obrisana.';
+$lang['deletefail']  = '"%s" ne može biti obrisana - provjerite dozvole na serveru.';
+$lang['mediainuse']  = 'Datoteka "%s" nije obrisana - još uvijek se koristi.';
+$lang['namespaces']  = 'Namespaces';
+$lang['mediafiles']  = 'Datoteke u';
+
+$lang['reference']   = 'Reference za';
+$lang['ref_inuse']   = 'Datoteka ne može biti obrisana jer se još uvijek koristi u sljedećim stranicama:';
+$lang['ref_hidden']  = 'Neke reference su na stranicama koje nemate dozvolu ćitati';
+
+$lang['hits']       = 'Nađeno';
+$lang['quickhits']  = 'Nađeno po imenima stranica';
+$lang['toc']        = 'Sadržaj';
+$lang['current']    = 'trenutno';
+$lang['yours']      = 'Vaša verzija';
+$lang['diff']       = 'prikaži razlike u odnosu na trenutnu verziju';
+$lang['line']       = 'Redak';
+$lang['breadcrumb'] = 'Trace';
+$lang['youarehere'] = 'Vi ste ovdje';
+$lang['lastmod']    = 'Zadnja izmjena';
+$lang['by']         = 'od';
+$lang['deleted']    = 'obrisano';
+$lang['created']    = 'kreirano';
+$lang['restored']   = 'old revision restored';
+$lang['summary']    = 'Sažetak izmjena';
+
+$lang['mail_newpage'] = 'stranica dodana:';
+$lang['mail_changed'] = 'stranica kreirana:';
+
+$lang['nosmblinks'] = 'Linkovi na share-ane foldere rade samo s Internet Explorerom. ';
+
+$lang['qb_alert']   = 'Unesite tekst kojeg želite formatirati.\nBiti će dodan na kraju dokumenta.';
+$lang['qb_bold']    = 'Podebljani tekst';
+$lang['qb_italic']  = 'Ukošeni tekst';
+$lang['qb_underl']  = 'Potcrtani tekst';
+$lang['qb_code']    = 'Kod';
+$lang['qb_strike']  = 'Precrtani tekst';
+$lang['qb_h1']      = 'Naslov - nivo 1';
+$lang['qb_h2']      = 'Naslov - nivo 2';
+$lang['qb_h3']      = 'Naslov - nivo 3';
+$lang['qb_h4']      = 'Naslov - nivo 4';
+$lang['qb_h5']      = 'Naslov - nivo 5';
+$lang['qb_link']    = 'Interni link';
+$lang['qb_extlink'] = 'Eksterni link';
+$lang['qb_hr']      = 'Vodoravna crta';
+$lang['qb_ol']      = 'Pobrojana lista';
+$lang['qb_ul']      = 'Lista';
+$lang['qb_media']   = 'Slike';
+$lang['qb_sig']     = 'Potpis';
+$lang['qb_smileys'] = 'Smile-ovi';
+$lang['qb_chars']   = 'Posebni znakovi';
+
+$lang['del_confirm']= 'Jeste li sigurni da ćelite obrisati?';
+$lang['admin_register']= 'Dodavanje korisnika';
+
+$lang['spell_start'] = 'Provjerite ispravnost teksta';
+$lang['spell_stop']  = 'Nastavite s izmjenama';
+$lang['spell_wait']  = 'Pričekajte trenutak...';
+$lang['spell_noerr'] = 'Nema grešaka';
+$lang['spell_nosug'] = 'Nema prijedloga';
+$lang['spell_change']= 'Izmjena';
+
+$lang['metaedit']    = 'Izmjena metapodataka';
+$lang['metasaveerr'] = 'Izmjena metapodataka nije uspjela';
+$lang['metasaveok']  = 'Meta';
+$lang['img_backto']  = 'Povratak na';
+$lang['img_title']   = 'Naslov';
+$lang['img_caption'] = 'Caption';
+$lang['img_date']    = 'Datum';
+$lang['img_fname']   = 'Ime datoteke';
+$lang['img_fsize']   = 'Veličina';
+$lang['img_artist']  = 'Fotograf';
+$lang['img_copyr']   = 'Copyright';
+$lang['img_format']  = 'Format';
+$lang['img_camera']  = 'Kamera';
+$lang['img_keywords']= 'Ključne riječi';
+
+$lang['subscribe_success']  = 'Dodan korisnik %s kao pretplatnik na %s';
+$lang['subscribe_error']    = 'Greška pri dodavanju korisnika %s kao pretplatnika na %s';
+$lang['subscribe_noaddress']= 'Nema email adrese za vaše korisničko ime, ne možete biti dodani na listu pretplatnika';
+$lang['unsubscribe_success']= 'Maknut %s sa liste pretplatnika %s';
+$lang['unsubscribe_error']  = 'Greška pri micanju korisnika %s s liste pretplatnika za %s';
+
+/* auth.class language support */
+$lang['authmodfailed']   = 'Greška pri konfiguraciji za autentifikaciju. Molimo Vas da kontaktirate administratora.';
+$lang['authtempfail']    = 'Autentifikacija korisnika je privremeno nedostupna. Molimo Vas da kontaktirate administratora.';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
