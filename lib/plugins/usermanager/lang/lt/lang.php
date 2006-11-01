26535e55a2e02592e9756feaaa6c0353d564cff8
Andreas Gohr <andi@splitbrain.org>
2006-11-01 17:26:18 +0100

    Lithuanian language update
    darcs-hash:20061101162618-7ad00-c54e13d7effb1f9af23abe61150900b884a79332.gz

---
+++ lib/plugins/usermanager/lang/lt/lang.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Lithuanian language file
+ *
+ * @author grawity <grawity@gmail.com>
+ */
+
+$lang['menu'] = 'Vartotojų administravimas';
+
+// custom language strings for the plugin
+$lang['noauth']      = '(vartotojų autentifikacija neprieinama)';
+$lang['nosupport']   = '(vartotojų administravimas nepalaikomas)';
+
+$lang['badauth']     = 'neteisingas autentifikacijos būdas';
+// should never be displayed!
+
+$lang['user_id']     = 'Vartotojas';
+$lang['user_pass']   = 'Slaptažodis';
+$lang['user_name']   = 'Vardas';
+$lang['user_mail']   = 'El.paštas';
+$lang['user_groups'] = 'Grupės';
+
+$lang['field']       = 'Laukas';
+$lang['value']       = 'Turinys';
+$lang['add']         = 'Pridėti';
+$lang['delete']      = 'Pašalinti';
+$lang['delete_selected'] = 'Pašalinti pažymėtus';
+$lang['edit']        = 'Redaguoti';
+$lang['edit_prompt'] = 'Redaguoti šį vartotoją';
+$lang['modify']      = 'Išsaugoti';
+$lang['search']      = 'Paieška';
+$lang['search_prompt'] = 'Ieškoti';
+$lang['clear']       = 'Panaikinti filtrą';
+$lang['filter']      = 'Filtras';
+
+$lang['summary']     = 'Rodomi vartotojai %1$d-%2$d iš %3$d rastų. Iš viso %4$d vartotojų.';
+$lang['nonefound']   = 'Vartotojų nerasta. Iš viso %d vartotojų.';
+$lang['delete_ok']   = 'Pašalinta %d vartotojų';
+$lang['delete_fail'] = '%d nepavyko pašalinti.';
+$lang['update_ok']   = 'Vartotojas sėkmingai pakeistas';
+$lang['update_fail'] = 'Vartotojo pakeitimas nepavyko';
+$lang['update_exists'] = 'Vartotojo vardo pakeitimas nepavyko, nes nurodytas vartotojo vardas (%s) jau yra (kiti pakeitimai įvykdyti).';
+
+$lang['start']  = 'pradžia';
+$lang['prev']   = 'atgal';
+$lang['next']   = 'pirmyn';
+$lang['last']   = 'pabaiga';
+
+// added after 2006-03-09 release
+$lang['edit_usermissing'] = 'Pasirinktas vartotojas nerastas, nurodytas vartotojo vardas galėjo būti pašalintas ar pakeistas kitur.';
+$lang['user_notify'] = 'Įspėti vartotoją';
+$lang['note_notify'] = 'Įspėjimas siunčiamas tik tada, kai vartotojui priskiriamas naujas slaptažodis.';
+$lang['note_group'] = 'Jei grupė nenurodyta, nauji vartotojai pridedami į pagrindinę grupę (%s).';
+$lang['add_ok'] = 'Vartotojas sėkmingai pridėtas';
+$lang['add_fail'] = 'Vartotojo pridėjimas nepavyko';
+$lang['notify_ok'] = 'Įspėjimo el.laiškas išsiųstas';
+$lang['notify_fail'] = 'Įspėjimo el.laiško išsiųsti nepavyko';
