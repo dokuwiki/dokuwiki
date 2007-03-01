a8fe108b00a44fe91f90820a1e3cf44b55de2259
Guy Brand <gb@isis.u-strasbg.fr>
2007-03-02 00:03:09 +0100

    Allow @USER@ variable in ACLs
    This saves a lot of ACL lines for users namespaces for example:
    
    users:*               @ALL        1
    users:@USER@          @USER@      8
    
    darcs-hash:20070301230309-19e2d-90a00b70a2af546fd5194ade614c130e9f7864eb.gz

--- inc/auth.php
+++ inc/auth.php
@@ -76,6 +76,9 @@
     global $AUTH_ACL;
     if(is_readable(DOKU_CONF.'acl.auth.php')){
       $AUTH_ACL = file(DOKU_CONF.'acl.auth.php');
+      if(isset($_SERVER['REMOTE_USER']){
+        $AUTH_ACL = str_replace('@USER@',$_SERVER['REMOTE_USER'],$AUTH_ACL);
+      }
     }else{
       $AUTH_ACL = array();
     }
