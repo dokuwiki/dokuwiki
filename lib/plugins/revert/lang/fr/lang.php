9be05bde2838b8a3ae2a0353b76dfdfc2c6199f0
Guy Brand <gb@isis.u-strasbg.fr>
2007-03-05 17:38:50 +0100

    French lang for revert plugin
    darcs-hash:20070305163850-19e2d-38f2e618020d45e85dad50082e5dc619ca1cfa9f.gz

---
+++ lib/plugins/revert/lang/fr/lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * french language file
+ */
+
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'Gestionnaire de réversion';
+
+// custom language strings for the plugin
+
+$lang['filter']   = 'Trouver les pages spammées ';
+$lang['note']     = 'Note : cette recherche est insensible à la casse';
+$lang['revert']   = 'Annuler les éditions sélectionnées';
+$lang['reverted'] = '%s restauré à la révision %s';
+$lang['removed']  = '%s supprimé';
+$lang['revstart'] = 'Processus de réversion démarré. Ceci peut prendre longtemps. Si
+                     le script dépasse le délai avant de terminer, vous devrez restaurer
+                     de plus petits groupes de pages.';
+$lang['revstop']  = 'Processus de réversion terminé avec succès.';
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
