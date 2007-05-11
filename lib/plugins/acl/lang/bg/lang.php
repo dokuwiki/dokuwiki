b7ab6f2f26a5380cb3d0aaeb64116aa79b9dcebe
Nikolay Vladimirov <nikolay@vladimiroff.com>
2007-05-11 20:02:44 +0200

    Bulgarian Translations
    darcs-hash:20070511180244-5dabd-5c414e320c1ec24ef8b09209cdf5f192018cfdbb.gz

---
+++ lib/plugins/acl/lang/bg/lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * bulgarian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Nikolay Vladimirov <nikolay@vladimiroff.com> 
+ */
+
+$lang['admin_acl']  = 'Управление на списъците за достъп';
+$lang['acl_group']  = 'Група';
+$lang['acl_user']   = 'Потребител';
+$lang['acl_perms']  = 'Права за';
+$lang['page']       = 'Страница';
+$lang['namespace']  = 'Именнопространство';
+
+$lang['acl_perm1']  = 'Четене';
+$lang['acl_perm2']  = 'Редактиране';
+$lang['acl_perm4']  = 'Създаване';
+$lang['acl_perm8']  = 'Качване';
+$lang['acl_perm16'] = 'Изтриване';
+$lang['acl_new']    = 'Добавяне на ново';
+//Setup VIM: ex: et ts=2 enc=utf-8 :
