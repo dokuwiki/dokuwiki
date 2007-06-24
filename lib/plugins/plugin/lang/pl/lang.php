a8422a927454df870e8dd61ab6fcd6e2d1391aa6
Andreas Gohr <andi@splitbrain.org>
2007-06-24 12:59:48 +0200

    Polish language update
    darcs-hash:20070624105948-7ad00-9b0c7bb2b718cc45302756b3e8b04cf2472d92bd.gz

--- lib/plugins/plugin/lang/pl/lang.php
+++ lib/plugins/plugin/lang/pl/lang.php
@@ -3,70 +3,46 @@
  * polish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Michał Tkacz <mehow@autocom.pl>
- * @author     Grzegorz Żur <grzegorz.zur@gmail.com>
+ * @author Michał Tkacz <mehow@autocom.pl>
+ * @author Grzegorz Żur <grzegorz.zur@gmail.com>
  */
-$lang['menu'] = 'Menadżer wtyczek'; 
-
-// custom language strings for the plugin
-$lang['download'] = "Ściągnij i zainstaluj nową wtyczkę";
-$lang['manage'] = "Zainstalowane Wtyczki";
-
-$lang['btn_info'] = 'Informacje';
-$lang['btn_update'] = 'Aktualizuj';
-$lang['btn_delete'] = 'Usuń';
-$lang['btn_settings'] = 'Ustawienia';
-$lang['btn_download'] = 'Pobierz';
-$lang['btn_enable'] = 'Zapisz';
-
-$lang['url'] = 'Adres URL';
-
-$lang['installed'] = 'Instalacja:';
-$lang['lastupdate'] = 'Ostatnio zaktualizowana:';
-$lang['source'] = 'Źródło:';
-$lang['unknown'] = 'nieznane';
-
-// ..ing = header message
-// ..ed = success message
-
-$lang['updating'] = 'Aktualizuję...';
-$lang['updated'] = 'Aktualizacja wtyczki %s pomyślnie ściągnięta';
-$lang['updates'] = 'Aktualizacje następujących wtyczek zostały pomyślnie ściągnięte';
-$lang['update_none'] = 'Nie znaleziono aktualizacji.';
-
-$lang['deleting'] = 'Usuwam...';
-$lang['deleted'] = 'Wtyczka %s usunięta.';
-
-$lang['downloading'] = 'Pobieram...';
-$lang['downloaded'] = 'Wtyczka %s pomyślnie zainstalowana';
-$lang['downloads'] = 'Następujące wtyczki zostały pomyślnie zainstalowane:';
-$lang['download_none'] = 'Nie znaleziono wtyczek lub wystąpił nieznany problem podczas ściągania i instalacji.';
-
-// info titles
-$lang['plugin'] = 'Wtyczka:';
-$lang['components'] = 'Składniki';
-$lang['noinfo'] = 'Ta wtyczka nie zwróciła żadnych informacji, może być niepoprawna.';
-$lang['name'] = 'Nazwa:';
-$lang['date'] = 'Data:';
-$lang['type'] = 'Typ:';
-$lang['desc'] = 'Opis:';
-$lang['author'] = 'Autor:';
-$lang['www'] = 'WWW:';
-    
-// error messages
-$lang['error'] = 'Wystąpił nieznany błąd.';
-$lang['error_download'] = 'Nie powiodło się ściągnięcie pliku wtyczki: %s';
-$lang['error_badurl'] = 'Prawdopodobnie zły url - nie da się ustalić nazwy pliku na podstawie urla';
-$lang['error_dircreate'] = 'Nie powiodło się stworzenie tymczasowego katalogu na pobrane pliki';
-$lang['error_decompress'] = 'Menadżer wtyczek nie był w stanie rozpakować ściągniętego pliku. '.
-            'Może to być spowodowane przez nieudany transfer (w takim przypadku powinieneś spróbować ponownie) '.
-            'lub nieznany format kompresji (w takim przypadku będziesz musiał ściągnąć i zainstalować wtyczkę ręcznie).';
-
-$lang['error_copy'] = 'Wystąpił błąd podczas kopiowania pliku w trakcie instalacji wtyczki %s: '.
-            'być może dysk jest pełny lub prawa dostępu są niepoprawne. '.
-            'Efektem może być częściowo zainstalowana wtyczka co może spowodować niestabilność Twojej instalacji wiki.';   
-
-$lang['error_delete']     = 'Wystąpił błąd przy próbie usunięcia wtyczki <em>%s</em>. '.
-                            'Prawdopodobną przyczyną są niewystarczające uprawnienia do katalogu.';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang[menu]                    = 'Menadżer wtyczek';
+$lang[download]                = 'Ściągnij i zainstaluj nową wtyczkę';
+$lang[manage]                  = 'Zainstalowane Wtyczki';
+$lang[btn_info]                = 'Informacje';
+$lang[btn_update]              = 'Aktualizuj';
+$lang[btn_delete]              = 'Usuń';
+$lang[btn_settings]            = 'Ustawienia';
+$lang[btn_download]            = 'Pobierz';
+$lang[btn_enable]              = 'Zapisz';
+$lang[url]                     = 'Adres URL';
+$lang[installed]               = 'Instalacja:';
+$lang[lastupdate]              = 'Ostatnio zaktualizowana:';
+$lang[source]                  = 'Źródło:';
+$lang[unknown]                 = 'nieznane';
+$lang[updating]                = 'Aktualizuję...';
+$lang[updated]                 = 'Aktualizacja wtyczki %s pomyślnie ściągnięta';
+$lang[updates]                 = 'Aktualizacje następujących wtyczek zostały pomyślnie ściągnięte';
+$lang[update_none]             = 'Nie znaleziono aktualizacji.';
+$lang[deleting]                = 'Usuwam...';
+$lang[deleted]                 = 'Wtyczka %s usunięta.';
+$lang[downloading]             = 'Pobieram...';
+$lang[downloaded]              = 'Wtyczka %s pomyślnie zainstalowana';
+$lang[downloads]               = 'Następujące wtyczki zostały pomyślnie zainstalowane:';
+$lang[download_none]           = 'Nie znaleziono wtyczek lub wystąpił nieznany problem podczas ściągania i instalacji.';
+$lang[plugin]                  = 'Wtyczka:';
+$lang[components]              = 'Składniki';
+$lang[noinfo]                  = 'Ta wtyczka nie zwróciła żadnych informacji, może być niepoprawna.';
+$lang[name]                    = 'Nazwa:';
+$lang[date]                    = 'Data:';
+$lang[type]                    = 'Typ:';
+$lang[desc]                    = 'Opis:';
+$lang[author]                  = 'Autor:';
+$lang[www]                     = 'WWW:';
+$lang[error]                   = 'Wystąpił nieznany błąd.';
+$lang[error_download]          = 'Nie powiodło się ściągnięcie pliku wtyczki: %s';
+$lang[error_badurl]            = 'Prawdopodobnie zły url - nie da się ustalić nazwy pliku na podstawie urla';
+$lang[error_dircreate]         = 'Nie powiodło się stworzenie tymczasowego katalogu na pobrane pliki';
+$lang[error_decompress]        = 'Menadżer wtyczek nie był w stanie rozpakować ściągniętego pliku. Może to być spowodowane przez nieudany transfer (w takim przypadku powinieneś spróbować ponownie) lub nieznany format kompresji (w takim przypadku będziesz musiał ściągnąć i zainstalować wtyczkę ręcznie).';
+$lang[error_copy]              = 'Wystąpił błąd podczas kopiowania pliku w trakcie instalacji wtyczki %s: być może dysk jest pełny lub prawa dostępu są niepoprawne. Efektem może być częściowo zainstalowana wtyczka co może spowodować niestabilność Twojej instalacji wiki.';
+$lang[error_delete]            = 'Wystąpił błąd przy próbie usunięcia wtyczki <em>%s</em>. Prawdopodobną przyczyną są niewystarczające uprawnienia do katalogu.';
--- lib/plugins/revert/lang/pl/lang.php
+++ lib/plugins/revert/lang/pl/lang.php
@@ -3,24 +3,14 @@
  * polish language file
  * @author Grzegorz Żur <grzegorz.zur@gmail.com>
  */
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'Menadżer przywracania';
-
-// custom language strings for the plugin
-
-$lang['filter']   = 'Wyszukaj uszkodzone strony';
-$lang['note1']    = 'Uwaga: duże i małe litery są rozróżniane';
-$lang['revert']   = 'Napraw zaznaczone strony';
-$lang['reverted'] = 'Stronę %s zastąpiono wersją %s';
-$lang['removed']  = 'Stronę %s usunięto';
-$lang['revstart'] = 'Naprawa rozpoczęta. To może zająć kilka minut. Jeśli strona przestanie się ładować, spróbuj ponownie zaznaczając mniejszą liczbę stron.';
-
-
-$lang['revstop']  = 'Naprawa zakończona pomyślnie!';
-
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+$lang[encoding]                = 'utf-8';
+$lang[direction]               = 'ltr';
+$lang[menu]                    = 'Menadżer przywracania';
+$lang[filter]                  = 'Wyszukaj uszkodzone strony';
+$lang[revert]                  = 'Napraw zaznaczone strony';
+$lang[reverted]                = 'Stronę %s zastąpiono wersją %s';
+$lang[removed]                 = 'Stronę %s usunięto';
+$lang[revstart]                = 'Naprawa rozpoczęta. To może zająć kilka minut. Jeśli strona przestanie się ładować, spróbuj ponownie zaznaczając mniejszą liczbę stron.';
+$lang[revstop]                 = 'Naprawa zakończona pomyślnie!';
+$lang[note1]                   = 'Uwaga: duże i małe litery są rozróżniane';
+$lang[note2]                   = 'Uwaga: zostanie przywrócona ostatnia wersja strony niezawierająca wyrażenia <i>%s</i>.';
