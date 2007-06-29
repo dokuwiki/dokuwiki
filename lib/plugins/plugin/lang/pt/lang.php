a7800614e0b1731f48b8c5b4bb218549ba92fb45
Andreas Gohr <andi@splitbrain.org>
2007-06-29 23:23:40 +0200

    More Portuguese language updates
    darcs-hash:20070629212340-7ad00-ca30ab5a542bab7e1bf3b7650e3533cf57190170.gz

--- lib/plugins/plugin/lang/pt/lang.php
+++ lib/plugins/plugin/lang/pt/lang.php
@@ -4,3 +4,11 @@
  *
  * @author José Monteiro <Jose.Monteiro@DoWeDo-IT.com>
  */
+$lang[menu]                    = 'Gerir Plugins';
+$lang[manage]                  = 'Plugins Instalados';
+$lang[btn_info]                = 'info';
+$lang[btn_update]              = 'actualizar';
+$lang[btn_delete]              = 'remover';
+$lang[btn_settings]            = 'configurações';
+$lang[installed]               = 'Instalado em:';
+$lang[lastupdate]              = 'Actualizado em:';
