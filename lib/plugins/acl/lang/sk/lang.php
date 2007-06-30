4cab3ca7bcb592c65b93e21e7528f0c2a02deaf3
Andreas Gohr <andi@splitbrain.org>
2007-06-30 13:01:32 +0200

    Slovak language updates
    darcs-hash:20070630110132-7ad00-d2e4eca72ca53ff3aa5808ae6328d798a9e8b039.gz

--- lib/plugins/acl/lang/sk/lang.php
+++ lib/plugins/acl/lang/sk/lang.php
@@ -3,20 +3,17 @@
  * Slovak language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Ondrej Vegh <ov@vsieti.sk>
+ * @author Ondrej Vegh <ov@vsieti.sk>
  */
-
-$lang['admin_acl']  = 'Správa zoznamu prístupových práv';
-$lang['acl_group']  = 'Skupina';
-$lang['acl_user']   = 'Užívateľ';
-$lang['acl_perms']  = 'Práva pre';
-$lang['page']       = 'Stránka';
-$lang['namespace']  = 'Menný priestor';
-
-$lang['acl_perm1']  = 'Čítať';
-$lang['acl_perm2']  = 'Zmeniť';
-$lang['acl_perm4']  = 'Vytvoriť';
-$lang['acl_perm8']  = 'Nahrať súbor';
-$lang['acl_perm16'] = 'Zmazať';
-$lang['acl_new']    = 'Pridať novú položku';
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang[admin_acl]               = 'Správa zoznamu prístupových práv';
+$lang[acl_group]               = 'Skupina';
+$lang[acl_user]                = 'Užívateľ';
+$lang[acl_perms]               = 'Práva pre';
+$lang[page]                    = 'Stránka';
+$lang[namespace]               = 'Menný priestor';
+$lang[acl_perm1]               = 'Čítať';
+$lang[acl_perm2]               = 'Zmeniť';
+$lang[acl_perm4]               = 'Vytvoriť';
+$lang[acl_perm8]               = 'Nahrať súbor';
+$lang[acl_perm16]              = 'Zmazať';
+$lang[acl_new]                 = 'Pridať novú položku';
---
+++ lib/plugins/config/lang/sk/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Slovaklanguage file
+ *
+ */
+$lang[menu]                    = 'Nastavenia konfigurácie';
+$lang[error]                   = 'Nastavenia neboli aktualizované kvôli neplatnej hodnote, prosím skontrolujte vaše zmeny a znovu ich pošlite. <br />Nesprávna hodnota(y) bude ohraničená červeným okrajom.';
+$lang[updated]                 = 'Nastavenia úspešne aktualizované.';
+$lang[nochoice]                = '(žiadne ďalšie dostupné voľby)';
+$lang[locked]                  = 'Súbor s nastaveniami nemôže byť aktualizovaný, ak toto nie je zámerom, <br />
+uistite sa, že názov a práva lokálneho súboru sú správne.';
+$lang[_configuration_manager]  = 'Konfiguračný menežér';
+$lang[_header_dokuwiki]        = 'Nastavenia DokuWiki';
+$lang[template]                = 'Šablóna';
+$lang[recent]                  = 'Posledné zmeny';
