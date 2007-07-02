db4244666982113c629209be3c2f26a9201a17e9
Andreas Gohr <andi@splitbrain.org>
2007-07-02 21:53:24 +0200

    Norwegian language update
    darcs-hash:20070702195324-7ad00-ae4099158ec50f7345355738b2d80905ca63a543.gz

--- lib/plugins/acl/lang/no/lang.php
+++ lib/plugins/acl/lang/no/lang.php
@@ -3,21 +3,19 @@
  * Norwegian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Reidar Mosvold <Reidar.Mosvold@hit.no>
- * @author     Jorge Barrera Grandon <jorge@digitalwolves.org>
+ * @author Reidar Mosvold <Reidar.Mosvold@hit.no>
+ * @author Jorge Barrera Grandon <jorge@digitalwolves.org>
+ * @author Thomas Nygreen <nygreen@gmail.com>
  */
-
-$lang['admin_acl']  = 'Administrasjon av Access Control List';
-$lang['acl_group']  = 'Gruppe';
-$lang['acl_user']   = 'Bruker';
-$lang['acl_perms']  = 'Rettigheter for';
-$lang['page']       = 'Side';
-$lang['namespace']  = 'Wikinavn';
- 
-$lang['acl_perm1']  = 'Lese';
-$lang['acl_perm2']  = 'Redigere';
-$lang['acl_perm4']  = 'Lage';
-$lang['acl_perm8']  = 'Laste opp';
-$lang['acl_perm16'] = 'Slette';
-$lang['acl_new']    = 'Legg til ny oppføring';
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang[admin_acl]               = 'Administrasjon av lister for adgangskontroll (ACL)';
+$lang[acl_group]               = 'Gruppe';
+$lang[acl_user]                = 'Bruker';
+$lang[acl_perms]               = 'Rettigheter for';
+$lang[page]                    = 'Side';
+$lang[namespace]               = 'Navnerom';
+$lang[acl_perm1]               = 'Lese';
+$lang[acl_perm2]               = 'Redigere';
+$lang[acl_perm4]               = 'Lage';
+$lang[acl_perm8]               = 'Laste opp';
+$lang[acl_perm16]              = 'Slette';
+$lang[acl_new]                 = 'Legg til ny oppføring';
---
+++ lib/plugins/plugin/lang/no/lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Norwegianlanguage file
+ *
+ * @author Thomas Nygreen <nygreen@gmail.com>
+ */
+$lang[menu]                    = 'Behandle tillegg';
+$lang[download]                = 'Last ned og installer et tillegg';
+$lang[manage]                  = 'Installerte tillegg';
+$lang[btn_info]                = 'informasjon';
+$lang[btn_update]              = 'oppdater';
+$lang[btn_delete]              = 'slett';
+$lang[btn_settings]            = 'innstillinger';
+$lang[btn_download]            = 'Last ned';
+$lang[btn_enable]              = 'Lagre';
+$lang[url]                     = 'URL';
+$lang[installed]               = 'Installert:';
+$lang[lastupdate]              = 'Sist oppdatert:';
+$lang[source]                  = 'Kilde:';
+$lang[unknown]                 = 'ukjent';
+$lang[updating]                = 'Oppdaterer ...';
+$lang[updated]                 = 'Tillegget %s er oppdatert';
+$lang[updates]                 = 'De følgende tilleggene har blitt oppdatert';
+$lang[update_none]             = 'Ingen oppdateringer funnet.';
+$lang[deleting]                = 'Sletter ...';
+$lang[deleted]                 = 'Tillegget %s ble slettet.';
+$lang[downloading]             = 'Laster ned ...';
+$lang[downloaded]              = 'Tillegget %s ble installert';
+$lang[downloads]               = 'De følgende tilleggene ble installert';
+$lang[download_none]           = 'Ingen tillegg funnet, eller det har vært et ukjent problem under nedlasting og installering.';
+$lang[plugin]                  = 'Tillegg:';
+$lang[components]              = 'Komponenter';
+$lang[noinfo]                  = 'Dette tillegget ga ikke noe informasjon. Det kan være ugyldig.';
+$lang[name]                    = 'Navn:';
+$lang[date]                    = 'Dato:';
+$lang[type]                    = 'Type:';
+$lang[desc]                    = 'Beskrivelse:';
+$lang[author]                  = 'Forfatter:';
+$lang[www]                     = 'Nett:';
+$lang[error]                   = 'En ukjent feil oppstod.';
+$lang[error_download]          = 'Klarte ikke å laste ned tillegget i filen: %s';
+$lang[error_badurl]            = 'Mistenker feil URL - klarte ikke å finne filnavnet i URLen';
+$lang[error_dircreate]         = 'Klarte ikke å lage en midlertidig mappe for å laste ned';
+$lang[error_decompress]        = 'Tilleggsbehandleren klarte ikke å dekomprimere den nedlastede filen. Dette kan være på grunn av en feilet nedlasting, i så fall bør du prøve igjen, eller kompresjonsformatet kan være ukjent, i så fall må du laste ned og installere tillegget manuelt.';
+$lang[error_copy]              = 'Det skjedde en feil ved kopiering av en fil under installasjonen av <em>%s</em>: disken kan være full eller rettighetene satt feil. Dette kan ha ført til et delvist installert tillegg og gjort wikien ubrukelig.';
+$lang[error_delete]            = 'Det skjedde en feil under forsøket på å slette tillegget <em>%s</em>. Den mest sannsynlige grunnen er utilstrekkelige rettigheter for filene eller mappene.';
