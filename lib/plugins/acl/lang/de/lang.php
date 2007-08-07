a17ae368c3b68a6ada9f26e2e9a438851d05b275
Andreas Gohr <andi@splitbrain.org>
2007-08-08 00:14:00 +0200

    german language update
    darcs-hash:20070807221400-7ad00-c67b347e0528709a4eae84f3740c34d12f9e1bba.gz

--- lib/plugins/acl/lang/de/lang.php
+++ lib/plugins/acl/lang/de/lang.php
@@ -8,16 +8,17 @@
  * @author Anika Henke <henke@cosmocode.de>
  * @author Esther Brunner <esther@kaffeehaus.ch>
  * @author Matthias Grimm <matthiasgrimm@users.sourceforge.net>
+ * @author Michael Klier <chi@chimeric.de>
  */
-$lang['admin_acl']               = 'Zugangsverwaltung';
-$lang['acl_group']               = 'Gruppe';
-$lang['acl_user']                = 'Benutzer';
-$lang['acl_perms']               = 'Berechtigungen für';
-$lang['page']                    = 'Seite';
-$lang['namespace']               = 'Namensraum';
-$lang['acl_perm1']               = 'Lesen';
-$lang['acl_perm2']               = 'Bearbeiten';
-$lang['acl_perm4']               = 'Anlegen';
-$lang['acl_perm8']               = 'Hochladen';
-$lang['acl_perm16']              = 'Entfernen';
-$lang['acl_new']                 = 'Eintrag hinzufügen';
+$lang['admin_acl']             = 'Zugangsverwaltung';
+$lang['acl_group']             = 'Gruppe';
+$lang['acl_user']              = 'Benutzer';
+$lang['acl_perms']             = 'Berechtigungen für';
+$lang['page']                  = 'Seite';
+$lang['namespace']             = 'Namensraum';
+$lang['acl_perm1']             = 'Lesen';
+$lang['acl_perm2']             = 'Bearbeiten';
+$lang['acl_perm4']             = 'Anlegen';
+$lang['acl_perm8']             = 'Hochladen';
+$lang['acl_perm16']            = 'Entfernen';
+$lang['acl_new']               = 'Eintrag hinzufügen';
