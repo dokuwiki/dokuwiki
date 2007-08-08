27091d4f6935d6301cb3711cbed74cbc6b163364
Andreas Gohr <andi@splitbrain.org>
2007-08-08 20:08:25 +0200

    Portuguese language update
    darcs-hash:20070808180825-7ad00-63a8a7c23769cd92cccde54c3df267e94214eaae.gz

--- lib/plugins/acl/lang/pt/lang.php
+++ lib/plugins/acl/lang/pt/lang.php
@@ -6,15 +6,15 @@
  * @author José Carlos Monteiro <jose.c.monteiro@netcabo.pt>
  * @author José Monteiro <Jose.Monteiro@DoWeDo-IT.com>
  */
-$lang['admin_acl']               = 'Gestão de ACLs';
-$lang['acl_group']               = 'Grupo';
-$lang['acl_user']                = 'Utilizador';
-$lang['acl_perms']               = 'Permissão para';
-$lang['page']                    = 'Documento';
-$lang['namespace']               = 'Namespace';
-$lang['acl_perm1']               = 'Ler';
-$lang['acl_perm2']               = 'Editar';
-$lang['acl_perm4']               = 'Criar';
-$lang['acl_perm8']               = 'Carregar';
-$lang['acl_perm16']              = 'Remover';
-$lang['acl_new']                 = 'Adicionar nova entrada';
+$lang['admin_acl']             = 'Gestão de ACLs';
+$lang['acl_group']             = 'Grupo';
+$lang['acl_user']              = 'Utilizador';
+$lang['acl_perms']             = 'Permissão para';
+$lang['page']                  = 'Documento';
+$lang['namespace']             = 'Namespace';
+$lang['acl_perm1']             = 'Ler';
+$lang['acl_perm2']             = 'Editar';
+$lang['acl_perm4']             = 'Criar';
+$lang['acl_perm8']             = 'Carregar';
+$lang['acl_perm16']            = 'Remover';
+$lang['acl_new']               = 'Adicionar nova entrada';
--- lib/plugins/plugin/lang/pt/lang.php
+++ lib/plugins/plugin/lang/pt/lang.php
@@ -4,11 +4,37 @@
  *
  * @author José Monteiro <Jose.Monteiro@DoWeDo-IT.com>
  */
-$lang['menu']                    = 'Gerir Plugins';
-$lang['manage']                  = 'Plugins Instalados';
-$lang['btn_info']                = 'info';
-$lang['btn_update']              = 'actualizar';
-$lang['btn_delete']              = 'remover';
-$lang['btn_settings']            = 'configurações';
-$lang['installed']               = 'Instalado em:';
-$lang['lastupdate']              = 'Actualizado em:';
+$lang['menu']                  = 'Gerir Plugins';
+$lang['download']              = 'Descarregar e instalar um novo plugin';
+$lang['manage']                = 'Plugins Instalados';
+$lang['btn_info']              = 'info';
+$lang['btn_update']            = 'actualizar';
+$lang['btn_delete']            = 'remover';
+$lang['btn_settings']          = 'configurações';
+$lang['btn_download']          = 'Descarregar';
+$lang['btn_enable']            = 'Guardar';
+$lang['installed']             = 'Instalado em:';
+$lang['lastupdate']            = 'Actualizado em:';
+$lang['source']                = 'Fonte:';
+$lang['unknown']               = 'desconhecida';
+$lang['updating']              = 'Actualizando ...';
+$lang['updated']               = 'Plugin %s actualizado com sucesso.';
+$lang['updates']               = 'Os seguintes plguins foram actualizados com sucesso:';
+$lang['update_none']           = 'Não foram encontradas actualizações.';
+$lang['deleting']              = 'Removendo ...';
+$lang['deleted']               = 'Plugin %s removido.';
+$lang['downloading']           = 'Descarregando ...';
+$lang['downloaded']            = 'Plugin %s instalado com sucesso.';
+$lang['downloads']             = 'Os seguintes plguins foram instalados com sucesso:';
+$lang['download_none']         = 'Nenhum plugin encontrado ou ocorreu um problema ao descarregar ou instalar.';
+$lang['components']            = 'Componentes';
+$lang['noinfo']                = 'Este plugin não retornou qualquer informação, pode estar inválido.';
+$lang['name']                  = 'Nome:';
+$lang['date']                  = 'Data:';
+$lang['type']                  = 'Tipo:';
+$lang['desc']                  = 'Descrição:';
+$lang['author']                = 'Autor:';
+$lang['error']                 = 'Ocorreu um erro desconhecido.';
+$lang['error_download']        = 'Impossível descarregar o ficheiro do plugin: %s';
+$lang['error_badurl']          = 'URL suspeito ou errado - impossível determinar o ficheiro a partir do URL';
+$lang['error_dircreate']       = 'Impossível criar pasta temporária para receber os ficheiros a descarregar';
