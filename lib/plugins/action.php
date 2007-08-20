e05d5c7655b4142e92ffbc6b8b0de2cc6354ece1
Michael Klier <chi@chimeric.de>
2007-08-20 19:58:42 +0200

    typo fix in action plugin prototype class
    darcs-hash:20070820175842-23886-3af1d5a1585545b6a84cc8df526f8fe53be537d7.gz

--- lib/plugins/action.php
+++ lib/plugins/action.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Admin Plugin Prototype
+ * Action Plugin Prototype
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Christopher Smith <chris@jalakai.co.uk>
@@ -12,13 +12,15 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_INC.'inc/plugin.php');
 
 /**
- * All DokuWiki plugins to extend the admin function
+ * All DokuWiki plugins to interfere with the event system
  * need to inherit from this class
  */
 class DokuWiki_Action_Plugin extends DokuWiki_Plugin {
 
-  function register($controller) {
-    trigger_error('register() not implemented in '.get_class($this), E_USER_WARNING);
-  }
-
+     /**
+      * Registers a callback function for a given event
+      */
+      function register($controller) {
+        trigger_error('register() not implemented in '.get_class($this), E_USER_WARNING);
+      }
 }
