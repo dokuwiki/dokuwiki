98a80b9578f9a83339e9d41dfac91dc039332f91
Andreas Gohr <andi@splitbrain.org>
2007-09-06 20:30:38 +0200

    Esperanto update
    darcs-hash:20070906183038-7ad00-7bfdf2858d146a17fbe5824773ae5d9c985e0e28.gz

--- lib/plugins/plugin/lang/eo/lang.php
+++ lib/plugins/plugin/lang/eo/lang.php
@@ -5,3 +5,43 @@
  * @author Felipe Castro <fefcas@uol.com.br>
  * @author Felipe Castro <fefcas@gmail.com>
  */
+$lang['menu']                  = 'Administri Kromaĵojn';
+$lang['download']              = 'Elŝuti kaj instali novan kromaĵon';
+$lang['manage']                = 'Instalitaj Kromaĵoj';
+$lang['btn_info']              = 'info';
+$lang['btn_update']            = 'ĝisdatigo';
+$lang['btn_delete']            = 'forigi';
+$lang['btn_settings']          = 'agordoj';
+$lang['btn_download']          = 'Elŝuti';
+$lang['btn_enable']            = 'Registri';
+$lang['url']                   = 'URL';
+$lang['installed']             = 'Instalite:';
+$lang['lastupdate']            = 'Laste ĝisdatigite:';
+$lang['source']                = 'Fonto:';
+$lang['unknown']               = 'nekonate';
+$lang['updating']              = 'Ĝisdatigante ...';
+$lang['updated']               = 'Kromaĵo %s estas sukcese ĝisdatigita';
+$lang['updates']               = 'La jenaj kromaĵoj estas sukcese ĝisdatigitaj';
+$lang['update_none']           = 'Neniu ĝisdatigo troviĝas.';
+$lang['deleting']              = 'Forigante ...';
+$lang['deleted']               = 'Kromaĵo %s estas forigita.';
+$lang['downloading']           = 'Elŝutante ...';
+$lang['downloaded']            = 'La kromaĵo %s estas sukcese instalita';
+$lang['downloads']             = 'La jenaj kromaĵoj estas sukcese instalitaj:';
+$lang['download_none']         = 'Neniu kromaĵo troveblas, aŭ eble okazis nekonata problemo dum elŝuto kaj instalo.';
+$lang['plugin']                = 'Kromaĵo:';
+$lang['components']            = 'Komponeroj';
+$lang['noinfo']                = 'Tiu ĉi kromaĵo liveris neniun informon: eble ĝi ne validas.';
+$lang['name']                  = 'Nomo:';
+$lang['date']                  = 'Dato:';
+$lang['type']                  = 'Tipo:';
+$lang['desc']                  = 'Priskribo:';
+$lang['author']                = 'Aŭtoro:';
+$lang['www']                   = 'Retpaĝo:';
+$lang['error']                 = 'Nekonata eraro okazis.';
+$lang['error_download']        = 'Maleblas elŝuti la dosieron de kromaĵo: %s';
+$lang['error_badurl']          = 'Suspektinda malbona URL - malblas difini la dosieran nomon el la URL';
+$lang['error_dircreate']       = 'Maleblas krei intertempa dosierujo por enhavi elŝutaĵon';
+$lang['error_decompress']      = 'La administrilo de kromaĵoj ne kapablis malkompakti la elŝutitan dosieron. Tio povas esti pro malkompleta elŝuto, tiaokaze vi devus provi refoje; aŭ eble la kompakta formato ne estas konata, tiaokaze vi bezonos elŝuti kaj instali la kromaĵon malaŭtomate.';
+$lang['error_copy']            = 'Okazis eraro de dosierkopio dum provo instali dosierojn por la kromaĵo <em>%s</em>: la disko povus esti plenplena aux alir-rajtoj povus esti misdifinitaj. Tio povus rezulti en malkomplete instalita kromaĵo kaj igi vian vikion malstabilan.';
+$lang['error_delete']          = 'Okazis eraro dum provo forigi la kromaĵon <em>%s</em>. Plejprobable tio sekvas de nesufiĉa rajto por aliri la dosieron aŭ ties ujon.';
--- lib/plugins/revert/lang/eo/lang.php
+++ lib/plugins/revert/lang/eo/lang.php
@@ -5,3 +5,12 @@
  * @author Felipe Castro <fefcas@uol.com.br>
  * @author Felipe Castro <fefcas@gmail.com>
  */
+$lang['menu']                  = 'Administro de Restarigo';
+$lang['filter']                = 'Serĉi spamecajn paĝojn';
+$lang['revert']                = 'Restarigi la elektitajn paĝojn';
+$lang['reverted']              = '%s estas restarigita al revizio %s';
+$lang['removed']               = '%s estas forigita';
+$lang['revstart']              = 'Restariga procezo estas ekita. Tio povas prokrasti longan tempon. Se la skripto tro prokrastos antaŭ plenumo, vi bezonos restarigi po pli etaj blokoj.';
+$lang['revstop']               = 'Renversiga procezo plenumiĝis sukcese.';
+$lang['note1']                 = 'Rimarko: tiu ĉi serĉo distingas usklecon';
+$lang['note2']                 = 'Rimarko: la paĝo restariĝos al la lasta versio ne enhavanta la menditan spaman terminon <i>%s</i>.';
