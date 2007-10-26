279c0a2e9325ca2a3bdf36e8849d132c6ae51559
Frederico Goncalves Guimaraes <frederico@teia.bio.br>
2007-10-26 13:07:11 +0200

    Brasilian Portuguese Update
    darcs-hash:20071026110711-b9abf-3c57eaea66fb90636e51fa9aa03dc7928824fb6f.gz

---
+++ lib/plugins/revert/lang/pt-br/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @author Frederico Gonçalves Guimarães <frederico@teia.bio.br>
+ */
+$lang['menu']                  = 'Gerenciador de reversões';
+$lang['filter']                = 'Procura por páginas com spam';
+$lang['revert']                = 'Reverte as páginas selecionadas';
+$lang['reverted']              = '%s revertida para a revisão %s';
+$lang['removed']               = '% removida';
+$lang['revstart']              = 'O processo de reversão foi iniciado. Isso pode levar muito tempo. Se o tempo de execução do script expirar antes dele encerrar, você deverá tentar novamente usando blocos menores.';
+$lang['revstop']               = 'O processo de reversão terminou com sucesso.';
+$lang['note1']                 = 'Nota: a busca diferencia maiúsculas/minúsculas';
+$lang['note2']                 = 'Nota: a página será revertida para a última versão que não contém o termo de spam <i>%s</i>.';
---
+++ lib/plugins/usermanager/lang/pt-br/lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @author Frederico Gonçalves Guimarães <frederico@teia.bio.br>
+ */
+$lang['menu']                  = 'Gerenciamento de Usuários';
+$lang['noauth']                = '(o gerenciamento de usuários não está disponível)';
+$lang['nosupport']             = '(o gerenciamento de usuários não é suportado)';
+$lang['badauth']               = 'mecanismo de autenticação inválido';
+$lang['user_id']               = 'Usuário';
+$lang['user_pass']             = 'Senha';
+$lang['user_name']             = 'Nome real';
+$lang['user_mail']             = 'E-mail';
+$lang['user_groups']           = 'Grupos';
+$lang['field']                 = 'Campo';
+$lang['value']                 = 'Valor';
+$lang['add']                   = 'Adicionar';
+$lang['delete']                = 'Excluir';
+$lang['delete_selected']       = 'Excluir a seleção';
+$lang['edit']                  = 'Editar';
+$lang['edit_prompt']           = 'Editar esse usuário';
+$lang['modify']                = 'Salvar alterações';
+$lang['search']                = 'Procurar';
+$lang['search_prompt']         = 'Executar a procura';
+$lang['clear']                 = 'Limpar o filtro de procura';
+$lang['filter']                = 'Filtro';
+$lang['summary']               = 'Exibindo usuários %1$d-%2$d de %3$d encontrados. %4$d usuários no total.';
+$lang['nonefound']             = 'Nenhum usuário encontrado. %d usuários no total.';
+$lang['delete_ok']             = '%d usuários excluídos';
+$lang['delete_fail']           = 'Erro na exclusão de %d usuários';
+$lang['update_ok']             = 'Usuário atualizado com sucesso';
+$lang['update_fail']           = 'A atualização do usuário falhou';
+$lang['update_exists']         = 'Não foi possível mudar o nome do usuário. O nome especificado (%s) já existe (as outras mudanças serão aplicadas).';
+$lang['start']                 = 'primeira';
+$lang['prev']                  = 'anterior';
+$lang['next']                  = 'próxima';
+$lang['last']                  = 'última';
+$lang['edit_usermissing']      = 'O usuário selecionado não foi encontrado, ele foi excluído ou o seu nome modificado.';
+$lang['user_notify']           = 'Notificar o usuário';
+$lang['note_notify']           = 'E-mails de notificação são enviados apenas se o usuário forneceu uma senha.';
+$lang['note_group']            = 'Novos usuários serão adicionados ao grupo padrão (%s), caso nenhum grupo seja especificado.';
+$lang['add_ok']                = 'O usuário foi adicionado com sucesso';
+$lang['add_fail']              = 'O usuário não foi adicionado';
+$lang['notify_ok']             = 'O e-mail de notificação foi enviado';
+$lang['notify_fail']           = 'Não foi possível enviar o e-mail de notificação';
