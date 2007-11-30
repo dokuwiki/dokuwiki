3c76c07133226021454b933a6cae686711bb4bc8
Andreas Gohr <andi@splitbrain.org>
2007-12-01 00:33:02 +0100

    Spanish language update
    darcs-hash:20071130233302-7ad00-9cbdf61a70e1a0771bb4ee1a77fc82c3b64dffa1.gz

--- lib/plugins/acl/lang/es/lang.php
+++ lib/plugins/acl/lang/es/lang.php
@@ -5,16 +5,31 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author Miguel Pagano <miguel.pagano@gmail.com>
  * @author Oscar M. Lage <r0sk10@gmail.com>
+ * @author Gabriel Castillo <gch@pumas.ii.unam.mx>
  */
-$lang['admin_acl']               = 'Administración de lista de control de acceso';
-$lang['acl_group']               = 'Grupo';
-$lang['acl_user']                = 'Usuario';
-$lang['acl_perms']               = 'Permiso para';
-$lang['page']                    = 'Página';
-$lang['namespace']               = 'Espacio de nombres';
-$lang['acl_perm1']               = 'Leer';
-$lang['acl_perm2']               = 'Editar';
-$lang['acl_perm4']               = 'Crear';
-$lang['acl_perm8']               = 'Subir un fichero';
-$lang['acl_perm16']              = 'Borrar';
-$lang['acl_new']                 = 'Agregar una nueva entrada';
+$lang['admin_acl']             = 'Administración de lista de control de acceso';
+$lang['acl_group']             = 'Grupo';
+$lang['acl_user']              = 'Usuario';
+$lang['acl_perms']             = 'Permiso para';
+$lang['page']                  = 'Página';
+$lang['namespace']             = 'Espacio de nombres';
+$lang['p_user_id']             = 'El usuario <b class="acluser">%s</b> tiene los siguientes permisos sobre la página <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'El usuario <b class="acluser">%s</b> tiene los siguientes permisos sobre el espacio de nombres <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_group_id']            = 'Los miembros del grupo <b class="aclgroup">%s</b> tiene los siguientes permisos sobre la página <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_ns']            = 'Los miembros del grupo <b class="aclgroup">%s</b> tiene los siguientes permisos sobre el espacio de nombres <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_choose_id']           = 'Por favor <b>proporciones un usuario o grupo</b>en el formulario arriba mostrado para ver o editar los permisos asignados sobre la página<b class="aclpage">%s</b>.';
+$lang['p_choose_ns']           = 'Por favor <b>proporciones un usuario o grupo</b>en el formulario arriba mostrado para ver o editar los permisos asignados sobre el espacio de nombres <b class="aclns">%s</b>.';
+$lang['p_inherited']           = 'Nota: Esos permisos no fueron establecidos explicitamente sino que fueron heredados desde otros grupos o espacios de nombres superiores';
+$lang['p_isadmin']             = 'Nota: El grupo o usuario seleccionado simepres tiene permisos totales debido a que se encuentra configurado como superusuario';
+$lang['current']               = 'Reglas ACL vigentes';
+$lang['where']                 = 'Pagina/Espacio de nombres';
+$lang['who']                   = 'Usuario/Grupo';
+$lang['perm']                  = 'Permisos';
+$lang['acl_perm0']             = 'ninguno';
+$lang['acl_perm1']             = 'Leer';
+$lang['acl_perm2']             = 'Editar';
+$lang['acl_perm4']             = 'Crear';
+$lang['acl_perm8']             = 'Subir un fichero';
+$lang['acl_perm16']            = 'Borrar';
+$lang['acl_new']               = 'Agregar una nueva entrada';
+$lang['acl_mod']               = 'Modificar una entrada';
