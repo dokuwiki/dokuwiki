1f5c3e992aeff190a3952ab49b29bf6c3251e45e
Andreas Gohr <andi@splitbrain.org>
2007-12-04 21:22:46 +0100

    romanian language update
    darcs-hash:20071204202246-7ad00-5ddd212cd7ff6aad243218b3f7e1ca99cb3b8af5.gz

--- lib/plugins/acl/lang/ro/lang.php
+++ lib/plugins/acl/lang/ro/lang.php
@@ -4,3 +4,29 @@
  *
  * @author Sergiu Baltariu <s_baltariu@yahoo.com>
  */
+$lang['admin_acl']             = 'Managementul Listei de Control a Accesului';
+$lang['acl_group']             = 'Grup';
+$lang['acl_user']              = 'Utilizator';
+$lang['acl_perms']             = 'Autorizare pentru';
+$lang['page']                  = 'Pagina';
+$lang['namespace']             = 'Spaţiu de nume';
+$lang['p_user_id']             = 'Utilizatorul <b class="acluser">%s</b> are următoarele autorizaţii pe pagină <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'Utilizatorul <b class="acluser">%s</b> are următoarele autorizaţii pe spaţiul de nume <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_group_id']            = 'Membrii grupului <b class="aclgroup">%s</b> au următoarele autorizaţii pe pagină <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_ns']            = 'Membrii grupului <b class="aclgroup">%s</b> au următoarele autorizaţii pe spaţiul de nume <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_choose_id']           = '<b>Introduceţi un utilizator sau un grup</b> în formularul de mai sus pentru a vizualiza sau edita autorizaţiile paginii <b class="aclpage">%s</b>.';
+$lang['p_choose_ns']           = '<b>Introduceţi un utilizator sau un grup</b> în formularul de mai sus pentru a vizualiza sau edita autorizaţiile spaţiului de nume <b class="aclns">%s</b>.';
+$lang['p_inherited']           = 'Notă: Aceste autorizaţii nu au fost setate explicit ci au fost moştenite de la alte grupuri sau spaţii de nume superioare ierarhic.';
+$lang['p_isadmin']             = 'Notă: Grupul sau utilizatorul selectat are intotdeauna toate autorizatiile întrucât este configurat ca superutilizator.';
+$lang['current']               = 'Reguli ACL actuale';
+$lang['where']                 = 'Pagină/Spaţiu de nume';
+$lang['who']                   = 'Utilizator/Grup';
+$lang['perm']                  = 'Autorizaţii';
+$lang['acl_perm0']             = 'Nici una';
+$lang['acl_perm1']             = 'Citire';
+$lang['acl_perm2']             = 'Editare';
+$lang['acl_perm4']             = 'Creare';
+$lang['acl_perm8']             = 'Încărcare';
+$lang['acl_perm16']            = 'Ştergere';
+$lang['acl_new']               = 'Adaugă intrare nouă';
+$lang['acl_mod']               = 'Modifică intrare';
--- lib/plugins/usermanager/lang/ro/lang.php
+++ lib/plugins/usermanager/lang/ro/lang.php
@@ -4,3 +4,43 @@
  *
  * @author Sergiu Baltariu <s_baltariu@yahoo.com>
  */
+$lang['menu']                  = 'Manager Utilizatori';
+$lang['noauth']                = '(autentificarea utilizatorilor nu este disponibilă)';
+$lang['nosupport']             = '(menegementul utilizatorilor nu e suportat)';
+$lang['badauth']               = 'mecanism de autentificare invalid';
+$lang['user_id']               = 'Utilizator';
+$lang['user_pass']             = 'Parolă';
+$lang['user_name']             = 'Nume Real';
+$lang['user_mail']             = 'Email';
+$lang['user_groups']           = 'Grupuri';
+$lang['field']                 = 'Câmp';
+$lang['value']                 = 'Valoare';
+$lang['add']                   = 'Adaugă';
+$lang['delete']                = 'Şterge';
+$lang['delete_selected']       = 'Şterge selecţia';
+$lang['edit']                  = 'Editează';
+$lang['edit_prompt']           = 'Editează acest utilizator';
+$lang['modify']                = 'Salvează modificările';
+$lang['search']                = 'Caută';
+$lang['search_prompt']         = 'Se caută';
+$lang['clear']                 = 'Resetează Filtrul de Căutare';
+$lang['filter']                = 'Filtru';
+$lang['summary']               = 'Afişarea utilizatorilor %1$d-%2$d din %3$d găsită. %4$d utilizatori în total.';
+$lang['nonefound']             = 'Nici un utilizator nu a fost găsit. %d utilizatori în total.';
+$lang['delete_ok']             = '%d utilizatori şterşi';
+$lang['delete_fail']           = '%d eşuat la ştergere.';
+$lang['update_ok']             = 'Utilizatorul a fost actualizat cu succes';
+$lang['update_fail']           = 'Actualizarea utilizatorului a eşuat';
+$lang['update_exists']         = 'Modificarea numelui de utilizator a eşuat. Numele de utilizator specificat (%s) există deja (orice altă modificare se va aplica)';
+$lang['start']                 = 'început';
+$lang['prev']                  = 'anterior';
+$lang['next']                  = 'urmator';
+$lang['last']                  = 'sfârşit';
+$lang['edit_usermissing']      = 'Utilizatorul selectat nu a fost găsit. E posibil ca numele de utilizator specificat să fi fost şters sau modificat în altă parte.';
+$lang['user_notify']           = 'Notificare utilizator';
+$lang['note_notify']           = 'Emailurile de notificare sunt trimise numai dacă utilizatorului îi este dată o nouă parolă.';
+$lang['note_group']            = 'Noii utilizatori vor fi adăugaţi la grupul implicit (%s) dacă nu se specifică grupul.';
+$lang['add_ok']                = 'Utilizator adăugat cu succes';
+$lang['add_fail']              = 'Adăugarea utilizatorului a eşuat';
+$lang['notify_ok']             = 'Emailul de notificare a fost trimis';
+$lang['notify_fail']           = 'Emailul de notificare nu a putut fi trimis';
