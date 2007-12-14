867121c2a0811559de92ea96c3b5b3286b220675
Andreas Gohr <andi@splitbrain.org>
2007-12-14 11:53:24 +0100

    Romanian language update
    darcs-hash:20071214105324-7ad00-fb59fd40d8c20f2019f3fa769bd229ba18d40903.gz

--- lib/plugins/revert/lang/ro/lang.php
+++ lib/plugins/revert/lang/ro/lang.php
@@ -4,3 +4,12 @@
  *
  * @author Sergiu Baltariu <s_baltariu@yahoo.com>
  */
+$lang['menu']                  = 'Manager reveniri';
+$lang['filter']                = 'Caută pagini cu posibil spam';
+$lang['revert']                = 'Revenire pentru paginile selectate';
+$lang['reverted']              = '%s revenită la versiunea %s';
+$lang['removed']               = '%s eliminată';
+$lang['revstart']              = 'Procesul de revenire a început. Acesta poate dura mult timp.Dacă scriptul expiră înainte de finalizare, trebuie să reveniţi în paşi mai mici.';
+$lang['revstop']               = 'Procesul de revenire s-a finalizat cu succes.';
+$lang['note1']                 = 'Notă: această căutare este sensibilă la majuscule.';
+$lang['note2']                 = 'Notă: pagina va reveni la ultima versiune ce nu conţine termenul de spam <i>%s</i>.';
