2be4553e152086cb657b7f4c103f65d35b64c092
Andreas Gohr <andi@splitbrain.org>
2008-01-04 15:09:51 +0100

    Polish update
    darcs-hash:20080104140951-7ad00-8c563cb30ca014d5ce5f373f660d88204ed70632.gz

--- lib/plugins/acl/lang/pl/lang.php
+++ lib/plugins/acl/lang/pl/lang.php
@@ -5,15 +5,29 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author Grzegorz Żur <grzegorz.zur@gmail.com>
  */
-$lang['admin_acl']               = 'Zarządzanie uprawnieniami';
-$lang['acl_group']               = 'Grupa';
-$lang['acl_user']                = 'Użytkownik';
-$lang['acl_perms']               = 'Uprawnienia użytkownika';
-$lang['page']                    = 'Strona';
-$lang['namespace']               = 'Katalog';
-$lang['acl_perm1']               = 'Czytanie';
-$lang['acl_perm2']               = 'Zmiana';
-$lang['acl_perm4']               = 'Tworzenie';
-$lang['acl_perm8']               = 'Przesyłanie plików';
-$lang['acl_perm16']              = 'Usuwanie';
-$lang['acl_new']                 = 'Dodaj nowy wpis';
+$lang['admin_acl']             = 'Zarządzanie uprawnieniami';
+$lang['acl_group']             = 'Grupa';
+$lang['acl_user']              = 'Użytkownik';
+$lang['acl_perms']             = 'Uprawnienia użytkownika';
+$lang['page']                  = 'Strona';
+$lang['namespace']             = 'Katalog';
+$lang['p_user_id']             = 'Użytkownik <b class="acluser">%s</b> posiada następujące uprawnienia do strony <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'Użytkownik <b class="acluser">%s</b> posiada następujące uprawnienia do katalogów <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_group_id']            = 'Członkowie grupy <b class="aclgroup">%s</b> posiadają następujące uprawnienia do strony <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_ns']            = 'Członkowie grupy <b class="aclgroup">%s</b> posiadają następujące uprawnienia do strony <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_choose_id']           = 'Podaj <b>nazwę użytkownika lub grupy</b> w powyższym formularzu, by wyświetlić lub zmienić uprawnienia do strony <b class="aclpage">%s</b>.';
+$lang['p_choose_ns']           = 'Podaj <b>nazwę użytkownika lub grupy</b> w powyższym formularzu, by wyświetlić lub zmienić uprawnienia do katalogu <b class="aclns">%s</b>.';
+$lang['p_inherited']           = 'Uwaga: Uprawnienia nie zostały nadane wprost ale są dziedziczone z grupy lub katalogu.';
+$lang['p_isadmin']             = 'Uwaga: Wybrana grupa lub użytkownika zawsze dysponuje pełnymi uprawnieniami ponieważ posiada uprawnienia administratora.';
+$lang['current']               = 'Aktywne reguły zarządzania uprawnieniami';
+$lang['where']                 = 'Strona/Katalog';
+$lang['who']                   = 'Użytkownik/Grupa';
+$lang['perm']                  = 'Uprawnienie';
+$lang['acl_perm0']             = 'Żadne';
+$lang['acl_perm1']             = 'Czytanie';
+$lang['acl_perm2']             = 'Zmiana';
+$lang['acl_perm4']             = 'Tworzenie';
+$lang['acl_perm8']             = 'Przesyłanie plików';
+$lang['acl_perm16']            = 'Usuwanie';
+$lang['acl_new']               = 'Dodaj nowy wpis';
+$lang['acl_mod']               = 'Zmień wpis';
--- lib/plugins/revert/lang/pl/lang.php
+++ lib/plugins/revert/lang/pl/lang.php
@@ -3,12 +3,12 @@
  * polish language file
  * @author Grzegorz Żur <grzegorz.zur@gmail.com>
  */
-$lang['menu']                    = 'Menadżer przywracania';
-$lang['filter']                  = 'Wyszukaj uszkodzone strony';
-$lang['revert']                  = 'Napraw zaznaczone strony';
-$lang['reverted']                = 'Stronę %s zastąpiono wersją %s';
-$lang['removed']                 = 'Stronę %s usunięto';
-$lang['revstart']                = 'Naprawa rozpoczęta. To może zająć kilka minut. Jeśli strona przestanie się ładować, spróbuj ponownie zaznaczając mniejszą liczbę stron.';
-$lang['revstop']                 = 'Naprawa zakończona pomyślnie!';
-$lang['note1']                   = 'Uwaga: duże i małe litery są rozróżniane';
-$lang['note2']                   = 'Uwaga: zostanie przywrócona ostatnia wersja strony niezawierająca wyrażenia <i>%s</i>.';
+$lang['menu']                  = 'Menadżer przywracania';
+$lang['filter']                = 'Wyszukaj uszkodzone strony';
+$lang['revert']                = 'Napraw zaznaczone strony';
+$lang['reverted']              = 'Stronę %s zastąpiono wersją %s';
+$lang['removed']               = 'Stronę %s usunięto';
+$lang['revstart']              = 'Naprawa rozpoczęta. To może zająć kilka minut. Jeśli strona przestanie się ładować, spróbuj ponownie zaznaczając mniejszą liczbę stron.';
+$lang['revstop']               = 'Naprawa zakończona pomyślnie!';
+$lang['note1']                 = 'Uwaga: duże i małe litery są rozróżniane';
+$lang['note2']                 = 'Uwaga: zostanie przywrócona ostatnia wersja strony niezawierająca wyrażenia <i>%s</i>.';
