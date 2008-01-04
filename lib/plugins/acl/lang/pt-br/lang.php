6a18a7ed5e278e1670ab92be9ed024145196536e
Andreas Gohr <andi@splitbrain.org>
2008-01-04 15:07:33 +0100

    Brazilian Portuguese update
    darcs-hash:20080104140733-7ad00-e60bf288ff2d3c22b010e3e509c009c3af339b65.gz

--- lib/plugins/acl/lang/pt-br/lang.php
+++ lib/plugins/acl/lang/pt-br/lang.php
@@ -3,20 +3,33 @@
  * Brazilian Portuguese language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author    Luis Fernando Enciso <lfenciso@certto.com.br>
- * @author    Alauton/Loug
+ * @author Luis Fernando Enciso <lfenciso@certto.com.br>
+ * @author Alauton/Loug
+ * @author Frederico Gonçalves Guimarães <frederico@teia.bio.br>
  */
-
-$lang['admin_acl']  = 'Administrar a Lista de Controles de Acesso';
-$lang['acl_group']  = 'Grupo';
-$lang['acl_user']   = 'Usuário';
-$lang['acl_perms']  = 'Permissões para';
-$lang['page']       = 'Página';
-$lang['namespace']  = 'Prefixo';
-
-$lang['acl_perm1']  = 'Ler';
-$lang['acl_perm2']  = 'Editar';
-$lang['acl_perm4']  = 'Criar';
-$lang['acl_perm8']  = 'Upload';
-$lang['acl_perm16'] = 'Deletar';
-$lang['acl_new']    = 'Adicionar nova entrada';
+$lang['admin_acl']             = 'Administração da Lista de Controles de Acesso';
+$lang['acl_group']             = 'Grupo';
+$lang['acl_user']              = 'Usuário';
+$lang['acl_perms']             = 'Permissões para';
+$lang['page']                  = 'Página';
+$lang['namespace']             = 'Ramo';
+$lang['p_user_id']             = 'O usuário <b class="acluser">%s</b> possui as seguintes permissões na página <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'O usuário <b class="acluser">%s</b> possui as seguintes permissões no ramo <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_group_id']            = 'Os membros do grupo <b class="aclgroup">%s</b> possuem as seguintes permissões na página <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_ns']            = 'Os membros do grupo <b class="aclgroup">%s</b> possuem as seguintes permissões no ramo <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_choose_id']           = 'Por favor <b>digite um usuário ou grupo</b> no formulário acima para ver ou editar as permissões para a  página <b class="aclpage">%s</b>.';
+$lang['p_choose_ns']           = 'Por favor <b>digite um usuário ou grupo</b> no formulário acima para ver ou editar as permissões para o ramo <b class="aclpage">%s</b>.';
+$lang['p_inherited']           = 'Nota: Essas permissões não foram definidas explicitamente, mas sim herdadas de outros grupos ou ramos superiores.';
+$lang['p_isadmin']             = 'Nota: O grupo ou usuário selecionado sempre tem permissões completas, porque ele está configurado como superusuário.';
+$lang['current']               = 'Regras atuais da ACL';
+$lang['where']                 = 'Página/Ramo';
+$lang['who']                   = 'Usuário/Grupo';
+$lang['perm']                  = 'Permissões';
+$lang['acl_perm0']             = 'Nenhum';
+$lang['acl_perm1']             = 'Ler';
+$lang['acl_perm2']             = 'Editar';
+$lang['acl_perm4']             = 'Criar';
+$lang['acl_perm8']             = 'Upload';
+$lang['acl_perm16']            = 'Deletar';
+$lang['acl_new']               = 'Adicionar nova entrada';
+$lang['acl_mod']               = 'Modificar a entrada';
