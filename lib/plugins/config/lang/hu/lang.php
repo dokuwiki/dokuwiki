d44d2665a6769bdd87c8d68d00336dfaac043abd
Andreas Gohr <andi@splitbrain.org>
2008-01-30 23:00:34 +0100

    Hungarian update
    darcs-hash:20080130220034-7ad00-aa23168b9d0bbfc384f4944633d5adbce15c296b.gz

--- lib/plugins/config/lang/hu/lang.php
+++ lib/plugins/config/lang/hu/lang.php
@@ -4,3 +4,29 @@
  *
  * @author Sandor TIHANYI <stihanyi@gmail.com>
  */
+$lang['menu']                  = 'Beállító Központ';
+$lang['error']                 = 'Helytelen érték miatt a módosítások nem mentődtek. Nézd át a módosításokat, és ments újra.
+<br />A helytelen érték(ek)et piros kerettel jelöljük.';
+$lang['updated']               = 'A módosítások sikeresen beállítva.';
+$lang['nochoice']              = '(nincs egyéb lehetőség)';
+$lang['locked']                = 'A beállításokat tartalmazó fájlt nem tudtam frissíteni.<br />
+Nézd meg, hogy a fájl neve és jogosultságai helyesen vannak-e beállítva!';
+$lang['_configuration_manager'] = 'Beállító Központ';
+$lang['_header_dokuwiki']      = 'DokuWiki beállítások';
+$lang['_header_plugin']        = 'Bővítmények beállításai';
+$lang['_header_template']      = 'Sablon beállítások';
+$lang['_header_undefined']     = 'Nem definiált értékek';
+$lang['_basic']                = 'Alap beállítások';
+$lang['_display']              = 'Megjelenítés beállításai';
+$lang['_authentication']       = 'Azonosítás beállításai';
+$lang['_anti_spam']            = 'Annti-Spam beállítások';
+$lang['_editing']              = 'Szerkesztési beállítások';
+$lang['_links']                = 'Link beállítások';
+$lang['_media']                = 'Media beállítások';
+$lang['_advanced']             = 'Haladó beállítások';
+$lang['_network']              = 'Hálózati beállítások';
+$lang['_plugin_sufix']         = 'Bővítmények beállításai';
+$lang['_template_sufix']       = 'Sablon beállítások';
+$lang['_msg_setting_undefined'] = 'Nincs beállított meta-adat.';
+$lang['_msg_setting_no_class'] = 'Nincs beállított osztály.';
+$lang['_msg_setting_no_default'] = 'Nincs alapértelmezett érték.';
--- lib/plugins/revert/lang/hu/lang.php
+++ lib/plugins/revert/lang/hu/lang.php
@@ -4,3 +4,11 @@
  *
  * @author Sandor TIHANYI <stihanyi@gmail.com>
  */
+$lang['menu']                  = 'Visszaállítás kezelő (anti-SPAM)';
+$lang['filter']                = 'SPAM tartalmú oldalak keresése';
+$lang['revert']                = 'Kiválasztott oldalak visszaállítása';
+$lang['reverted']              = '%s a következő változatra lett visszaállítva: %s';
+$lang['removed']               = '%s törölve';
+$lang['revstop']               = 'A visszaállítási folyamat sikeresen befejeződött.';
+$lang['note1']                 = 'Megjegyzés: a keresés kisbetű-nagybetűre érzékeny';
+$lang['note2']                 = 'Megjegyzés: Az oldalt az utolsó olyan változatra állítjuk vissza, ami nem tartalmazza a megadott spam kifejezést: <i>%s</i>.';
