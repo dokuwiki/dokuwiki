838fc1b345833427b6e92980675c04efe5969257
Andreas Gohr <andi@splitbrain.org>
2008-03-09 18:35:38 +0100

    make sure there are no leading or trailing pipes on search highlight regexp
    darcs-hash:20080309173538-7ad00-07893a25205632ff4bc5d8b90595e7d1c9e49ff4.gz

--- inc/html.php
+++ inc/html.php
@@ -265,6 +265,7 @@ function html_draft(){
 function html_hilight($html,$regex){
   // strip everything that's special except pipes:
   $regex = preg_replace('![\[\]()/\\\\?\.+*]+!','',$regex);
+  $regex = trim($regex,' |');
 
   if ($regex === '') return $html;
   $html = preg_replace_callback("/((<[^>]*)|$regex)/ui",'html_hilight_callback',$html);
