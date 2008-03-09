ada32d75817221b0570a12ffe885830dc7d19554
Guy Brand <gb@isis.u-strasbg.fr>
2008-03-09 21:15:27 +0100

    Make config plugin aware of NS subscriptions and superuser/manager's list
    darcs-hash:20080309201527-19e2d-35f2f183214981ee3532e210f68fe8a5d6121c36.gz

--- lib/plugins/config/settings/config.metadata.php
+++ lib/plugins/config/settings/config.metadata.php
@@ -116,8 +116,8 @@ $meta['manager']     = array('string');
 $meta['profileconfirm'] = array('onoff');
 $meta['registernotify'] = array('email');
 $meta['disableactions'] = array('disableactions',
-                                '_choices' => array('backlink','index','recent','revisions','search','subscription','register','resendpwd','profile','edit','wikicode','check'),
-                                '_combine' => array('subscription' => array('subscribe','unsubscribe'), 'wikicode' => array('source','export_raw')));
+                                '_choices' => array('backlink','index','recent','revisions','search','subscription','nssubscription','register','resendpwd','profile','edit','wikicode','check'),
+                                '_combine' => array('subscription' => array('subscribe','unsubscribe'), 'wikicode' => array('source','export_raw'), 'nssubscription' => array('subscribens','unsubscribens')));
 $meta['sneaky_index'] = array('onoff');
 $meta['auth_security_timeout'] = array('numeric');
 
