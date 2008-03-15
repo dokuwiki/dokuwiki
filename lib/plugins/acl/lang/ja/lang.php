c06cd517522c06ce1095926770bef7a66cd2714e
Andreas Gohr <andi@splitbrain.org>
2008-03-15 18:55:15 +0100

    Japanese Language update
    darcs-hash:20080315175515-7ad00-a05574659f7595727825e22fa8768d208c1e16be.gz

--- lib/plugins/acl/lang/ja/lang.php
+++ lib/plugins/acl/lang/ja/lang.php
@@ -6,15 +6,29 @@
  * @author Davilin(Yuji Takenaka) <webmaster@davilin.com>
  * @author Yuji Takenaka <webmaster@davilin.com>
  */
-$lang['admin_acl']               = 'アクセスコントロール管理';
-$lang['acl_group']               = 'グループ';
-$lang['acl_user']                = 'ユーザー';
-$lang['acl_perms']               = '権限を追加';
-$lang['page']                    = '文書';
-$lang['namespace']               = '名前空間';
-$lang['acl_perm1']               = '読取';
-$lang['acl_perm2']               = '編集';
-$lang['acl_perm4']               = '作成';
-$lang['acl_perm8']               = 'アップロード';
-$lang['acl_perm16']              = '削除';
-$lang['acl_new']                 = '新規エントリ';
+$lang['admin_acl']             = 'アクセスコントロール管理';
+$lang['acl_group']             = 'グループ';
+$lang['acl_user']              = 'ユーザー';
+$lang['acl_perms']             = '権限を追加';
+$lang['page']                  = '文書';
+$lang['namespace']             = '名前空間';
+$lang['p_user_id']             = 'ユーザー <b class="acluser">%s</b> は、ページ <b class="aclpage">%s</b> に対して次の権限を持っています: <i>%s</i>';
+$lang['p_user_ns']             = 'ユーザー <b class="acluser">%s</b> は、名前空間 <b class="aclns">%s</b> に対して次の権限を持っています: <i>%s</i>';
+$lang['p_group_id']            = 'グループ <b class="aclgroup">%s</b> のメンバーは、ページ <b class="aclpage">%s</b> に対して次の権限を持っています: <i>%s</i>';
+$lang['p_group_ns']            = 'グループ <b class="aclgroup">%s</b> のメンバーは、名前空間 <b class="aclns">%s</b> に対して次の権限を持っています: <i>%s</i>';
+$lang['p_choose_id']           = 'ページ <b class="aclpage">%s</b> にセットされた権限を閲覧・編集するためには、上記のフォームに<b>ユーザー名もしくはグループ名を入力して下さい</b>。';
+$lang['p_choose_ns']           = '名前空間 <b class="aclns">%s</b> にセットされた権限を閲覧・編集するためには、上記のフォームに<b>ユーザー名もしくはグループ名を入力して下さい</b>。';
+$lang['p_inherited']           = '注意：これらの権限は明示されていませんが、他のグループもしくは上位の名前空間の権限を継承します。';
+$lang['p_isadmin']             = '注意：選択したグループもしくはユーザーはスーパーユーザーであるため、全ての権限があります。';
+$lang['current']               = '現在のACLルール';
+$lang['where']                 = 'ページ/名前空間';
+$lang['who']                   = 'ユーザー/グループ';
+$lang['perm']                  = '権限';
+$lang['acl_perm0']             = '無し';
+$lang['acl_perm1']             = '読取';
+$lang['acl_perm2']             = '編集';
+$lang['acl_perm4']             = '作成';
+$lang['acl_perm8']             = 'アップロード';
+$lang['acl_perm16']            = '削除';
+$lang['acl_new']               = '新規エントリ';
+$lang['acl_mod']               = 'エントリの編集';
