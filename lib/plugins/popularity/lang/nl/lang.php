17784d211c5d785ad88303ff15a6dc2f56b554f3
Andreas Gohr <andi@splitbrain.org>
2008-03-18 22:37:23 +0100

    Dutch language update
    darcs-hash:20080318213723-7ad00-39a2120d5265f622c2ca4ac10aaa421ac00480e6.gz

---
+++ lib/plugins/popularity/lang/nl/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Dutch language file
+ *
+ * @author Wouter Schoot <wouter@schoot.org>
+ */
+$lang['name']                  = 'Populariteits terugkoppeling (dit kan even duren om in te laden)';
+$lang['submit']                = 'Verstuur';
