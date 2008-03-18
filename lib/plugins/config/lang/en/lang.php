03305f56688ad6277968f7d43fbd491d20ee060d
Andreas Gohr <andi@splitbrain.org>
2008-03-18 21:00:05 +0100

    Typo fix
    darcs-hash:20080318200005-7ad00-aacfd0598c21b7e6d21564d40ccd09872a817b22.gz

--- lib/plugins/config/lang/en/lang.php
+++ lib/plugins/config/lang/en/lang.php
@@ -206,6 +206,6 @@ $lang['compression_o_bz2'] = 'bz2';
 
 /* xsendfile header */
 $lang['xsendfile_o_0'] = "don't use";
-$lang['xsendfile_o_1'] = 'Propritary lighttpd header (before release 1.5)';
+$lang['xsendfile_o_1'] = 'Proprietary lighttpd header (before release 1.5)';
 $lang['xsendfile_o_2'] = 'Standard X-Sendfile header';
-$lang['xsendfile_o_3'] = 'Propritary Nginx X-Accel-Redirect header';
+$lang['xsendfile_o_3'] = 'Proprietary Nginx X-Accel-Redirect header';
