b5279fd6c85a90506ab6ad55bb3493533903d773
Andreas Gohr <andi@splitbrain.org>
2008-03-18 22:40:14 +0100

    Galician language update
    darcs-hash:20080318214014-7ad00-6699be7fd768f18ac727f4e7329bdae80be37d09.gz

---
+++ lib/plugins/popularity/lang/gl/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Galician language file
+ *
+ * @author CiberIrmandade da Fala <infoxeral@ciberirmandade.org>
+ */
+$lang['name']                  = 'Resposta de Popularidade (pode demorar un tempo a cargar)';
+$lang['submit']                = 'Enviar Datos';
