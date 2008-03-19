1775272d5c09cf755dc3bcc9f40521a82ff7cce1
Andreas Gohr <andi@splitbrain.org>
2008-03-19 20:11:19 +0100

    Brazilian Portuguese language update
    darcs-hash:20080319191119-7ad00-bba4a993f391da855452b5fa9fe37b25cb3183cf.gz

---
+++ lib/plugins/popularity/lang/pt-br/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Portuguese language file
+ *
+ * @author Frederico Gonçalves Guimarães <frederico@teia.bio.br>
+ */
+$lang['name']                  = 'Retorno de popularidade (pode demorar um pouco para carregar)';
+$lang['submit']                = 'Enviar dados';
