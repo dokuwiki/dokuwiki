30ae9eb68632f1670094ba56e4bfe158eb3f12c0
Andreas Gohr <andi@splitbrain.org>
2008-03-19 20:08:38 +0100

    Esperanto language update
    darcs-hash:20080319190838-7ad00-79155bb0dc3060077a86a5e7f8946a6dea43f9c0.gz

---
+++ lib/plugins/popularity/lang/eo/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Esperanto language file
+ *
+ * @author Felipo Kastro <fefcas@gmail.com>
+ */
+$lang['name']                  = 'Populareca Esploro (eble la ŝargo prokrastos iomete)';
+$lang['submit']                = 'Sendi Datenaron';
