ba0b2baf89f088dfb447115f8fbc5d0c8c041395
Andreas Gohr <andi@splitbrain.org>
2008-03-21 00:21:36 +0100

    Hungarian language update
    darcs-hash:20080320232136-7ad00-7bc11cc69cbbc3627e36ee208c728a548ad2e765.gz

---
+++ lib/plugins/popularity/lang/hu/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Hungarian language file
+ *
+ * @author Sandor TIHANYI <stihanyi@gmail.com>
+ */
+$lang['name']                  = 'Visszajelzés a DokuWiki használatáról (sok időt vehet igénybe a betöltése)';
+$lang['submit']                = 'Adatok elküldése';
