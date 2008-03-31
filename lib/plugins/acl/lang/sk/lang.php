7e089c87a86bb19bfcb72fe835f4cfd3984eb992
Andreas Gohr <andi@splitbrain.org>
2008-03-31 23:04:52 +0200

    Slovak language update
    darcs-hash:20080331210452-7ad00-107e48033c010cc6c322f35e98822812a1edf7da.gz

--- lib/plugins/acl/lang/sk/lang.php
+++ lib/plugins/acl/lang/sk/lang.php
@@ -4,16 +4,23 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author Ondrej Vegh <ov@vsieti.sk>
+ * @author Michal Mesko <michal.mesko@gmail.com>
  */
-$lang['admin_acl']               = 'Správa zoznamu prístupových práv';
-$lang['acl_group']               = 'Skupina';
-$lang['acl_user']                = 'Užívateľ';
-$lang['acl_perms']               = 'Práva pre';
-$lang['page']                    = 'Stránka';
-$lang['namespace']               = 'Menný priestor';
-$lang['acl_perm1']               = 'Čítať';
-$lang['acl_perm2']               = 'Zmeniť';
-$lang['acl_perm4']               = 'Vytvoriť';
-$lang['acl_perm8']               = 'Nahrať súbor';
-$lang['acl_perm16']              = 'Zmazať';
-$lang['acl_new']                 = 'Pridať novú položku';
+$lang['admin_acl']             = 'Správa zoznamu prístupových práv';
+$lang['acl_group']             = 'Skupina';
+$lang['acl_user']              = 'Užívateľ';
+$lang['acl_perms']             = 'Práva pre';
+$lang['page']                  = 'Stránka';
+$lang['namespace']             = 'Menný priestor';
+$lang['current']               = 'Aktuálne pravidlá prístupu (ACL)';
+$lang['where']                 = 'Stránka/Menný priestor';
+$lang['who']                   = 'Používateľ/Skupina';
+$lang['perm']                  = 'Povolenia';
+$lang['acl_perm0']             = 'Žiadne';
+$lang['acl_perm1']             = 'Čítať';
+$lang['acl_perm2']             = 'Zmeniť';
+$lang['acl_perm4']             = 'Vytvoriť';
+$lang['acl_perm8']             = 'Nahrať súbor';
+$lang['acl_perm16']            = 'Zmazať';
+$lang['acl_new']               = 'Pridať nový záznam';
+$lang['acl_mod']               = 'Upraviť záznam';
---
+++ lib/plugins/popularity/lang/sk/lang.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Slovak language file
+ *
+ * @author Michal Mesko <michal.mesko@gmail.com>
+ */
+$lang['submit']                = 'Poslať dáta';
