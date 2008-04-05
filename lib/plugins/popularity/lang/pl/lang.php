26af6668e7516717c25f3752b09b8ed21eb301f3
Andreas Gohr <andi@splitbrain.org>
2008-04-05 22:30:30 +0200

    Polish language update
    darcs-hash:20080405203030-7ad00-e75be6c0466b2fef950d92c7aa183e8ef39fd927.gz

---
+++ lib/plugins/popularity/lang/pl/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Polish language file
+ *
+ * @author Grzegorz Żur <grzegorz.zur@gmail.com>
+ */
+$lang['name']                  = 'Informacja o popularności (ładowanie może zająć dłuższą chwilę)';
+$lang['submit']                = 'Wyślij dane';
