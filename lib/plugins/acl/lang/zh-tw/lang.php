e8c699db4de3456233b0561e0a4140fd89edb1b2
Andreas Gohr <andi@splitbrain.org>
2008-05-13 22:31:26 +0200

    Chinese language update
    darcs-hash:20080513203126-7ad00-d947d2d0d168be4321d90c094c7d70d28cb47d7a.gz

--- lib/plugins/acl/lang/zh-tw/lang.php
+++ lib/plugins/acl/lang/zh-tw/lang.php
@@ -3,21 +3,27 @@
  * Chinese(Traditional) language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     chinsan <chinsan@mail2000.com.tw>
+ * @author chinsan <chinsan@mail2000.com.tw>
+ * @author Li-Jiun Huang <ljhuang.tw@gmail.com>
  */
-
-$lang['admin_acl']  = '設定 ACL 存取名單';
-$lang['acl_group']  = '群組';
-$lang['acl_user']   = '帳號';
-$lang['acl_perms']  = '設定權限於';
-$lang['page']       = 'Page';
-$lang['namespace']  = 'Namespace';
-
-$lang['acl_perm1']  = '讀取權限';
-$lang['acl_perm2']  = '編輯頁面';
-$lang['acl_perm4']  = '新增頁面';
-$lang['acl_perm8']  = '上傳圖檔';
-$lang['acl_perm16'] = '刪除檔案';
-$lang['acl_new']    = '新增管理規則';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['admin_acl']             = '設定 ACL 存取名單';
+$lang['acl_group']             = '群組';
+$lang['acl_user']              = '帳號';
+$lang['acl_perms']             = '設定權限於';
+$lang['page']                  = 'Page';
+$lang['namespace']             = 'Namespace';
+$lang['btn_select']            = '選擇';
+$lang['p_user_id']             = '使用者 <b class="acluser">%s</b> 在頁面 <b class="aclpage">%s</b>目前擁有以下的權限: <i>%s</i>.';
+$lang['p_group_id']            = '群組 <b class="aclgroup">%s</b> 的成員目前對於頁面 <b class="aclpage">%s</b> 擁有以下的權限: <i>%s</i>.';
+$lang['p_choose_id']           = '請在上方的表格中 <b>輸入一個帳號或群組</b> 來觀看或編輯頁面 <b class="aclpage">%s</b> 的權限.';
+$lang['current']               = '目前的ACL規則';
+$lang['who']                   = '使用者/群組';
+$lang['perm']                  = '權限';
+$lang['acl_perm0']             = '無';
+$lang['acl_perm1']             = '讀取權限';
+$lang['acl_perm2']             = '編輯頁面';
+$lang['acl_perm4']             = '新增頁面';
+$lang['acl_perm8']             = '上傳圖檔';
+$lang['acl_perm16']            = '刪除檔案';
+$lang['acl_new']               = '新增管理規則';
+$lang['acl_mod']               = '修改規則';
