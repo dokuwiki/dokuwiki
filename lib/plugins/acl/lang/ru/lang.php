63d6ac6da587192d955ffba89b955cff006a7373
Andreas Gohr <andi@splitbrain.org>
2008-05-13 22:36:42 +0200

    Russian language update
    darcs-hash:20080513203642-7ad00-a78526ae0edf0f95e7ba4eb0dedbc435ea3f36e4.gz

--- lib/plugins/acl/lang/ru/lang.php
+++ lib/plugins/acl/lang/ru/lang.php
@@ -3,21 +3,32 @@
  * Russian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author    Denis Simakov <akinoame1@gmail.com>
+ * @author Denis Simakov <akinoame1@gmail.com>
  */
-
-$lang['admin_acl']  = 'Управление списками контроля доступа';
-$lang['acl_group']  = 'Группа';
-$lang['acl_user']   = 'Пользователь';
-$lang['acl_perms']  = 'Права доступа для';
-$lang['page']       = 'Страница';
-$lang['namespace']  = 'Пространство имен';
-
-$lang['acl_perm1']  = 'Чтение';
-$lang['acl_perm2']  = 'Правка';
-$lang['acl_perm4']  = 'Создание';
-$lang['acl_perm8']  = 'Загрузка файлов';
-$lang['acl_perm16'] = 'Удаление';
-$lang['acl_new']    = 'Добавить новую запись';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['admin_acl']             = 'Управление списками контроля доступа';
+$lang['acl_group']             = 'Группа';
+$lang['acl_user']              = 'Пользователь';
+$lang['acl_perms']             = 'Права доступа для';
+$lang['page']                  = 'Страница';
+$lang['namespace']             = 'Пространство имен';
+$lang['btn_select']            = 'Выбрать';
+$lang['p_user_id']             = 'Сейчас пользователь <b class="acluser">%s</b> имеет следующие права на доступ к странице <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'Сейчас пользователь <b class="acluser">%s</b> имеет следующие права на доступ к пространству имён <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_group_id']            = 'Сейчас члены группы <b class="aclgroup">%s</b> имеют следующие права на доступ к странице <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_ns']            = 'Сейчас члены группы <b class="aclgroup">%s</b> cимеют следующие права на доступ к пространству имён <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['p_choose_id']           = 'Пожалуйста, <b>введите пользователя или группу</b> в форме выше, чтобы просмотреть или отредактировать права на доступ к странице <b class="aclpage">%s</b>.';
+$lang['p_choose_ns']           = 'Пожалуйста, <b>введите пользователя или группу</b> в форме выше, чтобы просмотреть или отредактировать права на доступ к пространству имён <b class="aclns">%s</b>.';
+$lang['p_inherited']           = 'Замечание: Эти права доступа не были заданы явно, а были унаследованы от других групп или пространств имён более высокого порядка.';
+$lang['p_isadmin']             = 'Замечание: Выбранный пользователь всегда имеет полные права так, как он является суперпользователем.';
+$lang['current']               = 'Текущие права ACL';
+$lang['where']                 = 'Страница/Пространство имён';
+$lang['who']                   = 'Пользователь/Группа';
+$lang['perm']                  = 'Права доступа';
+$lang['acl_perm0']             = 'Замечание';
+$lang['acl_perm1']             = 'Чтение';
+$lang['acl_perm2']             = 'Правка';
+$lang['acl_perm4']             = 'Создание';
+$lang['acl_perm8']             = 'Загрузка файлов';
+$lang['acl_perm16']            = 'Удаление';
+$lang['acl_new']               = 'Добавить новую запись';
+$lang['acl_mod']               = 'Отредактировать запись';
