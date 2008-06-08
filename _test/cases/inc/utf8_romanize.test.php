b312ac050bd5bf1498dc74b918b430acb116eaf9
Andreas Gohr <andi@splitbrain.org>
2008-06-08 12:09:06 +0200

    class redeclaration error in test cases fixed
    darcs-hash:20080608100906-7ad00-f88da77c1b33641e54672a6e020dc25f6c5bd6f2.gz

--- _test/cases/inc/utf8_romanize.test.php
+++ _test/cases/inc/utf8_romanize.test.php
@@ -3,7 +3,7 @@
 if(!defined('UTF8_NOMBSTRING')) define('UTF8_NOMBSTRING',1);
 require_once DOKU_INC.'inc/utf8.php';
 
-class utf8_substr_test extends UnitTestCase {
+class utf8_romanize_test extends UnitTestCase {
 
     /**
      * Check Japanese romanization
