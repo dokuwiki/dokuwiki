5ea6676b927dd8a117d9af40207e132ad6d1d97b
Ivan Petrovic <ivan.petrovic@boxgroup.net>
2008-07-23 20:01:01 +0200

    Serbian language update
    darcs-hash:20080723180101-351b5-65ddcd688b52a185fe63c88f16535029a240d6da.gz

--- lib/plugins/acl/lang/sr/lang.php
+++ lib/plugins/acl/lang/sr/lang.php
@@ -3,21 +3,33 @@
  * serbian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Filip Brcic <brcha@users.sourceforge.net>
+ * @author Filip Brcic <brcha@users.sourceforge.net>
+ * @author Иван Петровић petrovicivan@ubuntusrbija.org
  */
-
-$lang['admin_acl']  = 'Управљање листом контроле приступа';
-$lang['acl_group']  = 'Група';
-$lang['acl_user']   = 'Корисник';
-$lang['acl_perms']  = 'Дозволе за';
-$lang['page']       = 'Страница';
-$lang['namespace']  = 'Именски простор';
-
-$lang['acl_perm1']  = 'Читање';
-$lang['acl_perm2']  = 'Измена';
-$lang['acl_perm4']  = 'Прављење';
-$lang['acl_perm8']  = 'Слање';
-$lang['acl_perm16'] = 'Брисање';
-$lang['acl_new']    = 'Додај нови унос';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['admin_acl']             = 'Управљање листом контроле приступа';
+$lang['acl_group']             = 'Група';
+$lang['acl_user']              = 'Корисник';
+$lang['acl_perms']             = 'Дозволе за';
+$lang['page']                  = 'Страница';
+$lang['namespace']             = 'Именски простор';
+$lang['btn_select']            = 'Одабери';
+$lang['p_user_id']             = 'Корисник <b class="acluser">%s</b> тренутно има следеће дозволе за ову страницу <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'Корисник <b class="acluser">%s</b> тренутно има следеће дозволе за овај именски простор <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_id']            = 'Чланови групе <b class="acluser">%s</b> тренутно имају следеће дозволе за ову страницу <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_group_ns']            = 'Чланови групе <b class="acluser">%s</b> тренутно имају следеће дозволе за овај именски простор <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_choose_id']           = 'Молим Вас <b>унесите корисника или групу</b> у формулар изнад да бисте приказали или изменили дозволе за страницу <b class="aclpage">%s</b>.';
+$lang['p_choose_ns']           = 'Молим Вас <b>унесите корисника или групу</b> у формулар изнад да бисте приказали или изменили дозволе за именски простор <b class="aclpage">%s</b>.';
+$lang['p_inherited']           = 'Напомена: Ове дозволе се не постављају експлицитно већ само тамо где се не сударају са осталим групама или вишем иманском простору.';
+$lang['p_isadmin']             = 'Напомена: Одабран корисник или група има увек пуне дозволе јер је постављен за суперкорисника.';
+$lang['current']               = 'Тренутна правила проступа';
+$lang['where']                 = 'Страница/Именски простор';
+$lang['who']                   = 'Корисник/Група';
+$lang['perm']                  = 'Дозволе';
+$lang['acl_perm0']             = 'Ништа';
+$lang['acl_perm1']             = 'Читање';
+$lang['acl_perm2']             = 'Измена';
+$lang['acl_perm4']             = 'Прављење';
+$lang['acl_perm8']             = 'Слање';
+$lang['acl_perm16']            = 'Брисање';
+$lang['acl_new']               = 'Додај нови унос';
+$lang['acl_mod']               = 'Измени унос';
---
+++ lib/plugins/popularity/lang/sr/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Serbian language file
+ *
+ * @author Иван Петровић petrovicivan@ubuntusrbija.org
+ */
+$lang['name']                  = 'Мерење популарности (може потрајати док се не учита)';
+$lang['submit']                = 'Пошаљи податке';
