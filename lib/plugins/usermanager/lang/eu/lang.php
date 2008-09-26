2a4cae0eb05d63ceb7fb0d9a5fd30269d1692a94
Inko I.A <inkoia@gmail.com>
2008-09-26 15:55:07 +0200

    Basque language update
    darcs-hash:20080926135507-c8790-a733ebe20ac57b0ee44fff2ae0b96a2533ef5dce.gz

--- lib/plugins/usermanager/lang/eu/lang.php
+++ lib/plugins/usermanager/lang/eu/lang.php
@@ -4,3 +4,44 @@
  *
  * @author Inko Illarramendi <inko.i.a@gmail.com>
  */
+$lang['menu']                  = 'Erabiltzaile Kudeatzailea';
+$lang['noauth']                = '(erabiltzaile kautotzea ez dago erabilgarri)';
+$lang['nosupport']             = '(erabiltzaile kudeaketa ez dago earbilgarri)';
+$lang['badauth']               = 'kautotze mekanismo baliogabea';
+$lang['user_id']               = 'Erabiltzailea';
+$lang['user_pass']             = 'Pasahitza';
+$lang['user_name']             = 'Benetako Izena';
+$lang['user_mail']             = 'Posta-e';
+$lang['user_groups']           = 'Taldeak';
+$lang['field']                 = 'Eremu';
+$lang['value']                 = 'Balioa';
+$lang['add']                   = 'Gehitu';
+$lang['delete']                = 'Ezabatu';
+$lang['delete_selected']       = 'Ezabatu Hautatutakoak';
+$lang['edit']                  = 'Editatu';
+$lang['edit_prompt']           = 'Editatu erabiltzaile hau';
+$lang['modify']                = 'Gorde Aldaketak';
+$lang['search']                = 'Bilatu';
+$lang['search_prompt']         = 'Egin bilaketa';
+$lang['clear']                 = 'Berrasieratu Bilaketa Iragazkia';
+$lang['filter']                = 'Iragazi';
+$lang['summary']               = 'Erakusten diren erabiltzaileak %1$d-%2$d bilatutako %3$d erabiltzailetatik. %4$d erabiltzaile guztira.';
+$lang['nonefound']             = 'Ez da erabiltzailerik aurkitu. %d erabiltzaile guztira.';
+$lang['delete_ok']             = '%d erabiltzaile ezabatuak';
+$lang['delete_fail']           = '%d huts ezabatzean.';
+$lang['update_ok']             = 'Erabiltzailea arrakastaz eguneratuak';
+$lang['update_fail']           = 'Erabiltzaile eguneratzeak huts egin du ';
+$lang['update_exists']         = 'Erabiltzaile izen aldaketak huts egin du, zehaztutako erabiltzaile izena (%s) lehendik existitzen zen (beste edozein aldaketa ezarri egingo da).';
+$lang['start']                 = 'hasi';
+$lang['prev']                  = 'aurrekoa';
+$lang['next']                  = 'hurrengoa';
+$lang['last']                  = 'azkena';
+$lang['edit_usermissing']      = 'Aukeratutako erabiltzailea ez da aurkitu, zehaztutako erabiltzaile izena beste nonbait ezabatua edo aldatua izana gerta zitekeen.';
+$lang['user_notify']           = 'Erabiltzailea jakinarazi';
+$lang['note_notify']           = 'Jakinarazpen postak erabiltzaileari pasahitz berria ematen bazaio bakarrik bidaltzen dira.';
+$lang['note_group']            = 'Erabiltzaile berriak (%s) talde lehenetsira gehituko dira ez bada talderik zehazten.';
+$lang['note_pass']             = 'Pasahitza automatikoki sortuko da eremua hutsik uzten bada eta erabiltzailearen jakinarazpena gaitua badago.';
+$lang['add_ok']                = 'Erabiltzailea arrakastaz gehitua';
+$lang['add_fail']              = 'Erabiltzaile gehitzeak huts egin du';
+$lang['notify_ok']             = 'Jakinarazpen posta-e bidalia';
+$lang['notify_fail']           = 'Jakinarazpen posta-e ezin izan da bidali';
