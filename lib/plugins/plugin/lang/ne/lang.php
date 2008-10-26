f21751f277ace4a7abdc7ffed70a52547aec60b6
Saroj Dhakal <lotusnagarkot@yahoo.com>
2008-10-26 23:03:28 +0100

    Nepali language update
    darcs-hash:20081026220328-919a1-e5585c1bf8239fcdb460c7b8099c635b58f44e2d.gz

--- lib/plugins/plugin/lang/ne/lang.php
+++ lib/plugins/plugin/lang/ne/lang.php
@@ -4,3 +4,33 @@
  *
  * @author Saroj Kumar Dhakal <lotusnagarkot@gmail.com>
  */
+$lang['menu']                  = 'प्लगिन व्यवस्थापन गर्नुहोस।';
+$lang['download']              = 'नयाँ प्लगिन डाउनलोड गरी स्थापना गर्नुहोस्';
+$lang['manage']                = 'स्थापित प्लगिनहरु';
+$lang['btn_info']              = 'जानकारी';
+$lang['btn_update']            = 'अध्यावधिक गर्नुहोस';
+$lang['btn_delete']            = 'मेटाउनुहोस्';
+$lang['btn_settings']          = 'व्यवस्थापन';
+$lang['btn_download']          = 'डाउनलोड गर्नुहोस्';
+$lang['btn_enable']            = 'वचत गर्नुहोस्';
+$lang['url']                   = 'URL';
+$lang['installed']             = 'स्थापित';
+$lang['lastupdate']            = 'अन्तिम अध्यावधिक :';
+$lang['source']                = 'स्रोत:';
+$lang['unknown']               = 'थाह नभएको';
+$lang['updating']              = 'अध्यावधिक गर्दै......';
+$lang['updated']               = 'प्लगिन %s सफलतापूर्वक अध्यावधिक भयो ';
+$lang['updates']               = 'निम्न प्लगिनहरु सफलतापूर्वक अध्यावधिक भए।';
+$lang['update_none']           = 'कुनै पनि  अध्यावधिकम भेटिएन ।';
+$lang['deleting']              = 'हटाउदै ......';
+$lang['deleted']               = 'प्लगिन %s हटाइयो ।';
+$lang['downloading']           = 'डाउनलोड गर्दै ........';
+$lang['downloaded']            = 'प्लगिन %s सफलतापूर्वक स्थापित भयो ';
+$lang['downloads']             = 'निम्न प्लगिनहरु सफलतापूर्वक स्थापित भए';
+$lang['plugin']                = 'प्लगिन:';
+$lang['name']                  = 'नाम:';
+$lang['date']                  = 'मिति:';
+$lang['type']                  = 'प्रकार :';
+$lang['desc']                  = 'जानकारी:';
+$lang['author']                = 'जारीकर्ता:';
+$lang['www']                   = 'वेब:';
