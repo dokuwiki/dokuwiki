fc0648ac14f5f6afa9babddd865c4a72990acc47
Padmanabh Suresh Kulkarni <PadmanabhS_Kulkarni@infosys.com>
2008-10-29 20:41:17 +0100

    Marathi translation added
    darcs-hash:20081029194117-8bc3f-95a3d3b09d99dbf2d122900c65a30c94979385f7.gz

---
+++ lib/plugins/acl/lang/mr/lang.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Marathi language file
+ *
+ * @author ghatothkach@hotmail.com
+ * @author Padmanabh Kulkarni <kulkarnipadmanabh@gmail.com>
+ */
---
+++ lib/plugins/config/lang/mr/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Marathi language file
+ *
+ * @author ghatothkach@hotmail.com
+ * @author Padmanabh Kulkarni <kulkarnipadmanabh@gmail.com>
+ */
+$lang['signature']             = 'हस्ताक्शर';
