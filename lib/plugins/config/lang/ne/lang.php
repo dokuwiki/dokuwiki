e352d74b1f9c636f503ff922216134bd7473f39d
Saroj Dhakal <lotusnagarkot@yahoo.com>
2008-10-31 21:20:35 +0100

    Nepali language update
    darcs-hash:20081031202035-919a1-1155f22a8f5b76b752532e9d5473a8874eadc780.gz

--- lib/plugins/config/lang/ne/lang.php
+++ lib/plugins/config/lang/ne/lang.php
@@ -4,3 +4,22 @@
  *
  * @author Saroj Kumar Dhakal <lotusnagarkot@gmail.com>
  */
+$lang['nochoice']              = '(अरु विकल्पहरु अनुपलव्ध)';
+$lang['_configuration_manager'] = 'नियन्त्रण व्यवस्थापक';
+$lang['_header_dokuwiki']      = 'DokuWiki सेटिंङ्ग';
+$lang['_header_plugin']        = 'प्लगइन सेटिंङ्ग';
+$lang['_header_template']      = 'टेम्प्लेट सेटिंङ्ग';
+$lang['_header_undefined']     = 'नखुलेको सेटिंङ्ग';
+$lang['_basic']                = 'आधारभूत सेटिंङ्ग';
+$lang['_display']              = 'प्रदर्शन सेटिंङ्ग';
+$lang['_authentication']       = 'आधिकारिकता 	सेटिंङ्ग';
+$lang['_anti_spam']            = 'स्प्याम विरुद्धको सेटिंङ्ग';
+$lang['_editing']              = 'सम्पादन सेटिंङ्ग';
+$lang['_links']                = 'लिङ्क सेटिंङ्ग';
+$lang['_media']                = 'मिडिया सेटिंङ्ग';
+$lang['_advanced']             = 'विशिष्ठ सेटिंङ्ग';
+$lang['_network']              = 'सञ्जाल सेटिंङ्ग';
+$lang['_plugin_sufix']         = 'प्लगइन सेटिंङ्ग';
+$lang['_template_sufix']       = 'टेम्प्लेट सेटिंङ्ग';
+$lang['_msg_setting_undefined'] = 'सेटिंङ्ग मेटाडाटा नभएको';
+$lang['_msg_setting_no_class'] = 'सेटिंङ्ग वर्ग नभएको';
