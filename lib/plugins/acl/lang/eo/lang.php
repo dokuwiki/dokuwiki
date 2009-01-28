fb822afdbedff49b0fb406593777c346c66cba1f
Felipe Castro <fefcas@gmail.com>
2009-01-28 16:13:47 +0100

    Esperanto language update
    Ignore-this: 654a8677c2e07728125e2dcf97144576
    
    darcs-hash:20090128151347-25afe-8bb53cebb5ea8d519e9123526894db1c19203b1d.gz

--- lib/plugins/acl/lang/eo/lang.php
+++ lib/plugins/acl/lang/eo/lang.php
@@ -4,24 +4,27 @@
  *
  * @author Felipe Castro <fefcas@uol.com.br>
  * @author Felipo Kastro <fefcas@gmail.com>
+ * @author Felipe Castro <fefcas@gmail.com>
  */
 $lang['admin_acl']             = 'Administrado de Alirkontrola Listo (ACL)';
 $lang['acl_group']             = 'Grupo';
-$lang['acl_user']              = 'Uzulo';
+$lang['acl_user']              = 'Uzanto';
 $lang['acl_perms']             = 'Rajtoj por';
 $lang['page']                  = 'Paĝo';
 $lang['namespace']             = 'Nomspaco';
-$lang['p_user_id']             = 'Uzulo <b class="acluser">%s</b> aktuale havas la jenajn rajtojn en la paĝo <b class="aclpage">%s</b>: <i>%s</i>.';
-$lang['p_user_ns']             = 'Uzulo <b class="acluser">%s</b> aktuale havas la jenajn rajtojn en la nomspaco <b class="aclns">%s</b>: <i>%s</i>.';
+$lang['btn_select']            = 'Elekti';
+$lang['p_user_id']             = 'Uzanto <b class="acluser">%s</b> aktuale havas la jenajn rajtojn en la paĝo <b class="aclpage">%s</b>: <i>%s</i>.';
+$lang['p_user_ns']             = 'Uzanto <b class="acluser">%s</b> aktuale havas la jenajn rajtojn en la nomspaco <b class="aclns">%s</b>: <i>%s</i>.';
 $lang['p_group_id']            = 'Anoj de la grupo <b class="aclgroup">%s</b> aktuale havas la jenajn rajtojn en la paĝo <b class="aclpage">%s</b>: <i>%s</i>.';
 $lang['p_group_ns']            = 'Anoj de la grupo <b class="aclgroup">%s</b> aktuale havas la jenajn rajtojn en la nomspaco <b class="aclns">%s</b>: <i>%s</i>.';
 $lang['p_choose_id']           = 'Bonvole <b>enmeti uzulon aŭ grupon</b> en la suba agordilo por rigardi aŭ redakti la aron da rajtoj por la paĝo <b class="aclpage">%s</b>.';
 $lang['p_choose_ns']           = 'Bonvole <b>enmeti uzulon aŭ grupon</b> en la suba agordilo por rigardi aŭ redakti la aron da rajtoj por la nomspaco <b class="aclns">%s</b>.';
 $lang['p_inherited']           = 'Rimarko: tiuj rajtoj ne estis rekte difinitaj, sed ili estis hereditaj el aliaj pli superaj grupoj aŭ nomspacoj.';
-$lang['p_isadmin']             = 'Rimarko: la elektita grupo aŭ uzulo ĉiam havas plenan rajtaron ĉar ĝi estas difinita kiel superuzulo.';
+$lang['p_isadmin']             = 'Rimarko: la elektita grupo aŭ uzanto ĉiam havas plenan rajtaron ĉar ĝi estas difinita kiel superuzanto.';
+$lang['p_include']             = 'Plialtaj permesoj inkluzivas malpli altajn. La permesoj por Krei, Alŝuti kaj Forigi nur aplikeblas al nomspacoj, ne al paĝoj.';
 $lang['current']               = 'Aktuala regularo ACL';
 $lang['where']                 = 'Paĝo/Nomspaco';
-$lang['who']                   = 'Uzulo/Grupo';
+$lang['who']                   = 'Uzanto/Grupo';
 $lang['perm']                  = 'Rajtoj';
 $lang['acl_perm0']             = 'Nenio';
 $lang['acl_perm1']             = 'Legi';
--- lib/plugins/popularity/lang/eo/lang.php
+++ lib/plugins/popularity/lang/eo/lang.php
@@ -3,6 +3,7 @@
  * Esperanto language file
  *
  * @author Felipo Kastro <fefcas@gmail.com>
+ * @author Felipe Castro <fefcas@gmail.com>
  */
 $lang['name']                  = 'Populareca Esploro (eble la ŝargo prokrastos iomete)';
 $lang['submit']                = 'Sendi Datenaron';
