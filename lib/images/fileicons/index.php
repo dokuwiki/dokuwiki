46c43bd84a8cc2409c69f303fc6d6da653b37452
otto <otto@valjakko.net>
2009-02-12 12:57:32 +0100

    missing_head
    darcs-hash:20090212115732-89fa1-f7c6fd4653b094c56d2bd1352cf840dce8fb6c54.gz

--- lib/images/fileicons/index.php
+++ lib/images/fileicons/index.php
@@ -2,6 +2,7 @@
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"
  lang="en" dir="ltr">
+<head>
     <title>filetype icons</title>
 
     <style type="text/css">
