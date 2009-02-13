e5556ddd5b54d4619ac55d963d272664c443382d
Omid Mottaghi <omidmr@gmail.com>
2009-02-13 16:18:03 +0100

    Persian language update
    Ignore-this: 8ca435cd1a86ac53914f7f5b5f17ab6d
    
    darcs-hash:20090213151803-bd3aa-a7102bd0c603b9ea51e4d805d707c77943ab3950.gz

--- lib/plugins/config/lang/fa/lang.php
+++ lib/plugins/config/lang/fa/lang.php
@@ -7,6 +7,7 @@
  * @author omidmr@gmail.com
  * @author Omid Mottaghi <omidmr@gmail.com>
  */
+$lang['menu']                  = 'تنظیمات پیکر‌بندی';
 $lang['_configuration_manager'] = 'مدیریت تنظیمات';
 $lang['_basic']                = 'تنظیمات مقدماتی';
 $lang['_display']              = 'تنظیمات نمایش';
@@ -28,6 +29,7 @@ $lang['rss_type_o_rss1']       = 'RSS 1.0';
 $lang['rss_type_o_rss2']       = 'RSS 2.0';
 $lang['rss_type_o_atom']       = 'Atom 0.3';
 $lang['rss_type_o_atom1']      = 'Atom 1.0';
+$lang['rss_linkto_o_rev']      = 'لیست نگارش‌ها';
 $lang['compression_o_0']       = 'هیچ';
 $lang['compression_o_gz']      = 'gzip';
 $lang['compression_o_bz2']     = 'bz2';
