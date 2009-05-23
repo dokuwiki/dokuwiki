b5ab155c4d2ce845f94b239cf51790ef967b5743
Padmanabh Kulkarni <kulkarnipadmanabh@gmail.com>
2009-05-23 12:16:53 +0200

    Marathi language update
    Ignore-this: 7083042bac15e28defd65b8f12e33345
    
    darcs-hash:20090523101653-00d84-eddc99fbfe1c6e1165c0aa88816226aca8d68cc1.gz

--- lib/plugins/popularity/lang/mr/lang.php
+++ lib/plugins/popularity/lang/mr/lang.php
@@ -7,4 +7,5 @@
  * @author Padmanabh Kulkarni<kulkarnipadmanabh@gmail.com>
  * @author shantanoo@gmail.com
  */
+$lang['name']                  = 'लोकप्रियता फीडबॅक ( लोड होण्यास थोडा वेळ लागेल )';
 $lang['submit']                = 'माहीती पाठवा';
