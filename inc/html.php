6d233a0c4569a276d9bf1896713009df8b84ecff
Andy Webber <dokuwiki@andywebber.com>
2009-05-31 15:25:14 +0200

    Enhance "locked by" page
    Make "locked by" name in main part of locked page use "showuseras" config (via editorinfo()). This was missed from editor_info_patch of October 2008
    
    darcs-hash:20090531132514-6ad63-9ae8e1aad5b7f68573cb0c41fad8c141db2ead6f.gz

--- inc/html.php
+++ inc/html.php
@@ -391,7 +391,7 @@ function html_locked(){
 
   print p_locale_xhtml('locked');
   print '<ul>';
-  print '<li><div class="li"><strong>'.$lang['lockedby'].':</strong> '.$INFO['locked'].'</div></li>';
+  print '<li><div class="li"><strong>'.$lang['lockedby'].':</strong> '.editorinfo($INFO['locked']).'</div></li>';
   print '<li><div class="li"><strong>'.$lang['lockexpire'].':</strong> '.$expire.' ('.$min.' min)</div></li>';
   print '</ul>';
 }
