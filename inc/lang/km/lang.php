43bfbfa65b14b697782c1c719ecea861bf0c6067
ratana lim <aerorat@gmail.com>
2009-06-28 10:54:26 +0200

    Khmer translation added
    Ignore-this: 2641a9e2b1802d325329fe2ce288ac8f
    
    darcs-hash:20090628085426-90339-86afd285bd36e307648823a738787ba6822622ab.gz

---
+++ inc/lang/km/lang.php
@@ -0,0 +1,233 @@
+﻿<?php
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
+ * @author     Anika Henke <henke@cosmocode.de>
+ * @author     Matthias Grimm <matthiasgrimm@users.sourceforge.net>
+ * @author     Ratana Lim <aerorat@yahoo.com>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+$lang['doublequoteopening']  = '«'; //'“';//&ldquo;
+$lang['doublequoteclosing']  = '»'; //'”';//&rdquo;
+$lang['singlequoteopening']  = '‘';//&lsquo;
+$lang['singlequoteclosing']  = '’';//&rsquo;
+$lang['apostrophe']          = '’';//&rsquo;
+
+$lang['btn_edit']   = 'កែទំព័រនេះ'; //'Edit this page';
+$lang['btn_source'] = 'បង្ហាងប្រភពទំព័រ'; // 'Show pagesource';
+$lang['btn_show']   = 'បង្ហាងទំព័រ'; // 'Show page';
+$lang['btn_create'] = 'បង្កើតទំព័រនេះ'; // 'Create this page';
+$lang['btn_search'] = 'ស្វែងរក'; // 'Search';
+$lang['btn_save']   = 'រក្សាទុក'; // 'Save';
+$lang['btn_preview']= 'បង្ហាញ'; // 'Preview';
+$lang['btn_top']    = 'ទៅលើ'; // 'Back to top';
+$lang['btn_newer']  = '<<ទំព័រទំនើប'; // '<< more recent';
+$lang['btn_older']  = 'ទំព័រថ្មែសម័យ>>'; // 'less recent >>';
+$lang['btn_revs']   = 'ទំព័រចាស់ៗ'; // 'Old revisions';
+$lang['btn_recent'] = 'ទំព័រថ្មីៗ'; // 'Recent changes';
+$lang['btn_upload'] = 'ដាកលើង'; // 'Upload';
+$lang['btn_cancel'] = 'បោះបង់'; // 'Cancel';
+$lang['btn_index']  = 'លិបិក្រម'; // 'Index';
+$lang['btn_secedit']= 'កែ'; // 'Edit';
+$lang['btn_login']  = 'កត់ចូល'; // 'Login';
+$lang['btn_logout'] = 'កត់ចេញ'; // 'Logout';
+$lang['btn_admin']  = 'អ្នកគ្រប់គ្រង'; // 'Admin';
+$lang['btn_update'] = 'កែឡើង'; // 'Update';
+$lang['btn_delete'] = 'លុបចោល'; // 'Delete';
+$lang['btn_back']   = 'ត្រឡប់'; // 'Back';
+$lang['btn_backlink']    = 'ខ្សែចំណងក្រោយ'; // "Backlinks";
+$lang['btn_backtomedia'] = 'ទៅប្រព័នឯកសាវិញ'; // 'Back to Mediafile Selection';
+$lang['btn_subscribe']   = 'ដាក់ដំណឹងផ្លស់ប្តូរ'; // 'Subscribe Changes';
+$lang['btn_unsubscribe'] = 'ដកដំណឹងផ្លស់ប្តូរ'; // 'Unsubscribe Changes';
+$lang['btn_profile']     = 'កែប្រវត្តិរូប'; // 'Update Profile';
+$lang['btn_reset']       = 'កមណត់ឡើងរិញ'; // 'Reset';
+$lang['btn_resendpwd']   = 'ផ្ញើពាក្សសម្ងាត់'; // 'Send new password';
+$lang['btn_draft']    = 'កែគំរោង'; // 'Edit draft';
+$lang['btn_recover']  = 'ស្រោះគំរោងឡើង'; // 'Recover draft';
+$lang['btn_draftdel'] = 'លុបគំរោង'; // 'Delete draft';
+
+$lang['loggedinas'] = 'អ្នកប្រើ'; // 'Logged in as';
+$lang['user']       = 'នាមបម្រើ'; // 'Username';
+$lang['pass']       = 'ពាក្សសម្ងត់'; //'Password';
+$lang['newpass']    = 'ពាក្សសម្ងាត់ថ្មី'; //'New password';
+$lang['oldpass']    = 'បន្ជាកពាក្សសម្ងាត់'; //'Confirm current password';
+$lang['passchk']    = 'ម្ដងទាត'; //'once again';
+$lang['remember']   = 'ចំណាំខ្ញុំ'; //'Remember me';
+$lang['fullname']   = 'នាមត្រគោល'; //'Full name';
+$lang['email']      = 'អ៊ីមែល'; //'E-Mail';
+$lang['register']   = 'ចុះឈ្មោះ';//'Register';
+$lang['profile']    = 'ប្រវត្តិរូប';// 'User Profile';
+$lang['badlogin']   = 'សុំអាទោស​ នាមបំរើ ឬ ពាក្សសម្ងាតមិនត្រវទេ។'; //'Sorry, username or password was wrong.';
+$lang['minoredit']  = 'កែបបណ្តិចបណ្តួច';// 'Minor Changes';
+$lang['draftdate']  = 'គំរោង កត់ស្វ័យប្រវត្ត'; //'Draft autosaved on'; // full dformat date will be added
+
+$lang['regmissing'] = 'សុំអាទោស​ អ្នកត្រវបំពេញក្របវាល។'; //'Sorry, you must fill in all fields.';
+$lang['reguexists'] = 'សុំអាទោស​ នាមប្រើនេះមានរួចហើ។'; //'Sorry, a user with this login already exists.';
+$lang['regsuccess'] = 'អ្នកប្រើបានបង្កើតហើយ និងពាក្សសម្ងាតក៏បានផ្ញើទៀត។'; // 'The user has been created and the password was sent by email.';
+$lang['regsuccess2']= 'អ្នកប្រើបានបង្កើតហើយ។'; // 'The user has been created.';
+$lang['regmailfail']= 'មើលទៅដុចជាមានកំហុសក្នុង....សុំទាកទងអ្នកក្របក្រង'; // 'Looks like there was an error on sending the password mail. Please contact the admin!';
+$lang['regbadmail'] = 'អ៊ីមេលអ្នកសាសេមិនត្រូវបញ្ជរ&mdash;បើអ្នកកិតថានេះជាកំហុសបដិបត្តិ សុំទាកទងអ្នកក្របគ្រោង។'; // 'The given email address looks invalid - if you think this is an error, contact the admin';
+$lang['regbadpass'] = 'គូពាក្សសម្ងាតមិនដូចគ្នាទេ សមសាកទៀត។'; // 'The two given passwords are not identical, please try again.';
+$lang['regpwmail']  = 'ពាក្សសម្ងាតអ្នក'; // 'Your DokuWiki password';
+$lang['reghere']    = 'អ្នកឥតមានបញ្ជីនាមបម្រើទេ?  សុំចល់ចុះឈ្មោះធ្វើគណនីសម្របប្រើប្រស'; // 'You don\'t have an account yet? Just get one';
+
+$lang['profna']       = 'មិនអាចកែ'; // 'This wiki does not support profile modification';
+$lang['profnochange'] = 'ឥតផ្លាស់ប្ដូរ ក្មានអ្វីធ្វើទេ។'; // 'No changes, nothing to do.';
+$lang['profnoempty']  = 'នាមេឬអីមេលទទេ'; // 'An empty name or email address is not allowed.';
+$lang['profchanged']  = 'ប្រវត្តិរូបអ្នកប្រើបាន ។'; // 'User profile sucessfully updated.';
+
+$lang['pwdforget'] = 'ភ្លិចពាក្សសម្ងាត់ យកមួយទាត។'; //'Forgotten your password? Get a new one';
+$lang['resendna']  = 'វីគីនេះមិនឧបរំផ្ញើពាក្សសម្ងាតម្ដងទៀតទេ។'; //'This wiki does not support password resending.';
+$lang['resendpwd'] = 'ផ្ញើពាក្សសម្ងាតឲ្យ'; //'Send new password for';
+$lang['resendpwdmissing'] = 'សុំអាទោស​ អ្នកត្រវបំពេញវាល។'; //'Sorry, you must fill in all fields.';
+$lang['resendpwdnouser']  = 'សុំអាទោស​ យាងរកអ្នកប្រើមិនឃើងទេ។'; // 'Sorry, we can\'t find this user in our database.';
+$lang['resendpwdbadauth'] = 'សុំអាទោស​ រហស្សលេខអនុញ្ញាតពំអាចប្រើបានទេ។ ខ្សែបន្ត'; //'Sorry, this auth code is not valid. Make sure you used the complete confirmation link.';
+$lang['resendpwdconfirm'] ='ខ្សែបន្ត'; // 'A confirmation link has been sent by email.';
+$lang['resendpwdsuccess'] = 'ពាក្សសម្ងាតអ្នកបានផ្ញើហើយ។'; //'Your new password has been sent by email.';
+
+$lang['txt_upload']   = 'ជ្រើសឯកសារដែលរុញ​ឡើង'; // 'Select file to upload';
+$lang['txt_filename'] = 'រុញឡើងជា (ស្រេច​ចិត្ត)'; // 'Upload as (optional)';
+$lang['txt_overwrt']  = 'កត់ពីលើ';//'Overwrite existing file';
+$lang['lockedby']     = 'ឥឡូវនេះចកជាប់​'; // 'Currently locked by';
+$lang['lockexpire']   = 'សោជាប់ផុត​កំណត់ម៉ោង'; // 'Lock expires at';
+$lang['willexpire']   = 'សោអ្នកចំពោះកែតម្រូវទំព័រនេះ ហួសពែលក្នុងមួយនាទី។\nកុំឲ្យមានជម្លោះ ប្រើ «បង្ហាញ»​ ទៅកំណត់​ឡើង​វិញ។'; // 'Your lock for editing this page is about to expire in a minute.\nTo avoid conflicts use the preview button to reset the locktimer.';
+
+$lang['notsavedyet'] = 'កម្រែមិនទានរុក្សាទកត្រូវបោះបង់។\nបន្តទៅទាឬទេ?'; // 'Unsaved changes will be lost.\nReally continue?';
+$lang['rssfailed']   = 'មានកំហុសពេលទៅ​ប្រមូល​យកមតិ​ព័ត៌មាន៖ '; // 'An error occured while fetching this feed: ';
+$lang['nothingfound']= 'រកមិនឃើញអ្វីទេ។'; // 'Nothing was found.';
+
+$lang['mediaselect'] = 'ឯកសារមីឌៀ'; // 'Media Files';
+$lang['fileupload']  = 'រុញឯកសារមីឌៀឡើង'; // 'Media File Upload';
+$lang['uploadsucc']  = 'រុញចូលមានជ័យ'; //'Upload successful';
+$lang['uploadfail']  = 'រុញឡើងបរាជ័យ។ ប្រហែលខុសសិទ្ឋានុញ្ញាត?'; //'Upload failed. Maybe wrong permissions?';
+$lang['uploadwrong'] = 'រុញឡើងត្រូវ​បាន​បដិសេធ។ ឯកសារ'; //'Upload denied. This file extension is forbidden!';
+$lang['uploadexist'] = 'ឯកសារមានហើយ។ ឥតមានធ្វើអ្វីទេ។'; //'File already exists. Nothing done.';
+$lang['uploadbadcontent'] = 'ធាតុចំរុញឡើងមិនត្រូវកន្ទុយឯកសារ %s ទេ។'; // 'The uploaded content did not match the %s file extension.';
+$lang['uploadspam']  = 'ចំរុញឡើង បង្ខាំង ដៅយ  '; //'The upload was blocked by the spam blacklist.';
+$lang['uploadxss']   = 'ចំរុញឡើង បង្ខាំង  '; //'The upload was blocked for possibly malicious content.';
+$lang['deletesucc']  = 'ឯកសារ «%s» បានលុបហើយ។'; //'The file "%s" has been deleted.';
+$lang['deletefail']  = '«%s» មិនអាចលុបទេ&mdashមើល'; //'"%s" couldn\'t be deleted - check permissions.';
+$lang['mediainuse']  = 'ឯកសារ «%s» ឥតទានលុបទេ&mdashមានគេកំភងទេជាប់ប្រើ។'; //'The file "%s" hasn\'t been deleted - it is still in use.';
+$lang['namespaces']  = 'នាមដ្ឋាន'; //'Namespaces';
+$lang['mediafiles']  = 'ឯកសារទំនេនៅក្នុង'; //'Available files in';
+
+$lang['js']['keepopen']    = 'ទុកបង្អួចបើក ពេលការជម្រើស'; //'Keep window open on selection';
+$lang['js']['hidedetails'] = 'បាំង'; //'Hide Details';
+$lang['mediausage']  = 'ប្រើ'; //'Use the following syntax to reference this file:';
+$lang['mediaview']   = 'មើលឯកសារដើម'; //'View original file';
+$lang['mediaroot']   = 'ឫស'; // 'root';
+$lang['mediaupload'] = 'រុញឯកសារឡើងទៅនាមដ្ឋាននេះ។ នាមដ្ឋាន «រុញឡើង»'; //'Upload a file to the current namespace here. To create subnamespaces, prepend them to your "Upload as" filename separated by colons.';
+$lang['mediaextchange'] = 'កន្ទុយឯកសារផ្លាសពី «%s» ទៅ «%s»!'; //'Filextension changed from .%s to .%s!';
+
+$lang['reference']   = 'អនុសាសនចំពោះ'; //'References for';
+$lang['ref_inuse']   = 'ឯកសារមិនអាចលុបពីព្រោះវានៅចាប់ប្រើដៅទំព័រ៖'; //'The file can\'t be deleted, because it\'s still used by the following pages:';
+$lang['ref_hidden']  = 'អនុសាសនខ្លះនៅលើទំព័រអ្នកគ្មានសេធអនុញ្ញាត'; //'Some references  are on pages you don\'t have permission to read';
+
+$lang['hits']       = 'ត្រូវ'; //'Hits';
+$lang['quickhits']  = 'ឈ្មោះទំព័រប្រៀបដូច'; //'Matching pagenames';
+$lang['toc']        = 'មាតិកា'; //'Table of Contents';
+$lang['current']    = 'ឥឡៅវ'; //'current';
+$lang['yours']      = 'តំណែអ្នាក'; //'Your Version';
+$lang['diff']       = 'បង្ហាងអសទិសភាពជាមួយតំណែឥឡូវ '; //'show differences to current version';
+$lang['line']       = 'ខ្សែ'; //'Line';
+$lang['breadcrumb'] = 'ដាន';
+$lang['youarehere'] = 'ដាន'; // 'អ្នកនៅនេះ'; // 'You are here';
+$lang['lastmod']    = 'ពេលកែចុងក្រោយ'; // 'Last modified';
+$lang['by']         = 'និពន្ឋដោយ'; //'by';
+$lang['deleted']    = 'យកចេញ'; //'removed';
+$lang['created']    = 'បង្កើត'; //'created';
+$lang['restored']   = 'ស្ដារបុនរាព្រឹតចាស់'; //'old revision restored';
+$lang['external_edit'] = 'កំរេពីក្រៅ'; // 'external edit';
+$lang['summary']    = 'កែតម្រា'; //'Edit summary';
+
+$lang['mail_newpage']  = 'ថែមទំព័រ'; //'page added:';
+$lang['mail_changed']  = 'ទំព័រប្រែប្រួល'; //'page changed:';
+$lang['mail_new_user'] = 'អ្នកប្រើថ្មី'; //'new user:';
+$lang['mail_upload']   = 'រុញអក្សាលើង'; //'file uploaded:';
+
+$lang['nosmblinks'] = 'ខ្សែបន្តទៅ «Windows share» ប្រើបានក្នុង «Microsoft IE»។ អ្នកអាច កាត់ឬបិត ខ្សែនេះ។'; // 'Linking to Windows shares only works in Microsoft Internet Explorer.\nYou still can copy and paste the link.';
+
+$lang['qb_alert']   = 'សុំចុតឃ្វាមអ្នកចង់ធ្វើ​ទ្រង់ទ្រាយ វាគងទែបន្ថែមពីចុងអត្ថបទ'; //'Please enter the text you want to format.\nIt will be appended to the end of the document.';
+$lang['qb_bold']    = 'ឃ្វាមក្រស'; //'Bold Text';
+$lang['qb_italic']  = 'ឃ្វាមជ្រៀង'; //'Italic Text';
+$lang['qb_underl']  = 'ឃ្វាម'; //'Underlined Text';
+$lang['qb_code']    = 'ឃ្វាមក្បួន'; //'Code Text';
+$lang['qb_strike']  = 'ឃ្វាម'; //'Strike-through Text';
+$lang['qb_h1']      = 'និវេទន៍ទី១'; // 'Level 1 Headline';
+$lang['qb_h2']      = 'និវេទន៍ទី២'; // 'Level 2 Headline';
+$lang['qb_h3']      = 'និវេទន៍ទី៣'; // 'Level 3 Headline';
+$lang['qb_h4']      = 'និវេទន៍ទី៤'; // 'Level 4 Headline';
+$lang['qb_h5']      = 'និវេទន៍ទី៥'; // 'Level 5 Headline';
+$lang['qb_link']    = 'ខ្សែបន្តក្នុង'; //'Internal Link';
+$lang['qb_extlink'] = 'ខ្សែបន្តក្រៅ'; //'External Link';
+$lang['qb_hr']      = 'បន្ទាផ្ដេក'; //'Horizontal Rule';
+$lang['qb_ol']      = 'តារាងត្រៀប'; //'Ordered List Item';
+$lang['qb_ul']      = 'តារាងអត្រៀប'; //'Unordered List Item';
+$lang['qb_media']   = 'បន្ថែមរូនឹងឯកសារឥទៀត'; //'Add Images and other files';
+$lang['qb_sig']     = 'ស៊កហត្ថលេខា'; //'Insert Signature';
+$lang['qb_smileys'] = 'សញ្ញាអារម្មណ៍'; //​'Smileys';​​//បដិមា
+$lang['qb_chars']   = 'អក្ខរៈពិសេស'; //'Special Chars';
+
+$lang['del_confirm']= 'លុប'; // 'Really delete selected item(s)?';
+$lang['admin_register']= 'តែមអ្នកប្រើ';//'Add new user';
+
+$lang['spell_start'] = 'ពិនិត្យអក្ខរាវិរុទ្ធ';//'Check Spelling';
+$lang['spell_stop']  = 'បណ្តកំរែ'; 'Resume Editing';
+$lang['spell_wait']  = 'សូមចាំ';//'Please wait...';
+$lang['spell_noerr'] = 'ឥតមានខុះទេ'; //'No Mistakes found';
+$lang['spell_nosug'] = 'ឥតមានយោបល់'; //'No Suggestions';
+$lang['spell_change']= 'ដូរជំនួស';//'Change';
+
+$lang['metaedit']    = 'កែទិន្នន័យអរូប';//'Edit Metadata';
+$lang['metasaveerr'] = 'ពំអាចកត់រទិន្នន័យអរូប';//'Writing metadata failed';
+$lang['metasaveok']  = 'ទិន្នន័យអរូប'; //'Metadata saved';
+$lang['img_backto']  = 'ថយក្រោយ'; //'Back to';
+$lang['img_title']   = 'អភិធេយ្យ'; //'Title';
+$lang['img_caption'] = 'ចំណងជើង'; //'Caption';
+$lang['img_date']    = 'ថ្ងៃខែ';//'Date';
+$lang['img_fname']   = 'ឈ្មោះឯកសារ'; //'Filename';
+$lang['img_fsize']   = 'ទំហំ';//'Size';
+$lang['img_artist']  = 'អ្នកថតរូប'; // 'Photographer';
+$lang['img_copyr']   = 'រក្សា​សិទ្ធិ'; // 'Copyright';
+$lang['img_format']  = 'ធុនប្រភេទ'; // 'Format';
+$lang['img_camera']  = 'គ្រឿងថត'; // 'Camera';
+$lang['img_keywords']= 'មេពាក្ស';//'Keywords';
+
+$lang['subscribe_success']  = ' ដកថែម %s ចូលបញ្ជីបរិវិសកមចំពោះ %s';  // 'Added %s to subscription list for %s';
+$lang['subscribe_error']    = 'មានកំហុសពេលបន្ថែម %s ចូលបញ្ជីបរិវិសកមចំពោះ %s';  // 'Error adding %s to subscription list for %s';
+$lang['subscribe_noaddress']= 'ឥតមានអាសយដ្ឋាន អ្នកមិនអាចកត់ចុល';  // 'There is no address associated with your login, you cannot be added to the subscription list';
+$lang['unsubscribe_success']= 'ដក %s ចេញពីបញ្ជីបរិវិសកមចំពោះ %s';  // 'Removed %s from subscription list for %s';
+$lang['unsubscribe_error']  = 'មានកំហុសពេលដក %s​  ចេញពីបញ្ជីបរិវិសកមចំពោះ %s';  // 'Error removing %s from subscription list for %s';
+
+/* auth.class language support */
+$lang['authmodfailed']   = 'និនផ្ទៀងផ្ទាត់​ភាព​​ត្រឹមត្រូវបានទេ។ សុំទាកទងអ្នកក្របគ្រោង។';  // 'Bad user authentication configuration. Please inform your Wiki Admin.';
+$lang['authtempfail']    = 'ការផ្ទៀងផ្ទាត់​ភាព​​ត្រឹមត្រូវឥតដំនេ។ ប្រើ ....';  // 'User authentication is temporarily unavailable. If this situation persists, please inform your Wiki Admin.';
+
+/* installer strings */
+$lang['i_chooselang'] = 'រើសពាស្សាអ្នក'; //'Choose your language';
+$lang['i_installer']  = 'ដំឡើងឌោគូវីគី'; // 'DokuWiki Installer';
+$lang['i_wikiname']   = 'នាមវីគី'; //'Wiki Name';
+$lang['i_enableacl']  = 'បើកប្រើ (អនុសាស)'; // 'Enable ACL (recommended)';
+$lang['i_superuser']  = 'អ្នកកំពូល'; // 'Superuser';
+$lang['i_problems']   = 'កម្មវិធី​ដំឡើងបានប៉ះឧបសគ្គ។ អ្នកមិនអាចបន្តទៅទៀត ដល់អ្នកជួសជុលវា។'; // 'The installer found some problems, indicated below. You can not continue until you have fixed them.';
+$lang['i_modified']   = ''; // 'For security reasons this script will only work with a new and unmodified Dokuwiki installation. You should either re-extract the files from the downloaded package or consult the complete    <a href="http://wiki.splitbrain.org/wiki:install">Dokuwiki installation instructions</a>';
+$lang['i_funcna']     = '<code>%s</code> '; // 'PHP function <code>%s</code> is not available. Maybe your hosting provider disabled it for some reason?';
+$lang['i_phpver']     = 'PHP ប្រវត់លេខ<code>%s</code> ជា'; // 'Your PHP version <code>%s</code> is lower than the needed <code>%s</code>. You need to upgrade your PHP install.';
+$lang['i_permfail']   = '<code>%s</code> មិនអាចសាស'; // '<code>%s</code> is not writable by DokuWiki. You need to fix the permission settings of this directory!';
+$lang['i_confexists'] = '<code>%s</code> មានហាយ'; // '<code>%s</code> already exists';
+$lang['i_writeerr']   = 'មិនអាចបណ្កើ<code>%s</code>។ អ្នកត្រវការពិនិត្យអធិក្រឹតិរបស់ថតនឹងឯកសារ។'; //'Unable to create <code>%s</code>. You will need to check directory/file permissions and create the file manually.';
+$lang['i_badhash']    = '(hash=<code>%s</code>)'; // 'unrecognised or modified dokuwiki.php (hash=<code>%s</code>)';
+$lang['i_badval']     = '<code>%s</code>&mdash;'; // '<code>%s</code> - illegal or empty value';
+$lang['i_success']    = ''; // 'The configuration was finished successfully. You may delete the install.php file now. Continue to   <a href="doku.php">your new DokuWiki</a>.';
+$lang['i_failure']    = 'ពលសាសារ'; // 'Some errors occured while writing the configuration files. You may need to fix them manually before you can use <a href="doku.php">your new DokuWiki</a>.';
+$lang['i_policy']     = 'បញ្ជីអនុញ្ញតផ្ដើម'; //'Initial ACL policy';
+$lang['i_pol0']       = 'វីគីបើកចំហ'; //'Open Wiki (read, write, upload for everyone)';
+$lang['i_pol1']       = 'វីគីសធារណៈ'; //'Public Wiki (read for everyone, write and upload for registered users)';
+$lang['i_pol2']       = 'វីគីបិទជិត'; //'Closed Wiki (read, write, upload for registered users only)';
+
+$lang['i_retry']      = 'ម្តងទៀត'; //'Retry';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
