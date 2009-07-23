0f2d4e8e3e80b7f107a570cc543a6c9d6c2e2bfa
Olafur Gunnlaugsson <olafur@gunnlaugsson.com>
2009-07-23 21:41:13 +0200

    Icelandic language update
    Ignore-this: df10fad7adb5ac3e9fe0dbb011cc4f45
    
    darcs-hash:20090723194113-c7d26-f7500b290f5b996caa19093358b124b1f2c12a46.gz

--- lib/plugins/acl/lang/is/lang.php
+++ lib/plugins/acl/lang/is/lang.php
@@ -5,3 +5,7 @@
  * @author Hrannar Baldursson <hrannar.baldursson@gmail.com>
  * @author Ólafur Gunnlaugsson <oli@audiotools.com>
  */
+$lang['acl_group']             = 'Hópur';
+$lang['acl_user']              = 'Notandi';
+$lang['page']                  = 'Síða';
+$lang['acl_perm16']            = 'Eyða';
--- lib/plugins/usermanager/lang/is/lang.php
+++ lib/plugins/usermanager/lang/is/lang.php
@@ -5,3 +5,9 @@
  * @author Hrannar Baldursson <hrannar.baldursson@gmail.com>
  * @author Ólafur Gunnlaugsson <oli@audiotools.com>
  */
+$lang['user_id']               = 'Notandi';
+$lang['user_pass']             = 'Lykilorð';
+$lang['user_name']             = 'Raunnafn';
+$lang['user_groups']           = 'Hópar';
+$lang['field']                 = 'Svæði';
+$lang['delete']                = 'Eyða';
