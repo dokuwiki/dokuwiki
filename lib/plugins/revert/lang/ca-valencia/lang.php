6dab2a4ded1a919ac8528bf6ed9e357ee7b03791
Carles Bellver <carles.bellver@cent.uji.es>
2009-12-01 19:29:20 +0100

    Valencian language update
    Ignore-this: caf0dd7ac30a794b866ca0d2b26142ba
    
    darcs-hash:20091201182920-52524-685fdb68020856e6d6f99d15412e2b121d0e0274.gz

--- lib/plugins/revert/lang/ca-valencia/lang.php
+++ lib/plugins/revert/lang/ca-valencia/lang.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * valencian language file
+ * Valencian language file
+ *
+ * @author Bernat Arlandis i Mañó <berarma@ya.com>
  * @author Bernat Arlandis <berarma@ya.com>
  */
-$lang['menu']                  = 'Gestor de reversions';
-$lang['filter']                = 'Buscar pàgines en spam';
-$lang['revert']                = 'Revertir pàgines seleccionades';
-$lang['reverted']              = '%s revertides a la versió %s';
-$lang['removed']               = '%s llevades';
-$lang['revstart']              = 'El procés de reversió ha començat. Açò pot dur prou de temps. Si es talla abans d\'acabar, haurà de revertir per parts.';
-$lang['revstop']               = 'El procés de reversió ha finalisat correctament.';
-$lang['note1']                 = 'Nota: esta busca és sensible a mayúscules';
-$lang['note2']                 = 'Nota: esta pàgina es revertirà a l\'última versió que no continga el spam definit pel terme <i>%s</i>.';
+$lang['menu']                  = 'Gestió de reversions';
+$lang['filter']                = 'Cerca pàgines brossa';
+$lang['revert']                = 'Reverteix les pàgines seleccionades';
+$lang['reverted']              = 'S\'ha revertit %s a la revisió %s';
+$lang['removed']               = 'S\'ha suprimit %s';
+$lang['revstart']              = 'S\'ha iniciat el procés de reversió. Això pot trigar una bona estona. Si s\'excedeix el temps d\'espera màxim del servidor, haureu de tornar a intentar-ho per parts.';
+$lang['revstop']               = 'El procés de reversió ha acabat amb èxit.';
+$lang['note1']                 = 'Nota: esta cerca distingeix entre majúscules i minúscules.';
+$lang['note2']                 = 'Nota: la pàgina es revertirà a la darrera versió que no continga el terme brossa especificat <em>%s</em>.';
