8e6d6cfde65cf5e68cf762424dc2a7048e6b2959
Martijn Dekker <martijn@inlv.org>
2010-02-12 23:37:21 +0100

    Interlingua Language Update

--- lib/plugins/popularity/lang/ia/lang.php
+++ lib/plugins/popularity/lang/ia/lang.php
@@ -5,3 +5,5 @@
  * @author robocap <robocap1@gmail.com>
  * @author Martijn Dekker <martijn@inlv.org>
  */
+$lang['name']                  = 'Datos de popularitate (pote prender alcun tempore pro cargar)';
+$lang['submit']                = 'Inviar datos';
