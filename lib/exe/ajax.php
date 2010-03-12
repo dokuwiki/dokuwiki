a958c48cfeaefd8e8c1aeebcd04e9c862f014a6d
Andreas Gohr <andi@splitbrain.org>
2010-03-12 14:35:43 +0100

    removed dead leftover code from media search

--- lib/exe/ajax.php
+++ lib/exe/ajax.php
@@ -202,17 +202,6 @@ function ajax_medialist(){
 }
 
 /**
- * Return list of search result for the Mediamanager
- *
- * @author Tobias Sarnowski <sarnowski@cosmocode.de>
- */
-function ajax_mediasearchlist(){
-  global $conf;
-
-  media_searchlist($_POST['ns']);
-}
-
-/**
  * Return sub index for index view
  *
  * @author Andreas Gohr <andi@splitbrain.org>
