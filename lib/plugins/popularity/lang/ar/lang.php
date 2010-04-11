b4877af37a38cedb703d975b6eca8ceb18652acd
Usama Akkad <uahello@gmail.com>
2010-04-11 11:50:04 +0200

    Arabic language update

--- lib/plugins/popularity/lang/ar/lang.php
+++ lib/plugins/popularity/lang/ar/lang.php
@@ -5,3 +5,5 @@
  * @author Yaman Hokan <always.smile.yh@hotmail.com>
  * @author Usama Akkad <uahello@gmail.com>
  */
+$lang['name']                  = 'رد الشعبية (قد يأخذ بعض الوقت ليحمل)';
+$lang['submit']                = 'أرسل البيانات';
