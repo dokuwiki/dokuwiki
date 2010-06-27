ee4d22e0a02a665a30a387ba95e614c22600862b
Christopher Smith <chris@jalakai.co.uk>
2010-06-27 14:54:00 +0100

    FS#1826, stop forcing rendering of metadata when updating metadata in addLogEntry()

--- inc/changelog.php
+++ inc/changelog.php
@@ -86,7 +86,7 @@ function addLogEntry($date, $id, $type=DOKU_CHANGE_TYPE_EDIT, $summary='', $extr
             if ($user) $meta['contributor'][$user] = $INFO['userinfo']['name'];
         }
         $meta['last_change'] = $logline;
-        p_set_metadata($id, $meta, true);
+        p_set_metadata($id, $meta);
     }
 
     // add changelog lines
