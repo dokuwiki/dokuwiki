dokuwiki/dokuwiki

commit d0bdf7659fce98c2922b151766d51d5c7e8814d6

Use namespace filter in quicksearch with empty search term

inc/fulltext.php
@@ -243,17 +243,21 @@ function _ft_pageLookup(&$data){
     $titles = array_map('rtrim', idx_getIndex('title', ''));
     $pages = array_combine($pages, $titles);
 
-    if($id !== '' && cleanID($id) !== '') {
+    if ($id !== '' && cleanID($id) !== '') {
         $cleaned = cleanID($id);
-        $matched_pages = array();
         foreach ($pages as $p_id => $p_title) {
-            if (((strpos($in_ns ? $p_id : noNSorNS($p_id), $cleaned) !== false) ||
-                 ($in_title && stripos($p_title, $id) !== false)) &&
-                (!isset($ns) || strpos($p_id, $ns) === 0)) {
-                $matched_pages[$p_id] = $p_title;
+            if ((strpos($in_ns ? $p_id : noNSorNS($p_id), $cleaned) === false) &&
+                ($in_title && stripos($p_title, $id) === false)) {
+                unset($pages[$p_id]);
             }
         }
-        $pages = $matched_pages;
+    }
+    if (isset($ns)) {
+        foreach (array_keys($pages) as $p_id) {
+            if (strpos($p_id, $ns) !== 0) {
+                unset($pages[$p_id]);
+            }
+        }
     }
 
     // discard hidden pages

commit 5479a8c3341247ca228026819f20f3ab5c34a80f

fixed page lookup when useheading is disabled
this was broken by d0bdf7659fce98c2922b151766d51d5c7e8814d6

inc/fulltext.php
@@ -243,11 +243,11 @@ function _ft_pageLookup(&$data){
     $titles = array_map('rtrim', idx_getIndex('title', ''));
     $pages = array_combine($pages, $titles);
 
-    if ($id !== '' && cleanID($id) !== '') {
-        $cleaned = cleanID($id);
+    $cleaned = cleanID($id);
+    if ($id !== '' && $cleaned !== '') {
         foreach ($pages as $p_id => $p_title) {
             if ((strpos($in_ns ? $p_id : noNSorNS($p_id), $cleaned) === false) &&
-                ($in_title && stripos($p_title, $id) === false)) {
+                (!$in_title || (stripos($p_title, $id) === false)) ) {
                 unset($pages[$p_id]);
             }
         }