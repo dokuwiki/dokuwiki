2366cb38b9d556b41d8ff4c1137225033cb86d56
Yagnesh Desai <yndesai@gmail.com>
2010-10-17 12:23:11 +0200

    Hindi language update

--- lib/plugins/plugin/lang/hi/lang.php
+++ lib/plugins/plugin/lang/hi/lang.php
@@ -3,4 +3,10 @@
  * Hindi language file
  *
  * @author Abhinav Tyagi <abhinavtyagi11@gmail.com>
+ * @author yndesai@gmail.com
  */
+$lang['unknown']               = 'अज्ञात';
+$lang['deleted']               = 'मिटाया हुआ';
+$lang['date']                  = 'दिनांक:';
+$lang['author']                = 'लेखक:';
+$lang['error']                 = 'अज्ञात त्रुटि हुइ';
