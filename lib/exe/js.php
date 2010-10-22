87e7175e210d4cb44555ac5885a0ed6aa31bf287
Andreas Gohr <andi@splitbrain.org>
2010-10-22 11:29:43 +0200

    disabled JS based hotkeys for next stable release FS#1958

--- lib/exe/js.php
+++ lib/exe/js.php
@@ -51,7 +51,7 @@ function js_out(){
                 DOKU_INC.'lib/scripts/linkwiz.js',
                 DOKU_INC.'lib/scripts/media.js',
                 DOKU_INC.'lib/scripts/subscriptions.js',
-                DOKU_INC.'lib/scripts/hotkeys.js',
+# disabled for FS#1958                DOKU_INC.'lib/scripts/hotkeys.js',
                 DOKU_TPLINC.'script.js',
             );
 
