dokuwiki/dokuwiki

commit 0b9facb482cf05fc5e67df8edd1eb6b80e50f579

Option to show recent changes relative to current namespace or global.
This change introduce new config option conf['recentrel']=1 (for compatibility).
Relative changes caption is displayed as $ID or as text (when conf['useheading']=1).

Pijoter.

inc/html.php
@@ -581,14 +581,16 @@ function html_recent($first=0){
     global $conf;
     global $lang;
     global $ID;
+
+    $ns=((integer)$conf['recentrel']!=0)? $ID: '';
     /* we need to get one additionally log entry to be able to
      * decide if this is the last page or is there another one.
      * This is the cheapest solution to get this information.
      */
-    $recents = getRecents($first,$conf['recent'] + 1,getNS($ID));
+    $recents = getRecents($first,$conf['recent'] + 1,getNS($ns));
     if(count($recents) == 0 && $first != 0){
         $first=0;
-        $recents = getRecents($first,$conf['recent'] + 1,getNS($ID));
+        $recents = getRecents($first,$conf['recent'] + 1,getNS($ns));
     }
     $hasNext = false;
     if (count($recents)>$conf['recent']) {
@@ -598,13 +600,16 @@ function html_recent($first=0){
 
     print p_locale_xhtml('recent');
 
-    if (getNS($ID) != '')
-        print '<div class="level1"><p>' . sprintf($lang['recent_global'], getNS($ID), wl('', 'do=recent')) . '</p></div>';
+    if ($conf['recentrel']) {
+        $heading = ($conf['useheading'])? p_get_first_heading(getNS($ns), false): getNS($ns);
+        if ($heading == '') $heading = getNS($ns);
+        print '<div class="level1"><p>' . sprintf($lang['recent_global'], $heading, wl('', 'do=recent')) . '</p></div>';
+    }
 
     $form = new Doku_Form(array('id' => 'dw__recent', 'method' => 'GET'));
     $form->addHidden('sectok', null);
     $form->addHidden('do', 'recent');
-    $form->addHidden('id', $ID);
+    $form->addHidden('id', $ns);
     $form->addElement(form_makeOpenTag('ul'));
 
     foreach($recents as $recent){

conf/dokuwiki.php
@@ -28,6 +28,7 @@ $conf['title']       = 'DokuWiki';        //what to show in the title
 $conf['template']    = 'default';         //see lib/tpl directory
 $conf['license']     = 'cc-by-nc-sa';     //see conf/license.php
 $conf['fullpath']    = 0;                 //show full path of the document or relative to datadir only? 0|1
+$conf['recentrel']   = 1;                 //show recent changes relative to current namespace (1) or global (0)
 $conf['recent']      = 20;                //how many entries to show in recent
 $conf['breadcrumbs'] = 10;                //how many recent visited pages to show
 $conf['youarehere']  = 0;                 //show "You are here" navigation? 0|1

commit 16e8cbc84f579fd7e4e3b71e8546f0589b34c4c2

Fixed typo introduced in commit 0b9facb482cf05fc5e67

inc/html.php
@@ -602,8 +602,8 @@ function html_recent($first=0){
 
     if ($conf['recentrel']) {
         $heading = ($conf['useheading'])? p_get_first_heading(getNS($ns), false): getNS($ns);
-        if ($heading == '') $heading = getNS($ns);
-        print '<div class="level1"><p>' . sprintf($lang['recent_global'], $heading, wl('', 'do=recent')) . '</p></div>';
+        if ($heading != '') 
+            print '<div class="level1"><p>' . sprintf($lang['recent_global'], $heading, wl('', 'do=recent')) . '</p></div>';
     }
 
     $form = new Doku_Form(array('id' => 'dw__recent', 'method' => 'GET'));