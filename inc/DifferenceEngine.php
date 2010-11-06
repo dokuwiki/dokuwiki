5e1ee188750eca4ed2f13227ede216598c9669c8
Xin LI <delphij@FreeBSD.org>
2010-11-07 02:59:21 +0800

    Fix unicode handling.
    Suggested by:   MQ <antinvidia gmail com>
    
    Signed-off-by: Xin LI <delphij@FreeBSD.org>

--- inc/DifferenceEngine.php
+++ inc/DifferenceEngine.php
@@ -890,7 +890,7 @@ class WordLevelDiff extends MappedDiff {
     }
 
     function _split($lines) {
-        if (!preg_match_all('/ ( [^\S\n]+ | [0-9_A-Za-z\x80-\xff]+ | . ) (?: (?!< \n) [^\S\n])? /xs',
+        if (!preg_match_all('/ ( [^\S\n]+ | [0-9_A-Za-z\x80-\xff]+ | . ) (?: (?!< \n) [^\S\n])? /xsu',
              implode("\n", $lines), $m)) {
             return array(array(''), array(''));
         }
