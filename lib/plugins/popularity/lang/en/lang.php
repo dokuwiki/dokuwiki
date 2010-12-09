fb7b772309f4272b67352a142b8afb1afd9743e5
Andreas Gohr <andi@splitbrain.org>
2010-12-09 22:13:06 +0100

    some language improvements(?) for the popularity plugin

--- lib/plugins/popularity/lang/en/lang.php
+++ lib/plugins/popularity/lang/en/lang.php
@@ -1,8 +1,8 @@
 <?php
 
-$lang['name'] = 'Popularity Feedback (may take some time to load)';
-$lang['submit'] = 'Send Data';
-$lang['autosubmit'] = 'Automatically send data once a month (may be (un)selected later)';
+$lang['name']             = 'Popularity Feedback (may take some time to load)';
+$lang['submit']           = 'Send Data';
+$lang['autosubmit']       = 'Automatically send data once a month';
 $lang['submissionFailed'] = 'The data couldn\'t be sent due to the following error:';
-$lang['submitDirectly'] = 'You may sent this data directly, validating the following form';
-$lang['autosubmitError'] = 'The last time we tried to autosubmit the data, we couldn\'t, because of the following error: ';
+$lang['submitDirectly']   = 'You can send the data manually by submitting the following form.';
+$lang['autosubmitError']  = 'The last autosubmit failed, because of the following error: ';
