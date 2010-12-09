98be6429ce6efaa013146af9145aa572816adb89
Guillaume Turri <guillaume.turri@gmail.com>
2010-12-09 22:03:59 +0100

    Add support for autosubmitting popularity data FS#2025

--- lib/plugins/popularity/lang/en/lang.php
+++ lib/plugins/popularity/lang/en/lang.php
@@ -2,3 +2,7 @@
 
 $lang['name'] = 'Popularity Feedback (may take some time to load)';
 $lang['submit'] = 'Send Data';
+$lang['autosubmit'] = 'Automatically send data once a month (may be (un)selected later)';
+$lang['submissionFailed'] = 'The data couldn\'t be sent due to the following error:';
+$lang['submitDirectly'] = 'You may sent this data directly, validating the following form';
+$lang['autosubmitError'] = 'The last time we tried to autosubmit the data, we couldn\'t, because of the following error: ';
