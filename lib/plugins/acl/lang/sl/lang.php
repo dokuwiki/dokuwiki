f2cfd2ce9ab3c204e78cd3e6589f8bb8d0079621
Gregor Skumavc <grega.skumavc@gmail.com>
2010-12-21 21:10:33 +0100

    Slovak language update

--- lib/plugins/acl/lang/sl/lang.php
+++ lib/plugins/acl/lang/sl/lang.php
@@ -4,14 +4,29 @@
  *
  * @author Dejan Levec <webphp@gmail.com>
  * @author Boštjan Seničar <senicar@gmail.com>
+ * @author Gregor Skumavc (grega.skumavc@gmail.com)
  */
+$lang['admin_acl']             = 'ACL administracija';
 $lang['acl_group']             = 'Skupina';
 $lang['acl_user']              = 'Uporabnik';
 $lang['acl_perms']             = 'Dovoljenja za';
 $lang['page']                  = 'Stran';
+$lang['namespace']             = 'Imenski prostor';
 $lang['btn_select']            = 'Izberi';
+$lang['p_user_id']             = 'Uporabnik %s ima trenutno naslednje pravice na strani %s: %s.';
+$lang['p_user_ns']             = 'Uporabnik %s ima trenutno naslednje pravice v imenskem prostoru %s: %s.';
+$lang['p_group_id']            = 'Uporabnikka skupina %s ima trenutno naslednje pravice na strani %s: %s.';
+$lang['p_group_ns']            = 'Uporabniška skupina %s ima trenutno naslednje pravice v imenskem prostoru %s: %s.';
+$lang['p_choose_id']           = 'Prosimo vnesite <b>uporabnika ali skupino</b> v zgornji obrazec za ogled ali urejanje pravic za stran %s.';
+$lang['p_choose_ns']           = 'Prosimo vnesite <b>uporabnika ali skupino</b> v zgornji obrazec za ogled ali urejanje pravic za imenski prostor %s.';
+$lang['p_inherited']           = 'Opomba: Te pravice niso bile posebej nastavljene, temveč prevzete iz drugih skupin ali višjih imenskih prostorov.';
+$lang['p_isadmin']             = 'Opomba: Izbrana skupina ali uporabnik imajo vedno vse pravice, ker so določeni kot superuporabniki.';
+$lang['p_include']             = 'Višje pravice vključujejo tudi nižje. ';
+$lang['current']               = 'Trenutna ACL pravila';
+$lang['where']                 = 'Stran / Imenski prostor';
 $lang['who']                   = 'Uporabnik/Skupina';
 $lang['perm']                  = 'Dovoljenja';
+$lang['acl_perm0']             = 'Nič';
 $lang['acl_perm1']             = 'Preberi';
 $lang['acl_perm2']             = 'Uredi';
 $lang['acl_perm4']             = 'Ustvari';
--- lib/plugins/usermanager/lang/sl/lang.php
+++ lib/plugins/usermanager/lang/sl/lang.php
@@ -4,17 +4,35 @@
  *
  * @author Dejan Levec <webphp@gmail.com>
  * @author Boštjan Seničar <senicar@gmail.com>
+ * @author Gregor Skumavc (grega.skumavc@gmail.com)
  */
+$lang['menu']                  = 'Urejanje uporabnikov';
+$lang['noauth']                = '(preverjanje uporabnikov ni na voljo)';
+$lang['nosupport']             = '(urejanje uporabnikov ni podprto)';
+$lang['badauth']               = 'neeljaven mehanizem za preverjanje';
 $lang['user_id']               = 'Uporabnik';
 $lang['user_pass']             = 'Geslo';
+$lang['user_name']             = 'Pravo ime';
 $lang['user_mail']             = 'Email';
 $lang['user_groups']           = 'Skupine';
+$lang['field']                 = 'Polje';
+$lang['value']                 = 'Vrednost';
 $lang['add']                   = 'Dodaj';
 $lang['delete']                = 'Izbriši';
 $lang['delete_selected']       = 'Izbriši izbrano';
 $lang['edit']                  = 'Uredi';
+$lang['edit_prompt']           = 'Uredi tega uporabnika';
 $lang['modify']                = 'Shrani spremembe';
 $lang['search']                = 'Iskanje';
+$lang['search_prompt']         = 'Išči';
+$lang['clear']                 = 'Ponastavi filter iskanja';
+$lang['filter']                = 'Filter';
+$lang['summary']               = 'Prikazujem uporabnike %1$d-%2$d od najdenih %3$d. Vseh je %4$d.';
+$lang['nonefound']             = 'Ni najdenih uporabnikov. Vseh uporabnikov je %d.';
+$lang['delete_ok']             = '%d uporabnikov izbrisanih';
+$lang['delete_fail']           = '%d ni bilo možno izbrisati';
+$lang['update_ok']             = 'Uporabnik uspešno posodobljen';
+$lang['update_fail']           = 'Posodobitev uporabnika ni uspela';
 $lang['prev']                  = 'Prejšnji';
 $lang['next']                  = 'Naslednji';
 $lang['last']                  = 'Zadnji';
