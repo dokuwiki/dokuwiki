178c8be8577764b678fc145787fb5a5fe5bd5bf7
Massimiliano Vassalli <vassalli.max@gmail.com>
2011-01-09 11:01:51 +0100

    Latin language update
    - better use of Latin terminology for DokuWiki
    - ACL is not translated as AAA (Aditus Administrationis Aditus) but
      with ICA (Index Custodiae Aditus), which is a more literal translation
    - minor corrections of grammar and logic
    - The word "namespace" is more literally translated "spatium
      nominis", but according to other translations (like the Italian one),
      I preferred to use "genus".

--- lib/plugins/acl/lang/la/lang.php
+++ lib/plugins/acl/lang/la/lang.php
@@ -2,25 +2,26 @@
 /**
  * Latin language file
  *
+ * @author Massimiliano Vassalli <vassalli.max@gmail.com>
  */
-$lang['admin_acl']             = 'Aditus Administrationis Aditus';
+$lang['admin_acl']             = 'Administratio Indicis Custodiae Aditus';
 $lang['acl_group']             = 'Grex';
 $lang['acl_user']              = 'Sodalis';
 $lang['acl_perms']             = 'Facultas:';
 $lang['page']                  = 'Pagina';
-$lang['namespace']             = 'Spatium nominis';
+$lang['namespace']             = 'Genus';
 $lang['btn_select']            = 'eligere';
 $lang['p_user_id']             = 'Sodalis <b class="acluser">%s</b> nunc has facultates paginae "<b class="aclpage">%s</b> habes: <i>%s</i>.';
-$lang['p_user_ns']             = 'Sodalis <b class="acluser">%s</b> nunc has facultates spatio nominis "<b class="aclns">%s</b> habes: <i>%s</i>.';
+$lang['p_user_ns']             = 'Sodalis <b class="acluser">%s</b> nunc has facultates generis "<b class="aclns">%s</b> habes: <i>%s</i>.';
 $lang['p_group_id']            = 'Socius\a gregis <b class="aclgroup">%s</b> nunc has facultates paginae "<b class="aclpage">%s</b> habes: <i>%s</i>.';
-$lang['p_group_ns']            = 'Socius\a gregis <b class="aclgroup">%s</b> nunc has facultates paginae "<b class="aclns">%s</b> habes: <i>%s</i>.';
+$lang['p_group_ns']            = 'Socius\a gregis <b class="aclgroup">%s</b> nunc has facultates generis "<b class="aclns">%s</b> habes: <i>%s</i>.';
 $lang['p_choose_id']           = '<b>Sodalis grexue</b> in campo insere ut facultates paginae <b class="aclpage">%s</b> uideas.';
-$lang['p_choose_ns']           = '<b>Sodalis grexue</b> in campo insere ut facultates paginae <b class="aclns">%s</b> uideas.';
-$lang['p_inherited']           = 'Caue: hae facultates et huic rei et aliis gregibus uel spatiis nominis legitimae sunt.';
+$lang['p_choose_ns']           = '<b>Sodalis grexue</b> in campo insere ut facultates generis <b class="aclns">%s</b> uideas.';
+$lang['p_inherited']           = 'Caue: hae facultates et huic rei et aliis gregibus uel generibus legitimae sunt.';
 $lang['p_isadmin']             = 'Caue: electi greges semper plenum ius habent, eo quod ut magister\stra elegitur.';
-$lang['p_include']             = 'Maiores facultates minores includunt. Creare, onerare uel delere facultates solum spatis nominis, non paginis potes';
-$lang['current']               = 'Communes AAA leges';
-$lang['where']                 = 'Pagina/Spatium nominis';
+$lang['p_include']             = 'Maiores facultates minores includunt. Creandi, onerandi uel delendi facultates solum generibus, non paginis sunt.';
+$lang['current']               = 'Communes ICA leges';
+$lang['where']                 = 'Pagina/Genus';
 $lang['who']                   = 'Sodalis/Grex';
 $lang['perm']                  = 'Facultates';
 $lang['acl_perm0']             = 'Nihil';
