8e65cd510bfa93eba1b847b6cbea1764fad480af
Otto Vainio <otto@valjakko.net>
2011-04-01 19:56:32 +0200

    Finnish language update

--- lib/plugins/popularity/lang/fi/lang.php
+++ lib/plugins/popularity/lang/fi/lang.php
@@ -7,3 +7,8 @@
  */
 $lang['name']                  = 'Suosion palaute (voi kestää jonkun aikaa latautua)';
 $lang['submit']                = 'Lähetä tiedot';
+$lang['autosubmit']            = 'Lähetä tiedot automaattisesti kerran kuussa';
+$lang['submissionFailed']      = 'Tietoja ei voitu lähettää seuraavan virheen vuoksi:';
+$lang['submitDirectly']        = 'Voit lähettää tiedot manuaalisesti.';
+$lang['autosubmitError']       = 'Edellinen automaattinen lähetys epäonnistui seuraavan virheen vuoksi:';
+$lang['lastSent']              = 'Tiedot on nyt lähettetty.';
