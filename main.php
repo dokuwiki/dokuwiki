c767156255555eb73bc3f80833ca0cb4026491fe
Anika Henke <anika@selfthinker.org>
2011-10-10 18:39:56 +0100

    improved page tools design, added 4 main icons, implemented 3 different temporary versions (version01-03)

--- main.php
+++ main.php
@@ -139,12 +139,12 @@ $showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && $_SERVER
                     <div class="tools">
                         <ul>
                             <?php
-                                tpl_action('edit', 1, 'li');
-                                tpl_action('revert', 1, 'li');
-                                tpl_action('history', 1, 'li');
-                                tpl_action('backlink', 1, 'li');
-                                tpl_action('subscribe', 1, 'li');
-                                tpl_action('top', 1, 'li');
+                                tpl_action('edit',      1, 'li', 0, '<span>', '</span>');
+                                tpl_action('revert',    1, 'li', 0, '<span>', '</span>');
+                                tpl_action('history',   1, 'li', 0, '<span>', '</span>');
+                                tpl_action('backlink',  1, 'li', 0, '<span>', '</span>');
+                                tpl_action('subscribe', 1, 'li', 0, '<span>', '</span>');
+                                tpl_action('top',       1, 'li', 0, '<span>', '</span>');
                             ?>
                         </ul>
                     </div>
