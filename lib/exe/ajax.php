dokuwiki/dokuwiki

commit 475aa19209642f8c6b5567884c76b70a1a68c156

Added urldecoding of query for qsearch

lib/exe/ajax.php
@@ -53,6 +53,8 @@ function ajax_qsearch(){
     $query = $_POST['q'];
     if(empty($query)) $query = $_GET['q'];
     if(empty($query)) return;
+    
+    $query = urldecode($query);
 
     $data = ft_pageLookup($query, true, useHeading('navigation'));
 

commit 2e646d615cc45b435080af6f34c1af04e92be3c9

fixed whitespace error introduced with 475aa19

lib/exe/ajax.php
@@ -53,7 +53,7 @@ function ajax_qsearch(){
     $query = $_POST['q'];
     if(empty($query)) $query = $_GET['q'];
     if(empty($query)) return;
-    
+
     $query = urldecode($query);
 
     $data = ft_pageLookup($query, true, useHeading('navigation'));