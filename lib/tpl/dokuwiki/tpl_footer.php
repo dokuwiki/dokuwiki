a4380643ef4658cfe5340f5963a54a045a529c5b
Anika Henke <anika@selfthinker.org>
2012-03-10 21:04:15 +0000

    improved footer readability and css

--- lib/tpl/dokuwiki/tpl_footer.php
+++ lib/tpl/dokuwiki/tpl_footer.php
@@ -1,18 +1,22 @@
 <!-- ********** FOOTER ********** -->
 <div id="dokuwiki__footer"><div class="pad">
+    <?php tpl_license(''); // license text ?>
+
+    <div class="buttons">
         <?php
-            $tgt = ($conf['target']['extern']) ? 'target="'.$conf['target']['extern'].'"' : '';
-            tpl_license(''); // text only
+            tpl_license('button', true, false, false); // license button, no wrapper
+            $target = ($conf['target']['extern']) ? 'target="'.$conf['target']['extern'].'"' : '';
         ?>
-
-    <div class="footerbuttons">
-
-        <?php tpl_license('button',true,false,false); // button, no wrapper?>
-        <a <?php echo $tgt?> href="http://www.dokuwiki.org/donate" title="Donate"><img src="<?php echo tpl_basedir(); ?>images/button-donate.gif" alt="Donate" width="80" height="15" /></a>
-        <a <?php echo $tgt?> href="http://www.php.net" title="Powered by PHP"><img src="<?php echo tpl_basedir(); ?>images/button-php.gif" width="80" height="15" alt="Powered by PHP" /></a>
-        <a <?php echo $tgt?> href="http://validator.w3.org/check/referer" title="Valid XHTML 1.0"><img src="<?php echo tpl_basedir(); ?>images/button-xhtml.png" width="80" height="15" alt="Valid XHTML 1.0" /></a>
-        <a <?php echo $tgt?> href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS"><img src="<?php echo tpl_basedir(); ?>images/button-css.png" width="80" height="15" alt="Valid CSS" /></a>
-        <a <?php echo $tgt?> href="http://dokuwiki.org/" title="Driven by DokuWiki"><img src="<?php echo tpl_basedir(); ?>images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" /></a>
+        <a href="http://www.dokuwiki.org/donate" title="Donate" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>images/button-donate.gif" width="80" height="15" alt="Donate" /></a>
+        <a href="http://www.php.net" title="Powered by PHP" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>images/button-php.gif" width="80" height="15" alt="Powered by PHP" /></a>
+        <a href="http://validator.w3.org/check/referer" title="Valid XHTML 1.0" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>images/button-xhtml.png" width="80" height="15" alt="Valid XHTML 1.0" /></a>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>images/button-css.png" width="80" height="15" alt="Valid CSS" /></a>
+        <a href="http://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>><img
+            src="<?php echo tpl_basedir(); ?>images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" /></a>
     </div>
 </div></div><!-- /footer -->
 
