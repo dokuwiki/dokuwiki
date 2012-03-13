9e760ee516dd6e50390490f6d5585b854b895808
Andreas Gohr <gohr@cosmocode.de>
2012-03-13 17:32:53 +0100

    added (failing) test for cleanText()
    The cleanText function is supposed to convert DOS to Unix lineendings
    but it seems that it doesn't always do that correctly as this thread
    suggests: http://forum.dokuwiki.org/thread/8141
    
    I added a unit test that currently fails but haven't found the real
    cause yet. Further testing (and a fix) is needed.

---
+++ _test/cases/inc/common_cleanText.test.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once DOKU_INC.'inc/init.php';
+require_once DOKU_INC.'inc/common.php';
+
+class common_clientIP_test extends UnitTestCase {
+
+    function test_unix(){
+        $unix = 'one
+                two
+
+                three';
+
+        $this->assertEqual($unix,cleanText($unix));
+    }
+
+    function test_win(){
+        $unix = 'one
+                two
+
+                three';
+        $win  = 'one
+                two
+                
+                three';
+        $this->assertNotEqual($unix,$win);
+        $this->assertEqual($unix,cleanText($win));
+    }
+}
+
+//Setup VIM: ex: et ts=4 :
