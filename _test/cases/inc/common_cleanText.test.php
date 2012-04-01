1a9261d2580d116502845e63eea2eb08c316cb8b
Dominik Eckelmann <deckelmann@gmail.com>
2012-04-01 22:03:16 +0200

    fixed testcase class name

--- _test/cases/inc/common_cleanText.test.php
+++ _test/cases/inc/common_cleanText.test.php
@@ -3,7 +3,7 @@
 require_once DOKU_INC.'inc/init.php';
 require_once DOKU_INC.'inc/common.php';
 
-class common_clientIP_test extends UnitTestCase {
+class common_cleanText extends UnitTestCase {
 
     function test_unix(){
         $unix = 'one
