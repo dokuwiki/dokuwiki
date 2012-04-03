2807b5cc8c1dc0b316d4eba2a467cd903078ac84
Dominik Eckelmann <deckelmann@gmail.com>
2012-04-03 15:05:45 +0200

    transformed untf8_ test to phpunit

---
+++ _testing/unittests/inc/utf8_stripspecials.test.php
@@ -0,0 +1,28 @@
+<?php
+// use no mbstring help here
+if(!defined('UTF8_NOMBSTRING')) define('UTF8_NOMBSTRING',1);
+require_once DOKU_INC.'inc/utf8.php';
+
+class utf8_stripspecials extends PHPUnit_Framework_TestCase {
+
+
+    function test1(){
+        // we test multiple cases here - format: string, repl, additional, test
+        $tests   = array();
+        $tests[] = array('asciistring','','','asciistring');
+        $tests[] = array('asciistring','','\._\-:','asciistring');
+        $tests[] = array('ascii.string','','\._\-:','asciistring');
+        $tests[] = array('ascii.string',' ','\._\-:','ascii string');
+        $tests[] = array('2.1.14',' ','\._\-:','2 1 14');
+        $tests[] = array('ascii.string','','\._\-:\*','asciistring');
+        $tests[] = array('ascii.string',' ','\._\-:\*','ascii string');
+        $tests[] = array('2.1.14',' ','\._\-:\*','2 1 14');
+        $tests[] = array('string with nbsps','_','\*','string_with_nbsps');
+
+        foreach($tests as $test){
+            $this->assertEquals(utf8_stripspecials($test[0],$test[1],$test[2]),$test[3]);
+        }
+    }
+
+}
+//Setup VIM: ex: et ts=4 :
