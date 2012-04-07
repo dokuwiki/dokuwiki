bde5bbdc09d9d6a01503fcd531261df2dd07188f
Dominik Eckelmann <deckelmann@gmail.com>
2012-04-07 22:30:32 +0200

    adjusted memory for report generation

--- _testing/unittests/bootstrap.php
+++ _testing/unittests/bootstrap.php
@@ -6,5 +6,5 @@ if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../')
 define('DOKU_CONF',realpath(dirname(__FILE__).'/../../conf').'/');
 
 error_reporting(E_ALL);
-set_time_limit(600);
-ini_set('memory_limit','128M');
+set_time_limit(0);
+ini_set('memory_limit','2048M');
