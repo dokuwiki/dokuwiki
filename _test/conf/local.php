95d9eba688b6b52170f84ec240414e540f11cdd5
Andreas Gohr <andi@splitbrain.org>
2012-04-23 12:37:07 +0200

    disable DNS lookups in tests

--- _test/conf/local.php
+++ _test/conf/local.php
@@ -7,3 +7,4 @@ $conf['title']       = 'My Test Wiki';    //what to show in the title
 $conf['useacl']      = 1;                 //Use Access Control Lists to restrict access?
 $conf['superuser']   = 'testuser';        //password: testpass
 
+$conf['dnslookups']  = 0;                 //speedup tests
