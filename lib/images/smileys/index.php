a0df75e62c09594d1d1ed11be9a911badab0699e
Elan Ruusamäe <glen@delfi.ee>
2012-09-17 11:08:34 +0300

    add index file similar to fileicons to show active smileys

---
+++ lib/images/smileys/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <title>simleys</title>
+
+    <style type="text/css">
+        body {
+            background-color: #ccc;
+            font-family: Arial;
+        }
+
+        .box {
+            width: 200px;
+            float:left;
+            padding: 0.5em;
+            margin: 0;
+        }
+
+        .white {
+            background-color: #fff;
+        }
+
+        .black {
+            background-color: #000;
+        }
+    </style>
+
+</head>
+<body>
+
+<div class="white box">
+<?php
+foreach (glob('*.gif') as $img) {
+    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+}
+?>
+</div>
+
+<div class="black box">
+<?php
+foreach (glob('*.gif') as $img) {
+    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+}
+?>
+</div>
+
+</body>
+</html>
