85159e75b55f6a10010af360b6ebf9f952994511
Anika Henke <anika@selfthinker.org>
2012-10-07 02:55:37 +0100

    removed stray closing div (FS#2633)

--- lib/tpl/dokuwiki/main.php
+++ lib/tpl/dokuwiki/main.php
@@ -91,7 +91,7 @@ $showSidebar = $hasSidebar && ($ACT=='show');
     </div></div><!-- /site -->
 
     <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
-    <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?></div>
+    <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
     <!--[if ( lte IE 7 | IE 8 ) ]></div><![endif]-->
 </body>
 </html>
