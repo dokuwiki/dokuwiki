e20faad8330af3e6f2edbd6e8dc3f70c05305492
Victor Westmann <victor.westmann@gmail.com>
2013-03-24 10:28:51 +0100

    Brazilian Portuguese language update

---
+++ lib/plugins/authmysql/lang/pt-br/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Brazilian Portuguese language file
+ *
+ * @author Victor Westmann <victor.westmann@gmail.com>
+ */
+$lang['server']                = 'Seu servidor MySQL';
+$lang['user']                  = 'usuário MySQL';
+$lang['password']              = 'Senha do usuário acima';
+$lang['database']              = 'Base de dados para usar';
+$lang['charset']               = 'Codificação de caracter usado na base de dados';
+$lang['debug']                 = 'Mostrar informações adicionais de depuração';
+$lang['forwardClearPass']      = 'Passar senhas de usuários como texto puro para comandos SQL abaixo, ao invés de usar opção passcrypt';
+$lang['TablesToLock']          = 'Lista separada por vírgulas para tabelas que devem estar travadas em operações de escrita';
+$lang['checkPass']             = 'Comandos SQL para verificar senhas';
+$lang['debug_o_0']             = 'nenhum';
+$lang['debug_o_1']             = 'apenas em erros';
+$lang['debug_o_2']             = 'todas as queries SQL';
