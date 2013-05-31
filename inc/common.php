a132f948f22ae344760ee3da82f9f92cf0f18b7b
Andreas Gohr <andi@splitbrain.org>
2013-05-31 09:25:43 +0200

    use HMAC for CSRF security tokens FS#2794

--- inc/common.php
+++ inc/common.php
@@ -56,7 +56,7 @@ function stripctl($string) {
  * @return  string
  */
 function getSecurityToken() {
-    return md5(auth_cookiesalt().session_id().$_SERVER['REMOTE_USER']);
+    return PassHash::hmac('md5', session_id().$_SERVER['REMOTE_USER'], auth_cookiesalt());
 }
 
 /**
