81ee3b1c4ea669b4278fc1160162c686b82e559c
Andreas Gohr <andi@splitbrain.org>
2013-06-16 18:09:34 +0200

    Swedish language update

---
+++ lib/plugins/authad/lang/sv/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Swedish language file
+ *
+ * @author Smorkster Andersson smorkster@gmail.com
+ */
+$lang['account_suffix']        = 'Ditt konto suffix. T.ex. <code>min.domän.org</code>';
+$lang['admin_password']        = 'Lösenord för användare ovan.';
+$lang['sso']                   = 'Ska Single-Sign-On via Kerberos eller NTLM användas?';
+$lang['use_ssl']               = 'Använda SSL anslutning? Om använd, möjliggör inte TLS nedan.';
+$lang['use_tls']               = 'Använda TLS anslutning? Om använd, möjliggör inte SSL ovan.';
