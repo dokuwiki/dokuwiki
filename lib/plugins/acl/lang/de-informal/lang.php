b243b168571b08a674d34701cfbb7ac0a03239e0
Volker Bödker <volker@boedker.de>
2013-07-12 17:04:41 +0200

    Informal German language update

--- lib/plugins/acl/lang/de-informal/lang.php
+++ lib/plugins/acl/lang/de-informal/lang.php
@@ -9,6 +9,7 @@
  * @author Christian Wichmann <nospam@zone0.de>
  * @author Pierre Corell <info@joomla-praxis.de>
  * @author Frank Loizzi <contact@software.bacal.de>
+ * @author Volker Bödker <volker@boedker.de>
  */
 $lang['admin_acl']             = 'Zugangsverwaltung';
 $lang['acl_group']             = 'Gruppe';
