22064dd98283bc61e1b444e45707c1c51230eb92
Márton Sebők <sebokmarton@gmail.com>
2013-07-28 02:12:20 +0200

    Hungarian language update

--- lib/plugins/revert/lang/hu/lang.php
+++ lib/plugins/revert/lang/hu/lang.php
@@ -8,6 +8,7 @@
  * @author Szabó Dávid <szabo.david@gyumolcstarhely.hu>
  * @author Sándor TIHANYI <stihanyi+dw@gmail.com>
  * @author David Szabo <szabo.david@gyumolcstarhely.hu>
+ * @author Marton Sebok <sebokmarton@gmail.com>
  */
 $lang['menu']                  = 'Visszaállítás kezelő (anti-SPAM)';
 $lang['filter']                = 'SPAM tartalmú oldalak keresése';
@@ -16,5 +17,5 @@ $lang['reverted']              = '%s a következő változatra lett visszaállí
 $lang['removed']               = '%s törölve';
 $lang['revstart']              = 'A visszaállítási folyamat elindult. Ez hosszú ideig eltarthat. Ha időtúllépés miatt nem tud lefutni, kisebb darabbal kell próbálkozni.';
 $lang['revstop']               = 'A visszaállítási folyamat sikeresen befejeződött.';
-$lang['note1']                 = 'Megjegyzés: a keresés kisbetű-nagybetűre érzékeny';
+$lang['note1']                 = 'Megjegyzés: a keresés kisbetű-nagybetű érzékeny';
 $lang['note2']                 = 'Megjegyzés: Az oldalt az utolsó olyan változatra állítjuk vissza, ami nem tartalmazza a megadott spam kifejezést: <i>%s</i>.';
