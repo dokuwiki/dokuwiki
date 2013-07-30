627fc03f93cc74f443af98f4fbf25564cc24d3fc
Guy Brand <gb@unistra.fr>
2013-07-30 11:10:34 +0200

    Fix for the FS#1833 parser error

--- inc/parser/parser.php
+++ inc/parser/parser.php
@@ -454,8 +454,8 @@ class Doku_Parser_Mode_table extends Doku_Parser_Mode {
     }
 
     function connectTo($mode) {
-        $this->Lexer->addEntryPattern('\n\^',$mode,'table');
-        $this->Lexer->addEntryPattern('\n\|',$mode,'table');
+        $this->Lexer->addEntryPattern('\s*\n\^',$mode,'table');
+        $this->Lexer->addEntryPattern('\s*\n\|',$mode,'table');
     }
 
     function postConnect() {
