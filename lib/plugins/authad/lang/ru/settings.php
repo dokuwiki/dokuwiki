bcc165558b4d58baab4ccc85d9066f0c4ba7de2a
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-08-20 23:05:58 +0200

    translation update

--- lib/plugins/authad/lang/ru/settings.php
+++ lib/plugins/authad/lang/ru/settings.php
@@ -1,6 +1,9 @@
 <?php
+
 /**
- * Russian language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Ivan I. Udovichenko (sendtome@mymailbox.pp.ua)
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
  */
+$lang['admin_password']        = 'Пароль для указанного пользователя.';
