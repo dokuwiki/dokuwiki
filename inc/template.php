d3d77c77340e55040b3eae8d39c9eb676a9f6821
Junling Ma <junlingm@gmail.com>
2013-10-05 22:42:07 -0700

    Remove tpl_media(), because it is now the media action html() method.

--- inc/template.php
+++ inc/template.php
@@ -1712,49 +1712,6 @@ function tpl_favicon($types = array('favicon')) {
 }
 
 /**
- * Prints full-screen media manager
- *
- * @author Kate Arzamastseva <pshns@ukr.net>
- */
-function tpl_media() {
-    global $NS, $IMG, $JUMPTO, $REV, $lang, $fullscreen, $INPUT;
-    $fullscreen = true;
-    require_once DOKU_INC.'lib/exe/mediamanager.php';
-
-    $rev   = '';
-    $image = cleanID($INPUT->str('image'));
-    if(isset($IMG)) $image = $IMG;
-    if(isset($JUMPTO)) $image = $JUMPTO;
-    if(isset($REV) && !$JUMPTO) $rev = $REV;
-
-    echo '<div id="mediamanager__page">'.NL;
-    echo '<h1>'.$lang['btn_media'].'</h1>'.NL;
-    html_msgarea();
-
-    echo '<div class="panel namespaces">'.NL;
-    echo '<h2>'.$lang['namespaces'].'</h2>'.NL;
-    echo '<div class="panelHeader">';
-    echo $lang['media_namespaces'];
-    echo '</div>'.NL;
-
-    echo '<div class="panelContent" id="media__tree">'.NL;
-    media_nstree($NS);
-    echo '</div>'.NL;
-    echo '</div>'.NL;
-
-    echo '<div class="panel filelist">'.NL;
-    tpl_mediaFileList();
-    echo '</div>'.NL;
-
-    echo '<div class="panel file">'.NL;
-    echo '<h2 class="a11y">'.$lang['media_file'].'</h2>'.NL;
-    tpl_mediaFileDetails($image, $rev);
-    echo '</div>'.NL;
-
-    echo '</div>'.NL;
-}
-
-/**
  * Return useful layout classes
  *
  * @author Anika Henke <anika@selfthinker.org>
