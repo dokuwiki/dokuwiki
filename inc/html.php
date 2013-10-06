edca285c7dcd587c7e939c82496c1b749fb7a426
Junling Ma <junlingm@gmail.com>
2013-10-05 18:19:36 -0700

    Implemented the lock action using the new Doku_Action interface.

--- inc/html.php
+++ inc/html.php
@@ -293,28 +293,6 @@ function html_search(){
 }
 
 /**
- * Display error on locked pages
- *
- * @author Andreas Gohr <andi@splitbrain.org>
- */
-function html_locked(){
-    global $ID;
-    global $conf;
-    global $lang;
-    global $INFO;
-
-    $locktime = filemtime(wikiLockFN($ID));
-    $expire = dformat($locktime + $conf['locktime']);
-    $min    = round(($conf['locktime'] - (time() - $locktime) )/60);
-
-    print p_locale_xhtml('locked');
-    print '<ul>';
-    print '<li><div class="li"><strong>'.$lang['lockedby'].':</strong> '.editorinfo($INFO['locked']).'</div></li>';
-    print '<li><div class="li"><strong>'.$lang['lockexpire'].':</strong> '.$expire.' ('.$min.' min)</div></li>';
-    print '</ul>';
-}
-
-/**
  * list old revisions
  *
  * @author Andreas Gohr <andi@splitbrain.org>
