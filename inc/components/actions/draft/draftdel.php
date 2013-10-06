d20e06060daa021f8d34e1dc58ac5d02dcd1090a
Junling Ma <junlingm@gmail.com>
2013-10-05 17:44:40 -0700

    Implemented the draftdel action using the new Doku_Action interface.

---
+++ inc/components/actions/draft/draftdel.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once(DOKU_INC . "/inc/components/action.php");
+
+/**
+ * Handler for the draftdel action
+ * 
+ * @author Junling Ma <junlingm@gmail.com>
+ */
+class Doku_Action_Draftdel extends Doku_Action
+{
+    /**
+     * Specifies the action name
+     * 
+     * @return string the action name
+     */
+    public function action() {
+        return "draftdel";
+    }
+
+    /**
+     * Specifies the required permissions to delete a draft
+     * 
+     * @return string the permission required
+     */
+    public function permission_required() {
+        return AUTH_EDIT;
+    }
+
+    /**
+     * handling draft deletion
+     *
+     * @global array $INFO
+     * @return string the next action
+     */
+    public function handle() {
+        global $INFO;
+        @unlink($INFO['draft']);
+        $INFO['draft'] = null;
+        return 'show';
+    }
+}
