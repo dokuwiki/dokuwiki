48e49553b3ae1dc9624cdd96b34ffe0175ba8ae3
Jens Hyllegaard <jens.hyllegaard@gmail.com>
2013-10-21 14:36:35 +0200

    translation update

---
+++ lib/plugins/authmysql/lang/da/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jens Hyllegaard <jens.hyllegaard@gmail.com>
+ */
+$lang['server']                = 'Din MySQL server';
+$lang['debug']                 = 'Vis yderligere debug output';
+$lang['debug_o_0']             = 'ingen';
+$lang['debug_o_1']             = 'kun ved fejl';
+$lang['debug_o_2']             = 'alle SQL forespørgsler';
