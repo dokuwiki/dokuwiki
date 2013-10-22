8fd314cd13c63a72ffe77c1935d30169c1a63186
Junling Ma <junlingm@gmail.com>
2013-10-21 19:36:07 -0700

    Implemented the "show" action handler and its renderer.

---
+++ inc/commands/show.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * The show action handler.
+ * 
+ * @author Junling Ma <junlingm@gmail.com>
+ */
+class Doku_Action_Show extends Doku_Action
+{
+    /**
+     * Specifies the action name
+     * 
+     * @return string the action name
+     */
+    public function action() {
+        return "show";
+    }
+
+    /**
+     * Specifies the required permissions to show a page.
+     * 
+     * @return string the permission required
+     */
+    public function permission_required() {
+        return AUTH_READ;
+    }
+}
+
+/**
+ * show action renderer
+ */
+class Doku_Action_Renderer_Show extends Doku_Action_Renderer
+{
+    /**
+     * Specifies the action name
+     * 
+     * @return string the action name
+     */
+    public function action() {
+        return "show";
+    }
+
+    /**
+     * render a page.
+     * 
+     * Adapted from html_show() originally written by 
+     * Andreas Gohr <andi@splitbrain.org>
+     * 
+     * @global string $ID
+     * @global string $REV
+     * @global string $HIGH
+     * @global array $INFO
+     */
+    public function xhtml() {
+        global $ID;
+        global $REV;
+        global $HIGH;
+        global $INFO;
+
+        // whether to show the secedit buttons
+        $secedit = !$REV;
+        if ($REV) print p_locale_xhtml('showrev');
+        $html = p_wiki_xhtml($ID,$REV,true);
+        $html = html_secedit($html,$secedit);
+        if($INFO['prependTOC']) $html = tpl_toc(true).$html;
+        $html = html_hilight($html,$HIGH);
+        echo $html;
+    }
+}
