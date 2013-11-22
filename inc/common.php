d81178045b12622c84f48c892ddda32c6aba5576
lisps <stummp@loewen.de>
2013-11-22 13:56:58 +0100

    restore old version of pageinfo()

--- inc/common.php
+++ inc/common.php
@@ -161,7 +161,7 @@ function pageinfo() {
     if($REV) {
         //check if current revision was meant
         if($info['exists'] && (@filemtime($info['filepath']) == $REV)) {
-            //$REV = ''; //if we enter a rev we want to see that revision (inc media files)
+            $REV = '';
         } elseif($RANGE) {
             //section editing does not work with old revisions!
             $REV   = '';
