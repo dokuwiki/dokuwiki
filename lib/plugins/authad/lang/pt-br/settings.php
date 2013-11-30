d2a843c310418267d3baedd63547d75917da2efe
Juliano Marconi Lanigra <juliano.marconi@gmail.com>
2013-11-30 23:10:58 +0100

    translation update

--- lib/plugins/authad/lang/pt-br/settings.php
+++ lib/plugins/authad/lang/pt-br/settings.php
@@ -5,6 +5,7 @@
  * 
  * @author Victor Westmann <victor.westmann@gmail.com>
  * @author Frederico Guimarães <frederico@teia.bio.br>
+ * @author Juliano Marconi Lanigra <juliano.marconi@gmail.com>
  */
 $lang['account_suffix']        = 'Sufixo de sua conta. Eg. <code>@meu.domínio.org</code>';
 $lang['base_dn']               = 'Sua base DN. Eg. <code>DC=meu,DC=domínio,DC=org</code>';
@@ -12,6 +13,7 @@ $lang['domain_controllers']    = 'Uma lista de controles de domínios separada p
 $lang['admin_username']        = 'Um usuário do Active Directory com privilégios para acessar os dados de todos os outros usuários. Opcional, mas necessário para realizar certas ações, tais como enviar mensagens de assinatura.';
 $lang['admin_password']        = 'A senha do usuário acima.';
 $lang['sso']                   = 'Usar Single-Sign-On através do Kerberos ou NTLM?';
+$lang['sso_charset']           = 'A codificação de caracteres que seu servidor web passará o nome de usuário Kerberos ou NTLM. Vazio para UTF-8 ou latin-1. Requere a extensão iconv.';
 $lang['real_primarygroup']     = 'O grupo primário real deve ser resolvido ao invés de assumirmos como "Usuários do Domínio" (mais lento)';
 $lang['use_ssl']               = 'Usar conexão SSL? Se usar, não habilitar TLS abaixo.';
 $lang['use_tls']               = 'Usar conexão TLS? se usar, não habilitar SSL acima.';
