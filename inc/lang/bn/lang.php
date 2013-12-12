0f50ca14027960e75aa0608ebbea52cfd89ceb08
ninetailz <ninetailz1125@gmail.com>
2013-12-12 04:35:14 +0100

    translation update

--- inc/lang/bn/lang.php
+++ inc/lang/bn/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Foysol <ragebot1125@gmail.com>
+ * @author ninetailz <ninetailz1125@gmail.com>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'itr';
@@ -25,3 +26,64 @@ $lang['btn_older']             = 'কম সাম্প্রতিক >>';
 $lang['btn_revs']              = 'প্রাচীন সংশোধন';
 $lang['btn_recent']            = 'সাধিত পরিবর্তনসমূহ';
 $lang['btn_upload']            = 'আপলোড করুন';
+$lang['btn_cancel']            = 'বাতিল করা';
+$lang['btn_index']             = 'সাইট ম্যাপ';
+$lang['btn_secedit']           = 'সম্পাদন করা';
+$lang['btn_login']             = 'লগইন';
+$lang['btn_logout']            = 'লগ আউট';
+$lang['btn_admin']             = 'অ্যাডমিন';
+$lang['btn_update']            = 'আধুনিক করা';
+$lang['btn_delete']            = 'মুছে ফেলা';
+$lang['btn_back']              = 'পিছনে';
+$lang['btn_backlink']          = 'ব্যাকলিঙ্কগুলি';
+$lang['btn_backtomedia']       = 'পিছনে Mediafile নির্বাচনে যান';
+$lang['btn_subscribe']         = 'সাবস্ক্রিপশন পরিচালনা করুন';
+$lang['btn_profile']           = 'প্রোফাইল আপডেট করুন';
+$lang['btn_reset']             = 'রিসেট করুন';
+$lang['btn_resendpwd']         = 'সেট করুন নতুন পাসওয়ার্ড';
+$lang['btn_draft']             = 'সম্পাদনা খসড়া';
+$lang['btn_recover']           = 'খসড়া উদ্ধার';
+$lang['btn_draftdel']          = 'খসড়া মুছে দিন';
+$lang['btn_revert']            = 'পুনরূদ্ধার করা';
+$lang['btn_register']          = 'খাতা';
+$lang['btn_apply']             = 'প্রয়োগ করা';
+$lang['btn_media']             = 'মিডিয়া ম্যানেজার';
+$lang['btn_deleteuser']        = 'আমার অ্যাকাউন্ট অপসারণ করুন';
+$lang['loggedinas']            = 'লগ ইন';
+$lang['user']                  = 'ইউজারনেম';
+$lang['pass']                  = 'পাসওয়ার্ড';
+$lang['newpass']               = 'নতুন পাসওয়ার্ড';
+$lang['oldpass']               = 'বর্তমান পাসওয়ার্ড নিশ্চিত করুন';
+$lang['passchk']               = 'আরো একবার';
+$lang['remember']              = 'আমাকে মনে রেখো';
+$lang['fullname']              = 'আমাকে মনে রেখো';
+$lang['email']                 = 'ই মেইল';
+$lang['profile']               = 'ব্যবহারকারী প্রোফাইল';
+$lang['badlogin']              = 'দুঃখিত, ব্যবহারকারীর নাম বা পাসওয়ার্ড ভুল ছিল.';
+$lang['badpassconfirm']        = 'দুঃখিত, পাসওয়ার্ড ভুল ছিল';
+$lang['minoredit']             = 'ক্ষুদ্র পরিবর্তনসমূহ';
+$lang['draftdate']             = 'খসড়া উপর স্বতঃসংরক্ষণ';
+$lang['nosecedit']             = 'পাতা ইতিমধ্যে পরিবর্তিত হয়েছিল, অধ্যায় তথ্যের পরিবর্তে পুরো পাতা লোড তারিখ সীমার বাইরে ছিল.
+';
+$lang['regmissing']            = 'দুঃখিত, আপনি সমস্ত ক্ষেত্রগুলি পূরণ করা আবশ্যক.';
+$lang['reguexists']            = 'দুঃখিত, এই লগইন সঙ্গে একটি ব্যবহারকারী ইতিমধ্যেই বিদ্যমান.';
+$lang['regsuccess']            = 'ব্যবহারকারী তৈরি করা হয়েছে এবং পাসওয়ার্ড ইমেইল করে পাঠানো হয়েছিল.';
+$lang['regsuccess2']           = 'ব্যবহারকারী তৈরি করা হয়েছে.';
+$lang['regmailfail']           = 'একটি ত্রুটি পাসওয়ার্ড মেইল পাঠানোর নেভিগেশন ছিল মনে হচ্ছে. অ্যাডমিন যোগাযোগ করুন!';
+$lang['regbadmail']            = 'প্রদত্ত ইমেইল ঠিকানা সঠিক মনে হচ্ছে - আপনি এই একটি ত্রুটি মনে হলে, অ্যাডমিন যোগাযোগ';
+$lang['regbadpass']            = 'দুটি প্রদত্ত পাসওয়ার্ড অভিন্ন নয়, আবার চেষ্টা করুন.';
+$lang['regpwmail']             = 'আপনার DokuWiki পাসওয়ার্ড';
+$lang['reghere']               = 'যদিও তোমার কোনো একাউন্ট নেই? শুধু একটি পেতে';
+$lang['profna']                = 'এই উইকি প্রোফাইল পরিবর্তন সমর্থন করে না';
+$lang['profnochange']          = 'এমন কোন পরিবর্তন, না কিছুই.';
+$lang['profnoempty']           = 'একটি খালি নাম অথবা ইমেইল ঠিকানা অনুমোদিত নয়.';
+$lang['profchanged']           = 'ইউজার প্রোফাইল সফলভাবে আপডেট.';
+$lang['profnodelete']          = 'এই উইকি ব্যবহারকারী মুছে ফেলার সমর্থন করে না';
+$lang['profdeleteuser']        = 'একাউন্ট মুছে দিন';
+$lang['profdeleted']           = 'আপনার অ্যাকাউন্টটি এই উইকি থেকে মুছে ফেলা হয়েছে';
+$lang['profconfdelete']        = 'আমি এই উইকি থেকে আমার অ্যাকাউন্ট অপসারণ করতে ইচ্ছুক. <br/> এই ক্রিয়াটি পূর্বাবস্থায় ফেরানো যায় না.';
+$lang['profconfdeletemissing'] = 'নিশ্চিতকরণ চেক বক্স ticked না';
+$lang['pwdforget']             = 'আপনার পাসওয়ার্ড ভুলে গেছেন? একটি নতুন পান';
+$lang['resendna']              = 'এই উইকি পাসওয়ার্ড পুনরায় প্রেরণ সমর্থন করে না.';
+$lang['resendpwd']             = 'জন্য সেট করুন নতুন পাসওয়ার্ড';
+$lang['resendpwdmissing']      = 'দুঃখিত, আপনি সমস্ত ক্ষেত্রগুলি পূরণ করা আবশ্যক.';
