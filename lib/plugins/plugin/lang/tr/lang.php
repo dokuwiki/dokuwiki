56305e5a7a8dafd9dc51350d2e39888f852f5638
huseyin can <huseyincan73@gmail.com>
2014-01-27 11:05:49 +0100

    translation update

---
+++ lib/plugins/plugin/lang/tr/lang.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aydın Coşkuner <aydinweb@gmail.com>
+ * @author Cihan Kahveci <kahvecicihan@gmail.com>
+ * @author Yavuz Selim <yavuzselim@gmail.com>
+ * @author Caleb Maclennan <caleb@alerque.com>
+ * @author farukerdemoncel@gmail.com
+ */
+$lang['menu']                  = 'Eklenti Yönetimi';
+$lang['download']              = 'Yeni bir eklenti indirip kur';
+$lang['manage']                = 'Kurulmuş Eklentiler';
+$lang['btn_info']              = 'bilgi';
+$lang['btn_update']            = 'güncelle';
+$lang['btn_delete']            = 'sil';
+$lang['btn_settings']          = 'Ayarlar';
+$lang['btn_download']          = 'İndir';
+$lang['btn_enable']            = 'Kaydet';
+$lang['url']                   = 'Web Adresi';
+$lang['installed']             = 'Kuruldu:';
+$lang['lastupdate']            = 'Son güncelleştirme:';
+$lang['source']                = 'Kaynak:';
+$lang['unknown']               = 'bilinmiyor';
+$lang['updating']              = 'Güncelleştiriyor ...';
+$lang['updated']               = '%s eklentisi başarıyla güncellendi';
+$lang['updates']               = 'Şu eklentiler başarıyla güncellendi';
+$lang['update_none']           = 'Yeni bir güncelleme bulunamadı.';
+$lang['deleting']              = 'Siliniyor ...';
+$lang['deleted']               = '%s eklentisi silindi.';
+$lang['downloading']           = 'İndiriyor ...';
+$lang['downloaded']            = '%s eklentisi başarıyla kuruldu';
+$lang['downloads']             = 'Şu eklentiler başarıyla kuruldu:';
+$lang['download_none']         = 'Eklenti bulunamadı veya indirirken/kurarken bilinmeyen bir hata oluştu.';
+$lang['plugin']                = 'Eklenti:';
+$lang['components']            = 'Parçalar';
+$lang['noinfo']                = 'Bu eklentinin bilgileri alınamadı, geçerli bir eklenti olmayabilir.';
+$lang['name']                  = 'Ad:';
+$lang['date']                  = 'Tarih:';
+$lang['type']                  = 'Tür:';
+$lang['desc']                  = 'Açıklama:';
+$lang['author']                = 'Yazar:';
+$lang['www']                   = 'Web Adresi:';
+$lang['error']                 = 'Bilinmeyen bir hata oluştu.';
+$lang['error_download']        = 'Şu eklenti indirilemedi: %s';
+$lang['error_badurl']          = 'Yanlış adres olabilir - verilen adresten dosya adı alınamadı';
+$lang['error_dircreate']       = 'İndirmek için geçici klasör oluşturulamadı';
+$lang['error_decompress']      = 'Eklenti yöneticisi indirilen sıkıştırılmış dosyayı açamadı. Bu yanlış indirmeden kaynaklanabilir (bu durumda tekrar denemelisiniz). Ya da indirilen dosyanın sıkıştırma biçimi bilinmemektedir (bu durumda eklentiyi indirerek kendiniz kurmalısınız).';
+$lang['error_copy']            = '<em>%s</em> eklentisi dosyalarını kurmaya çalışırken kopyalama hatası ortaya çıktı. Sürücü dolu olabilir veya yazma yetkisi bulunmuyor olabilir. Bunun sebebi tam kurulmamış bir eklentinin wiki kurulumunu bozması olabilir.';
+$lang['error_delete']          = '<em>%s</em> eklentisini silerken bir hata oluştu. Bu hata yetersiz dosya/klasör erişim yetkisinden kaynaklanabilir.';
+$lang['enabled']               = '%s eklentisi etkinleştirildi.';
+$lang['notenabled']            = '%s eklentisi etkinleştirilemedi, dosya yetkilerini kontrol edin.';
+$lang['disabled']              = '%s eklentisi devre dışı bırakıldı.';
+$lang['notdisabled']           = '%s eklentisi devre dışı bırakılamadı, dosya yetkilerini kontrol edin.';
