e4928db0b5d9004465515f79d942bdc68b15ef95
umriya afini <bigdream.power@gmail.com>
2014-01-31 04:35:42 +0100

    translation update

---
+++ lib/plugins/plugin/lang/id/lang.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Irwan Butar Butar <irwansah.putra@gmail.com>
+ * @author Yustinus Waruwu <juswaruwu@gmail.com>
+ */
+$lang['btn_info']              = 'Info';
+$lang['btn_update']            = 'Baharui';
+$lang['btn_delete']            = 'Hapus';
+$lang['btn_settings']          = 'Pengaturan';
+$lang['btn_download']          = 'Unduh';
+$lang['btn_enable']            = 'Simpan';
+$lang['url']                   = 'URL';
+$lang['installed']             = 'Instal';
+$lang['lastupdate']            = 'Pembaharuan terakhir:';
+$lang['source']                = 'Sumber:';
+$lang['unknown']               = 'Tidak kenal';
+$lang['updating']              = 'Terbaharui ...';
+$lang['update_none']           = 'Tidak ditemukan pembaharuan';
+$lang['deleting']              = 'Terhapus ...';
+$lang['deleted']               = 'Hapus Plugin %s.';
+$lang['downloading']           = 'Unduh ...';
+$lang['plugin']                = 'Plugin:';
+$lang['components']            = 'Komponen';
+$lang['name']                  = 'Nama:';
+$lang['date']                  = 'Tanggal:';
+$lang['type']                  = 'Tipe:';
+$lang['desc']                  = 'Penjelasan:';
+$lang['author']                = 'Autor:';
+$lang['www']                   = 'Web:';
