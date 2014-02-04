b50a334c0c43bf043a07d4f5e1cce6f0cb36c2a2
matej <mateju@svn.gnome.org>
2014-02-04 13:15:56 +0100

    translation update

---
+++ lib/plugins/plugin/lang/sl/lang.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Dejan Levec <webphp@gmail.com>
+ * @author Boštjan Seničar <senicar@gmail.com>
+ * @author Gregor Skumavc (grega.skumavc@gmail.com)
+ * @author Matej Urbančič (mateju@svn.gnome.org)
+ */
+$lang['menu']                  = 'Upravljanje vstavkov';
+$lang['download']              = 'Prejmi in namesti nov vstavek';
+$lang['manage']                = 'Nameščeni vstavki';
+$lang['btn_info']              = 'Podrobnosti';
+$lang['btn_update']            = 'Posodobi';
+$lang['btn_delete']            = 'Izbriši';
+$lang['btn_settings']          = 'Nastavitve';
+$lang['btn_download']          = 'Prejmi';
+$lang['btn_enable']            = 'Shrani';
+$lang['url']                   = 'URL';
+$lang['installed']             = 'Nameščeno:';
+$lang['lastupdate']            = 'Nazadnje posodobljeno:';
+$lang['source']                = 'Vir:';
+$lang['unknown']               = 'neznano';
+$lang['updating']              = 'Posodabljanje ...';
+$lang['updated']               = 'Vstavek %s je uspešno posodobljen';
+$lang['updates']               = 'Navedeni vstavki so uspešno posodobljeni';
+$lang['update_none']           = 'Posodobitev ni mogoče najti.';
+$lang['deleting']              = 'Brisanje ...';
+$lang['deleted']               = 'Vstavek %s je izbrisan.';
+$lang['downloading']           = 'Prejemanje ...';
+$lang['downloaded']            = 'Vstavek %s je uspešno nameščen';
+$lang['downloads']             = 'Navedeni vstavki so uspešno nameščeni:';
+$lang['download_none']         = 'Vstavkov ni mogoče najti ali pa je prišlo do napake med prejemanjem in nameščanjem.';
+$lang['plugin']                = 'Vstavek:';
+$lang['components']            = 'Sestavni deli';
+$lang['noinfo']                = 'Vstavek nima vpisanih podrobnih podatkov, kar pomeni, da je morda neveljaven.';
+$lang['name']                  = 'Ime:';
+$lang['date']                  = 'Datum:';
+$lang['type']                  = 'Vrsta:';
+$lang['desc']                  = 'Opis:';
+$lang['author']                = 'Avtor:';
+$lang['www']                   = 'Spletna stran:';
+$lang['error']                 = 'Prišlo je do neznane napake.';
+$lang['error_download']        = 'Ni mogoče prejeti datoteke vstavka: %s';
+$lang['error_badurl']          = 'Napaka naslova URL - ni mogoče določiti imena datoteke iz naslova URL';
+$lang['error_dircreate']       = 'Ni mogoče ustvariti začasne mape za prejemanje';
+$lang['error_decompress']      = 'Z upravljalnikom vstavkov ni mogoče razširiti prejetega arhiva vstavka. Najverjetneje je prišlo do napake med prejemanjem datoteke ali pa zapis arhiva ni znan. Poskusite znova ali pa napako odpravite z ročnim nameščanjem vstavka.';
+$lang['error_copy']            = 'Prišlo je do napake med nameščanjem datotek vstavka <em>%s</em>: najverjetneje so težave s prostorom za namestitev ali pa ni ustreznih dovoljenj za nameščanje. Zaradi nepopolne namestitve lahko nastopijo težave v delovanju sistema Wiki.';
+$lang['error_delete']          = 'Prišlo je do napake med brisanjem vstavka <em>%s</em>: najverjetneje ni ustreznih dovoljenj za dostop do datoteke ali mape';
+$lang['enabled']               = 'Vstavek %s je omogočen.';
+$lang['notenabled']            = 'Vstavka %s ni mogoče omogočiti zaradi neustreznih dovoljen.';
+$lang['disabled']              = 'Vstavek %s je onemogočen.';
+$lang['notdisabled']           = 'Vstavka %s ni mogoče onemogočiti zaradi neustreznih dovoljen.';
+$lang['packageinstalled']      = 'Paket vstavka (%d vstavkov: %s) je uspešno nameščen.';
