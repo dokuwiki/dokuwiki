e1f9e67413a02069aeac657145d08a39b0b5cb11
kloppel <p.k@k.fr>
2014-02-07 11:06:19 +0100

    translation update

---
+++ lib/plugins/plugin/lang/fr/lang.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Guy Brand <gb@unistra.fr>
+ * @author Delassaux Julien <julien@delassaux.fr>
+ * @author Maurice A. LeBlanc <leblancma@cooptel.qc.ca>
+ * @author stephane.gully@gmail.com
+ * @author Guillaume Turri <guillaume.turri@gmail.com>
+ * @author Erik Pedersen <erik.pedersen@shaw.ca>
+ * @author olivier duperray <duperray.olivier@laposte.net>
+ * @author Vincent Feltz <psycho@feltzv.fr>
+ * @author Philippe Bajoit <philippe.bajoit@gmail.com>
+ * @author Florian Gaub <floriang@floriang.net>
+ * @author Samuel Dorsaz samuel.dorsaz@novelion.net
+ * @author Johan Guilbaud <guilbaud.johan@gmail.com>
+ * @author schplurtz@laposte.net
+ * @author skimpax@gmail.com
+ * @author Yannick Aure <yannick.aure@gmail.com>
+ * @author Olivier DUVAL <zorky00@gmail.com>
+ * @author Anael Mobilia <contrib@anael.eu>
+ * @author Bruno Veilleux <bruno.vey@gmail.com>
+ */
+$lang['menu']                  = 'Gestion des extensions';
+$lang['download']              = 'Télécharger et installer une nouvelle extension';
+$lang['manage']                = 'Extensions installées';
+$lang['btn_info']              = 'Info';
+$lang['btn_update']            = 'Mettre à jour';
+$lang['btn_delete']            = 'Supprimer';
+$lang['btn_settings']          = 'Paramètres';
+$lang['btn_download']          = 'Télécharger';
+$lang['btn_enable']            = 'Enregistrer';
+$lang['url']                   = 'URL';
+$lang['installed']             = 'Installé :';
+$lang['lastupdate']            = 'Dernière mise à jour :';
+$lang['source']                = 'Source :';
+$lang['unknown']               = 'inconnu';
+$lang['updating']              = 'Mise à jour…';
+$lang['updated']               = 'Extension %s mise à jour avec succès';
+$lang['updates']               = 'Les extensions suivantes ont été mises à jour avec succès';
+$lang['update_none']           = 'Aucune mise à jour n\'a été trouvée.';
+$lang['deleting']              = 'Suppression…';
+$lang['deleted']               = 'Extension %s supprimée.';
+$lang['downloading']           = 'Téléchargement…';
+$lang['downloaded']            = 'Extension %s installée avec succès';
+$lang['downloads']             = 'Les extensions suivantes ont été installées avec succès :';
+$lang['download_none']         = 'Aucune extension n\'a été trouvée, ou un problème inconnu est survenu durant le téléchargement et l\'installation.';
+$lang['plugin']                = 'Extension :';
+$lang['components']            = 'Composants';
+$lang['noinfo']                = 'Cette extension n\'a transmis aucune information, elle pourrait être invalide.';
+$lang['name']                  = 'Nom :';
+$lang['date']                  = 'Date :';
+$lang['type']                  = 'Type :';
+$lang['desc']                  = 'Description :';
+$lang['author']                = 'Auteur :';
+$lang['www']                   = 'Site web :';
+$lang['error']                 = 'Une erreur inconnue est survenue.';
+$lang['error_download']        = 'Impossible de télécharger le fichier de l\'extension : %s';
+$lang['error_badurl']          = 'URL suspecte : impossible de déterminer le nom du fichier à partir de l\'URL';
+$lang['error_dircreate']       = 'Impossible de créer le répertoire temporaire pour effectuer le téléchargement';
+$lang['error_decompress']      = 'Le gestionnaire d\'extensions a été incapable de décompresser le fichier téléchargé. Ceci peut être le résultat d\'un mauvais téléchargement, auquel cas vous devriez réessayer ; ou bien le format de compression est inconnu, auquel cas vous devez télécharger et installer l\'extension manuellement.';
+$lang['error_copy']            = 'Une erreur de copie est survenue lors de l\'installation des fichiers de l\'extension <em>%s</em> : le disque est peut-être plein ou les autorisations d\'accès sont incorrects. Il a pu en résulter une installation partielle de l\'extension et laisser votre installation du wiki instable.';
+$lang['error_delete']          = 'Une erreur est survenue lors de la suppression de l\'extension <em>%s</em>. La raison la plus probable est l\'insuffisance des autorisations sur les fichiers ou les répertoires.';
+$lang['enabled']               = 'Extension %s activée.';
+$lang['notenabled']            = 'L\'extension %s n\'a pas pu être activée, vérifiez les autorisations des fichiers.';
+$lang['disabled']              = 'Extension %s désactivée.';
+$lang['notdisabled']           = 'L\'extension %s n\'a pas pu être désactivée, vérifiez les autorisations des fichiers.';
+$lang['packageinstalled']      = 'Ensemble d\'extensions (%d extension(s): %s) installé avec succès.';
