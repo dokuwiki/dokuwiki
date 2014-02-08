df4fc3c00a2bcad6222b4842540545a6e546c4cc
Marina Vladi <deldadam@gmail.com>
2014-02-08 13:25:20 +0100

    translation update

---
+++ inc/lang/hu-formal/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['doublequoteopening']    = '„';
+$lang['doublequoteclosing']    = '”';
+$lang['singlequoteopening']    = '‚';
+$lang['singlequoteclosing']    = '’';
+$lang['apostrophe']            = '’';
+$lang['btn_edit']              = 'Oldal módosítása';
+$lang['btn_source']            = 'Forrás megtekintése';
+$lang['btn_show']              = 'Oldal megtekintése';
+$lang['btn_create']            = 'Oldal létrehozása';
+$lang['btn_search']            = 'Keresés';
+$lang['btn_save']              = 'Mentés';
+$lang['btn_preview']           = 'Előnézet';
+$lang['btn_top']               = 'Oldal tetejére';
+$lang['btn_newer']             = '<< újabb';
+$lang['btn_older']             = 'régebbi >>';
+$lang['btn_revs']              = 'Korábbi változatok';
+$lang['btn_recent']            = 'Legújabb változások';
+$lang['btn_upload']            = 'Feltöltés';
