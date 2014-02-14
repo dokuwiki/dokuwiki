343aa1749f17e62fa68e26193011a5051025881a
Andreas Gohr <andi@splitbrain.org>
2014-02-14 13:12:58 +0100

    fix global $AUTH hiding $auth

--- lib/exe/detail.php
+++ lib/exe/detail.php
@@ -32,8 +32,8 @@ if($conf['allowdebug'] && $INPUT->has('debug')){
 
 $ERROR = false;
 // check image permissions
-$AUTH = auth_quickaclcheck($IMG);
-if($AUTH >= AUTH_READ){
+$AUTHLEVEL = auth_quickaclcheck($IMG);
+if($AUTHLEVEL >= AUTH_READ){
     // check if image exists
     $SRC = mediaFN($IMG);
     if(!@file_exists($SRC)){
