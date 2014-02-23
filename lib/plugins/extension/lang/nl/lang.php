4d51938bb0516f7cc033d8c93131f56af7525da0
Rene <wllywlnt@yahoo.com>
2014-02-23 22:56:02 +0100

    translation update

---
+++ lib/plugins/extension/lang/nl/lang.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['menu']                  = 'Extension Manager (Uitbreidings Beheerder)';
+$lang['tab_plugins']           = 'Geïnstalleerde Plugins';
+$lang['tab_templates']         = 'Geïnstalleerde Templates';
+$lang['tab_search']            = 'Zoek en installeer';
+$lang['tab_install']           = 'Handmatige installatie';
+$lang['notimplemented']        = 'Deze toepassing is nog niet geïnstalleerd';
+$lang['notinstalled']          = 'Deze uitbreiding is nog niet geïnstalleerd';
+$lang['alreadyenabled']        = 'Deze uitbreiding is reeds ingeschakeld';
+$lang['alreadydisabled']       = 'Deze uitbreiding is reeds uitgeschakeld';
+$lang['pluginlistsaveerror']   = 'Fout bij het opslaan van de plugin lijst';
+$lang['unknownauthor']         = 'Onbekende auteur';
+$lang['unknownversion']        = 'Onbekende versie';
+$lang['btn_info']              = 'Toon meer informatie';
+$lang['btn_update']            = 'Update';
+$lang['btn_uninstall']         = 'Deinstalleer';
+$lang['btn_enable']            = 'Schakel aan';
+$lang['btn_disable']           = 'Schakel uit';
+$lang['btn_install']           = 'Installeer';
+$lang['btn_reinstall']         = 'Her-installeer';
+$lang['js']['reallydel']       = 'Wilt u deze uitbreiding deinstalleren ?';
+$lang['search_for']            = 'Zoek Uitbreiding:';
+$lang['search']                = 'Zoek';
+$lang['extensionby']           = '<strong>%s</strong> by %s';
+$lang['screenshot']            = 'Schermafdruk bij %s';
+$lang['popularity']            = 'Populariteit:%s%%';
+$lang['homepage_link']         = 'Dokumenten';
