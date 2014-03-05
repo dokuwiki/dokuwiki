ddf7ce094f821dc45273f75203e5da7838d96e96
Cupen <Cupenoruler@foxmail.com>
2014-03-05 09:26:40 +0100

    translation update

---
+++ lib/plugins/extension/lang/zh/lang.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Cupen <Cupenoruler@foxmail.com>
+ */
+$lang['menu']                  = '扩展管理器';
+$lang['tab_plugins']           = '安装插件';
+$lang['tab_templates']         = '安装模板';
+$lang['tab_search']            = '搜索和安装';
+$lang['tab_install']           = '手动安装';
+$lang['notimplemented']        = '未实现的特性';
+$lang['notinstalled']          = '该扩展未安装';
+$lang['alreadyenabled']        = '该扩展已激活';
+$lang['alreadydisabled']       = '该扩展已关闭';
+$lang['pluginlistsaveerror']   = '保存插件列表时碰到个错误';
+$lang['unknownauthor']         = '未知作者';
+$lang['unknownversion']        = '未知版本';
+$lang['btn_info']              = '查看更多信息';
+$lang['btn_update']            = '更新';
+$lang['btn_uninstall']         = '卸载';
+$lang['btn_enable']            = '激活';
+$lang['btn_disable']           = '关闭';
+$lang['btn_install']           = '安装';
+$lang['btn_reinstall']         = '重新安装';
+$lang['js']['reallydel']       = '确定卸载这个扩展么？';
+$lang['search_for']            = '搜索扩展';
+$lang['search']                = '搜索';
+$lang['extensionby']           = '<strong>%s</strong> by %s';
+$lang['screenshot']            = '%s 的截图';
+$lang['popularity']            = '人气: %s%%';
+$lang['homepage_link']         = '文档';
