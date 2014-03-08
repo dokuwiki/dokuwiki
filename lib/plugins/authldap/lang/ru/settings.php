6bc2d8e51371c2ee17233d4c76112e3fefca437f
Aleksandr Selivanov <alexgearbox@yandex.ru>
2014-03-08 13:05:57 +0100

    translation update

--- lib/plugins/authldap/lang/ru/settings.php
+++ lib/plugins/authldap/lang/ru/settings.php
@@ -6,6 +6,8 @@
  * @author Ivan I. Udovichenko (sendtome@mymailbox.pp.ua)
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
  * @author Erli Moen <evseev.jr@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['deref']                 = 'Как расшифровывать псевдонимы?';
 $lang['bindpw']                = 'Пароль для указанного пользователя.';
+$lang['deref_o_0']             = 'LDAP_DEREF_NEVER';
