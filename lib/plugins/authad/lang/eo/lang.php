aafe66b6f625b26207700b72e9828ecc26234145
Robert Bogenschneider <bogi@uea.org>
2014-04-10 10:16:04 +0200

    translation update

---
+++ lib/plugins/authad/lang/eo/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['domain']                = 'Ensaluta domajno';
