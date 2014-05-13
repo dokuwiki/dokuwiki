93691af57f65173963a122e19915917814a32b71
ilker rifat kapaç <irifat@gmail.com>
2014-05-13 10:15:52 +0200

    translation update

---
+++ lib/plugins/authmysql/lang/tr/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author ilker rifat kapaç <irifat@gmail.com>
+ */
+$lang['server']                = 'Sizin MySQL sunucunuz';
+$lang['user']                  = 'MySQL kullanıcısının adı';
+$lang['password']              = 'Üstteki kullanıcı için şifre';
+$lang['database']              = 'Kullanılacak veritabanı';
+$lang['charset']               = 'Veritabanında kullanılacak karakter seti';
