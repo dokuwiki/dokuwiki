227a9832a9d65a12d88704e2bf18db63dae1ad57
lainme <lainme993@gmail.com>
2014-05-13 06:36:05 +0200

    translation update

--- lib/plugins/extension/lang/zh/lang.php
+++ lib/plugins/extension/lang/zh/lang.php
@@ -6,6 +6,7 @@
  * @author Cupen <Cupenoruler@foxmail.com>
  * @author xiqingongzi <Xiqingongzi@Gmail.com>
  * @author qinghao <qingxianhao@gmail.com>
+ * @author lainme <lainme993@gmail.com>
  */
 $lang['menu']                  = '扩展管理器';
 $lang['tab_plugins']           = '安装插件';
@@ -42,6 +43,7 @@ $lang['repository']            = '版本库：';
 $lang['unknown']               = '<em>未知的</em>';
 $lang['installed_version']     = '已安装版本：';
 $lang['install_date']          = '您的最后一次升级：';
+$lang['available_version']     = '可用版本：';
 $lang['compatible']            = '兼容于：';
 $lang['depends']               = '依赖于：';
 $lang['similar']               = '相似于：';
@@ -59,6 +61,7 @@ $lang['status_disabled']       = '禁用';
 $lang['status_unmodifiable']   = '不可修改';
 $lang['status_plugin']         = '插件';
 $lang['status_template']       = '模板';
+$lang['status_bundled']        = '内建';
 $lang['msg_enabled']           = '插件 %s 已启用';
 $lang['msg_disabled']          = '插件 %s 已禁用';
 $lang['msg_delete_success']    = '插件已经卸载';
@@ -70,3 +73,19 @@ $lang['msg_upload_failed']     = '上传文件失败';
 $lang['missing_dependency']    = '<strong>缺少或者被禁用依赖：</strong> %s';
 $lang['security_issue']        = '<strong>安全问题：</strong> %s';
 $lang['security_warning']      = '<strong>安全警告：</strong> %s';
+$lang['update_available']      = '<strong>更新：</strong>新版本 %s 已经可用。';
+$lang['wrong_folder']          = '<strong>插件安装不正确：</strong>重命名插件目录 "%s" 为 "%s"。';
+$lang['url_change']            = '<strong>URL已改变：</strong>自上次下载以来的下载 URL 已经改变。请在更新扩展前检查新 URL 是否有效。<br />新的：%s<br />旧的：%s';
+$lang['error_badurl']          = 'URL 应当以 http 或者 https 作为开头';
+$lang['error_dircreate']       = '无法创建用于保存下载的临时文件夹';
+$lang['error_download']        = '无法下载文件：%s';
+$lang['error_decompress']      = '无法解压下载的文件。这可能是由于文件损坏，在这种情况下您可以重试。这也可能是由于压缩格式是未知的，在这种情况下您需要手动下载并且安装。';
+$lang['error_findfolder']      = '无法识别扩展目录，您需要手动下载和安装';
+$lang['error_copy']            = '在尝试安装文件到目录 <em>%s</em> 时出现文件复制错误：磁盘可能已满或者文件访问权限不正确。这可能导致插件被部分安装并使您的维基处在不稳定状态';
+$lang['noperms']               = '扩展目录不可写';
+$lang['notplperms']            = '模板目录不可写';
+$lang['nopluginperms']         = '插件目录不可写';
+$lang['git']                   = '这个扩展是通过 git 安装的，您可能不想在这里升级它';
+$lang['install_url']           = '从 URL 安装：';
+$lang['install_upload']        = '上传扩展：';
+$lang['repo_error']            = '无法连接到插件仓库。请确定您的服务器可以连接 www.dokuwiki.org 并检查您的代理设置。';
