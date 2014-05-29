c2ec0658bd6ec161b2e8f8ceeb0934a6f60d6fbe
Antonio Castilla <antoniocastilla@trazoide.com>
2014-05-29 12:47:35 +0200

    translation update

--- lib/plugins/authpgsql/lang/es/settings.php
+++ lib/plugins/authpgsql/lang/es/settings.php
@@ -4,6 +4,9 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Antonio Bueno <atnbueno@gmail.com>
+ * @author Antonio Castilla <antoniocastilla@trazoide.com>
  */
 $lang['password']              = 'Contraseña del usuario indicado';
 $lang['database']              = 'Base de datos a usar';
+$lang['delUserRefs']           = 'Sentencia SQL para eliminar un usuario de todos los grupos';
+$lang['updateUser']            = 'Sentencia SQL para actualizar un perfil de usuario';
