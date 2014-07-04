f5ff3ceb78b35e5a23887a9948f761457a4ab9cc
Jonathan Hernández <me@jhalicea.com>
2014-07-04 08:20:59 +0200

    translation update

--- lib/plugins/extension/lang/es/lang.php
+++ lib/plugins/extension/lang/es/lang.php
@@ -5,7 +5,9 @@
  * 
  * @author Antonio Bueno <atnbueno@gmail.com>
  * @author Antonio Castilla <antoniocastilla@trazoide.com>
+ * @author Jonathan Hernández <me@jhalicea.com>
  */
+$lang['menu']                  = 'Administrador de Extensiones ';
 $lang['tab_plugins']           = 'Plugins instalados';
 $lang['tab_templates']         = 'Plantillas instaladas';
 $lang['tab_search']            = 'Buscar e instalar';
@@ -47,6 +49,8 @@ $lang['similar']               = 'Similar a:';
 $lang['conflicts']             = 'Conflictos con:';
 $lang['donate']                = '¿Cómo está?';
 $lang['donate_action']         = '¡Págale un café al autor!';
+$lang['repo_retry']            = 'Trate otra vez';
+$lang['provides']              = 'Provee: ';
 $lang['status']                = 'Estado:';
 $lang['status_installed']      = 'instalado';
 $lang['status_not_installed']  = 'no instalado';
@@ -56,6 +60,7 @@ $lang['status_disabled']       = 'desactivado';
 $lang['status_unmodifiable']   = 'no modificable';
 $lang['status_plugin']         = 'plugin';
 $lang['status_template']       = 'plantilla';
+$lang['status_bundled']        = 'agrupado';
 $lang['msg_enabled']           = 'Plugin %s activado';
 $lang['msg_disabled']          = 'Plugin %s desactivado';
 $lang['msg_delete_success']    = 'Extensión desinstalada';
@@ -64,3 +69,16 @@ $lang['msg_template_update_success'] = 'Plantilla %s actualizada con éxito';
 $lang['msg_plugin_install_success'] = 'Plugin %s instalado con éxito';
 $lang['msg_plugin_update_success'] = 'Plugin %s actualizado con éxito';
 $lang['msg_upload_failed']     = 'Falló la carga del archivo';
+$lang['missing_dependency']    = '<strong>Dependencia deshabilitada o perdida:</strong> %s';
+$lang['security_issue']        = '<strong>Problema de seguridad:</strong> %s';
+$lang['security_warning']      = '<strong>Aviso de seguridad:</strong> %s';
+$lang['update_available']      = '<strong>Actualizar:</strong> Nueva versión %s disponible.';
+$lang['wrong_folder']          = '<strong>"Plugin" instalado incorrectamente:</strong> Cambie el nombre del directorio del plugin "%s" a "%s".';
+$lang['url_change']            = '<strong>URL actualizada:</strong> El Download URL ha cambiado desde el último download. Verifica si el nuevo URL es valido antes de actualizar la extensión .<br />Nuevo: %s<br />Viejo: %s';
+$lang['error_badurl']          = 'URLs deberían empezar con http o https';
+$lang['error_dircreate']       = 'No es posible de crear un directorio temporero para poder recibir el download';
+$lang['error_download']        = 'No es posible descargar el documento: %s';
+$lang['git']                   = 'Esta extensión fue instalada a través de git, quizás usted no quiera actualizarla aquí mismo.';
+$lang['install_url']           = 'Instalar desde URL:';
+$lang['install_upload']        = 'Subir Extensión:';
+$lang['repo_error']            = 'El repositorio de plugins no puede ser contactado. Asegúrese que su servidor pueda contactar www.dokuwiki.org y verificar la configuración de su proxy.';
