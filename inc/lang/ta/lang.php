4b41682d1894b19dd7364659cbe4c9f48c261a5c
Naveen Venugopal <naveen.venugopal.anu@gmail.com>
2014-07-20 03:10:27 +0200

    translation update

---
+++ inc/lang/ta/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Naveen Venugopal <naveen.venugopal.anu@gmail.com>
+ */
+$lang['btn_revs']              = 'பழைய திருத்தங்கள்';
