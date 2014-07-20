121b0b091c4fc620da5474eeb03f5b80f131b3a4
Naveen Venugopal <naveen.venugopal.anu@gmail.com>
2014-07-20 03:10:14 +0200

    translation update

---
+++ inc/lang/ta/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Naveen Venugopal <naveen.venugopal.anu@gmail.com>
+ */
+$lang['btn_edit']              = 'இந்த பக்கத்தை திருத்து ';
+$lang['btn_create']            = 'இந்த பக்கத்தை உருவாக்கு ';
+$lang['btn_search']            = 'தேடு';
+$lang['btn_save']              = 'சேமி ';
+$lang['btn_revs']              = 'old திருத்தங்கள்';
+$lang['btn_recent']            = 'சமீபத்திய மாற்றங்கள்';
+$lang['btn_upload']            = 'பதிவேற்று';
+$lang['btn_cancel']            = 'ரத்து';
