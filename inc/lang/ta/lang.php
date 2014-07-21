8eec3169b7f7ddbd04d402426745a7df5b57e169
Naveen Venugopal <naveen.venugopal.anu@gmail.com>
2014-07-21 15:20:17 +0200

    translation update

--- inc/lang/ta/lang.php
+++ inc/lang/ta/lang.php
@@ -6,6 +6,7 @@
  * @author Naveen Venugopal <naveen.venugopal.anu@gmail.com>
  */
 $lang['btn_edit']              = 'இந்த பக்கத்தை திருத்து ';
+$lang['btn_show']              = 'பக்கத்தை காண்பி ';
 $lang['btn_create']            = 'இந்த பக்கத்தை உருவாக்கு ';
 $lang['btn_search']            = 'தேடு';
 $lang['btn_save']              = 'சேமி ';
@@ -13,3 +14,7 @@ $lang['btn_revs']              = 'old திருத்தங்கள்';
 $lang['btn_recent']            = 'சமீபத்திய மாற்றங்கள்';
 $lang['btn_upload']            = 'பதிவேற்று';
 $lang['btn_cancel']            = 'ரத்து';
+$lang['btn_index']             = 'தள வரைபடம்';
+$lang['btn_admin']             = 'நிர்வாகம்';
+$lang['btn_update']            = 'மேம்படுத்து ';
+$lang['btn_delete']            = 'நீக்கு';
