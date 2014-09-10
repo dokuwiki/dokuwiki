2c917972fa1601d7765e56a079884f4a24a69a7e
Mohamad Mehdi Habibi <habibi.esf@gmail.com>
2014-09-10 09:50:56 +0200

    translation update

---
+++ lib/plugins/authmysql/lang/fa/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mohamad Mehdi Habibi <habibi.esf@gmail.com>
+ */
+$lang['server']                = 'سرور MySQL';
+$lang['user']                  = 'نام کاربری MySQL';
+$lang['database']              = 'پایگاه داده مورد استفاده';
