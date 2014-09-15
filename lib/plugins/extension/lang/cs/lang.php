616de5f5944160b7aa9472ed1dea84b302018055
Viktor Zavadil <vzavadil@newps.cz>
2014-09-15 10:46:00 +0200

    translation update

---
+++ lib/plugins/extension/lang/cs/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Viktor Zavadil <vzavadil@newps.cz>
+ */
+$lang['menu']                  = 'Manager rozšíření';
+$lang['tab_plugins']           = 'Instalované moduly';
+$lang['tab_templates']         = 'Instalované šablony';
+$lang['tab_search']            = 'Vyhledej a instaluj';
+$lang['tab_install']           = 'Ruční instalování';
+$lang['notimplemented']        = 'Tato vychytávka není dosud implementována';
+$lang['notinstalled']          = 'Toto rozšíření není instalováno';
+$lang['alreadyenabled']        = 'Toto rozšíření je již povoleno';
+$lang['alreadydisabled']       = 'Toto rozšíření je již vypnuto';
+$lang['unknownauthor']         = 'Neznámý autor';
+$lang['unknownversion']        = 'Neznámá verze';
+$lang['btn_info']              = 'Zobrazit více informací';
