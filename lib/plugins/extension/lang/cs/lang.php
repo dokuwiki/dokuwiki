2c99198de3e2c36f9df1cd8897770ced395e3b8a
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2014-09-20 11:21:00 +0200

    translation update

--- lib/plugins/extension/lang/cs/lang.php
+++ lib/plugins/extension/lang/cs/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Viktor Zavadil <vzavadil@newps.cz>
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  */
 $lang['menu']                  = 'Manager rozšíření';
 $lang['tab_plugins']           = 'Instalované moduly';
@@ -17,3 +18,40 @@ $lang['alreadydisabled']       = 'Toto rozšíření je již vypnuto';
 $lang['unknownauthor']         = 'Neznámý autor';
 $lang['unknownversion']        = 'Neznámá verze';
 $lang['btn_info']              = 'Zobrazit více informací';
+$lang['btn_update']            = 'Aktualizovat';
+$lang['btn_uninstall']         = 'Odinstalovat';
+$lang['btn_enable']            = 'Povolit';
+$lang['btn_disable']           = 'Zakázat';
+$lang['btn_install']           = 'Instalovat';
+$lang['btn_reinstall']         = 'Přeinstalovat';
+$lang['js']['reallydel']       = 'Opravdu odinstalovat toto rozšíření?';
+$lang['search_for']            = 'Hledat rozšíření:';
+$lang['search']                = 'Hledat';
+$lang['popularity']            = 'Popularita: %s%%';
+$lang['homepage_link']         = 'Dokumenty';
+$lang['bugs_features']         = 'Chyby';
+$lang['tags']                  = 'Štítky:';
+$lang['author_hint']           = 'Vyhledat rozšíření podle tohoto autora';
+$lang['installed']             = 'Nainstalováno:';
+$lang['repository']            = 'Repozitář:';
+$lang['unknown']               = '<em>neznámý</em>';
+$lang['installed_version']     = 'Nainstalovaná verze:';
+$lang['install_date']          = 'Poslední aktualizace';
+$lang['available_version']     = 'Dostupná verze:';
+$lang['compatible']            = 'Kompatibilní s:';
+$lang['depends']               = 'Závisí na:';
+$lang['similar']               = 'Podobný jako:';
+$lang['donate']                = 'Líbí se ti to?';
+$lang['donate_action']         = 'Kup autorovi kávu!';
+$lang['repo_retry']            = 'Opakovat';
+$lang['provides']              = 'Poskytuje:';
+$lang['status']                = 'Stav:';
+$lang['status_installed']      = 'instalovaný';
+$lang['status_not_installed']  = 'nenainstalovaný';
+$lang['status_protected']      = 'chráněný';
+$lang['status_enabled']        = 'povolený';
+$lang['status_disabled']       = 'zakázaný';
+$lang['status_unmodifiable']   = 'neměnný';
+$lang['status_plugin']         = 'zásuvný modul';
+$lang['status_template']       = 'šablona';
+$lang['msg_delete_success']    = 'Rozšíření odinstalováno';
