8f553f69cf9a2cfac4ec7611770413ecb7a48966
Rainbow Spike <Rainbow_Spike@derpy.ru>
2014-10-01 20:56:05 +1100

    Update license.php
    Place this in special minipatch, of course. Also I mean version for 88x31 images with /button/badge/ replace. BTW cc.png is not valid as mark of any license now.
    
    P.S. Hey, my http://comicslate.org use CC BY-NC-SA 4.0 on second day from release (2013.11.26)! Be smart!

--- conf/license.php
+++ conf/license.php
@@ -6,31 +6,39 @@
  */
 
 $license['cc-zero'] = array(
-    'name' => 'CC0 1.0 Universal',
+    'name' => '<img src="/lib/images/license/button/cc-zero.png" />CC0 1.0 Universal',
     'url'  => 'http://creativecommons.org/publicdomain/zero/1.0/',
 );
 $license['publicdomain'] = array(
-    'name' => 'Public Domain',
+    'name' => '<img src="/lib/images/license/button/publicdomain.png" />Public Domain',
     'url'  => 'http://creativecommons.org/licenses/publicdomain/',
 );
 $license['cc-by'] = array(
-    'name' => 'CC Attribution 3.0 Unported',
-    'url'  => 'http://creativecommons.org/licenses/by/3.0/',
+    'name' => '<img src="/lib/images/license/button/cc-by.png" />CC Attribution 4.0 International',
+    'url'  => 'http://creativecommons.org/licenses/by/4.0/',
+);
+$license['cc-by-nd'] = array(
+    'name' => '<img src="/lib/images/license/button/cc-by-nd.png" />CC Attribution-NoDerivs 4.0 International',
+    'url'  => 'http://creativecommons.org/licenses/by-nd/4.0/',
 );
 $license['cc-by-sa'] = array(
-    'name' => 'CC Attribution-Share Alike 3.0 Unported',
-    'url'  => 'http://creativecommons.org/licenses/by-sa/3.0/',
+    'name' => '<img src="/lib/images/license/button/cc-by-sa.png" />CC Attribution-ShareAlike 4.0 International',
+    'url'  => 'http://creativecommons.org/licenses/by-sa/4.0/',
 );
 $license['gnufdl'] = array(
-    'name' => 'GNU Free Documentation License 1.3',
+    'name' => '<img src="/lib/images/license/button/gnufdl.png" />GNU Free Documentation License 1.3',
     'url'  => 'http://www.gnu.org/licenses/fdl-1.3.html',
 );
 $license['cc-by-nc'] = array(
-    'name' => 'CC Attribution-Noncommercial 3.0 Unported',
-    'url'  => 'http://creativecommons.org/licenses/by-nc/3.0/',
+    'name' => '<img src="/lib/images/license/button/cc-by-nc.png" />CC Attribution-NonCommercial 4.0 International',
+    'url'  => 'http://creativecommons.org/licenses/by-nc/4.0/',
+);
+$license['cc-by-nc-nd'] = array(
+    'name' => '<img src="/lib/images/license/button/cc-by-nc-nd.png" />CC Attribution-NonCommercial-NoDerivs 4.0 International',
+    'url'  => 'http://creativecommons.org/licenses/by-nc-nd/4.0/',
 );
 $license['cc-by-nc-sa'] = array(
-    'name' => 'CC Attribution-Noncommercial-Share Alike 3.0 Unported',
-    'url'  => 'http://creativecommons.org/licenses/by-nc-sa/3.0/',
+    'name' => '<img src="/lib/images/license/button/cc-by-nc-sa.png" />CC Attribution-NonCommercial-Share Alike 4.0 International',
+    'url'  => 'http://creativecommons.org/licenses/by-nc-sa/4.0/',
 );
 
