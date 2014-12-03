48b5d5e92851161ee3b3ebc7bf5b6ae6d1f0914d
Andreas Gohr <gohr@cosmocode.de>
2014-12-03 16:50:16 +0100

    warn about enabled, but not used auth plugins

--- lib/plugins/extension/helper/list.php
+++ lib/plugins/extension/helper/list.php
@@ -461,6 +461,7 @@ class helper_plugin_extension_list extends DokuWiki_Plugin {
      * @return string The HTML code
      */
     function make_actions(helper_plugin_extension_extension $extension) {
+        global $conf;
         $return = '';
         $errors = '';
 
@@ -490,6 +491,10 @@ class helper_plugin_extension_list extends DokuWiki_Plugin {
 
             if ($extension->isGitControlled()){
                 $errors .= '<p class="permerror">'.$this->getLang('git').'</p>';
+            }
+
+            if ($extension->isEnabled() && in_array('Auth', $extension->getTypes()) && $conf['auth'] != $extension->getID()) {
+                $errors .= '<p class="permerror">'.$this->getLang('auth').'</p>';
             }
 
         }else{
