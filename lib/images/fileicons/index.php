2ee7cc72bfdad5df9bf684b758288c9fd8269bfc
Rainbow Spike <Rainbow_Spike@derpy.ru>
2014-12-10 12:35:03 +1000

    Update index.php
    Replace direct echo compilation by precompiled string
    Fix one typo in \<title\>

--- lib/images/fileicons/index.php
+++ lib/images/fileicons/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-    <title>filetype icons</title>
+    <title>Filetype icons</title>
 
     <style type="text/css">
         body {
@@ -28,39 +28,30 @@
 </head>
 <body>
 
-<div class="white box">
 <?php
 foreach (glob('*.png') as $img) {
-    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+    $list .= '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
 }
-?>
+foreach (glob('32x32/*.png') as $img) {
+    $list32 .= '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
+}
+echo '<div class="white box">
+'.$list.'
 </div>
 
 <div class="black box">
-<?php
-foreach (glob('*.png') as $img) {
-    echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
-}
-?>
+'.$list.'
 </div>
 
 <br style="clear: left" />
 
 <div class="white box">
-    <?php
-    foreach (glob('32x32/*.png') as $img) {
-        echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
-    }
-    ?>
+'.$list32.'
 </div>
 
 <div class="black box">
-    <?php
-    foreach (glob('32x32/*.png') as $img) {
-        echo '<img src="'.$img.'" alt="'.$img.'" title="'.$img.'" /> ';
-    }
-    ?>
-</div>
+'.$list32.'
+</div>';?>
 
 
 </body>
