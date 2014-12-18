c3a1960bb74e2990be88c84bcff9e5057c20cd72
Guido Salatino <guidorafael23@gmail.com>
2014-12-18 04:15:24 +0100

    translation update

---
+++ inc/lang/português brasil/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Guido Salatino <guidorafael23@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['doublequoteopening']    = '“';
+$lang['doublequoteclosing']    = '”';
+$lang['singlequoteopening']    = '‘';
+$lang['singlequoteclosing']    = '’';
+$lang['apostrophe']            = '’';
+$lang['btn_edit']              = 'Editar esta página';
+$lang['btn_source']            = 'Mostrar a página fonte';
+$lang['btn_show']              = 'Mostrar a página';
+$lang['btn_create']            = 'Criar esta página';
+$lang['btn_search']            = 'Pesquisar';
+$lang['btn_save']              = 'Salvar';
+$lang['btn_preview']           = 'Visualizar';
+$lang['btn_top']               = 'Ir para o início';
+$lang['btn_newer']             = '<< mais recente';
+$lang['btn_older']             = 'menos recente >>';
+$lang['btn_revs']              = 'Revisões antigas';
+$lang['btn_recent']            = 'Alterações recentes';
+$lang['btn_upload']            = 'Upload';
