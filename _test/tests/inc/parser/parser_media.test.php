db42b6f8860359481248e9bc847b111497511d03
Michael Große <grosse@cosmocode.de>
2014-12-18 10:50:08 +0100

    Add test for internal video files

--- _test/tests/inc/parser/parser_media.test.php
+++ _test/tests/inc/parser/parser_media.test.php
@@ -65,6 +65,41 @@ class TestOfDoku_Parser_Media extends TestOfDoku_Parser {
         $rest = 'away.vid</a>';
         $this->assertEquals(substr($url,161),$rest);
     }
+
+
+    function testVideoOGVInternal() {
+        $file = 'wiki:kind_zu_katze.ogv';
+        $parser_response = p_get_instructions('{{' . $file . '}}');
+
+        $calls = array (
+            array('document_start',array()),
+            array('p_open',array()),
+            array('internalmedia',array($file,null,null,null,null,'cache','details')),
+            array('cdata',array(null)),
+            array('p_close',array()),
+            array('document_end',array()),
+        );
+        $this->assertEquals(array_map('stripbyteindex',$parser_response),$calls);
+
+        $Renderer = new Doku_Renderer_xhtml();
+        $url = $Renderer->externalmedia($file,null,null,null,null,'cache','details',true);
+
+        $video = '<video class="media" width="320" height="240" controls="controls" poster="/./lib/exe/fetch.php?media=wiki:kind_zu_katze.png">';
+        $this->assertEquals(substr($url,0,125),$video);
+
+        $source_webm = '<source src="/./lib/exe/fetch.php?media=wiki:kind_zu_katze.webm" type="video/webm" />';
+        $this->assertEquals(substr($url,126,85),$source_webm);
+        $source_ogv = '<source src="/./lib/exe/fetch.php?media=wiki:kind_zu_katze.ogv" type="video/ogg" />';
+        $this->assertEquals(substr($url,212,83),$source_ogv);
+
+        $a_webm = '<a href="/./lib/exe/fetch.php?id=&amp;cache=&amp;media=wiki:kind_zu_katze.webm" class="media mediafile mf_webm" title="wiki:kind_zu_katze.webm (99.2 KB)">kind_zu_katze.webm</a>';
+        $a_ogv = '<a href="/./lib/exe/fetch.php?id=&amp;cache=&amp;media=wiki:kind_zu_katze.ogv" class="media mediafile mf_ogv" title="wiki:kind_zu_katze.ogv (44.8 KB)">kind_zu_katze.ogv</a>';
+        $this->assertEquals(substr($url,296,176),$a_webm);
+        $this->assertEquals(substr($url,472,172),$a_ogv);
+
+        $rest = '</video>'."\n";
+        $this->assertEquals(substr($url,644),$rest);
+    }
 }
 
 /**
