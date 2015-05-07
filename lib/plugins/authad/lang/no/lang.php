c9a3a44ccbcf2ff1ed427df5f6911a1028989e43
Danny Buckhof <daniel.raknes@hotmail.no>
2015-05-07 14:45:55 +0200

    translation update

--- lib/plugins/authad/lang/no/lang.php
+++ lib/plugins/authad/lang/no/lang.php
@@ -5,6 +5,9 @@
  * 
  * @author Patrick <spill.p@hotmail.com>
  * @author Thomas Juberg <Thomas.Juberg@Gmail.com>
+ * @author Danny Buckhof <daniel.raknes@hotmail.no>
  */
 $lang['domain']                = 'Loggpå-domene';
 $lang['authpwdexpire']         = 'Ditt passord går ut om %d dager, du bør endre det snarest.';
+$lang['passchangefail']        = 'Feil ved endring av passord. Det kan være at passordet ikke er i tråd med passordpolicyen ';
+$lang['connectfail']           = 'Feil ved kontakt med Active Directory serveren.';
