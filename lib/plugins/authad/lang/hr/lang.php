9c9643569e11748094bd63810a3d805df0d3e9bf
Davor Turkalj <turki.bsc@gmail.com>
2015-05-14 15:51:29 +0200

    translation update

--- lib/plugins/authad/lang/hr/lang.php
+++ lib/plugins/authad/lang/hr/lang.php
@@ -7,3 +7,5 @@
  */
 $lang['domain']                = 'Domena za prijavu';
 $lang['authpwdexpire']         = 'Vaša lozinka će isteći za %d dana, trebate ju promijeniti.';
+$lang['passchangefail']        = 'Ne mogu izmijeniti lozinku. Možda nije zadovoljen set pravila za lozinke?';
+$lang['connectfail']           = 'Ne mogu se povezati s Active Directory poslužiteljem.';
