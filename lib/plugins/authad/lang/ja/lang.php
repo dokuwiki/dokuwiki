edc61c3ad8e143dbc6aaf62829b2241e6393606e
Ikuo Obataya <i.obataya@gmail.com>
2015-05-28 03:36:03 +0200

    translation update

--- lib/plugins/authad/lang/ja/lang.php
+++ lib/plugins/authad/lang/ja/lang.php
@@ -5,6 +5,9 @@
  * 
  * @author PzF_X <jp_minecraft@yahoo.co.jp>
  * @author Osaka <mr.osaka@gmail.com>
+ * @author Ikuo Obataya <i.obataya@gmail.com>
  */
 $lang['domain']                = 'ログオン時のドメイン';
 $lang['authpwdexpire']         = 'あなたのパスワードは、あと%d日で有効期限が切れます。パスワードを変更してください。';
+$lang['passchangefail']        = 'パスワードを変更できませんでした。パスワードのルールに合わなかったのかもしれません。';
+$lang['connectfail']           = 'Active Directoryサーバーに接続できませんでした。';
