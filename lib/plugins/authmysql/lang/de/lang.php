6a7af1ccf98fac8f5dd1097f5d4978df562a5d02
Hendrik Diel <diel.hendrik@gmail.com>
2015-06-07 10:46:45 +0200

    translation update

--- lib/plugins/authmysql/lang/de/lang.php
+++ lib/plugins/authmysql/lang/de/lang.php
@@ -4,5 +4,8 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Noel Tilliot <noeltilliot@byom.de>
+ * @author Hendrik Diel <diel.hendrik@gmail.com>
  */
+$lang['userexists']            = 'Entschuldigung, aber dieser Benutzername ist bereits vergeben.';
 $lang['usernotexists']         = 'Sorry, dieser Nutzer existiert nicht.';
+$lang['writefail']             = 'Die Benutzerdaten konnten nicht geändert werden. Bitte wenden Sie sich an den Wiki-Admin.';
