82246f10eb9ae3debeadf5913232105aa2694cec
Andreas Gohr <andi@splitbrain.org>
2015-07-15 21:44:22 +0200

    improve aria attribute handling. closes #1142
    adds aria handling to makeToggle and allows to supress it in dw_toggle

--- lib/tpl/dokuwiki/main.php
+++ lib/tpl/dokuwiki/main.php
@@ -38,12 +38,12 @@ $showSidebar = $hasSidebar && ($ACT=='show');
                 <!-- ********** ASIDE ********** -->
                 <div id="dokuwiki__aside"><div class="pad aside include group">
                     <h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
-                    <div class="content">
+                    <div class="content"><div class="group">
                         <?php tpl_flush() ?>
                         <?php tpl_includeFile('sidebarheader.html') ?>
                         <?php tpl_include_page($conf['sidebar'], true, true) ?>
                         <?php tpl_includeFile('sidebarfooter.html') ?>
-                    </div>
+                    </div></div>
                 </div></div><!-- /aside -->
             <?php endif; ?>
 
