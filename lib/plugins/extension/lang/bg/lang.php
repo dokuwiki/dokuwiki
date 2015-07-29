dbfd2a2c7bb8cab263b1e64c75a85ea93d6e0b60
Kiril <neohidra@gmail.com>
2015-07-29 18:31:07 +0200

    translation update

---
+++ lib/plugins/extension/lang/bg/lang.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Kiril <neohidra@gmail.com>
+ */
+$lang['tab_search']            = 'Търсене и инсталиране';
+$lang['tab_install']           = 'Ръчно инсталиране';
+$lang['notimplemented']        = 'Функционалността все още не реализирана';
+$lang['unknownauthor']         = 'Неизвестен автор';
+$lang['unknownversion']        = 'Неизвестна версия';
+$lang['btn_info']              = 'Повече информация';
+$lang['btn_update']            = 'Актуализиране';
+$lang['btn_uninstall']         = 'Деинсталиране';
+$lang['btn_install']           = 'Инсталиране';
+$lang['btn_reinstall']         = 'Преинсталиране';
+$lang['popularity']            = 'Популярност: %s%%';
+$lang['repository']            = 'Хранилище:';
+$lang['installed_version']     = 'Инсталирана версия:';
+$lang['available_version']     = 'Налична версия:';
+$lang['compatible']            = 'Съвместимост с:';
+$lang['provides']              = 'Осигурява:';
+$lang['error_badurl']          = 'URL адресите трябва да започват с http или https';
+$lang['error_download']        = 'Невъзможност за сваляне на файл: %s';
+$lang['install_url']           = 'Инсталиране от URL:';
