a0d2e3e98fe4e16dc1fa3ddaa4622a3373eb75ae
Carbain Frédéric <fcarbain@yahoo.fr>
2015-07-29 17:01:36 +0200

    translation update

--- lib/plugins/authplain/lang/fr/lang.php
+++ lib/plugins/authplain/lang/fr/lang.php
@@ -1,6 +1,9 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
+ * @author Carbain Frédéric <fcarbain@yahoo.fr>
  */
-$lang['userexists']     = 'Désolé, ce nom d\'utilisateur est déjà pris.';
+$lang['userexists']            = 'Désolé, ce nom d\'utilisateur est déjà pris.';
+$lang['usernotexists']         = 'Désolé, cet utilisateur n\'existe pas.';
