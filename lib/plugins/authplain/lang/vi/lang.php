a4fb25f78eda9f7ef1442a800ed0956a9a4e4101
hung nguyen <bancuamoinguoi999@gmail.com>
2015-07-30 03:05:29 +0200

    translation update

--- lib/plugins/authplain/lang/vi/lang.php
+++ lib/plugins/authplain/lang/vi/lang.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  */
-$lang['userexists']     = 'Bạn khác đã dùng username này rồi.';
+$lang['userexists']            = 'Bạn khác đã dùng username này rồi.';
