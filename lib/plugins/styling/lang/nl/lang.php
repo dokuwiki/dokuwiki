3b5e1a0ea28a4d5252a8b013997d8a3837dd5e28
hugo smet <hugo.smet@scarlet.be>
2015-07-31 13:01:27 +0200

    translation update

--- lib/plugins/styling/lang/nl/lang.php
+++ lib/plugins/styling/lang/nl/lang.php
@@ -4,7 +4,12 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Mark C. Prins <mprins@users.sf.net>
+ * @author hugo smet <hugo.smet@scarlet.be>
  */
+$lang['menu']                  = 'Sjabloon stijl instellingen';
+$lang['js']['loader']          = 'Voorbeeldweergave is aan het laden...<br />als dit niet verdwijnt zijn uw instellingen mogelijks foutief.';
+$lang['js']['popup']           = 'Open als popup';
+$lang['error']                 = 'Sorry, dit sjabloon ondersteunt deze functionaliteit niet.';
 $lang['btn_preview']           = 'Bekijk aanpassingen';
 $lang['btn_save']              = 'Sla aanpassingen op';
 $lang['btn_reset']             = 'Huidige aanpassingen verwerpen';
@@ -15,3 +20,5 @@ $lang['__text_alt__']          = 'Alternatieve tekstkleur';
 $lang['__background_alt__']    = 'Alternatieve achtergrondkleur';
 $lang['__text_neu__']          = 'Neutrale tekstkleur';
 $lang['__background_neu__']    = 'Neutrale achtergrondkleur';
+$lang['__border__']            = 'Kader kleur';
+$lang['__highlight__']         = 'Accentkleur (hoofdzakelijk voor zoekresultaten)';
