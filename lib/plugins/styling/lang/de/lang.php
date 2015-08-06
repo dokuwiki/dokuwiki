937959a1cc835709ee811f78c86d1fc28f07b74e
Anika Henke <anika@selfthinker.org>
2015-08-06 10:51:11 +0200

    translation update

---
+++ lib/plugins/styling/lang/de/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Anika Henke <anika@selfthinker.org>
+ */
+$lang['menu']                  = 'Einstellungen fürs Template-Design';
+$lang['js']['loader']          = 'Vorschau lädt...<br />Falls diese Nachricht nicht verschwindet, könnten Ihre Werte fehlerhaft sein';
+$lang['js']['popup']           = 'Öffne als Popup';
+$lang['error']                 = 'Dieses Template unterstützt diese Funktion nicht.';
+$lang['btn_preview']           = 'Vorschau der Änderungen anzeigen';
+$lang['btn_save']              = 'Änderungen speichern';
+$lang['btn_reset']             = 'Jetzige Änderungen rückgängig machen';
+$lang['btn_revert']            = 'Auf Templates Voreinstellungen zurückfallen';
+$lang['__text__']              = 'Haupttextfarbe';
+$lang['__background__']        = 'Haupthintergrundfarbe';
+$lang['__text_alt__']          = 'Alternative Textfarbe';
+$lang['__background_alt__']    = 'Alternative Hintergrundfarbe';
+$lang['__text_neu__']          = 'Neutrale Textfarbe';
+$lang['__background_neu__']    = 'Neutrale Hintergrundfarbe';
+$lang['__border__']            = 'Rahmenfarbe';
+$lang['__highlight__']         = 'Hervorhebungsfarbe (hauptsächlich für Suchergebnisse)';
