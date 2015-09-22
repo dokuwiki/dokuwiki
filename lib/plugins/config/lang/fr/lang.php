616ece991f01048f2eb28f619bfabc44aa2b23da
Legrand <legrand.an@gmail.com>
2015-09-22 14:25:16 +0200

    translation update

--- lib/plugins/config/lang/fr/lang.php
+++ lib/plugins/config/lang/fr/lang.php
@@ -24,6 +24,7 @@
  * @author Carbain Frédéric <fcarbain@yahoo.fr>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author Floriang <antispam@floriang.eu>
+ * @author Legrand <legrand.an@gmail.com>
  */
 $lang['menu']                  = 'Paramètres de configuration';
 $lang['error']                 = 'Paramètres non modifiés en raison d\'une valeur invalide, vérifiez vos réglages puis réessayez. <br />Les valeurs erronées sont entourées d\'une bordure rouge.';
