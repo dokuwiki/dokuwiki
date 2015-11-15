1f6acd00be4b3d9544f4d760ce73f938fedbacf6
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2015-11-15 12:26:24 +0100

    translation update

--- lib/plugins/authad/lang/cs/lang.php
+++ lib/plugins/authad/lang/cs/lang.php
@@ -9,4 +9,5 @@
 $lang['domain']                = 'Přihlašovací doména';
 $lang['authpwdexpire']         = 'Platnost vašeho hesla vyprší za %d dní, měli byste ho změnit co nejdříve.';
 $lang['passchangefail']        = 'Změna hesla selhala. Možná nebyla dodržena pravidla pro jejich tvorbu?';
+$lang['userchangefail']        = 'Změna atributů uživatele selhala. Možná nemá váš účet dostatečná oprávnění pro provádění změn. ';
 $lang['connectfail']           = 'Připojení k serveru Active Directory selhalo.';
