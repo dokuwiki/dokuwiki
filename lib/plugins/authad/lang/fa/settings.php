dd61e06f8c51d4d3153dd89c66adc9bc4d9bc06a
Mohmmad Razavi <sepent@gmail.com>
2015-12-27 10:11:15 +0100

    translation update

--- lib/plugins/authad/lang/fa/settings.php
+++ lib/plugins/authad/lang/fa/settings.php
@@ -4,7 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Omid Hezaveh <hezpublic@gmail.com>
+ * @author Mohmmad Razavi <sepent@gmail.com>
  */
+$lang['account_suffix']        = 'پسوند حساب کاربری شما. به عنوان مثال <code>@my.domain.org</code>';
+$lang['base_dn']               = 'DN پایه شما. به عنوان مثال <code>DC=my,DC=domain,DC=org</code>';
+$lang['domain_controllers']    = 'لیست کنترل کننده‌های دامنه که با کاما ازهم جدا شده اند. به عنوان مثال <code>srv1.domain.org,srv2.domain.org</code';
 $lang['admin_password']        = 'رمز کاربر بالایی ';
 $lang['use_ssl']               = 'از اس‌اس‌ال استفاده می‌کنید؟ در این صورت تی‌ال‌اس را در پایین فعال نکنید. ';
 $lang['use_tls']               = 'از تی‌ال‌اس استفاده می‌کنید؟ در این صورت اس‌اس‌ال را در بالا فعال نکنید. ';
+$lang['debug']                 = 'داده‌های اضافی خروجی دیباگ در هنگام بروز خطا نمایش داده شود؟';
+$lang['expirywarn']            = 'تعداد روزهایی که پس گذشتن آن برای تغییر رمزعبور به شما هشدار داده شود. باری غیرفعال سازی از مقدار 0 استفاده کنید.';
---
+++ lib/plugins/authmysql/lang/fa/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mohmmad Razavi <sepent@gmail.com>
+ */
+$lang['userexists']            = 'با عرض پوزش، یک کاربر با این نام از قبل وجود دارد.';
+$lang['usernotexists']         = 'با عرض پوزش، آن کاربر وجود نداشت.';
+$lang['writefail']             = 'امکان تغییر داده کاربر وجود نداشت. لطفا مسئول Wiki را آگاه کنید.';
