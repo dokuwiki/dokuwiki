c5cec702c5deebbeddeb00147d906a0cd0fc7ac0
diafol666 <ben.brynsadler@gmail.com>
2015-12-29 23:59:20 +0000

    Remaining plugin translations for Welsh (cy)

---
+++ lib/plugins/authplain/lang/cy/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['userexists']     = 'Sori, mae defnyddiwr gyda\'r enw hwnnw eisoes yn bodoli.';
+$lang['usernotexists']  = 'Sori, \'dyw\'r defnyddiwr hwnnw ddim yn bodoli.';
+$lang['writefail']      = 'Methu â newid data defnyddiwr. Rhowch wybod i Weinydd y Wici';
---
+++ lib/plugins/extension/lang/cy/lang.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Welsh language file for extension plugin
+ *
+ * @author Michael Hamann <michael@content-space.de>
+ * @author Christopher Smith <chris@jalakai.co.uk>
+ * @author Alan Davies <ben.brynsadler@gmail.com>
+ */
+
+$lang['menu']                         = 'Rheolwr Estyniadau';
+
+$lang['tab_plugins']                  = 'Ategion a Arsefydlwyd';
+$lang['tab_templates']                = 'Templedau a Arsefydlwyd';
+$lang['tab_search']                   = 'Chwilio ac Arsefydlu';
+$lang['tab_install']                  = 'Arsefydlu gan Law';
+
+$lang['notimplemented']               = '\'Dyw\'r nodwedd hon heb ei rhoi ar waith eto';
+$lang['notinstalled']                 = '\'Dyw\'r estyniad hwn heb ei arsefydlu';
+$lang['alreadyenabled']               = 'Cafodd yr estyniad hwn ei alluogi';
+$lang['alreadydisabled']              = 'Cafodd yr estyniad hwn ei analluogi';
+$lang['pluginlistsaveerror']          = 'Roedd gwall wrth gadw\'r rhestr ategion';
+$lang['unknownauthor']                = 'Awdur anhysbys';
+$lang['unknownversion']               = 'Fersiwn anhysbys';
+
+$lang['btn_info']                     = 'Dangos wybodaeth bellach';
+$lang['btn_update']                   = 'Diweddaru';
+$lang['btn_uninstall']                = 'Dad-arsefydlu';
+$lang['btn_enable']                   = 'Galluogi';
+$lang['btn_disable']                  = 'Analluogi';
+$lang['btn_install']                  = 'Arsyfydlu';
+$lang['btn_reinstall']                = 'Ail-arsefydlu';
+
+$lang['js']['reallydel']              = 'Ydych chi wir am ddad-arsefydlu\'r estyniad hwn?';
+
+$lang['search_for']                   = 'Chwilio Estyniadau:';
+$lang['search']                       = 'Chwilio';
+
+$lang['extensionby']                  = '<strong>%s</strong> gan %s';
+$lang['screenshot']                   = 'Sgrinlun %s';
+$lang['popularity']                   = 'Poblogrwydd: %s%%';
+$lang['homepage_link']                = 'Dogfennau';
+$lang['bugs_features']                = 'Bygiau';
+$lang['tags']                         = 'Tagiau:';
+$lang['author_hint']                  = 'Chwilio estyniadau gan awdur';
+$lang['installed']                    = 'Arsefydlwyd:';
+$lang['downloadurl']                  = 'URL Lawlwytho:';
+$lang['repository']                   = 'Ystorfa:';
+$lang['unknown']                      = '<em>anhysbys</em>';
+$lang['installed_version']            = 'Fersiwn a arsefydlwyd:';
+$lang['install_date']                 = 'Eich diweddariad diwethaf:';
+$lang['available_version']            = 'Fersiwn ar gael:';
+$lang['compatible']                   = 'Yn gydnaws â:';
+$lang['depends']                      = 'Yn dibynnu ar:';
+$lang['similar']                      = 'Yn debyg i:';
+$lang['conflicts']                    = 'Y gwrthdaro â:';
+$lang['donate']                       = 'Fel hwn?';
+$lang['donate_action']                = 'Prynwch goffi i\'r awdur!';
+$lang['repo_retry']                   = 'Ailgeisio';
+$lang['provides']                     = 'Darparu:';
+$lang['status']                       = 'Statws:';
+$lang['status_installed']             = 'arsefydlwyd';
+$lang['status_not_installed']         = 'heb ei arsefydlu';
+$lang['status_protected']             = 'amddiffynwyd';
+$lang['status_enabled']               = 'galluogwyd';
+$lang['status_disabled']              = 'analluogwyd';
+$lang['status_unmodifiable']          = 'methu addasu';
+$lang['status_plugin']                = 'ategyn';
+$lang['status_template']              = 'templed';
+$lang['status_bundled']               = 'bwndlwyd';
+
+$lang['msg_enabled']                  = 'Galluogwyd ategyn %s';
+$lang['msg_disabled']                 = 'Analluogwyd ategyn %s';
+$lang['msg_delete_success']           = 'Dad-arsefydlwyd estyniad %s';
+$lang['msg_delete_failed']            = 'Methodd dad-arsefydlu estyniad %s';
+$lang['msg_template_install_success'] = 'Arsefydlwyd templed %s yn llwyddiannus';
+$lang['msg_template_update_success']  = 'Diweddarwyd templed %s yn llwyddiannus';
+$lang['msg_plugin_install_success']   = 'Arsefydlwyd ategyn %s yn llwyddiannus';
+$lang['msg_plugin_update_success']    = 'Diweddarwyd ategyn %s yn llwyddiannus';
+$lang['msg_upload_failed']            = 'Methodd lanlwytho\'r ffeil';
+
+$lang['missing_dependency']           = '<strong>Missing or disabled dependency:</strong> %s';
+$lang['security_issue']               = '<strong>Mater Diogelwch:</strong> %s';
+$lang['security_warning']             = '<strong>Rhybudd Diogelwch:</strong> %s';
+$lang['update_available']             = '<strong>Diweddariad:</strong> Mae fersiwn newydd %s ar gael.';
+$lang['wrong_folder']                 = '<strong>Ategyn wedi\'i arsefydlu\'n anghywir:</strong> Ailenwch ffolder yr ategyn o "%s" i "%s".';
+$lang['url_change']                   = '<strong>Newid i\'r URL:</strong> Newidiodd yr URL lawlwytho ers y diweddariad diwethaf. Gwiriwch i weld os yw\'r URL newydd yn ddilys cyn diweddaru\'r estyniad.<br />Newydd: %s<br />Hen: %s';
+
+$lang['error_badurl']                 = 'Dylai URL ddechrau gyda http neu https';
+$lang['error_dircreate']              = 'Methu â chreu ffolder dros dro er mwyn derbyn y lawrlwythiad';
+$lang['error_download']               = 'Methu lawrlwytho\'r ffeil: %s';
+$lang['error_decompress']             = 'Methu datgywasgu\'r ffeil a lawrlwythwyd. Gall hwn fod o ganlyniad i lawrlwythiad gwael, felly ceisiwch eto; neu gall fod fformat y cywasgiad fod yn anhysbys, felly bydd yn rhaid i chi lawlwytho ac arsefydlu gan law.';
+$lang['error_findfolder']             = 'Methu ag adnabod ffolder yr estyniad, bydd angen lawrlwytho ac arsefydlu gan law';
+$lang['error_copy']                   = 'Roedd gwall copïo ffeil wrth geisio arsefydlu ffeiliau i\'r ffolder <em>%s</em>: gall fod y ddisgen yn llawn neu gall hawliau mynediad i ffeiliau fod yn anghywir. Gall hwn fod wedi achosi ategyn sydd wedi arsefydlu\'n rhannol ac sydd wedi ansefydlogi\'ch arsefydliad wici';
+
+$lang['noperms']                      = '\'Sdim modd ysgrifennu i\'r ffolder estyniadau';
+$lang['notplperms']                   = '\'Sdim modd ysgrifennu i\'r ffolder templedau';
+
+$lang['nopluginperms']                = '\'Sdim modd ysgrifennu i\'r ffolder ategion';
+$lang['git']                          = 'Cafodd yr estyniad hwn ei arsefydlu gan git, mae\'n bosib na fyddwch chi am ei ddiweddaru yma.';
+$lang['auth']                         = '\'Dyw\'r ategyn dilysu hwn heb ei alluogi yn y ffurfwedd, ystyriwch ei analluogi.';
+
+$lang['install_url']                  = 'Arsefydlu o URL:';
+$lang['install_upload']               = 'Lanlwytho Estyniad:';
+
+$lang['repo_error']                   = 'Doedd dim modd cysylltu â\'r ystorfa ategion. Sicrhewch fod hawl gan eich gweinydd i gysylltu â www.dokuwiki.org a gwiriwch eich gosodiadau procsi.';
+$lang['nossl']                        = 'Mae\'n debyg \'dyw eich PHP ddim yn cynnal SSL. Na fydd lawrlwytho yn gweithio ar gyfer nifer o estyniadau DokuWiki.';
+
+$lang['js']['display_viewoptions']    = 'Opsiynau Golwg:';
+$lang['js']['display_enabled']        = 'galluogwyd';
+$lang['js']['display_disabled']       = 'analluogwyd';
+$lang['js']['display_updatable']      = 'gallu diweddaru';
