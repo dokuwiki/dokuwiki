6cc3a794ea006c591ecd815293f6b4df08f18790
Michael Große <grosse@cosmocode.de>
2016-02-19 19:13:24 +0100

    Add more tests for action_plugin_struct_entry
    Github Issue #11, Issue #5

--- _test/entry.test.php
+++ _test/entry.test.php
@@ -6,7 +6,7 @@ use \plugin\struct\types\AbstractBaseType;
 use plugin\struct\meta;
 
 // we don't have the auto loader here
-spl_autoload_register(array('action_plugin_struct_autoloader', 'autoloader',));
+spl_autoload_register(array('action_plugin_struct_autoloader', 'autoloader'));
 
 class action_plugin_struct_entry extends \action_plugin_struct_entry {
 
@@ -67,7 +67,7 @@ class entry_struct_test extends \DokuWikiTest {
                     'new1' => array('label' => 'first', 'class' => 'Text', 'sort' => 10, 'ismulti' => 0),
                     'new2' => array('label' => 'second', 'class' => 'Text', 'sort' => 20, 'ismulti' => 1),
                     'new3' => array('label' => 'third', 'class' => 'Text', 'sort' => 30, 'ismulti' => 0),
-                    'new4' => array('label' => 'fourth', 'class' => 'Text', 'sort' => 40, 'ismulti' => 0),
+                    'new4' => array('label' => 'fourth', 'class' => 'Text', 'sort' => 40, 'ismulti' => 0)
                 )
             )
         );
@@ -80,7 +80,7 @@ class entry_struct_test extends \DokuWikiTest {
                     'new1' => array('label' => 'afirst', 'class' => 'Text', 'sort' => 10, 'ismulti' => 0),
                     'new2' => array('label' => 'asecond', 'class' => 'Text', 'sort' => 20, 'ismulti' => 1),
                     'new3' => array('label' => 'athird', 'class' => 'Text', 'sort' => 30, 'ismulti' => 0),
-                    'new4' => array('label' => 'afourth', 'class' => 'Text', 'sort' => 40, 'ismulti' => 0),
+                    'new4' => array('label' => 'afourth', 'class' => 'Integer', 'sort' => 40, 'ismulti' => 0)
                 )
             )
         );
@@ -179,5 +179,312 @@ class entry_struct_test extends \DokuWikiTest {
         $entry->validate($integer, 'label', null);
 
         $this->assertEquals(null,$MSG);
+    }
+
+    public function test_edit_page_wo_schema() {
+        $page = 'test_edit_page_wo_schema';
+
+        $request = new \TestRequest();
+        $response = $request->get(array('id' => $page, 'do' => 'edit'), '/doku.php');
+        $structElement = $response->queryHTML('.struct');
+
+        $this->assertEquals(1,count($structElement));
+        $this->assertEquals($structElement->html(),'');
+    }
+
+    public function test_edit_page_with_schema() {
+        $page = 'test_edit_page_with_schema';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+
+        $request = new \TestRequest();
+        $response = $request->get(array('id' => $page, 'do' => 'edit'), '/doku.php');
+        $structHTML = trim($response->queryHTML('.struct')->html());
+        $expectedHTML = '<h3>Schema2</h3>'."\n".'<label>afirst <input name="struct_schema_data[Schema2][afirst]" value=""></label><br><label>asecond <input name="struct_schema_data[Schema2][asecond]" value=""></label><br><label>athird <input name="struct_schema_data[Schema2][athird]" value=""></label><br><label>afourth <input name="struct_schema_data[Schema2][afourth]" value=""></label><br>';
+
+        $this->assertEquals($expectedHTML,$structHTML);
+    }
+
+    public function test_preview_page_invaliddata() {
+        $page = 'test_preview_page_invaliddata';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => 'Eve'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $response = $request->post(array('id' => $page, 'do' => 'preview'), '/doku.php');
+        $expected_errormsg = sprintf($this->lang['validation_prefix'] . $this->lang['Validation Exception Integer needed'],'afourth');
+        $actual_errormsg = $response->queryHTML('.error')->html();
+        $structHTML = trim($response->queryHTML('.struct')->html());
+        $expectedHTML = '<h3>Schema2</h3>'."\n".'<label>afirst <input name="struct_schema_data[Schema2][afirst]" value="foo"></label><br><label>asecond <input name="struct_schema_data[Schema2][asecond]" value="bar, baz"></label><br><label>athird <input name="struct_schema_data[Schema2][athird]" value="foobar"></label><br><label>afourth <input name="struct_schema_data[Schema2][afourth]" value="Eve"></label><br>';
+
+        $this->assertEquals($expected_errormsg, $actual_errormsg, 'If there is invalid data, then there should be an error message.');
+        $this->assertEquals($expectedHTML, $structHTML);
+    }
+
+    public function test_preview_page_validdata() {
+        $page = 'test_preview_page_validdata';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $response = $request->post(array('id' => $page, 'do' => 'preview'), '/doku.php');
+        $actual_errormsg = $response->queryHTML('.error')->get();
+        $structHTML = trim($response->queryHTML('.struct')->html());
+        $expectedHTML = '<h3>Schema2</h3>'."\n".'<label>afirst <input name="struct_schema_data[Schema2][afirst]" value="foo"></label><br><label>asecond <input name="struct_schema_data[Schema2][asecond]" value="bar, baz"></label><br><label>athird <input name="struct_schema_data[Schema2][athird]" value="foobar"></label><br><label>afourth <input name="struct_schema_data[Schema2][afourth]" value="42"></label><br>';
+
+        $this->assertEquals($actual_errormsg,array(), "If all data is valid, then there should be no error message.");
+        $this->assertEquals($expectedHTML,$structHTML);
+    }
+
+    public function test_fail_saving_empty_page() {
+        $page = 'test_fail_saving_empty_page';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('summary','only struct data saved');
+        $response = $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+        $expected_errormsg = $this->lang['emptypage'];
+        $actual_errormsg = $response->queryHTML('.error')->html();
+        $pagelog = new \PageChangeLog($page);
+        $revisions = $pagelog->getRevisions(-1, 200);
+
+        $this->assertEquals(0, count($revisions));
+        $this->assertEquals($expected_errormsg,$actual_errormsg, "An empty page should not be saved.");
+    }
+
+    public function test_fail_saveing_page_with_invaliddata() {
+        $page = 'test_fail_saveing_page_with_invaliddata';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+
+        $wikitext = 'teststring';
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => 'Eve'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext',$wikitext);
+        $request->setPost('summary','content and struct data saved');
+        $response = $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+        $actual_wikitext = trim($response->queryHTML('#wiki__text')->html());
+        $expected_wikitext = $wikitext;
+
+        $actual_errormsg = $response->queryHTML('.error')->html();
+        $expected_errormsg = sprintf($this->lang['validation_prefix'] . $this->lang['Validation Exception Integer needed'],'afourth');
+
+        $structHTML = trim($response->queryHTML('.struct')->html());
+        $expectedHTML = '<h3>Schema2</h3>'."\n".'<label>afirst <input name="struct_schema_data[Schema2][afirst]" value="foo"></label><br><label>asecond <input name="struct_schema_data[Schema2][asecond]" value="bar, baz"></label><br><label>athird <input name="struct_schema_data[Schema2][athird]" value="foobar"></label><br><label>afourth <input name="struct_schema_data[Schema2][afourth]" value="Eve"></label><br>';
+
+        $pagelog = new \PageChangeLog($page);
+        $revisions = $pagelog->getRevisions(-1, 200);
+
+        // assert
+        $this->assertEquals(0, count($revisions));
+        $this->assertEquals($expected_errormsg, $actual_errormsg, 'If there is invalid data, then there should be an error message.');
+        $this->assertEquals($expectedHTML,$structHTML);
+        $this->assertEquals($expected_wikitext,$actual_wikitext);
+        // todo: assert that no struct data has been saved
+    }
+
+    public function test_save_page() {
+        $page = 'test_save_page';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext','teststring');
+        $request->setPost('summary','content and struct data saved');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        $pagelog = new \PageChangeLog($page);
+        $revisions = $pagelog->getRevisions(-1, 200);
+        $revinfo = $pagelog->getRevisionInfo($revisions[0]);
+        $schemaData = new meta\SchemaData($schema, $page, 0);
+        $actual_struct_data = $schemaData->getDataArray();
+        $expected_struct_data = array(
+            'afirst' => 'foo',
+            'asecond' => array('bar', 'baz'),
+            'athird' => 'foobar',
+            'afourth' => 42
+        );
+
+        $this->assertEquals(1, count($revisions));
+        $this->assertEquals('content and struct data saved', $revinfo['sum']);
+        $this->assertEquals(DOKU_CHANGE_TYPE_CREATE, $revinfo['type']);
+        $this->assertEquals($expected_struct_data, $actual_struct_data);
+        // todo: assert that pagerevision and struct data have the same timestamp
+    }
+
+    /**
+     * @group slow
+     */
+    public function test_save_page_without_new_text() {
+        $page = 'test_save_page_without_new_text';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+        $wikitext = 'teststring';
+
+        // first save;
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext',$wikitext);
+        $request->setPost('summary','content and struct data saved');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        sleep(1);
+
+        // second save - only struct data
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo2',
+                'asecond' => 'bar2, baz2',
+                'athird' => 'foobar2',
+                'afourth' => '43'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext',$wikitext);
+        $request->setPost('summary','2nd revision');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        // assert
+        $pagelog = new \PageChangeLog($page);
+        $revisions = $pagelog->getRevisions(-1, 200);
+        $revinfo = $pagelog->getRevisionInfo($revisions[0]);
+        $schemaData = new meta\SchemaData($schema, $page, 0);
+        $actual_struct_data = $schemaData->getDataArray();
+        $expected_struct_data = array(
+            'afirst' => 'foo2',
+            'asecond' => array('bar2', 'baz2'),
+            'athird' => 'foobar2',
+            'afourth' => 43
+        );
+
+        $this->assertEquals(2, count($revisions), 'there should be 2 (two) revisions');
+        $this->assertEquals('2nd revision', $revinfo['sum']);
+        $this->assertEquals(DOKU_CHANGE_TYPE_EDIT, $revinfo['type']);
+        $this->assertEquals($expected_struct_data, $actual_struct_data);
+        // todo: assert that pagerevisions and struct entries have the same timestamps
+    }
+
+
+    /**
+     * @group slow
+     */
+    public function test_delete_page() {
+        $page = 'test_delete_page';
+        $assignment = new meta\Assignments();
+        $schema = 'Schema2';
+        $assignment->add($page, $schema);
+        $wikitext = 'teststring';
+
+        // first save;
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo',
+                'asecond' => 'bar, baz',
+                'athird' => 'foobar',
+                'afourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext',$wikitext);
+        $request->setPost('summary','content and struct data saved');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        sleep(1);
+
+        // delete
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'afirst' => 'foo2',
+                'asecond' => 'bar2, baz2',
+                'athird' => 'foobar2',
+                'afourth' => '43'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext','');
+        $request->setPost('summary','delete page');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        // assert
+        $pagelog = new \PageChangeLog($page);
+        $revisions = $pagelog->getRevisions(-1, 200);
+        $revinfo = $pagelog->getRevisionInfo($revisions[0]);
+        $schemaData = new meta\SchemaData($schema, $page, 0);
+        $actual_struct_data = $schemaData->getDataArray();
+        $expected_struct_data = array(
+            'afirst' => 'foo',
+            'asecond' => array('bar', 'baz'),
+            'athird' => 'foobar',
+            'afourth' => 42
+        );
+
+        $this->assertEquals(2, count($revisions), 'there should be 2 (two) revisions');
+        $this->assertEquals('delete page', $revinfo['sum']);
+        $this->assertEquals(DOKU_CHANGE_TYPE_DELETE, $revinfo['type']);
+        $this->assertEquals($expected_struct_data, $actual_struct_data);
+        // todo: timestamps?
     }
 }
