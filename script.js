d8882ef0c5fad05c37839acb5443c05e1876cce2
Michael Große <grosse@cosmocode.de>
2016-02-22 17:38:21 +0100

    Add image type
    SPR-322, GitHub Issue #13

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ jQuery(function(){
     jQuery('input.struct_date').datepicker({
         dateFormat: 'yy-mm-dd'
     });
+
+    jQuery('button.struct_img').click(function (event) {
+        var input_id = event.target.id.substr(0,event.target.id.length-'Button'.length);
+        window.open(
+            DOKU_BASE+"lib/exe/mediamanager.php?ns=:"+'&edid='+encodeURIComponent(input_id) + '&onselect=insertStructImage',
+            'mediaselect',
+            'width=750,height=500,left=20,top=20,scrollbars=yes,resizable=yes'); //
+    });
+
+    window.insertStructImage = function(edid, mediaid, opts, align) {
+        jQuery('#'+edid).val(mediaid).change();
+    };
 });
