a187dd5ebf7961babd471731ff715c58f6b24fb3
Andreas Gohr <gohr@cosmocode.de>
2016-02-23 10:48:06 +0100

    added JavaScript for adding new inputs to multi edit fields

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ jQuery(function(){
     jQuery('input.struct_date').datepicker({
         dateFormat: 'yy-mm-dd'
     });
+
+    /**
+     * Duplicate the elements in .newtemplate whenever any input in it changes
+     */
+    jQuery('#dw__editform').find('.struct .newtemplate').each(function(){
+        var $tplwrapper = jQuery(this);
+        var tpl = $tplwrapper.html();
+        $tplwrapper.on('change', 'input,textarea,select', function(){
+            if(jQuery(this).val() == '') return;
+            $tplwrapper.append(tpl);
+        });
+    });
+
 });
