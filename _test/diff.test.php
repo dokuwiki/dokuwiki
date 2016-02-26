fc99fe0b449bdbd56eaa7faffaf28329dbcdd5be
Michael Große <grosse@cosmocode.de>
2016-02-26 18:37:28 +0100

    Add incomplete test for diff view
    Github Issue #35

---
+++ _test/diff.test.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace plugin\struct\test;
+
+use \plugin\struct\types\AbstractBaseType;
+use plugin\struct\meta;
+
+// we don't have the auto loader here
+spl_autoload_register(array('action_plugin_struct_autoloader', 'autoloader'));
+
+/**
+ * Tests for the diff-view of the struct plugin
+ *
+ * @group plugin_struct
+ * @group plugins
+ *
+ * @covers action_plugin_struct_diff
+ *
+ *
+ */
+class diff_struct_test extends \DokuWikiTest {
+
+    protected $pluginsEnabled = array('struct','sqlite');
+
+    public function setUp() {
+        parent::setUp();
+
+        $schema = 'schema1';
+        $sb = new meta\SchemaBuilder(
+            $schema,
+            array(
+                'new' => array(
+                    'new1' => array('label' => 'first', 'class' => 'Text', 'sort' => 10, 'ismulti' => 0, 'isenabled' => 1),
+                    'new2' => array('label' => 'second', 'class' => 'Text', 'sort' => 20, 'ismulti' => 1, 'isenabled' => 1),
+                    'new3' => array('label' => 'third', 'class' => 'Text', 'sort' => 30, 'ismulti' => 0, 'isenabled' => 1),
+                    'new4' => array('label' => 'fourth', 'class' => 'Text', 'sort' => 40, 'ismulti' => 0, 'isenabled' => 1)
+                )
+            )
+        );
+        $sb->build();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+
+        /** @var \helper_plugin_struct_db $sqlite */
+        $sqlite = plugin_load('helper', 'struct_db');
+        $sqlite->resetDB();
+    }
+
+    public function test_diff() {
+        $page = 'test_save_page_without_new_text';
+        $assignment = new meta\Assignments();
+        $schema = 'schema1';
+        $assignment->add($page, $schema);
+        $wikitext = 'teststring';
+
+        // first save;
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'first' => 'foo',
+                'second' => 'bar, baz',
+                'third' => 'foobar',
+                'fourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext',$wikitext);
+        $request->setPost('summary','content and struct data saved');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        sleep(1);
+
+        // second save - only struct data
+        $request = new \TestRequest();
+        $structData = array(
+            $schema => array(
+                'first' => 'foo',
+                'second' => 'bar2, baz2',
+                'third' => 'foobar2',
+                'fourth' => '42'
+            )
+        );
+        $request->setPost('struct_schema_data',$structData);
+        $request->setPost('wikitext',$wikitext);
+        $request->setPost('summary','2nd revision');
+        $request->post(array('id' => $page, 'do' => 'save'), '/doku.php');
+
+        // diff
+        $request = new \TestRequest();
+        $response = $request->post(array('id' => $page, 'do' => 'diff'), '/doku.php');
+        print_r($response); // todo: do something with it
+        $this->markTestIncomplete('Assertations needed. There are many strange whitespace chars in the html?');
+
+    }
+
+}
