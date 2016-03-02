5841d63f744d0ade8cf2aaef8f67f90accf86353
Andreas Gohr <gohr@cosmocode.de>
2016-03-02 16:28:08 +0100

    added some tests for user type #35

---
+++ _test/Type_User.test.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace plugin\struct\test;
+
+use plugin\struct\types\User;
+
+spl_autoload_register(array('action_plugin_struct_autoloader', 'autoloader'));
+
+/**
+ * Testing the User Type
+ *
+ * @group plugin_struct
+ * @group plugins
+ */
+class Type_User_struct_test extends \DokuWikiTest {
+
+    protected $pluginsEnabled = array('struct', 'sqlite');
+
+    /**
+     * @expectedException \plugin\struct\meta\ValidationException
+     */
+    public function test_validate_fail() {
+        $user = new User();
+        $user->validate('nosuchuser');
+    }
+
+    public function test_validate_success() {
+        $user = new User();
+        $user->validate('testuser');
+        $this->assertTrue(true); // we simply check that no exceptions are thrown
+    }
+
+    public function test_ajax() {
+        global $INPUT;
+
+        $user = new User(
+            array(
+                'fullname' => true,
+                'autocomplete' => array(
+                    'mininput' => 2,
+                    'maxresult' => 5,
+                ),
+            )
+        );
+
+        $INPUT->set('search', 'test');
+        $this->assertEquals(array(array('label'=>'Arthur Dent [testuser]', 'value'=>'testuser')), $user->handleAjax());
+
+        $INPUT->set('search', 'dent');
+        $this->assertEquals(array(array('label'=>'Arthur Dent [testuser]', 'value'=>'testuser')), $user->handleAjax());
+
+        $INPUT->set('search', 'd'); // under mininput
+        $this->assertEquals(array(), $user->handleAjax());
+
+        $user = new User(
+            array(
+                'fullname' => false,
+                'autocomplete' => array(
+                    'mininput' => 2,
+                    'maxresult' => 5,
+                ),
+            )
+        );
+
+        $INPUT->set('search', 'test');
+        $this->assertEquals(array(array('label'=>'Arthur Dent [testuser]', 'value'=>'testuser')), $user->handleAjax());
+
+        $INPUT->set('search', 'dent');
+        $this->assertEquals(array(), $user->handleAjax());
+
+        $user = new User(
+            array(
+                'fullname' => false,
+                'autocomplete' => array(
+                    'mininput' => 2,
+                    'maxresult' => 0,
+                ),
+            )
+        );
+
+        $INPUT->set('search', 'test');
+        $this->assertEquals(array(), $user->handleAjax());
+    }
+}
