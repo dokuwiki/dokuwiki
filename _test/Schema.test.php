6c4d84cb06ab3ef2cca1767c0f6352f4ee19f794
Andreas Gohr <gohr@cosmocode.de>
2016-03-03 16:05:28 +0100

    simplified test
    no need to repeat a value

--- _test/Schema.test.php
+++ _test/Schema.test.php
@@ -27,37 +27,30 @@ class schema_struct_test extends \DokuWikiTest {
             array(
                 'abc',
                 'abc',
-                'abc',
             ),
             array(
                 '123abc',
                 'abc',
-                '123abc',
             ),
             array(
-                'abc123',
                 'abc123',
                 'abc123',
             ),
             array(
                 '_a_b_c_',
                 'a_b_c_',
-                '_a_b_c_',
             ),
             array(
                 '-a-b-c-',
                 'abc',
-                '-a-b-c-',
             ),
             array(
                 '/a/b/c/',
                 'abc',
-                '/a/b/c/',
             ),
             array(
                 '\\a\\b\\c\\',
                 'abc',
-                '\\a\\b\\c\\',
             )
         );
     }
@@ -69,13 +62,11 @@ class schema_struct_test extends \DokuWikiTest {
      *
      * @param string $input_name
      * @param string $expected_cleaned_name
-     * @param string $msg
      */
-    public function test_cleanTableName($input_name, $expected_cleaned_name, $msg) {
-
-
+    public function test_cleanTableName($input_name, $expected_cleaned_name) {
         $actual_cleaned_name = Schema::cleanTableName($input_name);
-
-        $this->assertSame($expected_cleaned_name, $actual_cleaned_name, $msg);
+        $this->assertSame($expected_cleaned_name, $actual_cleaned_name, $input_name);
     }
+
+
 }
