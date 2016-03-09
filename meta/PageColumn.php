9447008b781fc4da36a7831d9c7a4a0e1da33c14
Andreas Gohr <gohr@cosmocode.de>
2016-03-09 15:55:45 +0100

    make sure page columns have a label

--- meta/PageColumn.php
+++ meta/PageColumn.php
@@ -38,4 +38,11 @@ class PageColumn extends Column {
         return $col;
     }
 
+    /**
+     * @return string always '%pageid%'
+     */
+    public function getLabel() {
+        return '%pageid%';
+    }
+
 }
