3ca214409347ca39d0ac303da8a0755f3897f800
Andreas Gohr <andi@splitbrain.org>
2016-03-12 09:18:24 +0100

    remove postgres 9.3+ feature from dump
    this hopefully fixes the tests on travis

--- lib/plugins/authpdo/_test/pgsql/django.sql
+++ lib/plugins/authpdo/_test/pgsql/django.sql
@@ -6,7 +6,6 @@
 -- Dumped by pg_dump version 9.5.1
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -14,14 +13,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
