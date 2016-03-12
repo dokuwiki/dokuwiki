1d46eaf15e643a68facf0421651474b70f938bce
Andreas Gohr <andi@splitbrain.org>
2016-03-12 10:22:23 +0100

    removed accidentally checked in function
    this was an intermediate woriking state but got replaced by indivdual
    dump skipping. forgot to remove it again

--- lib/plugins/authpdo/_test/pgsql.test.php
+++ lib/plugins/authpdo/_test/pgsql.test.php
@@ -10,12 +10,4 @@ class pgsql_plugin_authpdo_test extends mysql_plugin_authpdo_test {
 
     protected $driver = 'pgsql';
 
-    public function test_requirements() {
-        parent::test_requirements();
-
-        if(!function_exists('hash_pbkdf2') || !in_array('sha256', hash_algos())){
-            $this->markTestSkipped("Skipped {$this->driver} tests. Missing pbkdf2 hash support to check passwords");
-        }
-    }
-
 }
