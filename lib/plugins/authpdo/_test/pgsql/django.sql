44f1c4b79060e507d4e212337b29b8afe46cbabb
Andreas Gohr <andi@splitbrain.org>
2016-03-12 09:22:13 +0100

    removed another new feature from the postgres dump

--- lib/plugins/authpdo/_test/pgsql/django.sql
+++ lib/plugins/authpdo/_test/pgsql/django.sql
@@ -10,7 +10,6 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
