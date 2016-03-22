c8500b4d30b96e5077963b7f15d8c021961b786d
Andreas Gohr <gohr@cosmocode.de>
2016-03-22 11:26:55 +0100

    added json import/export tests
    the json facilities can now be used for other tests as well

---
+++ _test/SchemaImporter.test.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace plugin\struct\test;
+
+use plugin\struct\meta;
+
+spl_autoload_register(array('action_plugin_struct_autoloader', 'autoloader'));
+
+/**
+ * @group plugin_struct
+ * @group plugins
+ *
+ */
+class SchemaImporter_struct_test extends \DokuWikiTest {
+
+    protected $pluginsEnabled = array('struct', 'sqlite');
+
+    protected function tearDown() {
+        parent::tearDown();
+
+        /** @var \helper_plugin_struct_db $sqlite */
+        $sqlite = plugin_load('helper', 'struct_db');
+        $sqlite->resetDB();
+    }
+
+    public function test_export() {
+        $sb = new meta\SchemaBuilder(
+            'schema1',
+            array(
+                'new' => array(
+                    'new1' => array('label' => 'first', 'class' => 'Text', 'sort' => 10, 'ismulti' => 0, 'isenabled' => 1),
+                    'new2' => array('label' => 'second', 'class' => 'Text', 'sort' => 20, 'ismulti' => 1, 'isenabled' => 1),
+                    'new3' => array('label' => 'third', 'class' => 'Text', 'sort' => 30, 'ismulti' => 0, 'isenabled' => 1),
+                    'new4' => array('label' => 'fourth', 'class' => 'Text', 'sort' => 40, 'ismulti' => 0, 'isenabled' => 1),
+                )
+            )
+        );
+        $sb->build();
+
+        $schema = new meta\Schema('schema1');
+        $expect = json_decode(file_get_contents(__DIR__.'/json/schema1.schema.json'), true);
+        $actual = json_decode($schema->toJSON(), true);
+        // we don't expect this to match
+        unset($expect['structversion']);
+        unset($actual['structversion']);
+        $this->assertEquals($expect, $actual);
+    }
+
+    public function test_import() {
+        $sb = new meta\SchemaImporter('foobar', file_get_contents(__DIR__.'/json/schema1.schema.json'));
+        $this->assertTrue($sb->build());
+
+        $schema = new meta\Schema('foobar');
+        $expect = json_decode(file_get_contents(__DIR__.'/json/schema1.schema.json'), true);
+        $actual = json_decode($schema->toJSON(), true);
+        // we don't expect this to match
+        unset($expect['structversion']);
+        unset($actual['structversion']);
+        $expect['schema'] = 'foobar'; // we exported the new schema
+        $this->assertEquals($expect, $actual);
+    }
+
+
+}
