f2e141a0254a80bdb2492f5df1ceed95fa8b6a92
Andreas Gohr <gohr@cosmocode.de>
2016-03-24 12:13:36 +0100

    catch JSON error in importer

--- meta/SchemaImporter.php
+++ meta/SchemaImporter.php
@@ -28,6 +28,33 @@ class SchemaImporter extends SchemaBuilder {
         $existing = count($this->oldschema->getColumns());
 
         $input = json_decode($json, true);
+        if($input === null) {
+            switch(json_last_error()) {
+                case JSON_ERROR_NONE:
+                    $error = 'No errors';
+                    break;
+                case JSON_ERROR_DEPTH:
+                    $error = 'Maximum stack depth exceeded';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    $error = 'Underflow or the modes mismatch';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    $error = 'Unexpected control character found';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    $error = 'Syntax error, malformed JSON';
+                    break;
+                case JSON_ERROR_UTF8:
+                    $error = 'Malformed UTF-8 characters, possibly incorrectly encoded';
+                    break;
+                default:
+                    $error = 'Unknown error';
+                    break;
+            }
+
+            throw new StructException('JSON couldn\'t be decoded: ' . $error);
+        }
         $data = array(
             'cols' => array(),
             'new' => array()
