f18db579a484f6017d9817ea954090e53016b940
Andreas Gohr <andi@splitbrain.org>
2016-04-15 14:25:29 +0200

    copied language files from authmysql

--- lib/plugins/authpdo/lang/en/lang.php
+++ lib/plugins/authpdo/lang/en/lang.php
@@ -7,7 +7,6 @@
 
 $lang['connectfail']    = 'Failed to connect to database.';
 $lang['userexists']     = 'Sorry, a user with this login already exists.';
-$lang['usernotexists']  = 'Sorry, that user doesn\'t exist.';
 $lang['writefail']      = 'Unable to modify user data. Please inform the Wiki-Admin';
 
 //Setup VIM: ex: et ts=4 :
---
+++ lib/plugins/authpdo/lang/nl/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hugo Smet <hugo.smet@scarlet.be>
+ */
+$lang['connectfail']           = 'Connectie met de database mislukt.';
+$lang['userexists']            = 'Sorry, een gebruiker met deze login bestaat reeds.';
+$lang['writefail']             = 'Onmogelijk om de gebruikers data te wijzigen. Gelieve de Wiki-Admin te informeren.';
