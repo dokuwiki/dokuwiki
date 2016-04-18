a1eba280ac2bc479d2d1f3b329702444f5974eb5
Gerrit Uitslag <klapinklapin@gmail.com>
2016-04-18 23:01:01 +0200

    Remove support for .disable in plugin folder
    Close #1323

--- inc/plugincontroller.class.php
+++ inc/plugincontroller.class.php
@@ -177,18 +177,6 @@ class Doku_Plugin_Controller {
                     // disabling mechanism was changed back very soon again
                     // to keep everything simple we just skip the plugin completely
                     continue;
-                } elseif (file_exists(DOKU_PLUGIN.$plugin.'/disabled')) {
-                    /**
-                     * treat this as a default disabled plugin(over-rideable by the plugin manager)
-                     * @deprecated 2011-09-10 (usage of disabled files)
-                     */
-                    if (empty($this->plugin_cascade['local'][$plugin])) {
-                        $all_plugins[$plugin] = 0;
-                    } else {
-                        $all_plugins[$plugin] = 1;
-                    }
-                    $this->plugin_cascade['default'][$plugin] = 0;
-
                 } elseif ((array_key_exists($plugin,$this->tmp_plugins) && $this->tmp_plugins[$plugin] == 0) ||
                           ($plugin === 'plugin' && isset($conf['pluginmanager']) && !$conf['pluginmanager'])){
                     $all_plugins[$plugin] = 0;
