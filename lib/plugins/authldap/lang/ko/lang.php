c256465607f803426acd75a1337ccdc2a3a338fe
Myeongjin <aranet100@gmail.com>
2016-06-26 12:36:11 +0200

    translation update

--- lib/plugins/authldap/lang/ko/lang.php
+++ lib/plugins/authldap/lang/ko/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['connectfail']           = 'LDAP가 연결할 수 없습니다: %s';
