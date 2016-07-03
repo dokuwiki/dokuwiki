a2a22f5ee65d08cca3bea99d1c2e0967f2fe7156
Laynee <seedfloyd@gmail.com>
2016-07-03 21:36:08 +0200

    Create settings.php

---
+++ lang/fr/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * French language file for struct plugin
+ *
+ * @author Andreas Gohr, Michael Große <dokuwiki@cosmocode.de>
+ * @author Laynee <seedfloyd@gmail.com>
+ */
+
+// keys need to match the config setting name
+# $lang['dformat'] = 'Date format (see PHP\'s <a href="http://php.net/manual/en/function.date.php#refsect1-function.date-parameters">date</a> function)';
+
+
+
+//Setup VIM: ex: et ts=4 :
