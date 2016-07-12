ecc947547e297aac8666c036f265ff044e632a3e
Andreas Gohr <andi@splitbrain.org>
2016-07-12 14:40:20 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+$lang['menu']                  = 'Struct Schema Editor';
+$lang['menu_assignments']      = 'Struct Schema Zuweisungen';
+$lang['headline']              = 'Strukturierte Daten';
+$lang['edithl']                = 'Schema <i>%s</i> bearbeiten';
+$lang['create']                = 'Neues Schema anlegen';
+$lang['schemaname']            = 'Schema-Name:';
+$lang['save']                  = 'Speichern';
+$lang['createhint']            = 'Achtung: Schemas können später nicht umbenannt werden';
+$lang['pagelabel']             = 'Seite';
+$lang['summary']               = 'Struct-Daten geändert';
+$lang['export']                = 'Schema als JSON exportieren';
+$lang['btn_export']            = 'Exportieren';
