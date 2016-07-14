01493ed83fdbb5952a92beea3d06ff3a5e12557b
Michael Grosse <mic.grosse@posteo.de>
2016-07-14 14:57:20 +0200

    Test overwriting in SELECTs and arbitrary SELECTs

--- _test/QueryBuilderSelect.test.php
+++ _test/QueryBuilderSelect.test.php
@@ -25,6 +25,30 @@ class QueryBuilderSelect_struct_test extends StructTest {
         $this->assertEquals(array(), $actual_opts);
     }
 
+    public function test_overwrite_selects() {
+        $qb = new QueryBuilder();
+
+        $qb->addTable('first', 'T1');
+        $qb->addSelectColumn('T1','colbar_original','colAlias');
+        $qb->addSelectColumn('T1','colfoo_overwritten','colAlias');
+
+        $expectedSQL = 'SELECT T1.colfoo_overwritten AS colAlias FROM first AS T1 WHERE';
+        list($actual_sql, $actual_opts) = $qb->getSQL();
+        $this->assertEquals($this->cleanWS($expectedSQL), $this->cleanWS($actual_sql));
+        $this->assertEquals(array(), $actual_opts);
+    }
+
+    public function test_arbitrary_selects() {
+        $qb = new QueryBuilder();
+
+        $qb->addSelectStatement('a.b','C');
+
+        $expectedSQL = 'SELECT a.b AS C FROM WHERE';
+        list($actual_sql, $actual_opts) = $qb->getSQL();
+        $this->assertEquals($this->cleanWS($expectedSQL), $this->cleanWS($actual_sql));
+        $this->assertEquals(array(), $actual_opts);
+    }
+
     /**
      * @expectedException \dokuwiki\plugin\struct\meta\StructException
      */
