1336841072ceafa32d5fcd03e7e6f45425c6899d
Sjoerd <sjoerd@sjomar.eu>
2016-08-10 12:02:29 +0200

    translation update

--- lib/plugins/authldap/lang/nl/lang.php
+++ lib/plugins/authldap/lang/nl/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hugo Smet <hugo.smet@scarlet.be>
  */
 $lang['connectfail']           = 'LDAP kan niet connecteren: %s';
