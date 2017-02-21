aa31d8d0d3cb70d880cec09ff9bad568df9874ec
controlonline.net <controlonline.net@gmail.com>
2017-02-21 13:15:55 +0100

    translation update

---
+++ lib/plugins/authad/lang/ca/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author controlonline.net <controlonline.net@gmail.com>
+ */
+$lang['account_suffix']        = 'El teu nom de compte. Ej.<code>@my.domain.org</code>';
+$lang['base_dn']               = 'Nom base DN. Ej. <code>DC=my,DC=domain,DC=org</code>';
+$lang['domain_controllers']    = 'Llista separada per coma dels controladors de domini. Ej.<code>DC=my,DC=domain,DC=org</code>';
