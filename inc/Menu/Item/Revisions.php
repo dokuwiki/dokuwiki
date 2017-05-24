e5226905cab9cec5dd7e6d745539ef2fcaa4b9aa
Andreas Gohr <gohr@cosmocode.de>
2017-05-24 16:37:43 +0200

    fix do parameter of revisions type

--- inc/Menu/Item/Revisions.php
+++ inc/Menu/Item/Revisions.php
@@ -14,7 +14,6 @@ class Revisions extends AbstractItem {
         parent::__construct();
 
         $this->type = 'revs';
-        $this->params['do'] = 'revs';
         $this->svg = DOKU_INC . 'lib/images/menu/07-revisions_history.svg';
     }
 
