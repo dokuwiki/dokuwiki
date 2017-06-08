741fc20e753ea87df39d7321ea4b8cce981623c2
root <root@web.iqo.uni-hannover.de>
2017-06-08 12:59:34 +0200

    Added negotiate auth support

--- lib/plugins/authldap/conf/default.php
+++ lib/plugins/authldap/conf/default.php
@@ -1,22 +1,25 @@
 <?php
 
-$conf['server']      = '';
-$conf['port']        = 389;
-$conf['usertree']    = '';
-$conf['grouptree']   = '';
-$conf['userfilter']  = '';
-$conf['groupfilter'] = '';
-$conf['version']     = 2;
-$conf['starttls']    = 0;
-$conf['referrals']   = -1;
-$conf['deref']       = 0;
-$conf['binddn']      = '';
-$conf['bindpw']      = '';
+$conf['server']         = '';
+$conf['port']           = 389;
+$conf['usertree']       = '';
+$conf['grouptree']      = '';
+$conf['userfilter']     = '';
+$conf['groupfilter']    = '';
+$conf['version']        = 2;
+$conf['starttls']       = 0;
+$conf['referrals']      = -1;
+$conf['deref']          = 0;
+$conf['binddn']         = '';
+$conf['bindpw']         = '';
 //$conf['mapping']['name']  unsupported in config manager
 //$conf['mapping']['grps']  unsupported in config manager
-$conf['userscope']  = 'sub';
-$conf['groupscope'] = 'sub';
-$conf['userkey']    = 'uid';
-$conf['groupkey']   = 'cn';
-$conf['debug']      = 0;
-$conf['modPass']    = 1;
+$conf['userscope']      = 'sub';
+$conf['groupscope']     = 'sub';
+$conf['userkey']        = 'uid';
+$conf['groupkey']       = 'cn';
+$conf['debug']          = 0;
+$conf['modPass']        = 1;
+$conf['negauthkeytab']  = '';
+$conf['ldapauthkeytab'] = '';
+$conf['ldapauthprinc']  = '';
