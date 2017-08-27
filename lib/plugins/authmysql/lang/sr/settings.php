d39e9bea24a7ba90507d3480f95066fdbe3699a8
Марко М. Костић <marko.m.kostic@gmail.com>
2017-08-27 23:41:09 +0200

    translation update

---
+++ lib/plugins/authmysql/lang/sr/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Марко М. Костић <marko.m.kostic@gmail.com>
+ */
+$lang['server']                = 'Ваш MySQL сервер';
+$lang['user']                  = 'MySQL корисничко име';
+$lang['password']              = 'Лозинка корисника изнад';
+$lang['database']              = 'База коју треба користити';
+$lang['charset']               = 'Кодни распоред коришћен у бази';
+$lang['debug']                 = 'Прикажи додатне податке за поправљање грешака';
+$lang['forwardClearPass']      = 'Пренеси корисничке лозинке као чист текст у SQL изјавама испод уместо коришћења passcrypt опције';
---
+++ lib/plugins/extension/lang/sr/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Марко М. Костић <marko.m.kostic@gmail.com>
+ */
+$lang['menu']                  = 'Управник проширења';
+$lang['tab_plugins']           = 'Инсталирани прикључци';
+$lang['tab_templates']         = 'Инсталирани шаблони';
+$lang['tab_search']            = 'Тражи и инсталирај';
+$lang['tab_install']           = 'Ручно инсталирање';
+$lang['notimplemented']        = 'Ова могућност још није израђена';
+$lang['notinstalled']          = 'Ово проширење још није инсталирано';
+$lang['alreadyenabled']        = 'Ово проширење је већ омогућено';
+$lang['alreadydisabled']       = 'Ово проширење је већ онемогућено';
