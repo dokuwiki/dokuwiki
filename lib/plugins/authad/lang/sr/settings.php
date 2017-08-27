b5e63f63ddb301be0f0cebc0ce984b10332d72fb
Марко М. Костић <marko.m.kostic@gmail.com>
2017-08-27 21:41:14 +0200

    translation update

---
+++ lib/plugins/authad/lang/sr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Марко М. Костић <marko.m.kostic@gmail.com>
+ */
+$lang['account_suffix']        = 'Суфикс на вашем налогу. Нпр.: <code>@moj.domen.rs</code>';
