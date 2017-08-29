16a367d4fc734ab4d7ded6a2ca8ba3ed94d7a0a5
Andreas Gohr <andi@splitbrain.org>
2017-08-29 21:00:33 +0200

    refactor Ajax functions into a class
    This doesn't really do much except wrapping the functions into a
    namespaced class. Autoloading takes care of loading the file.
    It's now possible to call lib/exe/ajax.php multiple times in a test
    request.

--- lib/exe/ajax.php
+++ lib/exe/ajax.php
@@ -6,29 +6,20 @@
  * @author     Andreas Gohr <andi@splitbrain.org>
  */
 
-if(!defined('DOKU_INC')) define('DOKU_INC',dirname(__FILE__).'/../../');
-require_once(DOKU_INC.'inc/init.php');
-require_once(DOKU_INC.'inc/ajax_functions.php');
+if(!defined('DOKU_INC')) define('DOKU_INC', dirname(__FILE__) . '/../../');
+require_once(DOKU_INC . 'inc/init.php');
 
 //close session
 session_write_close();
 
+// default header, ajax call may overwrite it later
 header('Content-Type: text/html; charset=utf-8');
 
 //call the requested function
+global $INPUT;
 if($INPUT->has('call')) {
     $call = $INPUT->filter('utf8_stripspecials')->str('call');
-    $callfn = 'ajax_'.$call;
-
-    if(function_exists($callfn)) {
-        $callfn();
-    } else {
-        $evt = new Doku_Event('AJAX_CALL_UNKNOWN', $call);
-        if ($evt->advise_before()) {
-            print "AJAX call '".htmlspecialchars($call)."' unknown!\n";
-        } else {
-            $evt->advise_after();
-            unset($evt);
-        }
-    }
+    new \dokuwiki\Ajax($call);
+} else {
+    http_status(404);
 }
