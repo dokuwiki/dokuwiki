70fa7d20ce4bf46af331a7f992ed9eaf0f0d1bb8
Szymon Olewniczak <solewniczak@rid.pl>
2017-09-13 14:23:46 +0200

    typos fix

--- lib/plugins/config/lang/en/lang.php
+++ lib/plugins/config/lang/en/lang.php
@@ -174,8 +174,8 @@ $lang['renderer__core']   = '%s (dokuwiki core)';
 $lang['renderer__plugin'] = '%s (plugin)';
 $lang['rev_handle']  = 'How to handle old revisions?';
 $lang['rev_handle_o_normal']     = 'parse them as standard wiki pages (default)';
-$lang['rev_handle_o_only_media'] = 'internal links works normally, media files points to historical versions';
-$lang['rev_handle_o_at']         = 'links and media files points to historical versions';
+$lang['rev_handle_o_only_media'] = 'internal links works normally, media files point to historical versions';
+$lang['rev_handle_o_at']         = 'links and media files point to historical versions';
 
 /* Network Options */
 $lang['dnslookups'] = 'DokuWiki will lookup hostnames for remote IP addresses of users editing pages. If you have a slow or non working DNS server or don\'t want this feature, disable this option';
