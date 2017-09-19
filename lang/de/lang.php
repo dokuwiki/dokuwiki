9b4977e0bced4f20e020c234b5e7df43c873613a
Malte Lembeck <malte.lembeck@outlook.de>
2017-09-19 15:25:10 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,18 +4,31 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Andreas Gohr <andi@splitbrain.org>
+ * @author Malte Lembeck <malte.lembeck@outlook.de>
  */
 $lang['menu']                  = 'Struct Schema Editor';
 $lang['menu_assignments']      = 'Struct Schema Zuweisungen';
 $lang['headline']              = 'Strukturierte Daten';
-$lang['edithl']                = 'Schema <i>%s</i> bearbeiten';
 $lang['create']                = 'Neues Schema anlegen';
 $lang['schemaname']            = 'Schema-Name:';
 $lang['save']                  = 'Speichern';
 $lang['createhint']            = 'Achtung: Schemas können später nicht umbenannt werden';
 $lang['pagelabel']             = 'Seite';
+$lang['rowlabel']              = 'Reihe #';
+$lang['revisionlabel']         = 'Zuletzt geändert';
+$lang['userlabel']             = 'letzter Bearbeitender';
 $lang['summary']               = 'Struct-Daten geändert';
 $lang['export']                = 'Schema als JSON exportieren';
 $lang['btn_export']            = 'Exportieren';
-
+$lang['btn_import']            = 'Importieren';
+$lang['btn_delete']            = 'Löschen';
 $lang['js']['confirmAssignmentsDelete'] = 'Wollen Sie wirklich die Zuweisung von Schma "{0}" zu Seite/Namensraum "{1}" löschen?';
+$lang['tab_export']            = 'Importieren/Exportieren';
+$lang['tab_delete']            = 'Löschen';
+$lang['editor_sort']           = 'Sortieren';
+$lang['editor_conf']           = 'Konfiguration';
+$lang['editor_type']           = 'Eingeben';
+$lang['editor_enabled']        = 'Aktiviert';
+$lang['assign_add']            = 'Hinzufügen';
+$lang['assign_del']            = 'Löschen';
+$lang['multidropdown']         = 'Halte STRG oder CMD um mehrere Werte auszuwählen.';
