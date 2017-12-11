cae2ec44b84adb8b2364364aca166b1318be123a
Oskars Pakers <oskars.pakers@gmail.com>
2017-12-11 08:31:03 +0100

    translation update

--- lib/plugins/authldap/lang/lv/settings.php
+++ lib/plugins/authldap/lang/lv/settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Aivars Miška <allefm@gmail.com>
  */
 $lang['starttls']              = 'Lietot TLS  savienojumus?';
--- lib/plugins/authmysql/lang/lv/settings.php
+++ lib/plugins/authmysql/lang/lv/settings.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Oskars Pakers <oskars.pakers@gmail.com>
  * @author Aivars Miška <allefm@gmail.com>
  */
+$lang['server']                = 'Jūsu MySQL serveris';
 $lang['user']                  = 'MySQL lietotāja vārds';
 $lang['password']              = 'Lietotāja parole';
 $lang['delUser']               = 'SQL pieprasījums lietotāja dzēšanai';
