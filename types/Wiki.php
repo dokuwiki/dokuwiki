b0002ceb977b7ffb58e9d95e55ac666c375b2d24
Randolf Rotta <rrotta@informatik.tu-cottbus.de>
2018-01-02 23:03:35 +0100

    inherit Wiki type from LongText to reduce code duplication

--- types/Wiki.php
+++ types/Wiki.php
@@ -3,15 +3,7 @@ namespace dokuwiki\plugin\struct\types;
 
 use dokuwiki\plugin\struct\meta\QueryBuilderWhere;
 
-class Wiki extends AbstractBaseType {
-    use TraitFilterPrefix;
-
-    protected $config = array(
-        'prefix' => '',
-        'postfix' => '',
-        'rows' => '5',
-        'cols' => '50'
-    );
+class Wiki extends LongText {
 
     /**
      * @param int|string $value
@@ -24,40 +16,5 @@ class Wiki extends AbstractBaseType {
         $doc = p_render($mode, p_get_instructions($value), $info);
         $R->doc .= $doc; // FIXME this probably does not work for all renderers
         return true;
-    }
-
-    /**
-     * Clean line endings
-     *
-     * @param int|string $rawvalue
-     * @return int|string
-     */
-    public function validate($rawvalue) {
-        $rawvalue = rtrim($rawvalue);
-        $rawvalue = cleanText($rawvalue);
-        return $rawvalue;
-    }
-
-    /**
-     * Use a text area for input
-     *
-     * @param string $name
-     * @param string $rawvalue
-     * @param string $htmlID
-     *
-     * @return string
-     */
-    public function valueEditor($name, $rawvalue, $htmlID) {
-        $rawvalue = formText($rawvalue);
-        $params = array(
-            'name' => $name,
-            'class' => 'struct_'.strtolower($this->getClass()),
-            'id' => $htmlID,
-            'rows' => $this->config['rows'],
-            'cols' => $this->config['cols']
-        );
-        $attributes = buildAttributes($params, true);
-
-        return "<textarea $attributes>$rawvalue</textarea>";
     }
 }
