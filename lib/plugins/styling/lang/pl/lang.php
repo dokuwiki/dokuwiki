5e88fd52ec45b26a25038d806bf61bab7a4b1177
Max <maxrb146@gmail.com>
2018-01-05 16:46:13 +0100

    translation update

---
+++ lib/plugins/styling/lang/pl/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Max <maxrb146@gmail.com>
+ */
+$lang['menu']                  = 'Ustawienia Szablonu';
+$lang['js']['popup']           = 'Otwórz w nowym oknie';
+$lang['error']                 = 'Przepraszamy, ten szablon nie wspiera tej funkcjonalności';
+$lang['btn_preview']           = 'Pokaż zmiany ';
+$lang['btn_save']              = 'Zapisz zmiany';
+$lang['btn_reset']             = 'Cofnij zmiany ';
+$lang['__text__']              = 'Kolor tekstu ';
+$lang['__background__']        = 'Kolor tła ';
+$lang['__text_alt__']          = 'Inny kolor tekstu';
+$lang['__background_alt__']    = 'Inny kolor tła';
+$lang['__border__']            = 'kolor obramowania ';
