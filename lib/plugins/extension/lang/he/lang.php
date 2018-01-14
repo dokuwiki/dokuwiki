dcbec55d04f3bfcbd64a907b736ce6aa04c36d9d
Guy Yakobovitch <guy.yakobovitch@gmail.com>
2018-01-14 11:45:56 +0100

    translation update

---
+++ lib/plugins/extension/lang/he/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Guy Yakobovitch <guy.yakobovitch@gmail.com>
+ */
+$lang['menu']                  = 'מנהל הרחבות';
+$lang['tab_plugins']           = 'תוספים מותקנים';
+$lang['tab_templates']         = 'תבניות מותקנות';
+$lang['tab_install']           = 'התקנה ידנית';
+$lang['notinstalled']          = 'הרחבה זו לא מותקנת';
