5404d005e011a1c67e804e94952b0af1fbad18e5
Simon <sbx@protonmail.ch>
2018-02-10 14:31:28 +0100

    translation update

--- lib/plugins/popularity/lang/sl/lang.php
+++ lib/plugins/popularity/lang/sl/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Matej Urbančič (mateju@svn.gnome.org)
  */
 $lang['name']                  = 'Poročilo o uporabi (nalaganje strani je lahko dolgotrajno)';
