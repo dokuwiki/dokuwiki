6a2655bc67e01c95c7a4ad5e1bf9b9393a7c2915
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-03-08 03:24:06 +1000

    Languageable licenses (edit 4 PHP)
    Tested, works

--- conf/license.php
+++ conf/license.php
@@ -4,33 +4,31 @@
  * wiki contents under. Do not change this file, but create a
  * license.local.php instead.
  */
-
 $license['cc-zero'] = array(
     'name' => 'CC0 1.0 Universal',
-    'url'  => 'http://creativecommons.org/publicdomain/zero/1.0/',
+    'url'  => 'http://creativecommons.org/publicdomain/zero/1.0/deed.'.$conf['lang'],
 );
 $license['publicdomain'] = array(
     'name' => 'Public Domain',
-    'url'  => 'http://creativecommons.org/licenses/publicdomain/',
+    'url'  => 'http://creativecommons.org/licenses/publicdomain/deed.'.$conf['lang'],
 );
 $license['cc-by'] = array(
     'name' => 'CC Attribution 4.0 International',
-    'url'  => 'http://creativecommons.org/licenses/by/4.0/',
+    'url'  => 'http://creativecommons.org/licenses/by/4.0/deed.'.$conf['lang'],
 );
 $license['cc-by-sa'] = array(
     'name' => 'CC Attribution-Share Alike 4.0 International',
-    'url'  => 'http://creativecommons.org/licenses/by-sa/4.0/',
+    'url'  => 'http://creativecommons.org/licenses/by-sa/4.0/deed.'.$conf['lang'],
 );
 $license['gnufdl'] = array(
     'name' => 'GNU Free Documentation License 1.3',
-    'url'  => 'http://www.gnu.org/licenses/fdl-1.3.html',
+    'url'  => 'http://www.gnu.org/licenses/fdl-1.3.'.$conf['lang'].'.html',
 );
 $license['cc-by-nc'] = array(
     'name' => 'CC Attribution-Noncommercial 4.0 International',
-    'url'  => 'http://creativecommons.org/licenses/by-nc/4.0/',
+    'url'  => 'http://creativecommons.org/licenses/by-nc/4.0/deed.'.$conf['lang'],
 );
 $license['cc-by-nc-sa'] = array(
     'name' => 'CC Attribution-Noncommercial-Share Alike 4.0 International',
-    'url'  => 'http://creativecommons.org/licenses/by-nc-sa/4.0/',
+    'url'  => 'http://creativecommons.org/licenses/by-nc-sa/4.0/deed.'.$conf['lang'],
 );
-
