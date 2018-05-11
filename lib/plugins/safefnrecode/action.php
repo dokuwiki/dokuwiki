661d78e851c0ed5f6d0d6a0022010c9e95f30f85
Andreas Gohr <andi@splitbrain.org>
2018-05-11 11:42:39 +0200

    PSR-2 for safefnrecode plugin

--- lib/plugins/safefnrecode/action.php
+++ lib/plugins/safefnrecode/action.php
@@ -6,63 +6,63 @@
  * @author  Andreas Gohr <andi@splitbrain.org>
  */
 
-// must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
+class action_plugin_safefnrecode extends DokuWiki_Action_Plugin
+{
 
-require_once DOKU_PLUGIN.'action.php';
-
-class action_plugin_safefnrecode extends DokuWiki_Action_Plugin {
-
-    public function register(Doku_Event_Handler $controller) {
-
-        $controller->register_hook('INDEXER_TASKS_RUN', 'BEFORE', $this, 'handle_indexer_tasks_run');
-
+    /** @inheritdoc */
+    public function register(Doku_Event_Handler $controller)
+    {
+        $controller->register_hook('INDEXER_TASKS_RUN', 'BEFORE', $this, 'handleIndexerTasksRun');
     }
 
-    public function handle_indexer_tasks_run(Doku_Event &$event, $param) {
+    /**
+     * Handle indexer event
+     *
+     * @param Doku_Event $event
+     * @param $param
+     */
+    public function handleIndexerTasksRun(Doku_Event $event, $param)
+    {
         global $conf;
-        if($conf['fnencode'] != 'safe') return;
+        if ($conf['fnencode'] != 'safe') return;
 
-        if(!file_exists($conf['datadir'].'_safefn.recoded')){
+        if (!file_exists($conf['datadir'].'_safefn.recoded')) {
             $this->recode($conf['datadir']);
             touch($conf['datadir'].'_safefn.recoded');
         }
 
-        if(!file_exists($conf['olddir'].'_safefn.recoded')){
+        if (!file_exists($conf['olddir'].'_safefn.recoded')) {
             $this->recode($conf['olddir']);
             touch($conf['olddir'].'_safefn.recoded');
         }
 
-        if(!file_exists($conf['metadir'].'_safefn.recoded')){
+        if (!file_exists($conf['metadir'].'_safefn.recoded')) {
             $this->recode($conf['metadir']);
             touch($conf['metadir'].'_safefn.recoded');
         }
 
-        if(!file_exists($conf['mediadir'].'_safefn.recoded')){
+        if (!file_exists($conf['mediadir'].'_safefn.recoded')) {
             $this->recode($conf['mediadir']);
             touch($conf['mediadir'].'_safefn.recoded');
         }
-
     }
 
     /**
      * Recursive function to rename all safe encoded files to use the new
      * square bracket post indicator
      */
-    private function recode($dir){
+    private function recode($dir)
+    {
         $dh = opendir($dir);
-        if(!$dh) return;
+        if (!$dh) return;
         while (($file = readdir($dh)) !== false) {
-            if($file == '.' || $file == '..') continue;           # cur and upper dir
-            if(is_dir("$dir/$file")) $this->recode("$dir/$file"); #recurse
-            if(strpos($file,'%') === false) continue;             # no encoding used
-            $new = preg_replace('/(%[^\]]*?)\./','\1]',$file);    # new post indicator
-            if(preg_match('/%[^\]]+$/',$new)) $new .= ']';        # fix end FS#2122
-            rename("$dir/$file","$dir/$new");                     # rename it
+            if ($file == '.' || $file == '..') continue;           # cur and upper dir
+            if (is_dir("$dir/$file")) $this->recode("$dir/$file"); #recurse
+            if (strpos($file, '%') === false) continue;             # no encoding used
+            $new = preg_replace('/(%[^\]]*?)\./', '\1]', $file);    # new post indicator
+            if (preg_match('/%[^\]]+$/', $new)) $new .= ']';        # fix end FS#2122
+            rename("$dir/$file", "$dir/$new");                     # rename it
         }
         closedir($dh);
     }
-
 }
-
-// vim:ts=4:sw=4:et:
