7a371ad48872e8a22314d3ecda74aeadc443e9d5
Andreas Gohr <andi@splitbrain.org>
2018-05-11 13:36:05 +0200

    deleted deprecated auth plugins
    authmysql and authpgsql were replaced by authpdo two releases ago.

--- lib/plugins/authmysql/lang/eo/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Kristjan SCHMIDT <kristjan.schmidt@googlemail.com>
- */
-$lang['usernotexists']         = 'Pardonu, tiu uzanto ne ekzistas.';
