4af80fe8e39fd3a0e35794f0f7e7b595f9614510
Michael Große <grosse@cosmocode.de>
2018-05-16 15:16:28 +0200

    refactor: Rename lib/exe/indexer.php to taskrunner.php 🔨

--- inc/template.php
+++ inc/template.php
@@ -1169,7 +1169,7 @@ function tpl_indexerWebBug() {
     global $ID;
 
     $p           = array();
-    $p['src']    = DOKU_BASE.'lib/exe/indexer.php?id='.rawurlencode($ID).
+    $p['src']    = DOKU_BASE.'lib/exe/taskrunner.php?id='.rawurlencode($ID).
         '&'.time();
     $p['width']  = 2; //no more 1x1 px image because we live in times of ad blockers...
     $p['height'] = 1;
