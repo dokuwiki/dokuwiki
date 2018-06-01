a6e1db4a9c0b6bd991bac30cd7f60fe26f4f5b5a
Andreas Gohr <andi@splitbrain.org>
2018-06-01 11:19:20 +0200

    fallback classes for plugins inheriting from old settings classes
    This will still throw a signature mismatch warning on PHP7 but at least
    it is no longer fatal.

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -11,7 +11,7 @@
 class RemoteAccessDeniedException extends \dokuwiki\Remote\AccessDeniedException {
     /** @inheritdoc */
     public function __construct($message = "", $code = 0, Throwable $previous = null) {
-        dbg_deprecated('dokuwiki\Remote\AccessDeniedException');
+        dbg_deprecated(\dokuwiki\Remote\AccessDeniedException::class);
         parent::__construct($message, $code, $previous);
     }
 
@@ -24,12 +24,11 @@ class RemoteAccessDeniedException extends \dokuwiki\Remote\AccessDeniedException
 class RemoteException extends \dokuwiki\Remote\RemoteException {
     /** @inheritdoc */
     public function __construct($message = "", $code = 0, Throwable $previous = null) {
-        dbg_deprecated('dokuwiki\\Remote\\RemoteException');
+        dbg_deprecated(\dokuwiki\Remote\RemoteException::class);
         parent::__construct($message, $code, $previous);
     }
 
 }
-
 
 /**
  * Escapes regex characters other than (, ) and /
@@ -39,6 +38,42 @@ class RemoteException extends \dokuwiki\Remote\RemoteException {
  * @deprecated 2018-05-04
  */
 function Doku_Lexer_Escape($str) {
-    dbg_deprecated('dokuwiki\\Parsing\\Lexer\\Lexer::escape');
+    dbg_deprecated('\\dokuwiki\\Parsing\\Lexer\\Lexer::escape()');
     return \dokuwiki\Parsing\Lexer\Lexer::escape($str);
+}
+
+/**
+ * @inheritdoc
+ * @deprecated 2018-06-01
+ */
+class setting extends \dokuwiki\plugin\config\core\Setting\Setting {
+    /** @inheritdoc */
+    public function __construct($key, array $params = null) {
+        dbg_deprecated(\dokuwiki\plugin\config\core\Setting\Setting::class);
+        parent::__construct($key, $params);
+    }
+}
+
+/**
+ * @inheritdoc
+ * @deprecated 2018-06-01
+ */
+class setting_authtype extends \dokuwiki\plugin\config\core\Setting\SettingAuthtype {
+    /** @inheritdoc */
+    public function __construct($key, array $params = null) {
+        dbg_deprecated(\dokuwiki\plugin\config\core\Setting\SettingAuthtype::class);
+        parent::__construct($key, $params);
+    }
+}
+
+/**
+ * @inheritdoc
+ * @deprecated 2018-06-01
+ */
+class setting_string extends \dokuwiki\plugin\config\core\Setting\SettingString {
+    /** @inheritdoc */
+    public function __construct($key, array $params = null) {
+        dbg_deprecated(\dokuwiki\plugin\config\core\Setting\SettingString::class);
+        parent::__construct($key, $params);
+    }
 }
