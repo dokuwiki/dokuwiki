724970e65d3b777d7844a92ab2419c85c3e28c6b
Andreas Gohr <andi@splitbrain.org>
2018-06-02 09:19:30 +0200

    correctly avoid notice in init.

--- inc/init.php
+++ inc/init.php
@@ -551,7 +551,7 @@ EOT;
 function fullpath($path,$exists=false){
     static $run = 0;
     $root  = '';
-    $iswin = (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' || @$GLOBALS['DOKU_UNITTEST_ASSUME_WINDOWS']);
+    $iswin = (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' || !empty($GLOBALS['DOKU_UNITTEST_ASSUME_WINDOWS']));
 
     // find the (indestructable) root of the path - keeps windows stuff intact
     if($path{0} == '/'){
