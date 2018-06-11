dokuwiki/dokuwiki

commit 96d46bf41a428b555c02f30f1bb496535d41bc5d

feat(RemoteAPI): Add call to delete Users to the remote API ✨

inc/RemoteAPICore.php
@@ -66,6 +66,10 @@ class RemoteAPICore {
                 'args' => array('string', 'string', 'array'),
                 'return' => 'bool',
                 'doc' => 'Append text to a wiki page.'
+            ), 'dokuwiki.deleteUsers' => array(
+                'args' => array('array'),
+                'return' => 'bool',
+                'doc' => 'Remove one or more users from the list of registered users.'
             ),  'wiki.getPage' => array(
                 'args' => array('string'),
                 'return' => 'string',
@@ -547,6 +551,23 @@ class RemoteAPICore {
             return $currentpage;
         }
         return $this->putPage($id, $currentpage.$text, $params);
+    }
+
+    /**
+     * Remove one or more users from the list of registered users
+     *
+     * @param string[] $usernames List of usernames to remove
+     *
+     * @return bool
+     */
+    public function deleteUsers($usernames)
+    {
+        if (!auth_isadmin()) {
+            return false;
+        }
+        /** @var DokuWiki_Auth_Plugin $auth */
+        global $auth;
+        return (bool)$auth->triggerUserMod('deleteUsers', $usernames);
     }
 
     /**

commit f823310a36837f10a50a510eae6976385713653b

✅ Add unittest for deleteUsers remote API call
Remote API calls should have some unittest as this test would have caught
the bug that was introduced in 96d46bf41a428b555c02f30f1bb496535d41bc5d
and fixed in ebf1744911831ec6d15325039a040effa41e466c

_test/tests/inc/remoteapicore.test.php
@@ -445,6 +445,20 @@ You can use up to five different levels of',
         $this->assertEquals(0, count($versions));
     }
 
+    public function test_deleteUser()
+    {
+        global $conf, $auth;
+        $auth = new Mock_Auth_Plugin();
+        $conf['remote'] = 1;
+        $conf['remoteuser'] = 'testuser';
+        $_SERVER['REMOTE_USER'] = 'testuser';
+        $params = [
+            ['testuser']
+        ];
+        $actualCallResult = $this->remote->call('dokuwiki.deleteUsers', $params);
+        $this->assertTrue($actualCallResult);
+    }
+
     public function test_aclCheck() {
         $id = 'aclpage';
 