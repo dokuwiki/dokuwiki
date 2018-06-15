1696f72579aa6761f1c0229005a25cd432bc480a
Andreas Gohr <andi@splitbrain.org>
2018-06-15 10:57:28 +0200

    remove debugging stuff from detail.php
    we haven't needed this in years

--- lib/exe/detail.php
+++ lib/exe/detail.php
@@ -17,26 +17,12 @@ trigger_event('DETAIL_STARTED', $tmp);
 //close session
 session_write_close();
 
-if($conf['allowdebug'] && $INPUT->has('debug')){
-    print '<pre>';
-    foreach(explode(' ','basedir userewrite baseurl useslash') as $x){
-        print '$'."conf['$x'] = '".$conf[$x]."';\n";
-    }
-    foreach(explode(' ','DOCUMENT_ROOT HTTP_HOST SCRIPT_FILENAME PHP_SELF '.
-                'REQUEST_URI SCRIPT_NAME PATH_INFO PATH_TRANSLATED') as $x){
-        print '$'."_SERVER['$x'] = '".$_SERVER[$x]."';\n";
-    }
-    print "getID('media'): ".getID('media')."\n";
-    print "getID('media',false): ".getID('media',false)."\n";
-    print '</pre>';
-}
-
 $ERROR = false;
 // check image permissions
 $AUTH = auth_quickaclcheck($IMG);
 if($AUTH >= AUTH_READ){
     // check if image exists
-    $SRC = mediaFN($IMG,$REV); 
+    $SRC = mediaFN($IMG,$REV);
     if(!file_exists($SRC)){
         //doesn't exist!
         http_status(404);
