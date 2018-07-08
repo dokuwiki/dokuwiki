6fc7582e8b60daf582a38d70b71226a619c2ff15
Rodrigo Romero <rcromero0106@gmail.com>
2018-07-08 03:31:39 +0200

    translation update

--- lib/plugins/usermanager/lang/pt/lang.php
+++ lib/plugins/usermanager/lang/pt/lang.php
@@ -3,11 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Rodrigo Romero <rcromero0106@gmail.com>
  * @author José Monteiro <Jose.Monteiro@DoWeDo-IT.com>
  * @author Enrico Nicoletto <liverig@gmail.com>
  * @author Fil <fil@meteopt.com>
  * @author André Neves <drakferion@gmail.com>
- * @author José Campos zecarlosdecampos@gmail.com
+ * @author José Campos <zecarlosdecampos@gmail.com>
  * @author Guido Salatino <guidorafael23@gmail.com>
  * @author Romulo Pereira <romuloccomp@gmail.com>
  * @author Paulo Carmino <contato@paulocarmino.com>
