65a878f98c684b62bd8d0a4ebb6065325206d957
mahir <mahirtakak@gmail.com>
2018-07-26 23:41:31 +0200

    translation update

--- lib/plugins/acl/lang/tr/lang.php
+++ lib/plugins/acl/lang/tr/lang.php
@@ -2,13 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Selim Farsakoğlu <farsakogluselim@yahoo.de>
  * @author Aydın Coşkuner <aydinweb@gmail.com>
  * @author Cihan Kahveci <kahvecicihan@gmail.com>
  * @author Yavuz Selim <yavuzselim@gmail.com>
  * @author Caleb Maclennan <caleb@alerque.com>
- * @author farukerdemoncel@gmail.com
+ * @author farukerdemoncel <farukerdemoncel@gmail.com>
  */
 $lang['admin_acl']             = 'Erişim Kontrol Listesi (ACL) Yönetimi';
 $lang['acl_group']             = 'Grup:';
---
+++ lib/plugins/authad/lang/tr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author mahir <mahirtakak@gmail.com>
+ */
+$lang['admin_password']        = 'Yukarıdaki kullanıcının şifresi.';
