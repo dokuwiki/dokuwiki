7c12e6ba3571433b62153ffbfbadb312bc0d395a
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-08 17:29:57 +1000

    Add files via upload

--- inc/lang/be/jquery.ui.datepicker.js
+++ inc/lang/be/jquery.ui.datepicker.js
@@ -1,5 +1,5 @@
-/* Belarusian initialisation for the jQuery UI date picker plugin. */
-/* Written by Pavel Selitskas <p.selitskas@gmail.com> */
+/* Belarussian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
 ( function( factory ) {
 	if ( typeof define === "function" && define.amd ) {
 
@@ -14,17 +14,17 @@
 
 datepicker.regional.be = {
 	closeText: "Зачыніць",
-	prevText: "&larr;Папяр.",
-	nextText: "Наст.&rarr;",
-	currentText: "Сёньня",
-	monthNames: [ "Студзень","Люты","Сакавік","Красавік","Травень","Чэрвень",
-	"Ліпень","Жнівень","Верасень","Кастрычнік","Лістапад","Сьнежань" ],
-	monthNamesShort: [ "Сту","Лют","Сак","Кра","Тра","Чэр",
-	"Ліп","Жні","Вер","Кас","Ліс","Сьн" ],
-	dayNames: [ "нядзеля","панядзелак","аўторак","серада","чацьвер","пятніца","субота" ],
-	dayNamesShort: [ "ндз","пнд","аўт","срд","чцв","птн","сбт" ],
-	dayNamesMin: [ "Нд","Пн","Аў","Ср","Чц","Пт","Сб" ],
-	weekHeader: "Тд",
+	prevText: "&#x3C;Папя",
+	nextText: "Наст&#x3E;",
+	currentText: "Сёння",
+	monthNames: [ "Студзень","Люты","Сакавік","Красавік","Май","Чэрвень",
+	"Ліпень","Жнівень","Верасень","Кастрычнік","Лістапад","Снежань" ],
+	monthNamesShort: [ "Сту","Лют","Сак","Кра","Май","Чэр",
+	"Ліп","Жні","Вер","Кас","Ліс","Сне" ],
+	dayNames: [ "нядзеля","панядзелак","аўторак","серада","чацвер","пятніца","субота" ],
+	dayNamesShort: [ "ндз","пнд","атр","срд","чцв","птн","сбт" ],
+	dayNamesMin: [ "Нд","Пн","Ат","Ср","Чц","Пт","Сб" ],
+	weekHeader: "Тыд",
 	dateFormat: "dd.mm.yy",
 	firstDay: 1,
 	isRTL: false,
