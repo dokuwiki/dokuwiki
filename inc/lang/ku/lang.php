357862acf4f0e911a133232d67a398564a6715d1
qezwan <qezwan@gmail.com>
2018-08-28 22:45:14 +0200

    translation update

--- inc/lang/ku/lang.php
+++ inc/lang/ku/lang.php
@@ -1,45 +1,45 @@
 <?php
+
 /**
  * kurdish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @translator Erdal Ronahî <erdal.ronahi@gmail.com>
+ *
+ * @author qezwan <qezwan@gmail.com>
  */
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-$lang['btn_edit']   = 'Vê rûpelê biguherîne';
-$lang['btn_source'] = 'Çavkaniya rûpelê nîşan bide';
-$lang['btn_show']   = 'Rûpelê nîşan bide';
-$lang['btn_create'] = 'Vê rûpelê biafirîne';
-$lang['btn_search'] = 'Lêbigere';
-$lang['btn_save']   = 'Tomar bike';
-$lang['btn_preview']= 'Pêşdîtin';
-$lang['btn_top']    = 'Biçe ser';
-$lang['btn_newer']  = '<< nûtir';
-$lang['btn_older']  = 'kevntir >>';
-$lang['btn_revs']   = 'Revîziyonên kevn';
-$lang['btn_recent'] = 'Guherandinên dawî';
-$lang['btn_upload'] = 'Bar bike';
-$lang['btn_cancel'] = 'Betal';
-$lang['btn_index']  = 'Îndeks';
-$lang['btn_secedit']= 'Biguherîne';
-$lang['btn_login']  = 'Têkeve';
-$lang['btn_logout'] = 'Derkeve';
-$lang['btn_admin']  = 'Admin';
-$lang['btn_update'] = 'Rojanekirin';
-$lang['btn_delete'] = 'Jê bibe';
-$lang['btn_back']   = 'Paş';
-$lang['btn_backlink']    = 'Girêdanên paş';
-
-$lang['nothingfound']= 'Tiştek nehat dîtin.';
-$lang['reference']   = 'Referansa';
-$lang['toc']        = 'Tabloya Navêrokê';
-$lang['line']       = 'Rêz';
-$lang['breadcrumb'] = 'Şop:';
-$lang['lastmod']    = 'Guherandina dawî:';
-$lang['deleted']    = 'hat jê birin';
-$lang['created']    = 'hat afirandin';
-$lang['summary']    = 'Kurteya guhartinê';
-
-//Setup VIM: ex: et ts=2 :
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['btn_edit']              = 'Vê rûpelê biguherîne';
+$lang['btn_source']            = 'Çavkaniya rûpelê nîşan bide';
+$lang['btn_show']              = 'Rûpelê nîşan bide';
+$lang['btn_create']            = 'Vê rûpelê biafirîne';
+$lang['btn_search']            = 'Lêbigere';
+$lang['btn_save']              = 'Tomar bike';
+$lang['btn_preview']           = 'Pêşdîtin';
+$lang['btn_top']               = 'Biçe ser';
+$lang['btn_newer']             = '<< nûtir';
+$lang['btn_older']             = 'kevntir >>';
+$lang['btn_revs']              = 'Revîziyonên kevn';
+$lang['btn_recent']            = 'Guherandinên dawî';
+$lang['btn_upload']            = 'Bar bike';
+$lang['btn_cancel']            = 'Betal';
+$lang['btn_index']             = 'Îndeks';
+$lang['btn_secedit']           = 'Biguherîne';
+$lang['btn_login']             = 'Têkeve';
+$lang['btn_logout']            = 'Derkeve';
+$lang['btn_admin']             = 'Admin';
+$lang['btn_update']            = 'Rojanekirin';
+$lang['btn_delete']            = 'Jê bibe';
+$lang['btn_back']              = 'Paş';
+$lang['btn_backlink']          = 'Girêdanên paş';
+$lang['passchk']               = 'باریکی دیکە';
+$lang['nothingfound']          = 'Tiştek nehat dîtin.';
+$lang['reference']             = 'Referansa';
+$lang['toc']                   = 'Tabloya Navêrokê';
+$lang['line']                  = 'Rêz';
+$lang['breadcrumb']            = 'Şop:';
+$lang['lastmod']               = 'Guherandina dawî:';
+$lang['deleted']               = 'hat jê birin';
+$lang['created']               = 'hat afirandin';
+$lang['summary']               = 'Kurteya guhartinê';
