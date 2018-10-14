45c4a35b9393df4410b300bda8b77db81d3234bb
Dmytro Marchenko <dmytro.marchenko1989@gmail.com>
2018-10-14 11:36:07 +0200

    translation update

---
+++ lib/plugins/styling/lang/uk/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Dmytro Marchenko <dmytro.marchenko1989@gmail.com>
+ */
+$lang['js']['popup']           = 'Відкрити як спливаюче вікно';
+$lang['error']                 = 'На жаль, цей шаблон не підтримує цю функцію.';
+$lang['btn_preview']           = 'Попередній перегляд змін';
+$lang['btn_save']              = 'Зберегти зміни';
+$lang['btn_reset']             = 'Скидання поточних змін';
