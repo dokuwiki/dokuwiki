84cbcac6b7ceffc91a56bad9be6e1a607b53ce19
Katerina Katapodi <extragold1234@hotmail.com>
2018-11-21 19:01:00 +0100

    translation update

---
+++ lib/plugins/authldap/lang/el/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <extragold1234@hotmail.com>
+ */
+$lang['groupkey']              = 'Εγγραφή ομάδας ως μέλους από οιαδήποτε κατηγορία χρήστη (αντί των στάνταρ ομάδων AD) πχ ομάδα τμήματος ή αριθμός τηλεφώνου';
+$lang['deref_o_0']             = 'LDAP_DEREF_NEVER	';
+$lang['deref_o_1']             = 'LDAP_DEREF_SEARCHING';
+$lang['deref_o_2']             = 'LDAP_DEREF_FINDING	';
+$lang['deref_o_3']             = 'LDAP_DEREF_ALWAYS	';
