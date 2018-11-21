09fe0bcc208167f4589966eb82bcafb3bb25e183
Katerina Katapodi <extragold1234@hotmail.com>
2018-11-21 20:41:15 +0100

    translation update

---
+++ lib/plugins/extension/lang/el/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <extragold1234@hotmail.com>
+ */
+$lang['js']['reallydel']       = 'Να γίνει ακύρωση εγκατάστασης αυτής της προέκτασης ';
+$lang['js']['display_viewoptions'] = 'Δέστε Επιλογές ';
+$lang['js']['display_enabled'] = 'ενεργοποιήθηκε';
+$lang['js']['display_disabled'] = 'απενεργοποιήθηκε';
+$lang['js']['display_updatable'] = 'χωρίς δεδομένα';
+$lang['search_for']            = 'Προέκταση Αναζήτησης';
+$lang['search']                = 'Αναζήτηση';
+$lang['extensionby']           = '<strong>%s</strong> με %s	';
+$lang['screenshot']            = 'Εικονίδιο του  %s	';
+$lang['popularity']            = 'Επισκέψεις: %s%%	';
+$lang['homepage_link']         = 'Έγγραφα ';
+$lang['bugs_features']         = 'Λάθη';
+$lang['tags']                  = 'Τίτλοι ;';
+$lang['author_hint']           = 'Αναζητήστε τις προεκτάσεις αυτού του συγγραφέα';
+$lang['installed']             = 'Εγκατάσταση ;';
+$lang['downloadurl']           = 'Κατεβάστε το URL :';
+$lang['repository']            = 'Χώρος αρχείων :';
+$lang['unknown']               = '<em>άγνωστο</em>	';
+$lang['installed_version']     = 'Εγκαταστημένη εκδοχή:';
+$lang['install_date']          = 'Η τελευταία ενημέρωση αρχείου σας:';
