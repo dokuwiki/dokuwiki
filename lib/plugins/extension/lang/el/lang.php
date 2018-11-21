4dfd392f8e0b54f40c5f2150e0ddb46949a9a1ce
Gerrit Uitslag <klapinklapin@gmail.com>
2018-11-21 21:50:30 +0100

    Update lang.php

--- lib/plugins/extension/lang/el/lang.php
+++ lib/plugins/extension/lang/el/lang.php
@@ -5,23 +5,23 @@
  *
  * @author Katerina Katapodi <extragold1234@hotmail.com>
  */
-$lang['js']['reallydel']       = 'Να γίνει ακύρωση εγκατάστασης αυτής της προέκτασης ';
-$lang['js']['display_viewoptions'] = 'Δέστε Επιλογές ';
+$lang['js']['reallydel']       = 'Να γίνει ακύρωση εγκατάστασης αυτής της προέκτασης';
+$lang['js']['display_viewoptions'] = 'Δέστε Επιλογές';
 $lang['js']['display_enabled'] = 'ενεργοποιήθηκε';
 $lang['js']['display_disabled'] = 'απενεργοποιήθηκε';
 $lang['js']['display_updatable'] = 'χωρίς δεδομένα';
 $lang['search_for']            = 'Προέκταση Αναζήτησης';
 $lang['search']                = 'Αναζήτηση';
-$lang['extensionby']           = '<strong>%s</strong> με %s	';
+$lang['extensionby']           = '<strong>%s</strong> με %s';
 $lang['screenshot']            = 'Εικονίδιο του  %s	';
-$lang['popularity']            = 'Επισκέψεις: %s%%	';
-$lang['homepage_link']         = 'Έγγραφα ';
+$lang['popularity']            = 'Επισκέψεις: %s%%';
+$lang['homepage_link']         = 'Έγγραφα';
 $lang['bugs_features']         = 'Λάθη';
-$lang['tags']                  = 'Τίτλοι ;';
+$lang['tags']                  = 'Τίτλοι;';
 $lang['author_hint']           = 'Αναζητήστε τις προεκτάσεις αυτού του συγγραφέα';
 $lang['installed']             = 'Εγκατάσταση ;';
-$lang['downloadurl']           = 'Κατεβάστε το URL :';
-$lang['repository']            = 'Χώρος αρχείων :';
-$lang['unknown']               = '<em>άγνωστο</em>	';
+$lang['downloadurl']           = 'Κατεβάστε το URL:';
+$lang['repository']            = 'Χώρος αρχείων:';
+$lang['unknown']               = '<em>άγνωστο</em>';
 $lang['installed_version']     = 'Εγκαταστημένη εκδοχή:';
 $lang['install_date']          = 'Η τελευταία ενημέρωση αρχείου σας:';
