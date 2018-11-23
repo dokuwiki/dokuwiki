569fd3cf8b9ad55ac4e8dae3b89394cb8011a2bb
Katerina Katapodi <extragold1234@hotmail.com>
2018-11-23 11:06:03 +0100

    translation update

---
+++ lib/plugins/styling/lang/el/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <extragold1234@hotmail.com>
+ */
+$lang['btn_save']              = 'Αποθηκεύσετε τις αλλαγές';
+$lang['btn_reset']             = 'Ρυθμίστε ξανά τις τρέχουσες αλλαγές';
+$lang['btn_revert']            = 'Τα πρότυπα επαναφοράς πίσω στο επιλεγμένο πρότυπο';
+$lang['__text__']              = 'Κύριο χρώμα κειμένου';
+$lang['__background__']        = 'Κύριο χρώμα φόντου';
+$lang['__text_alt__']          = 'Εναλλακτικό χρώμα κειμένου';
+$lang['__background_alt__']    = 'Εναλλακτικό χρώμα φόντου';
