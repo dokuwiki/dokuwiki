ad1ea0895e7dbc54443c3f218e7487a09f859399
Phy <git@phy25.com>
2019-01-11 15:52:17 -0500

    add test for utf-8 obfuscate #2665

--- _test/tests/inc/common_obfuscate.test.php
+++ _test/tests/inc/common_obfuscate.test.php
@@ -15,6 +15,13 @@ class common_obfuscate_test extends DokuWikiTest {
         '&#x6a;&#x6f;&#x6e;&#x2d;&#x64;&#x6f;&#x65;&#x40;&#x65;&#x78;&#x61;&#x6d;&#x70;&#x6c;&#x65;&#x2e;&#x63;&#x6f;&#x6d;');
     }
 
+    function test_hex_utf32(){
+        global $conf;
+        $conf['mailguard'] = 'hex';
+        $this->assertEquals(obfuscate('user@example.com?subject=Привет'),
+        '&#x75;&#x73;&#x65;&#x72;&#x40;&#x65;&#x78;&#x61;&#x6D;&#x70;&#x6C;&#x65;&#x2E;&#x63;&#x6F;&#x6D;&#x3F;&#x73;&#x75;&#x62;&#x6A;&#x65;&#x63;&#x74;&#x3D;&#x41F;&#x440;&#x438;&#x432;&#x435;&#x442;');
+    }
+
     function test_visible(){
         global $conf;
         $conf['mailguard'] = 'visible';
