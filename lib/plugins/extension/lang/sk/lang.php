c54d03d6756c4901cf0e37f2a612e68f53d3ebf2
Tibor Repček <tiborepcek@gmail.com>
2019-01-24 21:36:43 +0100

    Add files via upload (#2679)

--- lib/plugins/extension/lang/sk/lang.php
+++ lib/plugins/extension/lang/sk/lang.php
@@ -1,17 +1,28 @@
 <?php
 
 /**
+ * Slovak language file for extension plugin
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Martin Michalek <michalek.dev@gmail.com>
+ * @author Tibor Repček <tiborepcek@gmail.com>
  */
-$lang['tab_plugins']           = 'Inštalované pluginy';
+$lang['menu']                  = 'Správca rozšírení';
+
+$lang['tab_plugins']           = 'Inštalované rozšírenia';
 $lang['tab_templates']         = 'Inštalované šablóny';
-$lang['tab_search']            = 'Hľadanie e inštalácia';
+$lang['tab_search']            = 'Hľadanie a inštalácia';
 $lang['tab_install']           = 'Manuálna inštalácia';
+
 $lang['notimplemented']        = 'Táto vlastnosť ešte nebola implementovaná';
+$lang['notinstalled']          = 'Toto rozšírenie nie je nainštalované';
+$lang['alreadyenabled']        = 'Toto rozšírenie už bolo povolené';
+$lang['alreadydisabled']       = 'Toto rozšírenie už bolo zakázané';
+$lang['pluginlistsaveerror']   = 'Pri ukladaní zoznamu rozšírení sa vyskytla chyba';
 $lang['unknownauthor']         = 'Neznámy autor';
 $lang['unknownversion']        = 'Neznáma verzia';
+
 $lang['btn_info']              = 'Viac informácií';
 $lang['btn_update']            = 'Aktualizácia';
 $lang['btn_uninstall']         = 'Odinštalovanie';
@@ -19,43 +30,82 @@ $lang['btn_enable']            = 'Povolenie';
 $lang['btn_disable']           = 'Zablokovanie';
 $lang['btn_install']           = 'Inštalácia';
 $lang['btn_reinstall']         = 'Re-Inštalácia';
+
+$lang['js']['reallydel']       = 'Naozaj chcete toto rozšírenie odinštalovať?';
+
+$lang['search_for']            = 'Hľadať rozšírenie:';
 $lang['search']                = 'Vyhľadávanie';
+
 $lang['extensionby']           = '<strong>%s</strong> od %s';
 $lang['screenshot']            = 'Obrázok od %s';
 $lang['popularity']            = 'Popularita: %s%%';
 $lang['homepage_link']         = 'Dokumentácia';
 $lang['bugs_features']         = 'Chyby:';
 $lang['tags']                  = 'Kľúčové slová:';
+$lang['author_hint']           = 'Hľadať rozšírenia podľa autora';
+$lang['installed']             = 'Nainštalované:';
+$lang['downloadurl']           = 'Stiahnuť z:';
+$lang['repository']            = 'Repozitár:';
 $lang['unknown']               = '<em>neznámy</em>';
 $lang['installed_version']     = 'Inštalovaná verzia:';
 $lang['install_date']          = 'Posledná aktualizácia:';
 $lang['available_version']     = 'Dostupné verzie:';
 $lang['compatible']            = 'Kompaktibilita:';
+$lang['depends']               = 'Závislé na:';
 $lang['similar']               = 'Podobné:';
 $lang['conflicts']             = 'V konflikte:';
+$lang['donate']                = 'Páči sa Vám?';
+$lang['donate_action']         = 'Kúpte autorovi kávu!';
+$lang['repo_retry']            = 'Znovu skúsiť';
+$lang['provides']              = 'Poskytuje:';
+$lang['status']                = 'Stav:';
 $lang['status_installed']      = 'inštalovaný';
 $lang['status_not_installed']  = 'neinštalovaný';
 $lang['status_protected']      = 'chránený';
 $lang['status_enabled']        = 'povolený';
 $lang['status_disabled']       = 'nepovolený';
+$lang['status_unmodifiable']   = 'nezmeniteľný';
 $lang['status_plugin']         = 'plugin';
 $lang['status_template']       = 'šablóna';
+$lang['status_bundled']        = 'vstavaný';
+
 $lang['msg_enabled']           = 'Plugin %s povolený';
 $lang['msg_disabled']          = 'Plugin %s nepovolený';
+$lang['msg_delete_success']    = 'Rozšírenie %s odinštalované';
+$lang['msg_delete_failed']     = 'Odinštalovanie rozšírenia %s sa nepodarilo';
 $lang['msg_template_install_success'] = 'Šablóna %s úspešne nainštalovaná';
 $lang['msg_template_update_success'] = 'Šablóna %s úspešne aktualizovaná';
 $lang['msg_plugin_install_success'] = 'Plugin %s úspešne nainštalovaný';
 $lang['msg_plugin_update_success'] = 'Plugin %s úspešne aktualizovaný';
 $lang['msg_upload_failed']     = 'Nahrávanie súboru zlyhalo';
+
 $lang['missing_dependency']    = '<strong>Chýbajúca alebo nepovolená závislosť:</strong> %s';
 $lang['security_issue']        = '<strong>Bezpečnostný problém:</strong> %s';
 $lang['security_warning']      = '<strong>Bezpečnostné upozornenie:</strong> %s';
 $lang['update_available']      = '<strong>Aktualizácia:</strong> Nová verzia %s.';
 $lang['wrong_folder']          = '<strong>Plugin nesprávne nainštalovaný:</strong> Premenujte adresár s pluginom "%s" na "%s".';
+$lang['url_change']            = '<strong>URL sa zmenila:</strong> URL na stiahnutie sa od posledného sťahovania zmenila. Pred aktualizáciou rozšírenia skontrolujte, či je nová URL správna. <br />Nová: %s<br />Stará: %s';
+
 $lang['error_badurl']          = 'URL by mali mať na začiatku http alebo https';
 $lang['error_dircreate']       = 'Nie je možné vytvoriť dočasný adresár pre uloženie sťahovaného súboru';
 $lang['error_download']        = 'Nie je možné stiahnuť súbor: %s';
 $lang['error_decompress']      = 'Nie je možné dekomprimovať stiahnutý súbor. Môže to byť dôvodom chyby sťahovania (v tom prípade to skúste znova) alebo neznámym kompresným formátom (v tom prípade musíte stiahnuť a inštalovať manuálne).';
+$lang['error_findfolder']      = 'Nepodarilo sa identifikovať cestu rozšírenia. Rozšírenie musíte stiahnuť a inštalovať manuálne.';
 $lang['error_copy']            = 'Chyba kopírovania pri inštalácii do adresára <em>%s</em>: disk môže byť plný alebo nemáte potrebné prístupové oprávnenie. Dôsledkom može byť čiastočne inštalovaný plugin a nestabilná wiki inštalácia.';
-$lang['nopluginperms']         = 'Adresár s pluginom nie je zapisovateľný.';
+
+$lang['noperms']               = 'Do priečinka rozšírenia sa nedá zapisovať.';
+$lang['notplperms']            = 'Do priečinka s témou sa nedá zapisovať.';
+$lang['nopluginperms']         = 'Do priečinka s pluginom sa nedá zapisovať.';
+$lang['git']                   = 'Toto rozšírenie bolo nainštalované cez git. Nemali by ste ho tu aktualizovať.';
+$lang['auth']                  = 'Toto rozšírenie nie je povolené v nastaveniach. Zvážte jeho zakázanie.';
+
 $lang['install_url']           = 'Inštalácia z URL:';
+$lang['install_upload']        = 'Nahrať rozšírenie:';
+
+$lang['repo_error']            = 'Nepodarilo sa kontaktovať repozitár rozšírenia. Uistite sa, že váš server má povolené kontaktovať www.dokuwiki.org a skontrolujte nastavenia proxy.';
+$lang['nossl']                 = 'Vaše PHP zrejme nepodporuje SSL. Sťahovanie nebude funkčné pre mnohé rozšírenia.';
+
+$lang['js']['display_viewoptions']    = 'Zobraziť možnosti:';
+$lang['js']['display_enabled']        = 'povolené';
+$lang['js']['display_disabled']       = 'zakázané';
+$lang['js']['display_updatable']      = 'aktualizovateľné';
