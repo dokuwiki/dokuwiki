766e3907cf7b234c3f2fa7bf86f205c1c8c7c6e1
Anna Dabrowska <dabrowska@cosmocode.de>
2019-02-21 17:51:15 +0100

    Update StyleUtils test

--- _test/tests/inc/styleutils_cssstyleini.test.php
+++ _test/tests/inc/styleutils_cssstyleini.test.php
@@ -10,61 +10,29 @@ class styleutils_cssstyleini_test extends DokuWikiTest {
                 array (
                     'screen' =>
                         array (
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/basic.less' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_imgdetail.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_media_popup.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_media_fullscreen.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_fileuploader.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_tabs.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_links.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_toc.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_footnotes.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_search.less' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_recent.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_diff.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_edit.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_modal.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_forms.css' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/_admin.less' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/structure.less' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/design.less' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/usertools.less' => '/./lib/tpl/dokuwiki/',
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/pagetools.less' => '/./lib/tpl/dokuwiki/',
+                            DOKU_CONF . 'tpl/dokuwiki/css/_tests.less' => '/./',
                             DOKU_INC . 'lib/tpl/dokuwiki/css/content.less' => '/./lib/tpl/dokuwiki/',
-                        ),
-                    'all' =>
-                        array (
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/mobile.less' => '/./lib/tpl/dokuwiki/',
-                        ),
-                    'print' =>
-                        array (
-                            DOKU_INC . 'lib/tpl/dokuwiki/css/print.css' => '/./lib/tpl/dokuwiki/',
                         ),
                 ),
             'replacements' =>
                 array (
-                    '__text__' => '#5e4040',
+                    '__text__' => '#333',
                     '__background__' => '#f2ecec',
-                    '__text_alt__' => '#b39292',
-                    '__background_alt__' => '#dbcbcb',
-                    '__text_neu__' => '#664747',
-                    '__background_neu__' => '#b09f9f',
-                    '__border__' => '#d8b0b0',
-                    '__highlight__' => '#d699ff',
-                    '__link__' => '#44703c',
-                    '__background_site__' => '#c4d8d6',
-                    '__existing__' => '#154b15',
-                    '__missing__' => '#c900db',
-                    '__site_width__' => '85em',
-                    '__sidebar_width__' => '14em',
-                    '__tablet_width__' => '820px',
-                    '__phone_width__' => '490px',
-                    '__theme_color__' => '#004489',
+                    '__custom_variable__' => '#5e4040',
+                    '__custom_variable_two__' => 'url(' . DOKU_BASE . 'test/foo.png)',
                 ),
         );
 
         $actual = $util->cssStyleini();
 
-        $this->assertEquals($expected, $actual);
+        // check that all stylesheet levels are present
+        $this->assertArrayHasKey('all', $actual['stylesheets']);
+        $this->assertArrayHasKey('print', $actual['stylesheets']);
+
+        // check an original stylesheet and an additional one
+        $this->assertArraySubset($expected['stylesheets']['screen'], $actual['stylesheets']['screen']);
+
+        // merged config has an original value (text), an overridden value (background) and a new custom replacement (custom_variable)
+        $this->assertArraySubset($expected['replacements'], $actual['replacements']);
     }
 }
