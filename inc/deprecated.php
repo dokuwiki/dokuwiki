60b9af73d5be94f130ac24a6add338442a22fe6d
Michael Große <mic.grosse@googlemail.com>
2019-03-17 13:30:28 +0100

    Move deprecated HTTPClient classes to deprecated.php

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -266,3 +266,52 @@ class PassHash extends \dokuwiki\PassHash {
         dbg_deprecated(\dokuwiki\PassHash::class);
     }
 }
+
+/**
+ * @deprecated since 2019-03-17 use \dokuwiki\HTTPClient\HTTPClientException instead!
+ */
+class HTTPClientException extends \dokuwiki\HTTPClient\HTTPClientException {
+
+    /**
+     * @inheritdoc
+     * @deprecated 2019-03-17
+     */
+    public function __construct($message = '', $code = 0, $previous = null)
+    {
+        DebugHelper::dbgDeprecatedFunction(dokuwiki\HTTPClient\HTTPClientException::class);
+        parent::__construct($message, $code, $previous);
+    }
+}
+
+/**
+ * @deprecated since 2019-03-17 use \dokuwiki\HTTPClient\HTTPClient instead!
+ */
+class HTTPClient extends \dokuwiki\HTTPClient\HTTPClient {
+
+    /**
+     * @inheritdoc
+     * @deprecated 2019-03-17
+     */
+    public function __construct()
+    {
+        DebugHelper::dbgDeprecatedFunction(dokuwiki\HTTPClient\HTTPClient::class);
+        parent::__construct();
+    }
+}
+
+/**
+ * @deprecated since 2019-03-17 use \dokuwiki\HTTPClient\DokuHTTPClient instead!
+ */
+class DokuHTTPClient extends \dokuwiki\HTTPClient\DokuHTTPClient {
+
+    /**
+     * @inheritdoc
+     * @deprecated 2019-03-17
+     */
+    public function __construct()
+    {
+        DebugHelper::dbgDeprecatedFunction(dokuwiki\HTTPClient\DokuHTTPClient::class);
+        parent::__construct();
+    }
+
+}
