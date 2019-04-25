53db1e95813e129a7acf6a83cc6e6941baf35725
Alexander Kh <001.arx@gmail.com>
2019-04-25 10:41:18 +0200

    translation update

--- inc/lang/ru/lang.php
+++ inc/lang/ru/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Alexander Kh. <001.arx@gmail.com>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  * @author Vyacheslav Strenadko <vyacheslav.strenadko@gmail.com>
  * @author Wolterhon <hotmottot.1@gmail.com>
