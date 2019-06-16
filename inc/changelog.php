b306802e993a5fd803230d95ba3600b74fdcf874
Phy <git@phy25.com>
2019-06-16 19:10:16 -0400

    Renamed constant to RECENTS_ONLY_CREATION
    This clarifies that it also works when requesting media changelog.

--- inc/changelog.php
+++ inc/changelog.php
@@ -190,7 +190,7 @@ function addMediaLogEntry($date, $id, $type=DOKU_CHANGE_TYPE_EDIT, $summary='', 
  *
  * RECENTS_SKIP_DELETED   - don't include deleted pages
  * RECENTS_SKIP_MINORS    - don't include minor changes
- * RECENTS_ONLY_NEW_PAGES - only include new created pages
+ * RECENTS_ONLY_CREATION - only include new created pages and media
  * RECENTS_SKIP_SUBSPACES - don't include subspaces
  * RECENTS_MEDIA_CHANGES  - return media changes instead of page changes
  * RECENTS_MEDIA_PAGES_MIXED  - return both media changes and page changes
@@ -274,7 +274,7 @@ function getRecents($first,$num,$ns='',$flags=0){
  *
  * RECENTS_SKIP_DELETED   - don't include deleted pages
  * RECENTS_SKIP_MINORS    - don't include minor changes
- * RECENTS_ONLY_NEW_PAGES - only include new created pages
+ * RECENTS_ONLY_CREATION - only include new created pages and media
  * RECENTS_SKIP_SUBSPACES - don't include subspaces
  * RECENTS_MEDIA_CHANGES  - return media changes instead of page changes
  *
@@ -349,8 +349,8 @@ function _handleRecent($line,$ns,$flags,&$seen){
     // skip seen ones
     if(isset($seen[$recent['id']])) return false;
 
-    // skip changes, of only new pages are requested
-    if($recent['type']!==DOKU_CHANGE_TYPE_CREATE && ($flags & RECENTS_ONLY_NEW_PAGES)) return false;
+    // skip changes, of only new items are requested
+    if($recent['type']!==DOKU_CHANGE_TYPE_CREATE && ($flags & RECENTS_ONLY_CREATION)) return false;
 
     // skip minors
     if($recent['type']===DOKU_CHANGE_TYPE_MINOR_EDIT && ($flags & RECENTS_SKIP_MINORS)) return false;
@@ -748,7 +748,7 @@ abstract class ChangeLog {
 
         return array(array_reverse($revs1), array_reverse($revs2));
     }
-    
+
     /**
      * Checks if the ID has old revisons
      * @return boolean
