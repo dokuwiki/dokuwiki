0cb5233543d9415a7c9aeae78bbebc8fc97d464b
Robert Surý <rsurycz@seznam.cz>
2019-08-03 11:06:38 +0200

    translation update

---
+++ lib/plugins/authpdo/lang/cs/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Robert Surý <rsurycz@seznam.cz>
+ */
+$lang['debug']                 = 'Vytištění podrobných chybových zpráv. Po dokončení nastavení by mělo být deaktivováno.';
+$lang['dsn']                   = 'DSN pro připojení k databázi.';
+$lang['user']                  = 'Uživatel pro výše uvedené připojení k databázi (prázdný pro sqlite)';
+$lang['pass']                  = 'Heslo pro výše uvedené připojení k databázi (prázdné pro sqlite)';
+$lang['select-user']           = 'Příkaz SQL pro výběr dat jednoho uživatele';
+$lang['select-user-groups']    = 'Příkaz SQL pro výběr všech skupin jednoho uživatele';
+$lang['select-groups']         = 'Příkaz SQL pro výběr všech dostupných skupin';
+$lang['insert-user']           = 'Příkaz SQL pro vložení nového uživatele do databáze';
+$lang['delete-user']           = 'Příkaz SQL pro odebrání jednoho uživatele z databáze';
+$lang['list-users']            = 'Příkaz SQL pro výpis seznamu uživatelů odpovídajících filtru';
+$lang['count-users']           = 'Příkaz SQL pro spočítání uživatelů odpovídajících filtru';
+$lang['update-user-info']      = 'Příkaz SQL pro aktualizaci celého jména a e-mailové adresy jednoho uživatele';
+$lang['update-user-login']     = 'Příkaz SQL pro aktualizaci přihlašovacího jména jednoho uživatele';
+$lang['update-user-pass']      = 'Příkaz SQL pro aktualizaci hesla jednoho uživatele';
+$lang['insert-group']          = 'Příkaz SQL pro vložení nové skupiny do databáze';
+$lang['join-group']            = 'Příkaz SQL pro přidání uživatele do existující skupiny';
+$lang['leave-group']           = 'Příkaz SQL pro odebrání uživatele ze skupiny';
+$lang['check-pass']            = 'Příkaz SQL ke kontrole hesla uživatele. Může zůstat prázdný, pokud jsou informace o heslech vyvolány ve vybraném uživateli.';
--- lib/plugins/revert/lang/cs/lang.php
+++ lib/plugins/revert/lang/cs/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Robert Surý <rsurycz@seznam.cz>
  * @author Martin Hořínek <hev@hev.cz>
  * @author Jonáš Dyba <jonas.dyba@gmail.com>
  * @author Bohumir Zamecnik <bohumir@zamecnik.org>
