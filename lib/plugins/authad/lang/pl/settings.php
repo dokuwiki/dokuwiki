4568e613ea04e84a7b6fefcc766b6423f64292cf
Przemek <p_kudriawcew@o2.pl>
2019-08-06 16:36:20 +0200

    translation update

--- lib/plugins/authad/lang/pl/settings.php
+++ lib/plugins/authad/lang/pl/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Przemek <p_kudriawcew@o2.pl>
  * @author Wojciech Lichota <wojciech@lichota.pl>
  * @author Max <maxrb146@gmail.com>
  * @author Tomasz Bosak <bosak.tomasz@gmail.com>
