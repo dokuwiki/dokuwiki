56e5bab48ca2a2d898434539308fdbbfdbb134d5
Craig breeden <twincraig4@gmail.com>
2019-09-13 05:36:27 +0200

    translation update

--- lib/plugins/config/lang/ar/lang.php
+++ lib/plugins/config/lang/ar/lang.php
@@ -5,6 +5,7 @@
  *
  * Arabic language file
  *
+ * @author Craig breeden <twincraig4@gmail.com>
  * @author Khalid <khalid.aljahil@gmail.com>
  * @author Yaman Hokan <always.smile.yh@hotmail.com>
  * @author Usama Akkad <uahello@gmail.com>
--- lib/plugins/revert/lang/ar/lang.php
+++ lib/plugins/revert/lang/ar/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Craig breeden <twincraig4@gmail.com>
  * @author Khalid <khalid.aljahil@gmail.com>
  * @author Yaman Hokan <always.smile.yh@hotmail.com>
  * @author Usama Akkad <uahello@gmail.com>
