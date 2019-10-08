618a9504b8d0a3c2fc39602e6553d07e71dfdf20
Bartek S <sadupl@gmail.com>
2019-10-08 20:21:44 +0200

    translation update

--- lib/plugins/authad/lang/pl/settings.php
+++ lib/plugins/authad/lang/pl/settings.php
@@ -29,3 +29,4 @@ $lang['expirywarn']            = 'Dni poprzedzających powiadomienie użytkownik
 $lang['additional']            = 'Oddzielona przecinkami lista dodatkowych atrybutów AD do pobrania z danych użytkownika. Używane przez niektóre wtyczki.';
 $lang['update_name']           = 'Zezwól użytkownikom na uaktualnianie nazwy wyświetlanej w AD?';
 $lang['update_mail']           = 'Zezwól użytkownikom na uaktualnianie ich adresu email?';
+$lang['recursive_groups']      = 'Rozpatrz grupy zagnieżdżone dla odpowiednich członków (wolniej).';
