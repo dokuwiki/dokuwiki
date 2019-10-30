99e6c0409d18a2ba5b11f4aed0686942b95335c3
Syntaxseed <825423+syntaxseed@users.noreply.github.com>
2019-10-30 19:17:37 -0400

    Final project-wide check of curly brace deprecations.

--- lib/plugins/config/core/Setting/SettingSepchar.php
+++ lib/plugins/config/core/Setting/SettingSepchar.php
@@ -10,7 +10,7 @@ class SettingSepchar extends SettingMultichoice {
     /** @inheritdoc */
     public function __construct($key, $param = null) {
         $str = '_-.';
-        for($i = 0; $i < strlen($str); $i++) $this->choices[] = $str{$i};
+        for($i = 0; $i < strlen($str); $i++) $this->choices[] = $str[$i];
 
         // call foundation class constructor
         parent::__construct($key, $param);
