e1ff5079276451d1c4d5a8dc15d0c68517e4acef
Hase <german@netcologne.de>
2019-11-18 22:11:23 +0100

    translation update

--- inc/lang/de/lang.php
+++ inc/lang/de/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hase <german@netcologne.de>
  * @author C!own77 <clown77@posteo.de>
  * @author Anonymous <anonymous@example.org>
  * @author Michaelsy <github@informantum.de>
