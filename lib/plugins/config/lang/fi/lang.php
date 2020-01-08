f640692eeb0be3b0511c403cd32ea2ad3bdb5230
Tuomo Hartikainen <tuomo.hartikainen@heksia.fi>
2020-01-08 08:06:19 +0100

    translation update

--- lib/plugins/config/lang/fi/lang.php
+++ lib/plugins/config/lang/fi/lang.php
@@ -3,8 +3,8 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author otto@valjakko.net
- * @author Otto Vainio <otto@valjakko.net>
+ * @author Tuomo Hartikainen <tuomo.hartikainen@heksia.fi>
+ * @author otto <otto@valjakko.net>
  * @author Teemu Mattila <ghcsystems@gmail.com>
  * @author Sami Olmari <sami@olmari.fi>
  * @author Wiki Doku <SugarKidder@mailinator.com>
@@ -42,7 +42,7 @@ $lang['title']                 = 'Wikin nimi';
 $lang['start']                 = 'Alkusivun nimi';
 $lang['lang']                  = 'Kieli';
 $lang['template']              = 'Sivumalli';
-$lang['tagline']               = 'Apuotsikko - slogan sivustonimen yhteysteen (jos template käyttää)';
+$lang['tagline']               = 'Apuotsikko - slogan sivustonimen yhteyteen (jos template tukee)';
 $lang['sidebar']               = 'Sivupalkin sivunimi (jos template tukee sitä), tyhjä arvo poistaa sivupalkin';
 $lang['license']               = 'Millä lisenssillä sisältö pitäisi julkaista?';
 $lang['savedir']               = 'Hakemisto tietojen tallennukseen.';
@@ -114,7 +114,7 @@ $lang['subscribe_time']        = 'Aika jonka jälkeen tilauslinkit ja yhteenveto
 $lang['notify']                = 'Lähetä muutosilmoitukset tähän osoitteeseen';
 $lang['registernotify']        = 'Lähetä ilmoitus uusista rekisteröitymisistä tähän osoitteeseen';
 $lang['mailfrom']              = 'Sähköpostiosoite automaattisia postituksia varten';
-$lang['mailprefix']            = 'Etuliite automaattisesti lähetettyihin dähköposteihin';
+$lang['mailprefix']            = 'Etuliite automaattisesti lähetettyihin sähköposteihin';
 $lang['htmlmail']              = 'Lähetä paremman näköisiä, mutta isompia HTML multipart sähköposteja. Ota pois päältä, jos haluat vain tekstimuotoisia posteja.';
 $lang['sitemap']               = 'Luo Google sitemap (päiviä)';
 $lang['rss_type']              = 'XML-syötteen tyyppi';
