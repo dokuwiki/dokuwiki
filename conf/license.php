711609595679143ccf33f5922f68455de7861a75
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-01-20 11:45:50 +1000

    Languageable CC licenses

--- conf/license.php
+++ conf/license.php
@@ -7,19 +7,19 @@
 
 $license['cc-zero'] = array(
     'name' => 'CC0 1.0 Universal',
-    'url'  => 'https://creativecommons.org/publicdomain/zero/1.0/',
+    'url'  => 'https://creativecommons.org/publicdomain/zero/1.0/deed.'.$conf['lang'],
 );
 $license['publicdomain'] = array(
     'name' => 'Public Domain',
-    'url'  => 'https://creativecommons.org/licenses/publicdomain/',
+    'url'  => 'https://creativecommons.org/licenses/publicdomain/deed.'.$conf['lang'],
 );
 $license['cc-by'] = array(
     'name' => 'CC Attribution 4.0 International',
-    'url'  => 'https://creativecommons.org/licenses/by/4.0/',
+    'url'  => 'https://creativecommons.org/licenses/by/4.0/deed.'.$conf['lang'],
 );
 $license['cc-by-sa'] = array(
     'name' => 'CC Attribution-Share Alike 4.0 International',
-    'url'  => 'https://creativecommons.org/licenses/by-sa/4.0/',
+    'url'  => 'https://creativecommons.org/licenses/by-sa/4.0/deed.'.$conf['lang'],
 );
 $license['gnufdl'] = array(
     'name' => 'GNU Free Documentation License 1.3',
@@ -27,10 +27,10 @@ $license['gnufdl'] = array(
 );
 $license['cc-by-nc'] = array(
     'name' => 'CC Attribution-Noncommercial 4.0 International',
-    'url'  => 'https://creativecommons.org/licenses/by-nc/4.0/',
+    'url'  => 'https://creativecommons.org/licenses/by-nc/4.0/deed.'.$conf['lang'],
 );
 $license['cc-by-nc-sa'] = array(
     'name' => 'CC Attribution-Noncommercial-Share Alike 4.0 International',
-    'url'  => 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+    'url'  => 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.'.$conf['lang'],
 );
 
