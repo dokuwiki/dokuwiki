98548fef01f6eb5c04e2ab0aae2c46019fc2b448
Andreas Gohr <andi@splitbrain.org>
2020-04-29 19:34:11 +0200

    added failing test for #3036

--- _test/tests/inc/mailer.test.php
+++ _test/tests/inc/mailer.test.php
@@ -104,6 +104,11 @@ class mailer_test extends DokuWikiTest {
         $headers = $mail->prop('headers');
         $this->assertEquals('"Andreas Gohr" <andi@splitbrain.org>', $headers['To']);
 
+        $mail->to('andi@splitbrain.org,foo@example.com');
+        $mail->cleanHeaders();
+        $headers = $mail->prop('headers');
+        $this->assertEquals('andi@splitbrain.org, foo@example.com', $headers['To']);
+
         $mail->to('Andreas Gohr <andi@splitbrain.org> , foo <foo@example.com>');
         $mail->cleanHeaders();
         $headers = $mail->prop('headers');
