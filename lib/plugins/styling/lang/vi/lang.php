de37beb4116613fca3b4145c3e2a792f40356582
Thien Hau <thienhausoftware@gmail.com>
2020-05-06 14:11:43 +0200

    translation update

---
+++ lib/plugins/styling/lang/vi/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Thien Hau <thienhausoftware@gmail.com>
+ */
+$lang['menu']                  = 'Cài đặt kiểu dáng Chủ đề';
+$lang['js']['loader']          = 'Đang tải xem trước...<br />nếu cái này không biến mất, giá trị của bạn có thể bị lỗi';
+$lang['js']['popup']           = 'Mở dưới dạng cửa sổ bật lên';
+$lang['error']                 = 'Xin lỗi, chủ đề này không hỗ trợ chức năng này.';
+$lang['btn_preview']           = 'Xem trước thay đổi';
+$lang['btn_save']              = 'Lưu thay đổi';
+$lang['btn_reset']             = 'Đặt lại các thay đổi hiện tại';
+$lang['btn_revert']            = 'Lùi sửa các kiểu dáng về mặc định của chủ đề';
+$lang['__text__']              = 'Màu văn bản chính';
+$lang['__background__']        = 'Màu nền chính';
+$lang['__text_alt__']          = 'Màu văn bản thay thế';
+$lang['__background_alt__']    = 'Màu nền thay thế';
+$lang['__text_neu__']          = 'Màu văn bản trung tính';
+$lang['__background_neu__']    = 'Màu nền trung tính';
+$lang['__border__']            = 'Màu viền';
+$lang['__highlight__']         = 'Màu phần tô lên (chủ yếu cho kết quả tìm kiếm)';
