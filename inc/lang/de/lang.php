f66629bebd6d835d7044fc541cd4398288859923
Rolf Rued <rolf.rued@t-online.de>
2020-05-25 18:36:43 +0200

    translation update

--- inc/lang/de/lang.php
+++ inc/lang/de/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Rolf Rued <rolf.rued@t-online.de>
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Eric Haberstroh <ehaberstroh@gmail.com>
  * @author C!own77 <clown77@posteo.de>
