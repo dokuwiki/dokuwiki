1cf5d4a651217943d4587d2486146ce67c631c84
Henry Pan <github@phy25.com>
2020-06-26 19:35:52 -0400

    Update lib/plugins/authad/lang/ro/lang.php

--- lib/plugins/authad/lang/ro/lang.php
+++ lib/plugins/authad/lang/ro/lang.php
@@ -6,7 +6,6 @@
  * @author Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
  * @author Adrian Vesa <adrianvesa@dotwikis.com>
  */
-$lang['domain']                = '??? Sorry i dont know this one ???';
 $lang['authpwdexpire']         = 'Parola va expira în %d zile, ar trebui să o schimbi în curând.';
 $lang['passchangefail']        = 'Parola nu a putu fi schimbata. Poate politica pentru parole nu a fost indeplinita ?';
 $lang['userchangefail']        = 'Nu am putu schimba atributiile pentru acest utilizator. Poate nu ai permisiunea sa faci aceste schimbari ?';
