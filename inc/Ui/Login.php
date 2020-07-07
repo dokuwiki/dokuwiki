9cf9796dc65658a9c35e042a5042b2f04cad08b7
Satoshi Sahara <sahara.satoshi@gmail.com>
2020-07-08 08:27:12 +0900

    Ui\Login class replaces html_login()

---
+++ inc/Ui/Login.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace dokuwiki\Ui;
+
+use dokuwiki\Extension\Event;
+use dokuwiki\Form\Form;
+
+/**
+ * DokuWiki User Login Insterface (Login Form)
+ *
+ * @package dokuwiki\Ui
+ */
+class Login extends Ui
+{
+    /**
+     * Display the Login Form Panel
+     *
+     * @author   Andreas Gohr <andi@splitbrain.org>
+     *
+     * @triggers HTML_LOGINFORM_OUTPUT
+     * @param bool $svg Whether to show svg icons in the register and resendpwd links or not
+     * @return void
+     */
+    public function show($svg = false)
+    {
+        global $lang;
+        global $conf;
+        global $ID;
+        global $INPUT;
+
+        // print intro
+        print p_locale_xhtml('login');
+        print '<div class="centeralign">'.NL;
+
+        // create the login form
+        $form = new Form(['action' => wl($ID), 'id' => 'dw__login']);
+        $form->addTagOpen('div')->addClass('no');
+        $form->addFieldsetOpen($lang['btn_login']);
+        $form->setHiddenField('id', $ID);
+        $form->setHiddenField('do', 'login');
+
+        $input = $form->addTextInput('u', $lang['user'])->id('focus__this')->addClass('edit')
+            ->val((!$INPUT->bool('http_credentials')) ? $INPUT->str('u') : '');
+        $input->getLabel()->attr('class', 'block');
+        $form->addHTML("<br>\n");
+
+        $input = $form->addPasswordInput('p', $lang['pass'])->addClass('block edit');
+        $input->getLabel()->attr('class', 'block');
+        $form->addHTML("<br>\n");
+
+        if ($conf['rememberme']) {
+            $form->addCheckbox('r', $lang['remember'])->id('remember__me')->val('1');
+        }
+        $form->addButton('', $lang['btn_login'])->attr('type', 'submit');
+        $form->addFieldsetClose();
+        $form->addTagClose('div');
+
+        if(actionOK('register')){
+            $registerLink = (new \dokuwiki\Menu\Item\Register())->asHtmlLink('', $svg);
+            $form->addHTML('<p>'.$lang['reghere'].': '. $registerLink .'</p>');
+        }
+
+        if (actionOK('resendpwd')) {
+            $resendPwLink = (new \dokuwiki\Menu\Item\Resendpwd())->asHtmlLink('', $svg);
+            $form->addHTML('<p>'.$lang['pwdforget'].': '. $resendPwLink .'</p>');
+        }
+
+        // emit HTML_LOGINFORM_OUTPUT event, print the form
+        Event::createAndTrigger('HTML_LOGINFORM_OUTPUT', $form, 'html_form_output', false);
+
+        print '</div>'.DOKU_LF;
+    }
+
+}
