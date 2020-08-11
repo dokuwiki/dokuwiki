5d9cb4e188c4a7004ca937381383eef29de8318b
Andreas Gohr <andi@splitbrain.org>
2020-08-11 14:48:56 +0200

    cleanup for collator tests
    The proper use of data providers now make it much easier to add
    addtional languages to the test

--- _test/tests/inc/sort_with_collator.test.php
+++ _test/tests/inc/sort_with_collator.test.php
@@ -5,30 +5,14 @@ use dokuwiki\Utf8\Sort;
 /**
  * @author Moisés Braga Ribeiro <moisesbr@gmail.com>
  */
-class sort_with_collator_test extends DokuWikiTest {
-
-    private static $lang_before;
-
-    public function setUp() {
-        parent::setUp();
-
-        global $conf;
-        $conf['lang'] = 'eo'; // Esperanto
-    }
-
-    /**
-     * Collation for Esperanto.
-     * @return string
-     */
-    public function collation() {
-        // if sort by 1st letter fails, sort by 2nd letter gives the opposite result
-        return 'a b ci ĉe d e f go ĝi ho ĥa i ju ĵe k l m n o p r se ŝa t us ŭo v z';
-    }
+class sort_with_collator_test extends DokuWikiTest
+{
 
     /*
      * Dependency for tests that need "intl" extension.
      */
-    public function test_intl_extension() {
+    public function testIntlExtenstionAvailability()
+    {
         if (!class_exists('Collator')) {
             $this->markTestSkipped('Skipping all sort tests with collator, as they need "intl" extension');
         }
@@ -36,58 +20,110 @@ class sort_with_collator_test extends DokuWikiTest {
     }
 
     /**
-     * Pairs for collator-based intl_strcmp().
-     * @return array
+     * @return  Generator|array
+     * @see testStrcmp
      */
-    public function pairs() {
-        return array(
-            // if sort by 1st letter fails, sort by 2nd letter gives the opposite result
-            array('celo',     'ĉapo'     ),
-            array('ĉokolado', 'dento'    ), // c ĉ d
-            array('glacio',   'ĝirafo'   ),
-            array('ĝojo',     'haro'     ),
-            array('horo',     'ĥameleono'),
-            array('ĥoro',     'iam'      ), // g ĝ h ĥ i
-            array('jes',      'ĵaŭdo'    ),
-            array('ĵurnalo',  'kapo'     ), // j ĵ k
-            array('seka',     'ŝako'     ),
-            array('ŝuo',      'tablo'    ), // s ŝ t
-            array('urso',     'ŭaŭ'      ),
-            array('ŭo',       'vino'     ), // u ŭ v
+    public function provideWordPairs()
+    {
+        static $pairs = [
+            // Esperanto
+            'eo' => [
+                // if sort by 1st letter fails, sort by 2nd letter gives the opposite result
+                ['celo', 'ĉapo'],
+                ['ĉokolado', 'dento'], // c ĉ d
+                ['glacio', 'ĝirafo'],
+                ['ĝojo', 'haro'],
+                ['horo', 'ĥameleono'],
+                ['ĥoro', 'iam'], // g ĝ h ĥ i
+                ['jes', 'ĵaŭdo'],
+                ['ĵurnalo', 'kapo'], // j ĵ k
+                ['seka', 'ŝako'],
+                ['ŝuo', 'tablo'], // s ŝ t
+                ['urso', 'ŭaŭ'],
+                ['ŭo', 'vino'], // u ŭ v
 
-            // natural sort
-            array('paĝo 2',   'paĝo 10'  ),
-            array('paĝo 51',  'paĝo 100' )
-        );
+                // natural sort
+                ['paĝo 2', 'paĝo 10'],
+                ['paĝo 51', 'paĝo 100'],
+            ],
+        ];
+
+        foreach ($pairs as $lc => $list) {
+            foreach ($list as $pair) {
+                yield [$lc, $pair[0], $pair[1]];
+            }
+        }
     }
 
     /**
-     * @depends test_intl_extension
-     * @dataProvider pairs
-     * @param $str1
-     * @param $str2
+     * Provide a list of correctly sorted "words"
+     *
+     * @return Generator|array
+     * @see testSort
+     * @see testKSort
+     * @see testASort
+     * @see testASortFnUrl
+     * @see testASortFnSafe
+     * @see testASortFnUtf8
      */
-    public function test_intl_strcmp($str1, $str2) {
+    public function provideSortedCharList()
+    {
+        static $data = [
+            'eo' => 'a b ci ĉe d e f go ĝi ho ĥa i ju ĵe k l m n o p r se ŝa t us ŭo v z', // Esperanto
+            'de' => 'a ä b c d e f g h i j k l m n o ö p q r s ß t u ü v w x y z', // German
+        ];
+
+        foreach ($data as $lang => $chars) {
+            yield [$lang, $chars];
+        }
+    }
+
+    /**
+     * @depends      testIntlExtenstionAvailability
+     * @dataProvider provideWordPairs
+     * @param string $lang
+     * @param string $str1
+     * @param string $str2
+     */
+    public function testStrcmp($lang, $str1, $str2)
+    {
+        global $conf;
+        $conf['lang'] = $lang;
+
         $this->assertLessThan(0, Sort::strcmp($str1, $str2));
     }
 
     /**
-     * @depends test_intl_extension
+     * @dataProvider provideSortedCharList
+     * @depends      testIntlExtenstionAvailability
+     * @param string $lang
+     * @param string $chars
      */
-    public function test_intl_sort() {
-        $sorted = explode(' ', $this->collation());
-        $random = explode(' ', $this->collation());
+    public function testSort($lang, $chars)
+    {
+        global $conf;
+        $conf['lang'] = $lang;
+
+        $sorted = explode(' ', $chars);
+        $random = explode(' ', $chars);
         shuffle($random);
         Sort::sort($random);
         $this->assertEquals(array_values($random), array_values($sorted));
     }
 
     /**
-     * @depends test_intl_extension
+     * @dataProvider provideSortedCharList
+     * @depends      testIntlExtenstionAvailability
+     * @param string $lang
+     * @param string $chars
      */
-    public function test_intl_ksort() {
-        $sorted = array_flip(explode(' ', $this->collation()));
-        $random = explode(' ', $this->collation());
+    public function testKSort($lang, $chars)
+    {
+        global $conf;
+        $conf['lang'] = $lang;
+
+        $sorted = array_flip(explode(' ', $chars));
+        $random = explode(' ', $chars);
         shuffle($random);
         $random = array_flip($random);
         Sort::ksort($random);
@@ -95,62 +131,94 @@ class sort_with_collator_test extends DokuWikiTest {
     }
 
     /**
-     * @depends test_intl_extension
+     * @dataProvider provideSortedCharList
+     * @depends      testIntlExtenstionAvailability
+     * @param string $lang
+     * @param string $chars
      */
-    public function test_intl_asort() {
-        $sorted = explode(' ', $this->collation());
+    public function testASort($lang, $chars)
+    {
+        global $conf;
+        $conf['lang'] = $lang;
+
+        $sorted = explode(' ', $chars);
         $keys = array_keys($sorted);
         shuffle($keys);
-        foreach($keys as $key) $random[$key] = $sorted[$key];
+        foreach ($keys as $key) {
+            $random[$key] = $sorted[$key];
+        }
         Sort::asort($random);
         $this->assertEquals(array_values($random), array_values($sorted));
         $this->assertEquals(array_keys($random), array_keys($sorted));
     }
 
     /**
-     * @depends test_intl_extension
+     * @dataProvider provideSortedCharList
+     * @depends      testIntlExtenstionAvailability
+     * @param string $lang
+     * @param string $chars
      */
-    public function test_intl_asortFN_url() {
+    public function testASortFnUrl($lang, $chars)
+    {
         global $conf;
         $conf['fnencode'] = 'url';
+        $conf['lang'] = $lang;
 
-        $sorted = explode('+', urlencode($this->collation()));
+        $sorted = explode('+', urlencode($chars));
         $keys = array_keys($sorted);
         shuffle($keys);
-        foreach($keys as $key) $random[$key] = $sorted[$key];
+        foreach ($keys as $key) {
+            $random[$key] = $sorted[$key];
+        }
         Sort::asortFN($random);
         $this->assertEquals(array_values($random), array_values($sorted));
         $this->assertEquals(array_keys($random), array_keys($sorted));
     }
 
     /**
-     * @depends test_intl_extension
+     * @dataProvider provideSortedCharList
+     * @depends      testIntlExtenstionAvailability
+     * @param string $lang
+     * @param string $chars
      */
-    public function test_intl_asortFN_safe() {
+    public function testASortFnSafe($lang, $chars)
+    {
         global $conf;
         $conf['fnencode'] = 'safe';
+        $conf['lang'] = $lang;
 
-        $sorted = explode(' ', $this->collation());
-        foreach(array_keys($sorted) as $key) $sorted[$key] = SafeFN::encode($sorted[$key]);
+        $sorted = explode(' ', $chars);
+        foreach (array_keys($sorted) as $key) {
+            $sorted[$key] = SafeFN::encode($sorted[$key]);
+        }
         $keys = array_keys($sorted);
         shuffle($keys);
-        foreach($keys as $key) $random[$key] = $sorted[$key];
+        foreach ($keys as $key) {
+            $random[$key] = $sorted[$key];
+        }
         Sort::asortFN($random);
         $this->assertEquals(array_values($random), array_values($sorted));
         $this->assertEquals(array_keys($random), array_keys($sorted));
     }
 
     /**
-     * @depends test_intl_extension
+     * @dataProvider provideSortedCharList
+     * @depends      testIntlExtenstionAvailability
+     * @param string $lang
+     * @param string $chars
      */
-    public function test_intl_asortFN_utf8() {
+    public function testASortFnUtf8($lang, $chars)
+    {
         global $conf;
         $conf['fnencode'] = 'utf-8';
+        $conf['lang'] = $lang;
 
-        $sorted = explode(' ', $this->collation());
+        $sorted = explode(' ', $chars);
         $keys = array_keys($sorted);
         shuffle($keys);
-        foreach($keys as $key) $random[$key] = $sorted[$key];
+        foreach ($keys as $key) {
+            $random[$key] = $sorted[$key];
+        }
         Sort::asortFN($random);
         $this->assertEquals(array_values($random), array_values($sorted));
         $this->assertEquals(array_keys($random), array_keys($sorted));
