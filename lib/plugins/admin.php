e4aa0139d52e6547b2a34349ecebb02c9f1c4593
Andreas Gohr <andi@splitbrain.org>
2020-08-13 13:40:39 +0200

    better deprecation messages for self required plugin base files

--- lib/plugins/admin.php
+++ lib/plugins/admin.php
@@ -1,2 +1,4 @@
 <?php
-dbg_deprecated('Autoloading. Do not require() files yourself.');
+\dokuwiki\Debug\DebugHelper::dbgDeprecatedFunction(
+    'Do not require() files yourself. Autoloading', 1, basename(__FILE__)
+);
