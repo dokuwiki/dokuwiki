4197f8f13027a33d619fd305b4227c848e7c9014
qezwan <qezwan@gmail.com>
2020-10-05 22:45:09 +0200

    translation update

---
+++ lib/plugins/authldap/lang/ckb/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['userscope']             = 'سنووری بواری گەڕان بۆ گەڕانی بەکارهێنەر';
+$lang['groupscope']            = 'سنووری بواری گەڕان بۆ گەڕانی گروپ';
+$lang['userkey']               = 'تایبەتمەندی سڕینەوەی ناوی بەکارهێنەر; پێویستە هاوتەربێت لەگەڵ پاڵێوی بەکارهێنەر.';
+$lang['groupkey']              = 'ئەندامێتی گرووپ لە هەر تایبەتمەندییەکی بەکارهێنەر (لەجیاتی گروپەستانداردەکانی AD) بۆ وێنە لە گرووپ لە بەشی یان ژمارەی تەلەفۆن';
+$lang['modPass']               = 'ئایا دەکرێت نهێنوشەی LDAP بگۆڕدرێت لە ڕێگای dokuwiki؟';
+$lang['debug']                 = 'پیشاندانی زانیاری زیاتری ڕاستکردنەوەی هەڵە لەسەر هەڵەکان';
+$lang['deref_o_0']             = 'LDAP_DEREF_NEVER';
+$lang['deref_o_1']             = 'LDAP_DEREF_SEARCHING';
+$lang['deref_o_2']             = 'LDAP_DEREF_FINDING';
+$lang['deref_o_3']             = 'LDAP_DEREF_ALWAYS';
+$lang['referrals_o_-1']        = 'بەکارهێنانی بنەڕەت';
+$lang['referrals_o_0']         = 'بەدوای ئاماژەکان مەبە';
+$lang['referrals_o_1']         = 'بە دوای ئاماژەکان بکەوە';
