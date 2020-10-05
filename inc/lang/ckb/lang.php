2927f24d2f794e5673acfa14c4b80d30204f6fba
qezwan <qezwan@gmail.com>
2020-10-05 21:15:19 +0200

    translation update

---
+++ inc/lang/ckb/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['js']['medialnk']        = 'لینک کردن بۆ لاپەڕەی وردکاری';
+$lang['js']['mediadirect']     = 'لینکی ڕاستەوخۆ بۆ ڕەسەن	';
+$lang['js']['medianolnk']      = 'بێ لینک	';
+$lang['js']['medianolink']     = 'وێنەکە لینک مەکە';
+$lang['js']['medialeft']       = 'خۆدانەپاڵی وێنەکە لەلای چەپ.';
+$lang['js']['mediaright']      = 'خۆدانەپاڵی وێنەکە لەلای ڕاست.';
+$lang['js']['mediacenter']     = 'خۆدانەپاڵی وێنەکە لە ناوەڕاست.';
+$lang['js']['medianoalign']    = 'بێ لایەن بەکاربێنە.';
+$lang['js']['nosmblinks']      = 'لینک کردن بە هاوبەشیەکانی Windows تەنها لە Microsoft Internet Explorer کاردەکات.\nتۆ هێشتا دەتوانیت لینکەکە کۆپی و پەیست بکەیت.';
+$lang['js']['linkwiz']         = 'ویزاردی لینک';
+$lang['js']['linkto']          = 'لینک کردن بۆ:';
+$lang['js']['del_confirm']     = 'بەڕاستی ئایتمە دەستنیشانکراوەکان بسڕەوە؟';
+$lang['js']['restore_confirm'] = 'بەڕاستی ئەم وەشانە دەگەڕێنیتەوە؟';
+$lang['js']['media_diff']      = 'جیاوازیەکان نیشان بدە:';
+$lang['js']['media_diff_both'] = 'شان بە شان';
+$lang['js']['media_diff_opacity'] = 'درەوشاوە';
+$lang['js']['media_diff_portions'] = 'لێدەرکردن';
+$lang['js']['media_select']    = 'فایلەکان دیاریبکە...';
+$lang['js']['media_upload_btn'] = 'بارکردن';
+$lang['js']['media_done_btn']  = 'تەواوبوو';
+$lang['js']['media_drop']      = 'پەڕگەکان لێرە فڕێبدە بۆ بارکردن';
+$lang['js']['media_cancel']    = 'لابردن';
+$lang['js']['media_overwrt']   = 'فایلەکانی بەردەست بنووسەوە';
+$lang['search_exact_match']    = 'بەیەکگەیەنراوی دەقاودەق';
+$lang['search_starts_with']    = 'دەست پێدەکات بە';
+$lang['search_ends_with']      = 'کۆتایی دێت بە';
+$lang['search_contains']       = 'لەخۆدەگرێت';
+$lang['search_custom_match']   = 'ئاسایی';
+$lang['search_any_ns']         = 'هەر بۆشاییناوێک';
+$lang['search_any_time']       = 'هەر کاتێک';
