a2ad1875b4cc035cf6079aa7cbe18223765536e3
stivux <dr.stepan.nazar@gmail.com>
2020-10-15 17:51:11 +0200

    translation update

--- inc/lang/ru/lang.php
+++ inc/lang/ru/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author stivux <dr.stepan.nazar@gmail.com>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  * @author Анатолий <anatol_m@mail.ru>
  * @author Yuriy Skalko <yuriy.skalko@gmail.com>
