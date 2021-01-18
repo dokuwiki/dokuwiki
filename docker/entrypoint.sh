72c28222087761b57892e9e8ba69a201d3005894
Andreas Gohr <andi@splitbrain.org>
2021-01-18 17:24:20 +0100

    some cleanup
    As suggested by @glensc

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+# prepare overlay directories
+# https://stackoverflow.com/a/33275168/172068
 mkdir -p /overlay/storage/{dokuwiki,work}
 mount -t overlay \
       -o lowerdir=/overlay/original,upperdir=/overlay/storage/dokuwiki,workdir=/overlay/storage/work \
          overlay /var/www/html
 
-docker-php-entrypoint apache2-foreground
+exec docker-php-entrypoint apache2-foreground
