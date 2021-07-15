03af5d65e11c987258457d7e32e79fc9aa1a78a4
Erik Bjørn Pedersen <erik.pedersen@shaw.ca>
2021-07-15 08:01:05 +0200

    translation update

--- lib/plugins/authad/lang/eo/lang.php
+++ lib/plugins/authad/lang/eo/lang.php
@@ -3,9 +3,11 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Erik Bjørn Pedersen <erik.pedersen@shaw.ca>
  * @author Florian <florianmail55@gmail.com>
  * @author Robert Bogenschneider <bogi@uea.org>
  */
 $lang['domain']                = 'Ensaluta domajno';
 $lang['authpwdexpire']         = 'Via pasvorto malvalidos post %d tagoj, prefere ŝanĝu ĝin baldaũ.';
+$lang['passchangefail']        = 'Oni malsukcesis ŝanĝi la pasvorton. Ĉu la regularo pri pasvortoj eble ne plenumiĝis?';
 $lang['connectfail']           = 'Malsukcesis konekti al Aktivan Dosierumon servilo.';
