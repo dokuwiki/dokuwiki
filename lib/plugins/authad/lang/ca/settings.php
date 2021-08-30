75cba7af6e8911a1834f19f8c355304807aaed87
David Surroca <davidsurrocaestrada@gmail.com>
2021-08-30 21:11:13 +0200

    translation update

--- lib/plugins/authad/lang/ca/settings.php
+++ lib/plugins/authad/lang/ca/settings.php
@@ -3,10 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author David Surroca <davidsurrocaestrada@gmail.com>
  * @author Adolfo Jayme Barrientos <fito@libreoffice.org>
  * @author controlonline.net <controlonline.net@gmail.com>
  * @author Àngel Pérez Beroy <aperezberoy@gmail.com>
- * @author David Surroca <david.tb303@gmail.com>
  */
 $lang['account_suffix']        = 'El teu nom de compte. Ej.<code>@my.domain.org</code>';
 $lang['base_dn']               = 'Nom base DN. Ej. <code>DC=my,DC=domain,DC=org</code>';
@@ -18,3 +18,4 @@ $lang['sso']                   = 'S\'hauria de fer servir Kerberos o NTLM per in
 $lang['debug']                 = 'Mostrar informació addicional de depuració en cas d\'error?';
 $lang['expirywarn']            = 'Dies per endavant en avisar l\'usuari sobre la caducitat de la contrasenya. 0 per desactivar.';
 $lang['update_mail']           = 'Permetre els usuaris actualitzar la seva adreça de correu electrònic?';
+$lang['recursive_groups']      = 'Resoleu els grups imbricats als seus respectius membres (més lentament).';
--- lib/plugins/authldap/lang/ca/settings.php
+++ lib/plugins/authldap/lang/ca/settings.php
@@ -3,13 +3,16 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author David Surroca <davidsurrocaestrada@gmail.com>
  * @author Adolfo Jayme Barrientos <fito@libreoffice.org>
  * @author Àngel Pérez Beroy <aperezberoy@gmail.com>
- * @author David Surroca <david.tb303@gmail.com>
  * @author Aniol Marti <amarti@caliu.cat>
  */
+$lang['version']               = 'La versió del protocol a utilitzar. Pot ser que hagueu d’establir-ho a <code>3</code>';
 $lang['starttls']              = 'Utilitzar connexions TLS?';
 $lang['bindpw']                = 'Contrasenya de l\'usuari referit abans.';
 $lang['attributes']            = 'Atributs a demanar a la consulta LDAP.';
+$lang['userscope']             = 'Limiteu l\'abast de cerca per a la cerca d\'usuaris';
+$lang['groupscope']            = 'Limiteu l\'abast de cerca per a la cerca de grups';
 $lang['modPass']               = 'Es pot canviar la contrasenya del LDAP mitjançant el Dokuwiki?';
 $lang['debug']                 = 'Mostra informació addicional de depuració als errors.';
