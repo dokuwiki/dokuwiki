6146c10b0659c8de347f9b73890afcbc6cce818c
Satoshi Sahara <sahara.satoshi@gmail.com>
2021-10-24 15:41:59 +0900

    UnitTest, ApiCore::pageInfo()
    author is available from changelog::getRevisionInfo()

--- _test/tests/inc/XmlRpcServer.test.php
+++ _test/tests/inc/XmlRpcServer.test.php
@@ -34,17 +34,18 @@ class XmlRpcServerTest extends DokuWikiTest
         $file = wikiFN($pageName);
         $timestamp = filemtime($file);
         $ixrModifiedTime = (new DateTime('@' . $timestamp))->format(IXR_Date::XMLRPC_ISO8601);
+        $auther = '127.0.0.1'; // read from changelog, $info['ip']
 
         $request = <<<EOD
 <?xml version="1.0"?>
-   <methodCall>
-     <methodName>wiki.getPageInfo</methodName>
-     		<param> 
-			<value>
-				<string>$pageName</string>
-			</value>
-		</param>
-   </methodCall>
+  <methodCall>
+    <methodName>wiki.getPageInfo</methodName>
+      <param> 
+        <value>
+          <string>$pageName</string>
+        </value>
+      </param>
+  </methodCall>
 EOD;
         $expected = <<<EOD
 <methodResponse>
@@ -54,9 +55,9 @@ EOD;
         <struct>
   <member><name>name</name><value><string>wiki:dokuwiki</string></value></member>
   <member><name>lastModified</name><value><dateTime.iso8601>$ixrModifiedTime</dateTime.iso8601></value></member>
-  <member><name>author</name><value><string></string></value></member>
+  <member><name>author</name><value><string>$auther</string></value></member>
   <member><name>version</name><value><int>$timestamp</int></value></member>
-</struct>
+        </struct>
       </value>
     </param>
   </params>
