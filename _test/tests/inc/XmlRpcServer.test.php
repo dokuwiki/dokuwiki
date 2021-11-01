6e05e48ec0edea8d7253edc209addfaf01f55fdd
Satoshi Sahara <sahara.satoshi@gmail.com>
2021-11-01 15:17:06 +0900

    fix typo

--- _test/tests/inc/XmlRpcServer.test.php
+++ _test/tests/inc/XmlRpcServer.test.php
@@ -34,7 +34,7 @@ class XmlRpcServerTest extends DokuWikiTest
         $file = wikiFN($pageName);
         $timestamp = filemtime($file);
         $ixrModifiedTime = (new DateTime('@' . $timestamp))->format(IXR_Date::XMLRPC_ISO8601);
-        $auther = '127.0.0.1'; // read from changelog, $info['ip']
+        $author = '127.0.0.1'; // read from changelog, $info['user'] or $info['ip']
 
         $request = <<<EOD
 <?xml version="1.0"?>
@@ -55,7 +55,7 @@ EOD;
         <struct>
   <member><name>name</name><value><string>wiki:dokuwiki</string></value></member>
   <member><name>lastModified</name><value><dateTime.iso8601>$ixrModifiedTime</dateTime.iso8601></value></member>
-  <member><name>author</name><value><string>$auther</string></value></member>
+  <member><name>author</name><value><string>$author</string></value></member>
   <member><name>version</name><value><int>$timestamp</int></value></member>
 </struct>
       </value>
