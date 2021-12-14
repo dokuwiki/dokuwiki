9ccd4bfee38a3ca55175360c0b47b157a4ba763d
FaB <fabien@cozycloud.cc>
2021-12-14 19:41:37 +0100

    translation update

--- inc/lang/fr/lang.php
+++ inc/lang/fr/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author FaB <fabien@cozycloud.cc>
  * @author Olivier Humbert <trebmuh@tuxfamily.org>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Nicolas Friedli <nicolas@theologique.ch>
