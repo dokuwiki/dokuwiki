540b38e2d1ab27e8ce4e994e30aead6b308ccbd6
Anna Dabrowska <dabrowska@cosmocode.de>
2022-01-05 16:22:21 +0100

    Add event to denied action
    Let plugins modify the text displayed on the denied page

--- inc/Action/Denied.php
+++ inc/Action/Denied.php
@@ -2,6 +2,7 @@
 
 namespace dokuwiki\Action;
 
+use dokuwiki\Extension\Event;
 use dokuwiki\Ui;
 
 /**
@@ -22,11 +23,18 @@ class Denied extends AbstractAction
     /** @inheritdoc */
     public function tplContent()
     {
-        global $INPUT;
-        $this->showBanner();
-        if (empty($INPUT->server->str('REMOTE_USER')) && actionOK('login')) {
-            (new Ui\Login)->show();
+        $data = null;
+        $event = new Event('ACTION_DENIED_TPLCONTENT', $data);
+
+        if ($event->advise_before(true)) {
+            global $INPUT;
+            $this->showBanner();
+            if (empty($INPUT->server->str('REMOTE_USER')) && actionOK('login')) {
+                (new Ui\Login)->show();
+            }
         }
+
+        $event->advise_after();
     }
 
     /**
