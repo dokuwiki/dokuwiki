1498ac42d54ece2fdac23a00662a18ec8019fcf1
Andreas Gohr <andi@splitbrain.org>
2022-02-05 00:09:29 +0100

    abort XMLRPC later in serve()

--- inc/Remote/XmlRpcServer.php
+++ inc/Remote/XmlRpcServer.php
@@ -19,15 +19,20 @@ class XmlRpcServer extends Server
      */
     public function __construct($wait=false)
     {
-        global $conf;
-        if (!$conf['remote']) {
-            throw new ServerException("XML-RPC server not enabled.", -32605);
-        }
-
         $this->remote = new Api();
         $this->remote->setDateTransformation(array($this, 'toDate'));
         $this->remote->setFileTransformation(array($this, 'toFile'));
         parent::__construct(false, false, $wait);
+    }
+
+    /** @inheritdoc  */
+    public function serve($data = false)
+    {
+        global $conf;
+        if (!$conf['remote']) {
+            throw new ServerException("XML-RPC server not enabled.", -32605);
+        }
+        parent::serve($data);
     }
 
     /**
