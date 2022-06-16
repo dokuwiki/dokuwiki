8095a27ba2f08602940b9183d6b0ca02ac8c9037
Marek Adamski <fevbew@wp.pl>
2022-06-17 00:16:11 +0200

    translation update

--- lib/plugins/authad/lang/pl/settings.php
+++ lib/plugins/authad/lang/pl/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author pavulondit <pavloo@vp.pl>
  * @author Bartek S <sadupl@gmail.com>
  * @author Przemek <p_kudriawcew@o2.pl>
