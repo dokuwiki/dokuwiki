d8ee789d5cd05f8c5fe7a10f889f4edeed1698b1
marie <marie.beguin@hepl.ch>
2022-06-22 20:06:17 +0200

    translation update

--- inc/lang/fr/lang.php
+++ inc/lang/fr/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author marie <marie.beguin@hepl.ch>
  * @author Olivier Humbert <trebmuh@tuxfamily.org>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Nicolas Friedli <nicolas@theologique.ch>
