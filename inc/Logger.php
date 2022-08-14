25edeeca14acaa318ea8f0009bd3a8f20921558f
Andreas Gohr <andi@splitbrain.org>
2022-08-14 17:07:41 +0200

    log deprecation messages independent from allowdebug
    Only the logging configuration should influnece if deprecation messages
    are to be logged. By default they should be logged.

--- inc/Logger.php
+++ inc/Logger.php
@@ -147,6 +147,16 @@ class Logger
     }
 
     /**
+     * Is this logging instace actually logging?
+     *
+     * @return bool
+     */
+    public function isLogging()
+    {
+        return $this->isLogging;
+    }
+
+    /**
      * Formats the given data as loglines
      *
      * @param array $data Event data from LOGGER_DATA_FORMAT
@@ -193,7 +203,7 @@ class Logger
     {
         global $conf;
 
-        if($date !== null && !is_numeric($date)) {
+        if ($date !== null && !is_numeric($date)) {
             $date = strtotime($date);
         }
         if (!$date) $date = time();
