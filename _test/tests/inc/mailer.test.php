45230f135d8463a75929a23d75deee54be7896c7
Andreas Gohr <andi@splitbrain.org>
2022-08-14 07:28:52 +0200

    use self hosted msglint api. fixes #3736

--- _test/tests/inc/mailer.test.php
+++ _test/tests/inc/mailer.test.php
@@ -231,7 +231,7 @@ class mailer_test extends DokuWikiTest {
 
         // ask message lint if it is okay
         $html = new HTTPClient();
-        $results = $html->post('https://tools.ietf.org/tools/msglint/msglint', array('msg'=>$msg));
+        $results = $html->post('https://www.splitbrain.org/_static/msglint/', array('msg'=>$msg));
         if($results === false) {
             $this->markTestSkipped('no response from validator');
             return;
