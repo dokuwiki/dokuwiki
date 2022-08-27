a16ec1827325aa77ea808a6d5150e990b4929b16
Name <nam@example.com>
2022-08-27 14:30:22 +0200

    translation update

--- lib/plugins/authplain/lang/bn/lang.php
+++ lib/plugins/authplain/lang/bn/lang.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  */
-$lang['userexists']     = 'দুঃখিত, এই লগইন সঙ্গে একটি ব্যবহারকারী ইতিমধ্যেই বিদ্যমান.';
+$lang['userexists']            = 'দুঃখিত, এই লগইন সঙ্গে একটি ব্যবহারকারী ইতিমধ্যেই বিদ্যমান.';
