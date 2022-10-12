2d198df195d4a030538783bafbb57d0abedfc3eb
Aleksandr Selivanov <alexgearbox@yandex.ru>
2022-10-13 01:11:21 +0200

    translation update

--- lib/plugins/authldap/lang/ru/settings.php
+++ lib/plugins/authldap/lang/ru/settings.php
@@ -33,6 +33,6 @@ $lang['deref_o_0']             = 'LDAP_DEREF_NEVER';
 $lang['deref_o_1']             = 'LDAP_DEREF_SEARCHING';
 $lang['deref_o_2']             = 'LDAP_DEREF_FINDING';
 $lang['deref_o_3']             = 'LDAP_DEREF_ALWAYS';
-$lang['referrals_o_-1']        = 'исользовать по умолчанию';
+$lang['referrals_o_-1']        = 'использовать по умолчанию';
 $lang['referrals_o_0']         = 'не следовать за referrals';
 $lang['referrals_o_1']         = 'следовать за referrals';
