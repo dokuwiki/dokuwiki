4ec0f22bed519f692fe435ee4272b9a427d9ce28
Edney Rossi <prof.edneyrossi@gmail.com>
2022-10-26 16:56:15 +0200

    translation update

---
+++ lib/plugins/logviewer/lang/pt/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Edney Rossi <prof.edneyrossi@gmail.com>
+ */
+$lang['menu']                  = 'Ver registros';
+$lang['date']                  = 'Data';
+$lang['js']['filter']          = 'Filtrar linhas de registro';
