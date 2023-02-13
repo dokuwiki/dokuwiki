934970207e23db2a3f2112de08a3b42d080fd23d
Andreas Gohr <andi@splitbrain.org>
2023-02-13 16:35:00 +0100

    AuthPlain user parsing fix. #3833
    This removes workarounds we had for old PCRE versions. All modern PHP
    releases should be workable with the Regex we have.
    
    If splitting the user line results in less than 5 results, we log an
    error and pad the result for further use.

--- lib/plugins/authplain/_test/escaping.test.php
+++ lib/plugins/authplain/_test/escaping.test.php
@@ -15,13 +15,13 @@
  */
 class helper_plugin_authplain_escaping_test extends DokuWikiTest {
 
-    protected $pluginsEnabled = array('authplainharness');
-    /** @var  auth_plugin_authplain|auth_plugin_authplainharness */
+    protected $pluginsEnabled = array('authplain');
+    /** @var  auth_plugin_authplain */
     protected $auth;
 
     protected function reloadUsers() {
         /* auth caches data loaded from file, but recreated object forces reload */
-        $this->auth = new auth_plugin_authplainharness();
+        $this->auth = new auth_plugin_authplain();
     }
 
     function setUp() : void {
@@ -83,8 +83,6 @@ class helper_plugin_authplain_escaping_test extends DokuWikiTest {
     // really only required for developers to ensure this plugin will
     // work with systems running on PCRE 6.6 and lower.
     public function testLineSplit(){
-        $this->auth->setPregsplit_safe(false);
-
         $names = array(
           'plain',
           'ut-fठ8',
@@ -98,7 +96,7 @@ class helper_plugin_authplain_escaping_test extends DokuWikiTest {
         foreach ($names as $testname) {
             $escaped = str_replace(array('\\',':'),array('\\\\','\\:'),$testname);   // escape : & \
             $test_line = $userpass.$escaped.$other_user_data;
-            $result = $this->auth->splitUserData($test_line);
+            $result = $this->callInaccessibleMethod($this->auth, 'splitUserData', [$test_line]);
 
             $this->assertEquals($escaped, $result[2]);
         }
@@ -127,33 +125,5 @@ class helper_plugin_authplain_escaping_test extends DokuWikiTest {
     {
         $this->assertEquals($expected, $this->auth->cleanUser($input));
         $this->assertEquals($expected, $this->auth->cleanGroup($input));
-    }
-}
-
-/**
- * Class auth_plugin_authplainharness
- */
-class auth_plugin_authplainharness extends auth_plugin_authplain {
-
-    /**
-     * @param boolean $bool
-     */
-    public function setPregsplit_safe($bool) {
-        $this->pregsplit_safe = $bool;
-    }
-
-    /**
-     * @return bool|mixed
-     */
-    public function getPregsplit_safe(){
-        return $this->pregsplit_safe;
-    }
-
-    /**
-     * @param string $line
-     * @return array
-     */
-    public function splitUserData($line){
-        return parent::splitUserData($line);
     }
 }
