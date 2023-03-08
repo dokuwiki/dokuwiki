ad43fdbf71534d013c5204fdb0e4203acfcf9a95
asivery <asivery@protonmail.com>
2023-03-08 11:55:50 +0100

    Replace is_null() check with count()

--- inc/parser/xhtml.php
+++ inc/parser/xhtml.php
@@ -95,11 +95,10 @@ class Doku_Renderer_xhtml extends Doku_Renderer {
      * @author Adrian Lang <lang@cosmocode.de>
      */
     public function finishSectionEdit($end = null, $hid = null) {
-        $data = array_pop($this->sectionedits);
-        if(is_null($data)) {
+        if(count($this->sectionedits) == 0) {
             return;
         }
-
+        $data = array_pop($this->sectionedits);
         if(!is_null($end) && $end <= $data['start']) {
             return;
         }
