f37ae75634119f552dea87e5b6bf1bf601a72391
fiwswe <53953985+fiwswe@users.noreply.github.com>
2023-05-31 23:08:41 +0200

    Optimized attempt
    I think this is more concise and is documented to work correctly. See: https://www.php.net/manual/en/function.empty

--- inc/infoutils.php
+++ inc/infoutils.php
@@ -258,7 +258,7 @@ function check(){
         msg('Debugging support is disabled',1);
     }
 
-    if(is_array($INFO['userinfo']) && array_key_exists('name', $INFO['userinfo']) && $INFO['userinfo']['name']){
+    if(!empty($INFO['userinfo']['name'])){
         msg('You are currently logged in as '.$INPUT->server->str('REMOTE_USER').' ('.$INFO['userinfo']['name'].')',0);
         msg('You are part of the groups '.implode(', ', $INFO['userinfo']['grps']),0);
     }else{
