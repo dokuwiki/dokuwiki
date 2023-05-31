90d0656656d122acc72bb1d363dab602d9d33d2b
fiwswe <53953985+fiwswe@users.noreply.github.com>
2023-05-31 21:34:34 +0200

    Fix #3984

--- inc/infoutils.php
+++ inc/infoutils.php
@@ -258,7 +258,7 @@ function check(){
         msg('Debugging support is disabled',1);
     }
 
-    if($INFO['userinfo']['name']){
+    if(is_array($INFO['userinfo']) && array_key_exists('name', $INFO['userinfo']) && $INFO['userinfo']['name']){
         msg('You are currently logged in as '.$INPUT->server->str('REMOTE_USER').' ('.$INFO['userinfo']['name'].')',0);
         msg('You are part of the groups '.implode(', ', $INFO['userinfo']['grps']),0);
     }else{
