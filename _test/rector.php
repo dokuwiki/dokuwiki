26586410b0b4fa8184ee7c22916f5d3d2b8cbec9
Andreas Gohr <andi@splitbrain.org>
2023-09-03 19:12:54 +0200

    allow overriding the minimum PHP release for rector
    By setting the environment variable RECTOR_MIN_PHP the supported minimum
    PHP release can be set. This is needed to make plugin changes compatible
    with DokuWiki stable.

--- _test/rector.php
+++ _test/rector.php
@@ -62,9 +62,14 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->cacheClass(FileCacheStorage::class);
     $rectorConfig->cacheDirectory(__DIR__ . '/.rector-cache');
 
+    // supported minimum PHP version can be overridden by environment variable
+    [$major, $minor] = explode('.', $_SERVER['RECTOR_MIN_PHP'] ?? '7.4');
+    $phpset = LevelSetList::class . '::UP_TO_PHP_' . $major . $minor;
+    echo "Using PHP set $phpset\n";
+
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
+        constant($phpset),
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::CODING_STYLE,
