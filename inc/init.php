2a3397394de777c28b3aa2ad489cefe990ec250e
m-martin-78 <138781920+m-martin-78@users.noreply.github.com>
2023-11-13 16:55:40 +0100

    Check if trustedproxy is empty + changed behavior in is_ssl to match the same behavior

--- inc/init.php
+++ inc/init.php
@@ -495,9 +495,10 @@ function getBaseURL($abs = null)
 
     //split hostheader into host and port
     if (isset($_SERVER['HTTP_HOST'])) {
-        $cur_host = $_SERVER['HTTP_HOST'];
-        if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && preg_match('/'.$conf['trustedproxy'].'/', $_SERVER['REMOTE_ADDR'])) {
+        if ((!empty($conf['trustedproxy'])) && isset($_SERVER['HTTP_X_FORWARDED_HOST']) && preg_match('/'.$conf['trustedproxy'].'/', $_SERVER['REMOTE_ADDR'])) {
             $cur_host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+        } else {
+            $cur_host = $_SERVER['HTTP_HOST'];
         }
         $parsed_host = parse_url('http://' . $cur_host);
         $host = $parsed_host['host'] ?? '';
@@ -539,7 +540,7 @@ function getBaseURL($abs = null)
 function is_ssl()
 {
     // check if we are behind a reverse proxy
-    if (($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') == 'https') {
+    if ((!empty($conf['trustedproxy'])) && isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && preg_match('/'.$conf['trustedproxy'].'/', $_SERVER['REMOTE_ADDR']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') == 'https') {
         return true;
     }
 
