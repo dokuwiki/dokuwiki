6f8e03f5bc790d26e1215349cea98d5a73654139
Andreas Gohr <andi@splitbrain.org>
2023-11-28 16:47:44 +0100

    implement support for JSON-RPC 1.0, 1.1 and 2.0
    This implements the slightly more standardized JSON-RPC standard as an
    alternative to my home-grown version. The same server handled all formats
    including my own simpler variant.

--- inc/Remote/JsonRpcServer.php
+++ inc/Remote/JsonRpcServer.php
@@ -10,6 +10,9 @@ class JsonRpcServer
 
     protected $remote;
 
+    /** @var float The XML-RPC Version. 0 is our own simplified variant */
+    protected $version = 0;
+
     /**
      * JsonRpcServer constructor.
      */
@@ -47,16 +50,107 @@ class JsonRpcServer
             throw new RemoteException("JSON-RPC server only accepts application/json requests.", -32606);
         }
 
-        $call = $INPUT->server->str('PATH_INFO');
-        $call = trim($call, '/');
         try {
-            $args = json_decode(file_get_contents('php://input'), true, 512, JSON_THROW_ON_ERROR);
+            $data = json_decode(file_get_contents('php://input'), true, 512, JSON_THROW_ON_ERROR);
         } catch (\Exception $e) {
-            $args = [];
+            $data = [];
         }
-        if (!is_array($args)) $args = [];
 
-        return $this->call($call, $args);
+        return $this->createResponse($data);
+    }
+
+    /**
+     * This executes the method and returns the result
+     *
+     * This should handle all JSON-RPC versions and our simplified version
+     *
+     * @link https://en.wikipedia.org/wiki/JSON-RPC
+     * @link https://www.jsonrpc.org/specification
+     * @param array $data
+     * @return array
+     * @throws RemoteException
+     */
+    protected function createResponse($data)
+    {
+        global $INPUT;
+        $return = [];
+
+        if (isset($data['method'])) {
+            // this is a standard conform request (at least version 1.0)
+            $method = $data['method'];
+            $params = $data['params'] ?? [];
+            $this->version = 1;
+
+            // always return the same ID
+            if (isset($data['id'])) $return['id'] = $data['id'];
+
+            // version 2.0 request
+            if (isset($data['jsonrpc'])) {
+                $return['jsonrpc'] = $data['jsonrpc'];
+                $this->version = (float)$data['jsonrpc'];
+            }
+
+            // version 1.1 request
+            if (isset($data['version'])) {
+                $return['version'] = $data['version'];
+                $this->version = (float)$data['version'];
+            }
+        } else {
+            // this is a simplified request
+            $method = $INPUT->server->str('PATH_INFO');
+            $method = trim($method, '/');
+            $params = $data;
+            $this->version = 0;
+        }
+
+        // excute the method
+        $return['result'] = $this->call($method, $params);
+        $this->addErrorData($return); // handles non-error info
+        return $return;
+    }
+
+    /**
+     * Create an error response
+     *
+     * @param \Exception $exception
+     * @return array
+     */
+    public function returnError($exception)
+    {
+        $return = [];
+        $this->addErrorData($return, $exception);
+        return $return;
+    }
+
+    /**
+     * Depending on the requested version, add error data to the response
+     *
+     * @param array $response
+     * @param \Exception|null $e
+     * @return void
+     */
+    protected function addErrorData(&$response, $e = null)
+    {
+        if ($e !== null) {
+            // error occured, add to response
+            $response['error'] = [
+                'code' => $e->getCode(),
+                'message' => $e->getMessage()
+            ];
+        } else {
+            // no error, act according to version
+            if ($this->version > 0 && $this->version < 2) {
+                // version 1.* wants null
+                $response['error'] = null;
+            } elseif ($this->version < 1) {
+                // simplified version wants success
+                $response['error'] = [
+                    'code' => 0,
+                    'message' => 'success'
+                ];
+            }
+            // version 2 wants no error at all
+        }
     }
 
     /**
