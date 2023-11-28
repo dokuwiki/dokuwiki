3e383dbb925001185a417c579e16e000bca423a9
m-martin-78 <138781920+m-martin-78@users.noreply.github.com>
2023-11-28 10:33:17 +0100

    update tests to match new is_ssl behaviour

--- _test/tests/inc/init_checkssl.test.php
+++ _test/tests/inc/init_checkssl.test.php
@@ -4,14 +4,30 @@ class init_checkssl_test extends DokuWikiTest {
 
 	/**
 	 * Running behind an SSL proxy, HTTP between server and proxy
+     * Proxy (REMOTE_ADDR) is matched by default trustedproxy config regex
 	 * HTTPS not set
 	 * HTTP_X_FORWARDED_PROTO
 	 * set to https
 	 */
-	function test1() {
+	function test1a() {
+		$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 		$_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
 
 		$this->assertEquals(is_ssl(), true);
+	}
+    
+    /**
+	 * Running behind an SSL proxy, HTTP between server and proxy
+     * Proxy (REMOTE_ADDR) is not matched by default trustedproxy config regex
+	 * HTTPS not set
+	 * HTTP_X_FORWARDED_PROTO
+	 * set to https
+	 */
+	function test1b() {
+		$_SERVER['REMOTE_ADDR'] = '8.8.8.8';
+		$_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+
+		$this->assertEquals(is_ssl(), false);
 	}
 
 	/**
@@ -20,6 +36,7 @@ class init_checkssl_test extends DokuWikiTest {
 	 * HTTP_X_FORWARDED_PROTO set to http
 	 */
 	function test2() {
+		$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 		$_SERVER['HTTP_X_FORWARDED_PROTO'] = 'http';
 
 		$this->assertEquals(is_ssl(), false);
@@ -31,6 +48,7 @@ class init_checkssl_test extends DokuWikiTest {
 	 * HTTP_X_FORWARDED_PROTO set to https
 	 */
 	function test3() {
+		$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 		$_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
 		$_SERVER['HTTPS'] = 'off';
 
@@ -73,6 +91,7 @@ class init_checkssl_test extends DokuWikiTest {
 	 * HTTP_X_FORWARDED_PROTO set to https
 	 */
 	function test7() {
+		$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 		$_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
 		$_SERVER['HTTPS'] = 'on';
 
