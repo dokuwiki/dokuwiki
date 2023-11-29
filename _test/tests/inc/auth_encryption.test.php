06216d5e8b882e3d31933b8cba18145ea3eab940
Andreas Gohr <andi@splitbrain.org>
2023-11-29 11:37:04 +0100

    add (failing) compability test for AES encryption

--- _test/tests/inc/auth_encryption.test.php
+++ _test/tests/inc/auth_encryption.test.php
@@ -3,10 +3,23 @@
 /**
  * Tests the auth_decrypt and auth_encrypt-functions
  */
-class auth_encryption_test extends DokuWikiTest {
-    function testDeEncrypt() {
+class auth_encryption_test extends DokuWikiTest
+{
+    function testDeEncrypt()
+    {
         $data = "OnA28asdfäakgß*+!\"+*";
         $secret = "oeaf1öasdöflk§";
         $this->assertEquals($data, auth_decrypt(auth_encrypt($data, $secret), $secret));
+    }
+
+    /**
+     * Try to decode a known secret. This one has been created with phpseclib Version 2
+     */
+    function testCompatibility()
+    {
+        $secret = 'secret';
+        $plain = 'This is secret';
+        $crypt = '837e9943623a34fe340e89024c28f4e9be13bbcacdd139801ef16a27bffa7714';
+        $this->assertEquals($plain, auth_decrypt(hex2bin($crypt), $secret));
     }
 }
