6c931de5ac38f8e4ae96a4d621be0212dad692f8
Thalles Lázaro <thallesprofissional@gmail.com>
2023-12-02 12:46:23 +0100

    translation update

--- inc/lang/pt-br/lang.php
+++ inc/lang/pt-br/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Thalles Lázaro <thallesprofissional@gmail.com>
  * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  * @author Paul Nizan <paul_nizan@hormail.com>
  * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
@@ -171,6 +172,9 @@ $lang['profdeleted']           = 'Sua conta de usuário foi excluída desse wiki
 $lang['profconfdelete']        = 'Eu desejo remover minha conta dessa wiki. <br/> Essa ação não pode ser desfeita.';
 $lang['profconfdeletemissing'] = 'Caixa de confirmação não marcada';
 $lang['proffail']              = 'O perfil do usuário não foi atualizado.';
+$lang['proftokenlegend']       = 'Token de Autenticação';
+$lang['proftokengenerate']     = 'Redefinir Token';
+$lang['proftokeninfo']         = 'O token de autenticação pode ser usado para permitir que aplicativos de terceiros façam login e atuem em seu nome.  A redefinição do token invalidará o antigo e desconectará todos os aplicativos que usaram o token anterior.';
 $lang['pwdforget']             = 'Esqueceu sua senha? Solicite outra';
 $lang['resendna']              = 'Esse wiki não tem suporte para o reenvio de senhas.';
 $lang['resendpwd']             = 'Definir a nova senha para';
