17b57ea50e0801665e21e663c277137ca6a534e9
Andreas Gohr <andi@splitbrain.org>
2024-11-21 10:43:18 +0100

    Extension Manager: removed obsolete code

--- lib/plugins/extension/helper.php
+++ lib/plugins/extension/helper.php
@@ -5,8 +5,10 @@ use dokuwiki\Extension\Plugin;
 /**
  * DokuWiki Plugin extension (Helper Component)
  *
+ * This component currently does nothing, except giving access to common plugin functionality
+ * like getLang() to be used in the plugin classes.
+ *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
- * @author Michael Hamann <michael@content-space.de>
  */
 class helper_plugin_extension extends Plugin
 {
