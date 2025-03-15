999fe614933a32bd80b5e25f09685b6215d54998
Andreas Gohr <andi@splitbrain.org>
2025-03-15 10:39:24 +0100

    do not check for deprecated functions in installer
    our UTF-8 fallbacks should not fall back on the utf8_decode/encode
    functions anymore, so this check is no longer useful
    
    sligthly related to #4354

--- install.php
+++ install.php
@@ -633,11 +633,6 @@ function check_functions()
         'preg_replace file_get_contents htmlspecialchars_decode ' .
         'spl_autoload_register stream_select fsockopen pack xml_parser_create');
 
-    if (!function_exists('mb_substr')) {
-        $funcs[] = 'utf8_encode';
-        $funcs[] = 'utf8_decode';
-    }
-
     if (!function_exists('mail')) {
         if (strpos(ini_get('disable_functions'), 'mail') !== false) {
             $disabled = $lang['i_disabled'];
