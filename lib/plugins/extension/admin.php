7f647da18fa67e01159161acfe61a54efb1591a6
Andreas Gohr <gohr@cosmocode.de>
2025-07-22 21:23:22 +0200

    Extension manager: when url given, don't try to use upload, too

--- lib/plugins/extension/admin.php
+++ lib/plugins/extension/admin.php
@@ -62,8 +62,7 @@ class admin_plugin_extension extends AdminPlugin
             }
             if ($INPUT->post->str('installurl')) {
                 $installer->installFromURL($INPUT->post->str('installurl'));
-            }
-            if (isset($_FILES['installfile'])) {
+            } elseif (isset($_FILES['installfile'])) {
                 $installer->installFromUpload('installfile');
             }
         } catch (Exception $e) {
