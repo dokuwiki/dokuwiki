ba6d945f137b76d4feccb1ad84ccd6304a56e3b3
WillForan <willforan@gmail.com>
2025-12-04 17:44:29 -0500

    lint(Ip32.php): fix spacing
    still unsure about function names and order
    
    ```
    ./vendor/bin/phpcbf  --standard=PSR12  ../inc/Ip32.php
    
    ------------------------------------------------
    FILE                            FIXED  REMAINING
    ------------------------------------------------
    dokuwiki/inc/Ip32.php           35     0
    ```

--- inc/Ip32.php
+++ inc/Ip32.php
@@ -17,7 +17,8 @@ class Ip32
      *            likely from ipv6UpperLowerOn32
      * @param $pow 0-64 power of 2 for bitmask
      */
-    public static function bitmask64On32(string $u64, int $pow) : string {
+    public static function bitmask64On32(string $u64, int $pow): string
+    {
         $upper = bcdiv($u64, Ip32::$b32, 0);
         $lower = bcmod($u64, Ip32::$b32);
         // str of len 32 all 0s and 1s
@@ -25,13 +26,13 @@ class Ip32
         $lower_bin = Ip32::decimalToBinary32($lower);
         $bin = $upper_bin . $lower_bin;
 
-        $mask = Ip32::makeBitmaskOn32(64-$pow);
+        $mask = Ip32::makeBitmaskOn32(64 - $pow);
 
         // most right is lowest bit
-        $res='0';
-        for ($i=0; $i<64; $i++){
+        $res = '0';
+        for ($i = 0; $i < 64; $i++) {
             if (bcmul($bin[$i], $mask[$i]) == 1) {
-                $res = bcadd($res, bcpow(2, 63-$i));
+                $res = bcadd($res, bcpow(2, 63 - $i));
             }
         }
         return $res;
@@ -49,10 +50,11 @@ class Ip32
      * pow=64
      * 0000000000000000000000000000000000000000000000000000000000000000
      */
-    private static function makeBitmaskOn32(int $pow) : string {
+    private static function makeBitmaskOn32(int $pow): string
+    {
         $pow = $pow < 0 ? 64 - $pow : $pow;
-        $mask = sprintf("%064d",0);
-        for ($i=0; $i<64; $i++) {
+        $mask = sprintf("%064d", 0);
+        for ($i = 0; $i < 64; $i++) {
             if ($i >= $pow) {
                 $mask[63 - $i] = '1';
             }
@@ -71,18 +73,20 @@ class Ip32
      *
      * @return string[] upper 64 and lower 64 for ipToNumber
      */
-    public static function ipv6UpperLowerOn32(string $binary) {
+    public static function ipv6UpperLowerOn32(string $binary)
+    {
        // unpack into four 32-bit unsigned ints to recombine as 2 64-bit
-       $parts = unpack('N4', $binary);
-       $upper = Ip32::partsTo64($parts[1], $parts[2]);
-       $lower = Ip32::partsTo64($parts[3], $parts[4]);
-       return ['upper' => $upper, 'lower' => $lower];
+        $parts = unpack('N4', $binary);
+        $upper = Ip32::partsTo64($parts[1], $parts[2]);
+        $lower = Ip32::partsTo64($parts[3], $parts[4]);
+        return ['upper' => $upper, 'lower' => $lower];
     }
 
-    private static function partsTo64(string $high, string $low) : string {
+    private static function partsTo64(string $high, string $low): string
+    {
         // signed to unsigned
-        $high = $high<0 ? bcadd($high, Ip32::$b32) : $high;
-        $low  = $low <0 ? bcadd($low , Ip32::$b32) : $low;
+        $high = $high < 0 ? bcadd($high, Ip32::$b32) : $high;
+        $low  = $low < 0 ? bcadd($low, Ip32::$b32) : $low;
 
         return bcadd(bcmul($high, Ip32::$b32), $low);
     }
@@ -94,7 +98,8 @@ class Ip32
      * @param string $decimal The decimal number as string
      * @return string 32-bit binary representation
      */
-    private static function decimalToBinary32(string $decimal) : string {
+    private static function decimalToBinary32(string $decimal): string
+    {
         if (bccomp($decimal, '0') == 0) {
             return str_repeat('0', 32);
         }
@@ -111,4 +116,4 @@ class Ip32
         }
         return $binary;
     }
- }
+}
