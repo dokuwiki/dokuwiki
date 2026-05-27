1dbc46ac72720bdfa123c00b4e48c01647ee8fa7
Tzipirigu <tzipirigu.radu25683@gmail.com>
2026-05-27 20:06:05 +0200

    Translation update (ro)

--- lib/plugins/authad/lang/ro/lang.php
+++ lib/plugins/authad/lang/ro/lang.php
@@ -3,9 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Tzipirigu <tzipirigu.radu25683@gmail.com>
  * @author Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
  * @author Adrian Vesa <adrianvesa@dotwikis.com>
  */
 $lang['authpwdexpire']         = 'Parola va expira în %d zile, ar trebui să o schimbi în curând.';
-$lang['passchangefail']        = 'Parola nu a putu fi schimbata. Poate politica pentru parole nu a fost indeplinita ?';
-$lang['userchangefail']        = 'Nu am putu schimba atributiile pentru acest utilizator. Poate nu ai permisiunea sa faci aceste schimbari ?';
+$lang['passchangefail']        = 'Parola nu s-a putut schimba. Poate politica pentru parole nu a fost îndeplinită?';
+$lang['userchangefail']        = 'Nu s-au putut schimba atribuțiile pentru acest utilizator. Poate nu ai permisiunea să faci aceste schimbări?';
