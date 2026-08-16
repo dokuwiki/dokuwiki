6cef4d9aa36220b78c1fe6d1deb14c7856bd44d2
Andreas Gohr <andi@splitbrain.org>
2026-08-16 14:10:17 +0200

    Upped $updateVersion
    fixes #4730

--- doku.php
+++ doku.php
@@ -13,7 +13,7 @@ use dokuwiki\ChangeLog\PageChangeLog;
 use dokuwiki\Extension\Event;
 
 // update message version - always use a string to avoid localized floats!
-$updateVersion = "57";
+$updateVersion = "58";
 
 //  xdebug_start_profiling();
 
